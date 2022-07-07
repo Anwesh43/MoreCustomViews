f509d8d0b78cd22fb4e952bbabc43dbe1518f1c1
Anwesh43 <anweshthecool0@gmail.com>
2022-07-07 15:49:55 +0530

    [alternaterotlineview] adding AlternatedDotRotLine class

--- AlternateDotToRotLineDownView/src/main/java/com/example/alternatedottorotlinedownview/AlternateDotRotLineView.kt
+++ AlternateDotToRotLineDownView/src/main/java/com/example/alternatedottorotlinedownview/AlternateDotRotLineView.kt
@@ -164,4 +164,27 @@ class AlternateDotRotLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class AlternateDotRotLine(var i : Int, val state : State = State()) {
+
+        private var curr : ADRLNode = ADRLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
