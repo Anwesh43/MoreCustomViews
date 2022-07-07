1101eb7dbd3577abeeeb1e3ca8b51545d10067a6
Anwesh43 <anweshthecool0@gmail.com>
2022-07-07 16:28:20 +0530

    adding alternatedotrotlineview in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.morecustomviews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.alternatedottorotlinedownview.AlternateDotRotLineView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        AlternateDotRotLineView.create(this)
     }
 }
