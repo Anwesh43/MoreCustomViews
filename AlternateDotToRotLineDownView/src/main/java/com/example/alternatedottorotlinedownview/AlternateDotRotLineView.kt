a4cb5c5228545b7b5298f6915e6e62e365acb112
Anwesh43 <anweshthecool0@gmail.com>
2022-07-07 15:47:52 +0530

    [alternaterotlineview] adding ADRLNode class

--- AlternateDotToRotLineDownView/src/main/java/com/example/alternatedottorotlinedownview/AlternateDotRotLineView.kt
+++ AlternateDotToRotLineDownView/src/main/java/com/example/alternatedottorotlinedownview/AlternateDotRotLineView.kt
@@ -123,4 +123,45 @@ class AlternateDotRotLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ADRLNode(var i : Int, val state : State = State()) {
+
+        private var prev : ADRLNode? = null
+        private var next : ADRLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ADRLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawADRLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ADRLNode {
+            var curr : ADRLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
