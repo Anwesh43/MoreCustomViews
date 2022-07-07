bddf2c851e888a0e068c5ab87fbc8a9482aa1e88
Anwesh43 <anweshthecool0@gmail.com>
2022-07-07 15:40:14 +0530

    [altenratedotrotlineview] adding Canvas extension methods

--- AlternateDotToRotLineDownView/src/main/java/com/example/alternatedottorotlinedownview/AlternateDotRotLineView.kt
+++ AlternateDotToRotLineDownView/src/main/java/com/example/alternatedottorotlinedownview/AlternateDotRotLineView.kt
@@ -30,3 +30,33 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawAlternateDotRotLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+    rotate(rot * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-w / 2 - r + (w / 2  + r - size) * sc1, 0f)
+        drawCircle(0f, 0f, r, paint)
+        drawLine(0f, 0f, -size * sc2, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawADRLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawAlternateDotRotLine(scale, w, h, paint)
+}
