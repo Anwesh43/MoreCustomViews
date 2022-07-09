ae8ad56d6178aa8437498c1f6ee0f9e44bcd0500
Anwesh43 <anweshthecool0@gmail.com>
2022-07-09 12:37:27 +0530

    alternatelinetobarview: adding ALTBNode class

--- alternatelinetobarview/src/main/java/com/example/alternatelinetobarview/AlternateLineToBarView.kt
+++ alternatelinetobarview/src/main/java/com/example/alternatelinetobarview/AlternateLineToBarView.kt
@@ -125,4 +125,45 @@ class AlternateLineToBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALTBNode(var i : Int, val state : State = State()) {
+
+        private var next : ALTBNode? = null
+        private var prev : ALTBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALTBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALTBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALTBNode {
+            var curr : ALTBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
