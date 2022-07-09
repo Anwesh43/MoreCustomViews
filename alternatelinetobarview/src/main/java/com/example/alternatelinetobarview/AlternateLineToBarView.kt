64e38b9ebab74b50193ce9a7cbac9c595f400219
Anwesh43 <anweshthecool0@gmail.com>
2022-07-09 12:40:42 +0530

    alternatelinetobarview: adding AlternateLineToBar class

--- alternatelinetobarview/src/main/java/com/example/alternatelinetobarview/AlternateLineToBarView.kt
+++ alternatelinetobarview/src/main/java/com/example/alternatelinetobarview/AlternateLineToBarView.kt
@@ -166,4 +166,27 @@ class AlternateLineToBarView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class AlternateLineToBar(var i : Int) {
+
+        private var curr : ALTBNode = ALTBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
