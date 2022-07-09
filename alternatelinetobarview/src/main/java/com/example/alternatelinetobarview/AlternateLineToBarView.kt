766cf7440015b5c20c5d8f957cbf2eb5d8a77cc7
Anwesh43 <anweshthecool0@gmail.com>
2022-07-09 12:44:40 +0530

    alternatelinetobarview: adding Renderer class

--- alternatelinetobarview/src/main/java/com/example/alternatelinetobarview/AlternateLineToBarView.kt
+++ alternatelinetobarview/src/main/java/com/example/alternatelinetobarview/AlternateLineToBarView.kt
@@ -189,4 +189,27 @@ class AlternateLineToBarView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : AlternateLineToBarView) {
+
+        private val altb : AlternateLineToBar = AlternateLineToBar(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            altb.draw(canvas, paint)
+            animator.animate {
+                altb.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            altb.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
