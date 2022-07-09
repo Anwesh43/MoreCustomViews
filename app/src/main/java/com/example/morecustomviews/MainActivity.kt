e76af75b1fef3c36822fce6cccba20fe21fc7d75
Anwesh43 <anweshthecool0@gmail.com>
2022-07-09 13:06:18 +0530

    alternatelinetobarview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import com.example.alternatedottorotlinedownview.AlternateDotRotLineView
+import com.example.alternatelinetobarview.AlternateLineToBarView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-        AlternateDotRotLineView.create(this)
+        AlternateLineToBarView.create(this)
         fullScreen()
     }
 }
