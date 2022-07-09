15af407a188164f39f56a4b69a9f3043c26eb5f9
Anwesh43 <anweshthecool0@gmail.com>
2022-07-09 11:09:00 +0530

    [alternatelinetobarview] adding Canvas extension methods

--- alternatelinetobarview/src/main/java/com/example/alternatelinetobarview/AlternateLineToBarView.kt
+++ alternatelinetobarview/src/main/java/com/example/alternatelinetobarview/AlternateLineToBarView.kt
@@ -25,8 +25,40 @@ val sizeFactor : Float = 5.9f
 val delay : Long = 20
 val deg : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
-val barSize : Float = 12.3f
+val barSizeFactor : Float = 12.3f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawAlternateLineToBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val barSize : Float = Math.min(w, h) / barSizeFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc4)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        save()
+        translate((w / 2) * (1 - sc1), barSize)
+        drawLine(0f, 0f, size, 0f, paint)
+        restore()
+        drawRect(RectF(0f, 0f, size * sc2, barSize), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawALTBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawAlternateLineToBar(scale, w, h, paint)
+}
