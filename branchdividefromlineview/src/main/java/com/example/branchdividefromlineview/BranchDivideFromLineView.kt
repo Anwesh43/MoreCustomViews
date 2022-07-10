72f067e922f0d317b53ec4be8c575cc459f77901
Anwesh43 <anweshthecool0@gmail.com>
2022-07-10 10:52:21 +0530

    [branchdividefromlineview] adding BranchDivideFromLine class

--- branchdividefromlineview/src/main/java/com/example/branchdividefromlineview/BranchDivideFromLineView.kt
+++ branchdividefromlineview/src/main/java/com/example/branchdividefromlineview/BranchDivideFromLineView.kt
@@ -153,7 +153,7 @@ class BranchDivideFromLineView(ctx : Context) : View(ctx) {
             state.startUpdating(cb)
         }
 
-        fun getNext(dir : Int, cb : () -> Float) : BDFLNode {
+        fun getNext(dir : Int, cb : () -> Unit) : BDFLNode {
             var curr : BDFLNode? = prev
             if (dir == 1) {
                 curr = next
@@ -163,6 +163,29 @@ class BranchDivideFromLineView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class BranchDivideFromLine(var i : Int) {
+
+        private var curr : BDFLNode = BDFLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
