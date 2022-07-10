8ec0523f29fc8788cd2611d9cbf0ccc17f522b3a
Anwesh43 <anweshthecool0@gmail.com>
2022-07-10 10:42:49 +0530

    [branchdividefromlineview] adding BDFLNode class

--- branchdividefromlineview/src/main/java/com/example/branchdividefromlineview/BranchDivideFromLineView.kt
+++ branchdividefromlineview/src/main/java/com/example/branchdividefromlineview/BranchDivideFromLineView.kt
@@ -124,4 +124,45 @@ class BranchDivideFromLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BDFLNode(var i : Int, val state : State = State()) {
+
+        private var next : BDFLNode? = null
+        private var prev : BDFLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BDFLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBDFLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Float) : BDFLNode {
+            var curr : BDFLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
