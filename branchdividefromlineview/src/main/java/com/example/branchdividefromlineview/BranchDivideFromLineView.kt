b8864dc1daad6d296692c72bb40a2b3b84db2395
Anwesh43 <anweshthecool0@gmail.com>
2022-07-10 10:22:54 +0530

    branchdividefromlineview: adding Canvas extension methods

--- branchdividefromlineview/src/main/java/com/example/branchdividefromlineview/BranchDivideFromLineView.kt
+++ branchdividefromlineview/src/main/java/com/example/branchdividefromlineview/BranchDivideFromLineView.kt
@@ -25,7 +25,39 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
+val deg : Float = 45f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBranchDivideFromLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2 - (h / 2 + size) * sc4)
+    rotate(rot * sc3)
+    save()
+    translate(-w / 2 + w * 0.5f * sc1, 0f)
+    drawLine(0f, 0f, -size, 0f, paint)
+    restore()
+    for (j in 0..1) {
+        save()
+        rotate(deg * (1 - 2 * j))
+        drawLine(0f, 0f, 0f, size * sc2, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBDFLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBranchDivideFromLine(scale, w, h, paint)
+}
