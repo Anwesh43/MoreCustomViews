6ec18677bf1955d8f2dca2a3e3a18517a37aa7a8
Anwesh43 <anweshthecool0@gmail.com>
2022-07-11 13:37:59 +0530

    ballmovelinejoinview: adding Canvas extension methods

--- ballmovelinejoinview/src/main/java/com/example/ballmovelinejoinview/BallMoveLineJoinView.kt
+++ ballmovelinejoinview/src/main/java/com/example/ballmovelinejoinview/BallMoveLineJoinView.kt
@@ -29,3 +29,33 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBallMoveLineJoin(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = size / 4
+    save()
+    translate(w / 2, h / 2 + (h / 2 + 2 * size) * sc4)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        translate(size / 2, (h / 2 + r) * (1 - sc1))
+        drawCircle(0f, 0f, r, paint)
+        restore()
+    }
+    drawLine(-size * 0.5f * sc2, 0f, size * 0.5f * sc2, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawBMLJNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBallMoveLineJoin(scale, w, h, paint)
+}
