fde6e1291b26efd04e8272e1ad738fc0019b132d
Anwesh43 <anweshthecool0@gmail.com>
2022-07-13 03:12:22 +0530

    barlinerotleftdownview: adding renderer in view

--- barlinerotleftdownview/src/main/java/com/example/barlinerotleftdownview/BarLineRotLeftDownView.kt
+++ barlinerotleftdownview/src/main/java/com/example/barlinerotleftdownview/BarLineRotLeftDownView.kt
@@ -61,14 +61,16 @@ fun Canvas.drawBLRLDNode(i : Int, scale : Float, paint : Paint) {
 
 class BarLineRotLeftDownView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -193,7 +195,7 @@ class BarLineRotLeftDownView(ctx : Context) : View(ctx) {
         private val blrld : BarLineRotLeftDown = BarLineRotLeftDown(0)
         private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
             blrld.draw(canvas, paint)
             animator.animate {
