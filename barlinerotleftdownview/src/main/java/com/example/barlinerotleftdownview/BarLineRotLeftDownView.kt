1f1d4e8e7308ed1c5c7531fa3cd5b523dc9c0731
Anwesh43 <anweshthecool0@gmail.com>
2022-07-13 02:55:21 +0530

    barlinerotleftdownview: adding Canvas extension methods

--- barlinerotleftdownview/src/main/java/com/example/barlinerotleftdownview/BarLineRotLeftDownView.kt
+++ barlinerotleftdownview/src/main/java/com/example/barlinerotleftdownview/BarLineRotLeftDownView.kt
@@ -23,9 +23,38 @@ val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
-val deg : Float = 90f
+val deg : Float = 180f
 val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBarLineRotLeftDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate((size / 2), 0f)
+        rotate(deg * sc3)
+        drawRect(RectF(-size / 2, -size / 4, -size / 2 + size * 0.5f * sc1, size / 4), paint)
+        drawLine(0f, 0f,size * 0.5f * sc2, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBLRLDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarLineRotLeftDown(scale, w, h, paint)
+}
