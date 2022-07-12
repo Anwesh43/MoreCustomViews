b8cf7b18174df66522097f49fcf443345e99ffc4
Anwesh43 <anweshthecool0@gmail.com>
2022-07-13 03:05:42 +0530

    barlinerotleftdownview: adding BLRLDNode class

--- barlinerotleftdownview/src/main/java/com/example/barlinerotleftdownview/BarLineRotLeftDownView.kt
+++ barlinerotleftdownview/src/main/java/com/example/barlinerotleftdownview/BarLineRotLeftDownView.kt
@@ -122,4 +122,45 @@ class BarLineRotLeftDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLRLDNode(var i : Int, val state : State = State()) {
+
+        private var prev : BLRLDNode? = null
+        private var next : BLRLDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLRLDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLRLDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLRLDNode {
+            var curr : BLRLDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
