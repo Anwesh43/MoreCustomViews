066553b170aed34760853f0d0bd76ed97674fab8
Anwesh43 <anweshthecool0@gmail.com>
2022-07-15 01:48:19 +0530

    rightanglearrowshooterview: adding Canvas extension methods

--- rightanglearrowshooterview/src/main/java/com/example/rightanglearrowshooterview/RightAngleArrowShooterView.kt
+++ rightanglearrowshooterview/src/main/java/com/example/rightanglearrowshooterview/RightAngleArrowShooterView.kt
@@ -29,3 +29,38 @@ val arrowFactor : Float = 13.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawRightAngleArrowShooter(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val arrowSize : Float = size / arrowFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        rotate(deg * j * sc2)
+        translate(0f, (-(h / 2) * (1f - j) - (w / 2) * j) * sc4)
+        drawLine(0f, 0f, 0f, -size * sc1, paint)
+        for (i in 0..1) {
+            save()
+            translate(0f, -size)
+            rotate(deg * 0.5f * (1f - 2 * i))
+            drawLine(0f, 0f, 0f, arrowSize * sc3, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawRAASNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightAngleArrowShooter(scale, w, h, paint)
+}
