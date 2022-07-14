3945d881858c8bb667a9f3cdcdf75da12755f258
Anwesh43 <anweshthecool0@gmail.com>
2022-07-15 01:20:26 +0530

    rightanglearrowshooterview: adding Int and Float extension methods

--- rightanglearrowshooterview/src/main/java/com/example/rightanglearrowshooterview/RightAngleArrowShooterView.kt
+++ rightanglearrowshooterview/src/main/java/com/example/rightanglearrowshooterview/RightAngleArrowShooterView.kt
@@ -26,3 +26,6 @@ val sizeFactor : Float = 5.9f
 val strokeFactor : Float = 90f
 val arrowFactor : Float = 13.9f
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
