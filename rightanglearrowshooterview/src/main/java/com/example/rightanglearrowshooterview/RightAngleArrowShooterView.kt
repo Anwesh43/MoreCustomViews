c67faeb886b2829af8d45c5f495a8e9a9a132997
Anwesh43 <anweshthecool0@gmail.com>
2022-07-15 02:15:35 +0530

    rightanglearrowshooterview: adding RAASNode class

--- rightanglearrowshooterview/src/main/java/com/example/rightanglearrowshooterview/RightAngleArrowShooterView.kt
+++ rightanglearrowshooterview/src/main/java/com/example/rightanglearrowshooterview/RightAngleArrowShooterView.kt
@@ -127,4 +127,45 @@ class RightAngleArrowShootView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RAASNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RAASNode? = null
+        private var prev : RAASNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RAASNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRAASNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RAASNode {
+            var curr : RAASNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
