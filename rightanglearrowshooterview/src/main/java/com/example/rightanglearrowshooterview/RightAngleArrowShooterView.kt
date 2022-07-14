8a0df39c256ba37d1c4c7c57cfa7efabc1fd0814
Anwesh43 <anweshthecool0@gmail.com>
2022-07-15 01:18:49 +0530

    rightanglearrowshooterview: adding constants and libraries

---
+++ rightanglearrowshooterview/src/main/java/com/example/rightanglearrowshooterview/RightAngleArrowShooterView.kt
@@ -0,0 +1,28 @@
+package com.example.rightanglearrowshooterview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
+val sizeFactor : Float = 5.9f
+val strokeFactor : Float = 90f
+val arrowFactor : Float = 13.9f
+
