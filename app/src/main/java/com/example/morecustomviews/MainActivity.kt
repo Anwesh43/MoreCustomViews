0ea3107e1e3fd2bbf5dfc6678027f6ea0bf9f41f
Anwesh43 <anweshthecool0@gmail.com>
2022-07-15 02:24:37 +0530

    rightanglearrowshooterview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import com.example.biarrowshiftrotmoveview.BiArrowShiftRotMoveView
+import com.example.rightanglearrowshooterview.RightAngleArrowShootView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-        BiArrowShiftRotMoveView.create(this)
+        RightAngleArrowShootView.create(this)
         fullScreen()
     }
 }
