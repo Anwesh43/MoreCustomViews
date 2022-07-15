a80e620bd0c86d1aeb8a4978ea1738929ac1e54c
Anwesh43 <anweshthecool0@gmail.com>
2022-07-16 03:48:06 +0530

    reflectingballlinemoveview: adding ReflectingballLineMoveView class

--- reflectingballlinemoveview/src/main/java/com/example/reflectingballlinemoveview/ReflectingBallLineMoveView.kt
+++ reflectingballlinemoveview/src/main/java/com/example/reflectingballlinemoveview/ReflectingBallLineMoveView.kt
@@ -51,3 +51,19 @@ fun Canvas.drawRBLMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawReflectingBallLineMove(scale, w, h, paint)
 }
+
+class ReflectingBallLineMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
