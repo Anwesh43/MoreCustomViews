68bc4d4bb1d8f996cb1434c7351d47e2dff82a37
Anwesh43 <anweshthecool0@gmail.com>
2022-07-16 04:02:49 +0530

    reflectingballlinemoveview: adding RBLMNode class

--- reflectingballlinemoveview/src/main/java/com/example/reflectingballlinemoveview/ReflectingBallLineMoveView.kt
+++ reflectingballlinemoveview/src/main/java/com/example/reflectingballlinemoveview/ReflectingBallLineMoveView.kt
@@ -114,4 +114,45 @@ class ReflectingBallLineMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RBLMNode(var i : Int, val state : State = State()) {
+
+        private var next : RBLMNode? = null
+        private var prev : RBLMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RBLMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRBLMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RBLMNode {
+            var curr : RBLMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
