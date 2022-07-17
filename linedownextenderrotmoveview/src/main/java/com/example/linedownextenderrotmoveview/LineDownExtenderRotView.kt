f9e5ed3565e9dd5730830c5c9b767ac17e43da5e
Anwesh43 <anweshthecool0@gmail.com>
2022-07-17 09:30:41 +0530

    linedownextendeerrotmoveview: adding LineDownExtenderView class

--- linedownextenderrotmoveview/src/main/java/com/example/linedownextenderrotmoveview/LineDownExtenderRotView.kt
+++ linedownextenderrotmoveview/src/main/java/com/example/linedownextenderrotmoveview/LineDownExtenderRotView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawLDERNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineDownExtenderRot(scale, w, h, paint)
 }
+
+class LineDownExtenderRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
