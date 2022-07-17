1b4d642fb71ce6797936434d2d24c5449f18f3b1
Anwesh43 <anweshthecool0@gmail.com>
2022-07-17 09:57:01 +0530

    linedownextendeerrotmoveview: adding fixes

--- linedownextenderrotmoveview/src/main/java/com/example/linedownextenderrotmoveview/LineDownExtenderRotView.kt
+++ linedownextenderrotmoveview/src/main/java/com/example/linedownextenderrotmoveview/LineDownExtenderRotView.kt
@@ -36,7 +36,7 @@ fun Canvas.drawLineDownExtenderRot(scale : Float, w : Float, h : Float, paint : 
     val sc3 : Float = scale.divideScale(2, parts)
     val sc4 : Float = scale.divideScale(3, parts)
     save()
-    translate(w / 2 + (h / 2 + size) * sc4, h / 2)
+    translate(w / 2 - (w / 2 + size) * sc4, h / 2)
     rotate(deg * sc3)
     drawLine(0f, 0f, size * sc1, 0f, paint)
     save()
