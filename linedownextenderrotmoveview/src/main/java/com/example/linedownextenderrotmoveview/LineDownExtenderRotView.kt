cf7bb5621d98b3dead164e2e51b55a56ea1f249d
Anwesh43 <anweshthecool0@gmail.com>
2022-07-17 09:29:26 +0530

    linedownextendeerrotmoveview: adding Canvas extension methods

--- linedownextenderrotmoveview/src/main/java/com/example/linedownextenderrotmoveview/LineDownExtenderRotView.kt
+++ linedownextenderrotmoveview/src/main/java/com/example/linedownextenderrotmoveview/LineDownExtenderRotView.kt
@@ -28,3 +28,29 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineDownExtenderRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (h / 2 + size) * sc4, h / 2)
+    rotate(deg * sc3)
+    drawLine(0f, 0f, size * sc1, 0f, paint)
+    save()
+    translate(0f, -h / 2 + (h / 2) * sc2)
+    drawLine(0f, 0f, 0f, -size, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawLDERNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineDownExtenderRot(scale, w, h, paint)
+}
