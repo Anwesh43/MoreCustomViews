cbce73b36b8d178eb4b23036854804f5b5584dbc
Anwesh43 <anweshthecool0@gmail.com>
2022-07-18 14:58:49 +0530

    balllineslidedownview: adding Canvas extension methods

--- balllineslidedownview/src/main/java/com/example/balllineslidedownview/BallLineSlideDownView.kt
+++ balllineslidedownview/src/main/java/com/example/balllineslidedownview/BallLineSlideDownView.kt
@@ -17,13 +17,48 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 15.3f
+val rot : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBallLineSlideDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2 + r + size) * sc5)
+    rotate(rot * sc4)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        drawLine(size / 2, r, size / 2 - size * sc1 + size * 0.5f * sc3, r, paint)
+        save()
+        translate((-size + r) * (1 - sc3), 0f)
+        drawCircle(0f, 0f, r * sc2, paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBLSDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBallLineSlideDown(scale, w, h, paint)
+}
