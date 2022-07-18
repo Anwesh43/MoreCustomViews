fe4568d92c3b2c6d1edd50a1d8f48267ec294f1c
Anwesh43 <anweshthecool0@gmail.com>
2022-07-18 15:00:06 +0530

    balllineslidedownview: adding BallLineSlideDownView class

--- balllineslidedownview/src/main/java/com/example/balllineslidedownview/BallLineSlideDownView.kt
+++ balllineslidedownview/src/main/java/com/example/balllineslidedownview/BallLineSlideDownView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawBLSDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBallLineSlideDown(scale, w, h, paint)
 }
+
+class BallLineSlideDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
