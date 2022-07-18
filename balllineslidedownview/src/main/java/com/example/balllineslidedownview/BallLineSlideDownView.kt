36a2aa1545796c33d5d05fe783cbb9b9617dda0e
Anwesh43 <anweshthecool0@gmail.com>
2022-07-18 15:11:53 +0530

    balllineslidedownview: adding renderer in view

--- balllineslidedownview/src/main/java/com/example/balllineslidedownview/BallLineSlideDownView.kt
+++ balllineslidedownview/src/main/java/com/example/balllineslidedownview/BallLineSlideDownView.kt
@@ -65,14 +65,16 @@ fun Canvas.drawBLSDNode(i : Int, scale : Float, paint : Paint) {
 
 class BallLineSlideDownView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
@@ -194,10 +196,11 @@ class BallLineSlideDownView(ctx : Context) : View(ctx) {
 
         private val animator : Animator = Animator(view)
         private val blsd : BallLineSlideDown = BallLineSlideDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-        fun render(canvas : Canvas, paint : Paint) {
+        fun render(canvas : Canvas) {
             canvas.drawColor(backColor)
-            blsd.draw(canvas, paint)
+            blsd.draw(canvas)
             animator.animate {
                 blsd.update {
                     animator.stop()
