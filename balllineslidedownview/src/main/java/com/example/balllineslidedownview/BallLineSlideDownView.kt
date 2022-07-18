1aed71214f1c33b6d0bd58cb149e35bb3bb6c696
Anwesh43 <anweshthecool0@gmail.com>
2022-07-18 15:07:15 +0530

    balllineslidedownview: adding BallLineSlideDown class

--- balllineslidedownview/src/main/java/com/example/balllineslidedownview/BallLineSlideDownView.kt
+++ balllineslidedownview/src/main/java/com/example/balllineslidedownview/BallLineSlideDownView.kt
@@ -166,4 +166,27 @@ class BallLineSlideDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BallLineSlideDown(var i : Int) {
+
+        private var curr : BLSDNode = BLSDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
