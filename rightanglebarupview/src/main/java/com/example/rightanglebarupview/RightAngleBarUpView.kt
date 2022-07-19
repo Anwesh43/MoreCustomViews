301cd2a62e1f64ae43be9482a47516cde1e1109e
Anwesh43 <anweshthecool0@gmail.com>
2022-07-19 12:01:37 +0530

    rightanglebarupview: adding Canvas extension methods

--- rightanglebarupview/src/main/java/com/example/rightanglebarupview/RightAngleBarUpView.kt
+++ rightanglebarupview/src/main/java/com/example/rightanglebarupview/RightAngleBarUpView.kt
@@ -29,3 +29,31 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawRightAngleBarUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc1)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        rotate(-90f * j)
+        drawLine(0f, 0f, size * sc1, 0f, paint)
+        restore()
+    }
+    drawRect(RectF(size / 2, -size * 0.5f * sc2, size, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawRABUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightAngleBarUp(scale, w, h, paint)
+}
