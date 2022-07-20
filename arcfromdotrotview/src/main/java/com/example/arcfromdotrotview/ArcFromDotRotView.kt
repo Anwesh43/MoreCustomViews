fb2c7294bd8f893dab1d844d7f96fc685aa83ea1
Anwesh43 <anweshthecool0@gmail.com>
2022-07-20 12:19:19 +0530

    arcfromdotrotview: adding cosntants and libraries

---
+++ arcfromdotrotview/src/main/java/com/example/arcfromdotrotview/ArcFromDotRotView.kt
@@ -0,0 +1,28 @@
+package com.example.arcfromdotrotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#AB47BC",
+    "#1E88E5",
+    "#00E678",
+    "#FF3D00",
+    "#3D5AFE"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val rFactor : Float = 11.2f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
+val gapDeg : Float = 90f
