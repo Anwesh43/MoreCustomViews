35b586bc783febebabdcb913b608d6f67967f62d
Anwesh43 <anweshthecool0@gmail.com>
2022-07-20 12:46:52 +0530

    arcfromdotrotview: adding static create method

--- arcfromdotrotview/src/main/java/com/example/arcfromdotrotview/ArcFromDotRotView.kt
+++ arcfromdotrotview/src/main/java/com/example/arcfromdotrotview/ArcFromDotRotView.kt
@@ -216,4 +216,13 @@ class ArcFromDotRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : ArcFromDotRotView {
+            val view : ArcFromDotRotView = ArcFromDotRotView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
