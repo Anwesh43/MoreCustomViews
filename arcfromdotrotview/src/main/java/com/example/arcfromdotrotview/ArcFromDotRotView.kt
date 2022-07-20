f2b60f4aff4424fac54da9cd7cb8d46b060247af
Anwesh43 <anweshthecool0@gmail.com>
2022-07-20 12:38:55 +0530

    arcfromdotorotview: adding AFDRNode class

--- arcfromdotrotview/src/main/java/com/example/arcfromdotrotview/ArcFromDotRotView.kt
+++ arcfromdotrotview/src/main/java/com/example/arcfromdotrotview/ArcFromDotRotView.kt
@@ -127,4 +127,45 @@ class ArcFromDotRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AFDRNode(var i : Int, val state : State = State()) {
+
+        private var next : AFDRNode? = null
+        private var prev : AFDRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AFDRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAFDRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AFDRNode {
+            var curr : AFDRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
