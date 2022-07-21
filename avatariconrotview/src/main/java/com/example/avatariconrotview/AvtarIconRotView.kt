3bf7bff8bb946c00fcdcb5deba5ec72532d62d48
Anwesh43 <anweshthecool0@gmail.com>
2022-07-21 13:10:50 +0530

    avatariconrotview : adding AIRNode class

--- avatariconrotview/src/main/java/com/example/avatariconrotview/AvtarIconRotView.kt
+++ avatariconrotview/src/main/java/com/example/avatariconrotview/AvtarIconRotView.kt
@@ -129,4 +129,45 @@ class AvatarIconRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AIRNode(var i : Int, val state : State = State()) {
+
+        private var next : AIRNode? = null
+        private var prev : AIRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AIRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAIRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AIRNode {
+            var curr : AIRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
