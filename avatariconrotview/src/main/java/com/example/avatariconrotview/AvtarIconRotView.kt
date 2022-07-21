5fc4ebd5e5416f2e648987fabc462c7a813fda6a
Anwesh43 <anweshthecool0@gmail.com>
2022-07-21 13:05:16 +0530

    avatariconrotview: adding AvatarIconRotView class

--- avatariconrotview/src/main/java/com/example/avatariconrotview/AvtarIconRotView.kt
+++ avatariconrotview/src/main/java/com/example/avatariconrotview/AvtarIconRotView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawAIRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawAvatarIconRot(i, scale, w, h, paint)
 }
+
+class AvatarIconRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
