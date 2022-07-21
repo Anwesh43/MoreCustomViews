a9c6ad93534f14ca2d8058ff656433e756aec9d6
Anwesh43 <anweshthecool0@gmail.com>
2022-07-21 13:04:02 +0530

    avatariconrotview: adding Canvas extension methods

--- avatariconrotview/src/main/java/com/example/avatariconrotview/AvtarIconRotView.kt
+++ avatariconrotview/src/main/java/com/example/avatariconrotview/AvtarIconRotView.kt
@@ -30,3 +30,39 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawAvatarIconRot(i : Int, scale : Float, w : Float, h : Float, paint : Paint) {
+    val r : Float = Math.min(w, h) / rFactor
+    val eyeR : Float = Math.min(w, h) / eyeRFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 - (h / 2 + 2 * r) * sc4)
+    rotate(rot * sc4)
+    paint.style = Paint.Style.FILL
+    paint.color = colors[i]
+    drawCircle(0f, -r, r * sc1, paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        paint.style = Paint.Style.STROKE
+        paint.color = colors[i]
+        drawArc(RectF(r - 2 * r, -r, r + 2 * r, r), -90f, 90f * sc2, false, paint)
+        paint.style = Paint.Style.FILL
+        paint.color = backColor
+        drawCircle(- r / 3 + eyeR / 3, -r / 3 + eyeR / 3, eyeR * sc3, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawAIRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawAvatarIconRot(i, scale, w, h, paint)
+}
