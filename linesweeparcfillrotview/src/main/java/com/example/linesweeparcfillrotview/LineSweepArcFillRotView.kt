9877d043fc2a6e67a0110510d1d9f99fe3a40f40
Anwesh43 <anweshthecool0@gmail.com>
2022-07-22 09:40:01 +0530

    linesweeparcfillrotview: adding Canvas extension methods

--- linesweeparcfillrotview/src/main/java/com/example/linesweeparcfillrotview/LineSweepArcFillRotView.kt
+++ linesweeparcfillrotview/src/main/java/com/example/linesweeparcfillrotview/LineSweepArcFillRotView.kt
@@ -25,7 +25,34 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val deg : Float = -90f
+val rFactor : Float = 9.8f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineSweepArcFill(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val r : Float = Math.min(w, h) / rFactor
+    save()
+    translate(w / 2 + (w / 2 + size) * sc5, h / 2)
+    rotate(-deg * sc4)
+    drawLine(size, 0f, size - (size - r) * sc1, 0f, paint)
+    drawArc(RectF(-r, -r, r, r), 0f, 180f * sc2, false, paint)
+    drawLine(r, 0f, r + (size - r) * sc3, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawLSAFRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineSweepArcFill(scale, w, h, paint)
+}
