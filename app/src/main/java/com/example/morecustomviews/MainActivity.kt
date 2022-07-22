54d6b6f398ce9beb80e4965d1d9a78747d1ec6da
Anwesh43 <anweshthecool0@gmail.com>
2022-07-22 10:31:59 +0530

    linesweeparcfillrotview: adding LineSweepArcFillRotView in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.avatariconrotview.AvatarIconRotView
+import com.example.linesweeparcfillrotview.LineSweepArcFillRotView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
-        AvatarIconRotView.create(this)
+        LineSweepArcFillRotView.create(this)
         fullScreen()
     }
 }
