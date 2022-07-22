a5992889886b8ca5721e3caf6796aa22ea0c0c33
Anwesh43 <anweshthecool0@gmail.com>
2022-07-22 09:47:48 +0530

    linesweeparcfillrotview: adding LSAFRNode class

--- linesweeparcfillrotview/src/main/java/com/example/linesweeparcfillrotview/LineSweepArcFillRotView.kt
+++ linesweeparcfillrotview/src/main/java/com/example/linesweeparcfillrotview/LineSweepArcFillRotView.kt
@@ -118,4 +118,45 @@ class LineSweepArcFillRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSAFRNode(var i : Int, val state : State = State()) {
+
+        private var next : LSAFRNode? = null
+        private var prev : LSAFRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSAFRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSAFRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSAFRNode {
+            var curr : LSAFRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
