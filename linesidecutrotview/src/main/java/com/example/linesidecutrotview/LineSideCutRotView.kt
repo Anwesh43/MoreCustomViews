306842891bf69675e958b62ea89e31ad5f2efff6
Anwesh43 <anweshthecool0@gmail.com>
2022-07-25 00:29:54 +0530

    linesidecutrotview: adding Canvas extension methods

--- linesidecutrotview/src/main/java/com/example/linesidecutrotview/LineSideCutRotView.kt
+++ linesidecutrotview/src/main/java/com/example/linesidecutrotview/LineSideCutRotView.kt
@@ -28,3 +28,31 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineSideCutRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+    rotate(rot * sc3)
+    drawLine(0f, -size * 0.5f * sc1, 0f, size * 0.5f * sc1, paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        drawLine(0f, size / 4, size * 0.5f * sc2, size / 4, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLSCRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineSideCutRot(scale, w, h, paint)
+}
