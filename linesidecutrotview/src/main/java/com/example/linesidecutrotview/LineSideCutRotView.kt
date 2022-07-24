d00787507c68abdf302d4c21cea6a02f132ca05d
Anwesh43 <anweshthecool0@gmail.com>
2022-07-25 00:40:59 +0530

    linesidecutrotsview: adding LineSideCutRotView class

--- linesidecutrotview/src/main/java/com/example/linesidecutrotview/LineSideCutRotView.kt
+++ linesidecutrotview/src/main/java/com/example/linesidecutrotview/LineSideCutRotView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawLSCRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSideCutRot(scale, w, h, paint)
 }
+
+class LineSideCutRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
