d75bff57b1f5e30bec7e111d3ae9c209569ec230
Anwesh43 <anweshthecool0@gmail.com>
2022-07-25 00:48:48 +0530

    linesidecutrotsview: adding LSCRNode class

--- linesidecutrotview/src/main/java/com/example/linesidecutrotview/LineSideCutRotView.kt
+++ linesidecutrotview/src/main/java/com/example/linesidecutrotview/LineSideCutRotView.kt
@@ -119,4 +119,45 @@ class LineSideCutRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSCRNode(var i : Int, val state : State = State()) {
+
+        private var next : LSCRNode? = null
+        private var prev : LSCRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSCRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSCRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSCRNode {
+            var curr : LSCRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
