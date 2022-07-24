e7cf142d72fbc1f3d93284b3c646921ea21103aa
Anwesh43 <anweshthecool0@gmail.com>
2022-07-24 09:50:29 +0530

    cutsidebarupview: adding Canvas extension methods

--- cutsideupbarview/src/main/java/com/example/cutsideupbarview/CutSideUpBarView.kt
+++ cutsideupbarview/src/main/java/com/example/cutsideupbarview/CutSideUpBarView.kt
@@ -25,7 +25,36 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 11.2f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawCutSideUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+    rotate(rot * sc3)
+    drawRect(RectF(-size / 2, size / 2 - size * sc1, size / 2, size / 2), paint)
+    if (sc2 > 0f) {
+        val path : Path = Path()
+        path.moveTo(-size / 2, -size / 2)
+        path.lineTo(-size / 2, -size / 2 - size * sc2)
+        path.lineTo(size / 2, -size / 2)
+        path.lineTo(-size / 2, -size / 2)
+        drawPath(path, paint)
+    }
+    restore()
+}
+
+fun Canvas.drawCSUBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawCutSideUp(scale, w, h, paint)
+}
