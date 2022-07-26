8477d20e8431ec68b30e3e0f4eb75f8d03bff090
Anwesh43 <anweshthecool0@gmail.com>
2022-07-26 09:44:43 +0530

    pointlinerotupview: adding PointLineRotUp class

--- pointlinerotupview/src/main/java/com/example/pointlinerotupview/PointLineRotUpView.kt
+++ pointlinerotupview/src/main/java/com/example/pointlinerotupview/PointLineRotUpView.kt
@@ -171,4 +171,27 @@ class PointLineRotUpView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class PointLineRotUp(var i : Int) {
+
+        private var curr : PLRUNode = PLRUNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
