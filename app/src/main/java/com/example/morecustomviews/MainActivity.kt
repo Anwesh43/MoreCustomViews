98fb708fbc3b7e33c37554169ce84d977eff30a3
Anwesh43 <anweshthecool0@gmail.com>
2022-07-26 09:47:55 +0530

    pointlinerotupview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linesidecutrotview.LineSideCutRotView
+import com.example.pointlinerotupview.PointLineRotUpView
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
-        LineSideCutRotView.create(this)
+        PointLineRotUpView.create(this)
         fullScreen()
     }
 }
