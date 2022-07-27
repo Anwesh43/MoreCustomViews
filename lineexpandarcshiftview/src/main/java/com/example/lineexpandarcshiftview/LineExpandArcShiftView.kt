3e37edd269aaeb6352e733172db5c474ab850f89
Anwesh43 <anweshthecool0@gmail.com>
2022-07-27 10:21:54 +0530

    lineexpandarcshiftview: adding Canvas extension methods

--- lineexpandarcshiftview/src/main/java/com/example/lineexpandarcshiftview/LineExpandArcShiftView.kt
+++ lineexpandarcshiftview/src/main/java/com/example/lineexpandarcshiftview/LineExpandArcShiftView.kt
@@ -30,3 +30,29 @@ val rFactor : Float = 11.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineExpandArcShift(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val r : Float = Math.min(w, h) / rFactor
+    save()
+    translate(w / 2, h / 2)
+    save()
+    rotate(rot * sc2)
+    drawLine(0f, 0f, -size * sc1, 0f, paint)
+    restore()
+    drawArc(RectF(-r, -r, r, r), rot * sc3, rot * (sc2 - sc3), false, paint)
+    restore()
+}
+
+fun Canvas.drawLEASNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    drawLineExpandArcShift(scale, w, h, paint)
+}
