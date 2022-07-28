de739282f557a31335bef0711be83894a53abc14
Anwesh43 <anweshthecool0@gmail.com>
2022-07-28 15:33:25 +0530

    [lineexpandbarshiftview] adding module setup

--- lineexpandarcshiftview/src/main/java/com/example/lineexpandarcshiftview/LineExpandArcShiftView.kt
+++ lineexpandarcshiftview/src/main/java/com/example/lineexpandarcshiftview/LineExpandArcShiftView.kt
@@ -39,12 +39,12 @@ fun Canvas.drawLineExpandArcShift(scale : Float, w : Float, h : Float, paint : P
     val sc4 : Float = scale.divideScale(3, parts)
     val r : Float = Math.min(w, h) / rFactor
     save()
-    translate(w / 2, h / 2)
+    translate(w / 2 + (w / 2 ) * sc4, h / 2)
     save()
     rotate(rot * sc2)
     drawLine(0f, 0f, -size * sc1, 0f, paint)
     restore()
-    drawArc(RectF(-r, -r, r, r), rot * sc3, rot * (sc2 - sc3), false, paint)
+    drawArc(RectF(-r, -r, r, r), rot + rot * sc3, rot * (sc2 - sc3), false, paint)
     restore()
 }
 
@@ -53,6 +53,7 @@ fun Canvas.drawLEASNode(i : Int, scale : Float, paint : Paint) {
     val h : Float = height.toFloat()
     paint.color = colors[i]
     paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
     paint.style = Paint.Style.STROKE
     drawLineExpandArcShift(scale, w, h, paint)
 }
