e8513023503d0ffe84a29f2fa18b60e4c667ca4f
Anwesh43 <anweshthecool0@gmail.com>
2022-07-29 01:19:53 +0530

    [zlinerotmoveview]: adding Canvas extension methods

--- zlinerotmoveview/src/main/java/com/example/zlinerotmoveview/ZLineRotMoveView.kt
+++ zlinerotmoveview/src/main/java/com/example/zlinerotmoveview/ZLineRotMoveView.kt
@@ -29,3 +29,35 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawZLineRotMove(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + size) * sc4, h / 2 )
+    rotate(rot * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        for (k in 0..1) {
+            save()
+            rotate(-rot * sc2 * k)
+            drawLine(0f, 0f, size * 0.5f * sc1, 0f, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawZLRMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawZLineRotMove(scale, w, h, paint)
+}
