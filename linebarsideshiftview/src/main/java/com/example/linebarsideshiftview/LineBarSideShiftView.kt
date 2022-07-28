4a69e490ad99adf80d62cf88b9cd26d8e8097ffa
Anwesh43 <anweshthecool0@gmail.com>
2022-07-28 16:06:12 +0530

    linebarsideshiftview: adding LBSSNode class

--- linebarsideshiftview/src/main/java/com/example/linebarsideshiftview/LineBarSideShiftView.kt
+++ linebarsideshiftview/src/main/java/com/example/linebarsideshiftview/LineBarSideShiftView.kt
@@ -120,4 +120,45 @@ class LineBarSideShiftView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBSSNode(var i : Int, val state : State = State()) {
+
+        private var prev : LBSSNode? = null
+        private var next : LBSSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBSSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBSSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBSSNode {
+            var curr : LBSSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
