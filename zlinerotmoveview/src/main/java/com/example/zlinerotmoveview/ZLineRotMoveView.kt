4caffdcb71c23028895e90fcfd871ce9f3015fec
Anwesh43 <anweshthecool0@gmail.com>
2022-07-29 01:28:18 +0530

    [zlinerotmoveview]: adding ZLRMNode class

--- zlinerotmoveview/src/main/java/com/example/zlinerotmoveview/ZLineRotMoveView.kt
+++ zlinerotmoveview/src/main/java/com/example/zlinerotmoveview/ZLineRotMoveView.kt
@@ -124,4 +124,45 @@ class ZLineRotMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ZLRMNode(var i : Int, val state : State = State()) {
+
+        private var next : ZLRMNode? = null
+        private var prev : ZLRMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ZLRMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawZLRMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ZLRMNode {
+            var curr : ZLRMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
