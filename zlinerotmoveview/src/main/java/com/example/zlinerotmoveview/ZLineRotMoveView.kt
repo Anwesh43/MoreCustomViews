65bfb313b68cba9f7e5e571598214c2e946024c3
Anwesh43 <anweshthecool0@gmail.com>
2022-07-29 01:20:49 +0530

    [zlinerotmoveview]: adding ZLineRotMoveView class

--- zlinerotmoveview/src/main/java/com/example/zlinerotmoveview/ZLineRotMoveView.kt
+++ zlinerotmoveview/src/main/java/com/example/zlinerotmoveview/ZLineRotMoveView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawZLRMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawZLineRotMove(scale, w, h, paint)
 }
+
+class ZLineRotMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
