9980df9cc70215beb50c0a71e7f3fcd3e66ddde0
Anwesh43 <anweshthecool0@gmail.com>
2022-07-29 01:31:24 +0530

    [zlinerotmoveview]: adding Renderer class

--- zlinerotmoveview/src/main/java/com/example/zlinerotmoveview/ZLineRotMoveView.kt
+++ zlinerotmoveview/src/main/java/com/example/zlinerotmoveview/ZLineRotMoveView.kt
@@ -188,4 +188,27 @@ class ZLineRotMoveView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ZLineRotMoveView) {
+
+        private val animator : Animator = Animator(view)
+        private val zlrm : ZLineRotMove = ZLineRotMove(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            zlrm.draw(canvas, paint)
+            animator.animate {
+                zlrm.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            zlrm.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
