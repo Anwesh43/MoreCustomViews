ffaee0158cfd96590f7432ad5e59bfdb9107d280
Anwesh43 <anweshthecool0@gmail.com>
2022-07-28 16:15:13 +0530

    linebarsideshiftview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,14 +3,14 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.lineexpandarcshiftview.LineExpandArcShiftView
+import com.example.linebarsideshiftview.LineBarSideShiftView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
-        LineExpandArcShiftView.create(this)
+        LineBarSideShiftView.create(this)
         fullScreen()
     }
 }
