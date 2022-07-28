c83cb4da83975463702f7a1f70ea8a61b415883d
Anwesh43 <anweshthecool0@gmail.com>
2022-07-28 15:58:18 +0530

    linebarsideshiftview: adding LineBarSideShiftView class

--- linebarsideshiftview/src/main/java/com/example/linebarsideshiftview/LineBarSideShiftView.kt
+++ linebarsideshiftview/src/main/java/com/example/linebarsideshiftview/LineBarSideShiftView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLBSSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawLineBarSideShift(scale, w, h, paint)
 }
+
+class LineBarSideShiftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTocuhEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
