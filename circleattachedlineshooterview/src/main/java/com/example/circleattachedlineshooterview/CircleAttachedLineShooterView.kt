61aadb1ed9647b71a4d98ab6bebd8c48c05c95e0
Anwesh43 <anweshthecool0@gmail.com>
2022-07-30 14:11:40 +0530

    circleattachedlineshooterview: adding CALSNode class

--- circleattachedlineshooterview/src/main/java/com/example/circleattachedlineshooterview/CircleAttachedLineShooterView.kt
+++ circleattachedlineshooterview/src/main/java/com/example/circleattachedlineshooterview/CircleAttachedLineShooterView.kt
@@ -120,4 +120,45 @@ class CircleAttachedLineShooterView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class CALSNode(var i : Int, val state : State = State()) {
+
+        private var next : CALSNode? = null
+        private var prev : CALSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = CALSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawCALSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : CALSNode {
+            var curr : CALSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
