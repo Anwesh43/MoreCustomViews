4cbd81b109f50ba3d5a65ed4851408f50b0dc3ee
Anwesh43 <anweshthecool0@gmail.com>
2022-07-30 14:03:28 +0530

    circleattachedlineshooterview: adding CircleAttachedLineShooterView class

--- circleattachedlineshooterview/src/main/java/com/example/circleattachedlineshooterview/CircleAttachedLineShooterView.kt
+++ circleattachedlineshooterview/src/main/java/com/example/circleattachedlineshooterview/CircleAttachedLineShooterView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawCALSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawCircleAttachedLineShooter(scale, w, h, paint)
 }
+
+class CircleAttachedLineShooterView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
