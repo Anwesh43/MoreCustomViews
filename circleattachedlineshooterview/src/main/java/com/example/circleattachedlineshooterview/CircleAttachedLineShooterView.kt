fd357f4df68abf198ee9fab14a7a4e084062aefa
Anwesh43 <anweshthecool0@gmail.com>
2022-07-30 14:01:03 +0530

    circleattachedlineshooterview: adding Canvas extension methods

--- circleattachedlineshooterview/src/main/java/com/example/circleattachedlineshooterview/CircleAttachedLineShooterView.kt
+++ circleattachedlineshooterview/src/main/java/com/example/circleattachedlineshooterview/CircleAttachedLineShooterView.kt
@@ -29,3 +29,31 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawCircleAttachedLineShooter(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2 + (w / 2 + r) * sc5, h / 2)
+    rotate(rot * sc3)
+    drawCircle(0f, 0f, r * sc1, paint)
+    save()
+    translate(0f, -r - (h / 2) * sc4)
+    drawLine(0f, 0f, 0f, -size * sc2, paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawCALSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawCircleAttachedLineShooter(scale, w, h, paint)
+}
