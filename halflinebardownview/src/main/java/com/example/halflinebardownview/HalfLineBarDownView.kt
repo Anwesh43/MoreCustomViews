75770b9e40ffe587076a022ff1e55b86cf3d78d8
Anwesh43 <anweshthecool0@gmail.com>
2022-07-31 08:45:58 +0530

    halflinebardownview: adding Canvas extension methods

--- halflinebardownview/src/main/java/com/example/halflinebardownview/HalfLineBarDownView.kt
+++ halflinebardownview/src/main/java/com/example/halflinebardownview/HalfLineBarDownView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 5
+val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -29,3 +29,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawHalfLineBarDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        save()
+        rotate(-rot * sc3)
+        drawLine(0f, 0f, size * 0.5f * sc1, 0f, paint)
+        restore()
+        drawRect(RectF(0f, 0f, size * 0.5f * sc2, size * 0.25f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawHLBDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawHalfLineBarDown(scale, w, h, paint)
+}
