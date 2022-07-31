e25e5cc1b31059a245c253656e270dad5784aaeb
Anwesh43 <anweshthecool0@gmail.com>
2022-07-31 08:47:07 +0530

    halflinebardownview: adding HalfBarDownView class

--- halflinebardownview/src/main/java/com/example/halflinebardownview/HalfLineBarDownView.kt
+++ halflinebardownview/src/main/java/com/example/halflinebardownview/HalfLineBarDownView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawHLBDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawHalfLineBarDown(scale, w, h, paint)
 }
+
+class HalfLineBarDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
