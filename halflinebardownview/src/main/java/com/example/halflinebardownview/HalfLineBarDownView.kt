2d58a903c6c43566f5820f75377586a1b4ddb18d
Anwesh43 <anweshthecool0@gmail.com>
2022-07-31 08:55:43 +0530

    halflinebardownview: adding HLBDNode class

--- halflinebardownview/src/main/java/com/example/halflinebardownview/HalfLineBarDownView.kt
+++ halflinebardownview/src/main/java/com/example/halflinebardownview/HalfLineBarDownView.kt
@@ -122,4 +122,45 @@ class HalfLineBarDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HLBDNode(var i : Int, val state : State = State()) {
+
+        private var next : HLBDNode? = null
+        private var prev : HLBDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HLBDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHLBDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdaitng(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HLBDNode {
+            var curr : HLBDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
