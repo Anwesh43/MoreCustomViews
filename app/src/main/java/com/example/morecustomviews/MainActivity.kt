7294e09af0101db61844e7d50b85dc297c8cacdf
Anwesh43 <anweshthecool0@gmail.com>
2022-07-31 09:15:36 +0530

    halflinebardownview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,14 +3,16 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.circleattachedlineshooterview.CircleAttachedLineShooterView
+import com.example.halflinebardownview.HalfLineBarDownView
+
+//import com.example.circleattachedlineshooterview.CircleAttachedLineShooterView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
-        CircleAttachedLineShooterView.create(this)
+        HalfLineBarDownView.create(this)
         fullScreen()
     }
 }
