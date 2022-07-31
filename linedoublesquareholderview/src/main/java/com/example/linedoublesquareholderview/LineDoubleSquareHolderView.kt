e135a89ae69161488960aaaf89556b97a1978148
Anwesh43 <anweshthecool0@gmail.com>
2022-08-01 01:46:58 +0530

    linedoublesquareholderview: adding Canvas extension methods

--- linedoublesquareholderview/src/main/java/com/example/linedoublesquareholderview/LineDoubleSquareHolderView.kt
+++ linedoublesquareholderview/src/main/java/com/example/linedoublesquareholderview/LineDoubleSquareHolderView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -30,3 +30,35 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineDoubleSquareHolder(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sqSize : Float = Math.min(w, h) / sqFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2 + (w / 2 + size) * sc5, h / 2)
+    rotate(rot * sc4)
+    drawLine(0f, size / 2 - size * sc1, 0f, size / 2, paint)
+    drawLine(-size * 0.5f * sc2, -size / 2, size * 0.5f * sc2, -size / 2, paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-size / 2 + sqSize / 2, -h / 2 - sqSize / 2 + (h / 2 - size - sqSize / 2) * sc3)
+        drawRect(RectF(-sqSize / 2, -sqSize / 2, sqSize / 2, sqSize / 2), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLDSHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineDoubleSquareHolder(scale, w, h, paint)
+}
