abda5d4aee2d9ba0987b818dd9df8611cf7d7d17
Anwesh43 <anweshthecool0@gmail.com>
2022-07-31 09:10:33 +0530

    halflinebardownview: adding HalfLineBarDown class

--- halflinebardownview/src/main/java/com/example/halflinebardownview/HalfLineBarDownView.kt
+++ halflinebardownview/src/main/java/com/example/halflinebardownview/HalfLineBarDownView.kt
@@ -147,7 +147,7 @@ class HalfLineBarDownView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
-        fun startUpdaitng(cb : () -> Unit) {
+        fun startUpdating(cb : () -> Unit) {
             state.startUpdating(cb)
         }
 
@@ -161,6 +161,29 @@ class HalfLineBarDownView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class HalfLineBarDown(var i : Int) {
+
+        private var curr : HLBDNode = HLBDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
