345ac34e140bf0aa7d4bf44851edaabce62a64ff
Anwesh43 <anweshthecool0@gmail.com>
2022-08-02 13:53:33 +0530

    [lineextensionrotview]: adding Canvas extension methods

--- lineextensionrotview/src/main/java/com/example/lineextensionrotview/LineExtensionRotView.kt
+++ lineextensionrotview/src/main/java/com/example/lineextensionrotview/LineExtensionRotView.kt
@@ -28,3 +28,26 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineExtensionRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + size) * sc3, h / 2)
+    rotate(rot * sc3)
+    drawLine(0f, 0f, size * sc1, -size * sc1, paint)
+    drawLine(0f, 0f, 0f, size * sc2, paint)
+    restore()
+}
+
+fun Canvas.drawLERNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineExtensionRot(scale, w, h, paint)
+}
