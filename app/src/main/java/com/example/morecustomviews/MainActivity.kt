b3ec2e7f5b5064f5716ae05deeb0c478db6bfdd9
Anwesh43 <anweshthecool0@gmail.com>
2022-08-02 15:34:38 +0530

    lineextensionrotview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.lineextensionrotview.LineExtensionRotView
 
 //import com.example.circleattachedlineshooterview.CircleAttachedLineShooterView
 
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
+        LineExtensionRotView.create(this)
         fullScreen()
     }
 }
