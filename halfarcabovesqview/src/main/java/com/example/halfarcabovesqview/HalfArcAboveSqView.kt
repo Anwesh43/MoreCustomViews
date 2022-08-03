fbc107f52ba485acdcca7ee6a6256d05fddd7e85
Anwesh43 <anweshthecool0@gmail.com>
2022-08-03 12:29:48 +0530

    halfarcabovesqview: adding Canvas extension methods

--- halfarcabovesqview/src/main/java/com/example/halfarcabovesqview/HalfArcAboveSqView.kt
+++ halfarcabovesqview/src/main/java/com/example/halfarcabovesqview/HalfArcAboveSqView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -30,3 +30,34 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawHalfArcAboveSq(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2 + (w / 2 + size) * sc5, h / 2)
+    rotate(deg * sc4)
+    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), rot, rot * sc2, true, paint)
+    drawRect(RectF(size / 2 - size * sc1, 0f, size / 2, size), paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-size / 2, size / 2)
+        drawLine(0f, 0f, (-size / 3) * sc3, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawHAASNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawHalfArcAboveSq(scale, w, h, paint)
+}
