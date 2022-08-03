27d4a4008b535d4241a625c810c3d8f3acd17d88
Anwesh43 <anweshthecool0@gmail.com>
2022-08-03 16:19:56 +0530

    halfarcabovesqview: adding HalfArcAboveSq class

--- halfarcabovesqview/src/main/java/com/example/halfarcabovesqview/HalfArcAboveSqView.kt
+++ halfarcabovesqview/src/main/java/com/example/halfarcabovesqview/HalfArcAboveSqView.kt
@@ -165,4 +165,27 @@ class HalfArcAboveSqView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class HalfArcAboveSq(var i : Int) {
+
+        private var curr : HAASNode = HAASNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
