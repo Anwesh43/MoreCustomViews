592b62d998eee61577aff6b0cf6190c5a1ed3382
Anwesh43 <anweshthecool0@gmail.com>
2022-08-05 03:28:45 +0530

    vballmoverview : adding VBallLineMoverView class

--- vballmoverview/src/main/java/com/example/vballmoverview/VBallLineMoverView.kt
+++ vballmoverview/src/main/java/com/example/vballmoverview/VBallLineMoverView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawVBLMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawVBallLineMover(scale, w, h, paint)
 }
+
+class VBallLineMoverView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
