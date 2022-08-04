6b5a6afbedcd998f7285611c3f54d58aae9bdf34
Anwesh43 <anweshthecool0@gmail.com>
2022-08-05 03:34:27 +0530

    vballmoverview : adding VBLMNode class

--- vballmoverview/src/main/java/com/example/vballmoverview/VBallLineMoverView.kt
+++ vballmoverview/src/main/java/com/example/vballmoverview/VBallLineMoverView.kt
@@ -123,4 +123,45 @@ class VBallLineMoverView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class VBLMNode(var i : Int, val state : State = State()) {
+
+        private var next : VBLMNode? = null
+        private var prev : VBLMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = VBLMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawVBLMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : VBLMNode {
+            var curr : VBLMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
