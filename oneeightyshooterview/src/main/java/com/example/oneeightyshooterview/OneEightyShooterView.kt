c2e9681ff6443a3cab837486d8d108001ad4babb
Anwesh43 <anweshthecool0@gmail.com>
2022-08-04 11:59:40 +0530

    oneeightyshooterview: adding constants and libraries

---
+++ oneeightyshooterview/src/main/java/com/example/oneeightyshooterview/OneEightyShooterView.kt
@@ -0,0 +1,28 @@
+package com.example.oneeightyshooterview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 6.9f
+val r : Float = 19.2f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val rot : Float = 180f
+val deg : Float = 90f
