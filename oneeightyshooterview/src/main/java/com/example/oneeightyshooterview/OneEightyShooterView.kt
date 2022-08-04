832e3f6e39083dbe7fe0c747bc0959400d7f14d4
Anwesh43 <anweshthecool0@gmail.com>
2022-08-04 12:07:52 +0530

    oneeightyshooterview: adding Canvas extension methods

--- oneeightyshooterview/src/main/java/com/example/oneeightyshooterview/OneEightyShooterView.kt
+++ oneeightyshooterview/src/main/java/com/example/oneeightyshooterview/OneEightyShooterView.kt
@@ -21,7 +21,7 @@ val parts : Int = 6
 val scGap : Float = 0.05f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 6.9f
-val r : Float = 19.2f
+val rFactor  : Float = 19.2f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
 val rot : Float = 180f
@@ -31,3 +31,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawOneEightyShooter(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    val sc6 : Float = scale.divideScale(5, parts)
+    val r : Float = Math.min(w, h) / rFactor
+    save()
+    translate(w / 2, h / 2)
+    rotate(deg * sc5)
+    for (j in 0..1) {
+        save()
+        rotate(-rot * sc2 * j)
+        save()
+        translate(0f, (w / 2) * sc6)
+        drawLine(0f, 0f, 0f, size * sc2, paint)
+        restore()
+        drawCircle(0f, (h / 2 + r ) * sc4, r * sc3, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawOESNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawOneEightyShooter(scale, w, h, paint)
+}
