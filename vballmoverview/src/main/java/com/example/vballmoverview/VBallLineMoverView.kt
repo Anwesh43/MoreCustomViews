7e1ce906babdf0205ba99ec821ebdf27c4fe9d3d
Anwesh43 <anweshthecool0@gmail.com>
2022-08-05 03:27:43 +0530

    vballmoverview : adding Canvas extension methods

--- vballmoverview/src/main/java/com/example/vballmoverview/VBallLineMoverView.kt
+++ vballmoverview/src/main/java/com/example/vballmoverview/VBallLineMoverView.kt
@@ -31,4 +31,32 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawVBallLineMover(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val r : Float = Math.min(w, h) / rFactor
+    val a : Float = size * sc2
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size + r) * sc4)
+    rotate(deg * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawLine(0f, 0f, a, -a, paint)
+        drawCircle(a, -a, r * sc1, paint)
+        restore()
+    }
+    restore()
+}
 
+fun Canvas.drawVBLMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawVBallLineMover(scale, w, h, paint)
+}
