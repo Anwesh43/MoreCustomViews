923a4d9271771e25d88c614a722f9d574daed665
Anwesh43 <anweshthecool0@gmail.com>
2022-08-04 12:24:41 +0530

    oneeightyshooterview: adding OESNode class

--- oneeightyshooterview/src/main/java/com/example/oneeightyshooterview/OneEightyShooterView.kt
+++ oneeightyshooterview/src/main/java/com/example/oneeightyshooterview/OneEightyShooterView.kt
@@ -127,4 +127,46 @@ class OneEightyShooterView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class OESNode(var i : Int, val state : State = State()) {
+
+        private var next : OESNode? = null
+        private var prev : OESNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = OESNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawOESNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : OESNode {
+            var curr : OESNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+    }
 }
