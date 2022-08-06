cc35e665e912019c44ec047614008f7d0c02f9da
Anwesh43 <anweshthecool0@gmail.com>
2022-08-06 12:58:33 +0530

    linerotdownrectview: adding LRDRNode class

--- linerotdownrectview/src/main/java/com/example/linerotdownrectview/LineRotDownRectView.kt
+++ linerotdownrectview/src/main/java/com/example/linerotdownrectview/LineRotDownRectView.kt
@@ -119,4 +119,45 @@ class LineRotDownRectView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRDRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRDRNode? = null
+        private var prev : LRDRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRDRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRDRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRDRNode {
+            var curr : LRDRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
