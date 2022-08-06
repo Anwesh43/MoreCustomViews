f34840863580126d7e2ed65a95d70832d7e940cc
Anwesh43 <anweshthecool0@gmail.com>
2022-08-06 12:42:33 +0530

    linerotdownrectview: adding Canvas extension methods

--- linerotdownrectview/src/main/java/com/example/linerotdownrectview/LineRotDownRectView.kt
+++ linerotdownrectview/src/main/java/com/example/linerotdownrectview/LineRotDownRectView.kt
@@ -29,3 +29,31 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineRotDownRect(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2 - (h / 2 + size) * sc4)
+    for (j in 0..1) {
+        save()
+        rotate(rot * j * sc2)
+        drawLine(0f, 0f, size * sc1, 0f, paint)
+        restore()
+    }
+    drawRect(RectF(0f, -size * 0.5f * sc3, size, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawLRDRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotDownRect(scale, w, h, paint)
+}
+
