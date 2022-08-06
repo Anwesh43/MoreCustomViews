6ab5f1f2701dcbd8135404101add83daf9cc33d9
Anwesh43 <anweshthecool0@gmail.com>
2022-08-06 13:09:38 +0530

    linerotdownrectview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.vballmoverview.VBallLineMoverView
+import com.example.linerotdownrectview.LineRotDownRectView
 
 //import com.example.circleattachedlineshooterview.CircleAttachedLineShooterView
 
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
-        VBallLineMoverView.create(this)
+        LineRotDownRectView.create(this)
         fullScreen()
     }
 }
