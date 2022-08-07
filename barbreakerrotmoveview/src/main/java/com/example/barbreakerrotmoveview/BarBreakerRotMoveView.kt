08dcbe5154a5baee1f265c7588c50a7c2e040283
Anwesh43 <anweshthecool0@gmail.com>
2022-08-07 10:14:16 +0530

    barbreakrotmoveview: adding BarBreakerRotMoveView class

--- barbreakerrotmoveview/src/main/java/com/example/barbreakerrotmoveview/BarBreakerRotMoveView.kt
+++ barbreakerrotmoveview/src/main/java/com/example/barbreakerrotmoveview/BarBreakerRotMoveView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawBBRMNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBarBreakRotMove(scale, w, h, paint)
 }
+
+class BarBreakerRotMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
