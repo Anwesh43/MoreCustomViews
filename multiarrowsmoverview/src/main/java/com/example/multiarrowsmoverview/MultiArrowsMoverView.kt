402f80100729c3b842fb9e74aa05222ec4c95ce8
Anwesh43 <anweshthecool0@gmail.com>
2022-08-09 01:20:10 +0530

    multiarrowsmoverview: adding Canvas extension methods

--- multiarrowsmoverview/src/main/java/com/example/multiarrowsmoverview/MultiArrowsMoverView.kt
+++ multiarrowsmoverview/src/main/java/com/example/multiarrowsmoverview/MultiArrowsMoverView.kt
@@ -29,3 +29,37 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawMultiArrowsMover(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val lSize : Float = size / lines
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..(lines - 1)) {
+        val sc1j : Float = sc1.divideScale(j, parts)
+        val sc2j : Float = sc2.divideScale(j, parts)
+        val sc3j : Float = sc3.divideScale(j, parts)
+        save()
+        translate(-w / 2 + (w / 2) * sc1j + (w / 2 + lSize) * sc3j, -h / 2 - lSize / 2 + lSize * j)
+        for (k in 0..1) {
+            save()
+            rotate(deg * (1 - 2 * k) * sc2j)
+            drawLine(0f, 0f, -lSize, 0f, paint)
+            restore()
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawMAMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.color = colors[i]
+    drawMultiArrowsMover(scale, w, h, paint)
+}
