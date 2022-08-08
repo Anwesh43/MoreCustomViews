b40446f87229a424a74140bec323422a31987e0a
Anwesh43 <anweshthecool0@gmail.com>
2022-08-09 01:32:40 +0530

    multiarrowsmoverview: adding static create method

--- multiarrowsmoverview/src/main/java/com/example/multiarrowsmoverview/MultiArrowsMoverView.kt
+++ multiarrowsmoverview/src/main/java/com/example/multiarrowsmoverview/MultiArrowsMoverView.kt
@@ -215,4 +215,13 @@ class MultiArrowsMoverView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : MultiArrowsMoverView {
+            val view : MultiArrowsMoverView = MultiArrowsMoverView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
