11ad4eddb61525b363a9806bafeaac58a1a68be2
Anwesh43 <anweshthecool0@gmail.com>
2022-08-08 13:18:25 +0530

    bilaterallinebarupview: adding Canvas extension methods

--- bilaterallinebarupview/src/main/java/com/example/bilaterallinebarupview/BiLateralLineBarUpView.kt
+++ bilaterallinebarupview/src/main/java/com/example/bilaterallinebarupview/BiLateralLineBarUpView.kt
@@ -29,3 +29,31 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBiLateralLineBarUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate(-size / 2, -(h / 2) * scale.divideScale(3 + j, parts))
+        drawLine(0f, 0f, 0f, -size * sc1, paint)
+        drawRect(RectF(0f, -size * 0.5f * sc2, size / 2, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBLLBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiLateralLineBarUp(scale, w, h, paint)
+}
