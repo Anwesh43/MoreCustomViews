ff332dc2bd46d0444a16adf3de30f59f902f7bf6
Anwesh43 <anweshthecool0@gmail.com>
2022-08-09 01:28:00 +0530

    multiarrowsmoverview: adding MAMNode class

--- multiarrowsmoverview/src/main/java/com/example/multiarrowsmoverview/MultiArrowsMoverView.kt
+++ multiarrowsmoverview/src/main/java/com/example/multiarrowsmoverview/MultiArrowsMoverView.kt
@@ -126,4 +126,45 @@ class MultiArrowsMoverView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MAMNode(var i : Int, val state : State = State()) {
+
+        private var next : MAMNode? = null
+        private var prev : MAMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MAMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMAMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MAMNode {
+            var curr : MAMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
