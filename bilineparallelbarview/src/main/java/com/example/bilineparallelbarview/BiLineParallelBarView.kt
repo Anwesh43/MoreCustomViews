dc8e1adaada1f5cf7166d6b9c2aad7939ae252fb
Anwesh43 <anweshthecool0@gmail.com>
2022-08-10 01:27:49 +0530

    bilineparallelbarview: adding Canvas extension methods

--- bilineparallelbarview/src/main/java/com/example/bilineparallelbarview/BiLineParallelBarView.kt
+++ bilineparallelbarview/src/main/java/com/example/bilineparallelbarview/BiLineParallelBarView.kt
@@ -30,3 +30,33 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f /this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBiLineParallelBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val barH : Float = Math.min(w, h) / barHFactor
+    save()
+    translate(w / 2, h / 2 + (h / 2) * sc4)
+    rotate(rot * sc3)
+    drawLine(size * sc2, -size * 0.5f * sc1, size * sc2, size * 0.5f * sc1, paint)
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        translate(0f, -barH / 2)
+        drawLine(0f, 0f, size * sc2, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawBLPBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiLineParallelBar(scale, w, h, paint)
+}
