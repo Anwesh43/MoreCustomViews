a0288bef5e8f3309c90f2b43f984b3359ec29a8c
Anwesh43 <anweshthecool0@gmail.com>
2022-08-10 01:28:54 +0530

    bilineparallelbarview: adding BiLineParallelBarView class

--- bilineparallelbarview/src/main/java/com/example/bilineparallelbarview/BiLineParallelBarView.kt
+++ bilineparallelbarview/src/main/java/com/example/bilineparallelbarview/BiLineParallelBarView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawBLPBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineParallelBar(scale, w, h, paint)
 }
+
+class BiLineParallelBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
