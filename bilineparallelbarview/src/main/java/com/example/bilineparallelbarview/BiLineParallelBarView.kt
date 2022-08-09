c501a6a4f4c83f496a5db2c1fdda3ff62bc5bb41
Anwesh43 <anweshthecool0@gmail.com>
2022-08-10 01:55:22 +0530

    bilineparallelbarview: adding BiLineParallelBar class

--- bilineparallelbarview/src/main/java/com/example/bilineparallelbarview/BiLineParallelBarView.kt
+++ bilineparallelbarview/src/main/java/com/example/bilineparallelbarview/BiLineParallelBarView.kt
@@ -164,4 +164,27 @@ class BiLineParallelBarView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiLineParallelBar(var i : Int) {
+
+        private var curr : BLPBNode = BLPBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
