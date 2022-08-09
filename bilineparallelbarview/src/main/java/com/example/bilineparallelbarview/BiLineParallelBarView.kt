3df61d60d4f6f541b4fcedd9a0c228182c7ccba4
Anwesh43 <anweshthecool0@gmail.com>
2022-08-10 01:47:58 +0530

    bilineparallelbarview: adding BLPBNode class

--- bilineparallelbarview/src/main/java/com/example/bilineparallelbarview/BiLineParallelBarView.kt
+++ bilineparallelbarview/src/main/java/com/example/bilineparallelbarview/BiLineParallelBarView.kt
@@ -123,4 +123,45 @@ class BiLineParallelBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLPBNode(var i : Int, val state : State = State()) {
+
+        private var next : BLPBNode? = null
+        private var prev : BLPBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLPBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLPBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLPBNode {
+            var curr : BLPBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
