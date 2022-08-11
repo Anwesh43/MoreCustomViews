16a62e9a177006c8c76af09f45d63e4364869c7b
Anwesh43 <anweshthecool0@gmail.com>
2022-08-11 08:41:45 +0530

    minilinebarholderview: adding Canvas extension methods

--- minilinebarholderview/src/main/java/com/example/minilinebarholderview/MiniLineBarHolderView.kt
+++ minilinebarholderview/src/main/java/com/example/minilinebarholderview/MiniLineBarHolderView.kt
@@ -21,7 +21,7 @@ val colors : Array<Int> = arrayOf(
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
-val sizeFactor : Float = 4.9f
+val sizeFactor : Float = 11.9f
 val delay : Long = 20
 val rot : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
@@ -29,3 +29,30 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawMiniLineBarHolder(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        rotate(rot * j * sc2)
+        drawLine(0f, 0f, size * sc1, 0f, paint)
+        drawRect(RectF(0f, size * (j - 1) * sc3, size, size * j * sc3), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawMLBHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawMiniLineBarHolder(scale, w, h, paint)
+}
