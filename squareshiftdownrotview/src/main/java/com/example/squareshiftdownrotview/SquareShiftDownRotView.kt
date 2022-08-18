2a6786744c4684f029bbd0183d18ee043e75f69f
Anwesh43 <anweshthecool0@gmail.com>
2022-08-19 01:10:24 +0530

    [squareshiftdownrotview] adding SquareShiftRotDown class

--- squareshiftdownrotview/src/main/java/com/example/squareshiftdownrotview/SquareShiftDownRotView.kt
+++ squareshiftdownrotview/src/main/java/com/example/squareshiftdownrotview/SquareShiftDownRotView.kt
@@ -154,7 +154,7 @@ class SquareShiftDownRotView(ctx : Context) : View(ctx) {
             state.startUpdating(cb)
         }
 
-        fun getNexrt(dir : Int, cb : () -> Unit) : SSDRNode {
+        fun getNext(dir : Int, cb : () -> Unit) : SSDRNode {
             var curr : SSDRNode? = prev
             if (dir == 1) {
                 curr = next
@@ -164,6 +164,29 @@ class SquareShiftDownRotView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class SquareShiftDownRot(var i : Int) {
+
+        private var curr : SSDRNode = SSDRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
