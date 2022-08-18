02d452991bc4951e7c60b6ebee10e4ee131412e3
Anwesh43 <anweshthecool0@gmail.com>
2022-08-18 12:23:13 +0530

    linearcrotsidewiseview: adding LARSWNode class

--- linearcrotsidewiseview/src/main/java/com/example/linearcrotsidewiseview/LineArcRotSideWiseView.kt
+++ linearcrotsidewiseview/src/main/java/com/example/linearcrotsidewiseview/LineArcRotSideWiseView.kt
@@ -127,4 +127,45 @@ class LineArcRotSideWiseView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LARSWNode(var i : Int, val state : State = State()) {
+
+        private var next : LARSWNode? = null
+        private var prev : LARSWNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LARSWNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLARSWNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LARSWNode {
+            var curr : LARSWNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
