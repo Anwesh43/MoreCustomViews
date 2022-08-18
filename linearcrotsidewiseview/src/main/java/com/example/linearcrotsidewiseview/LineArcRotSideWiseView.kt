ddc3160638c81853fd292743959887bbb36fcefb
Anwesh43 <anweshthecool0@gmail.com>
2022-08-18 12:03:16 +0530

    linearcrotsidewiseview: adding Canvas extension methods

--- linearcrotsidewiseview/src/main/java/com/example/linearcrotsidewiseview/LineArcRotSideWiseView.kt
+++ linearcrotsidewiseview/src/main/java/com/example/linearcrotsidewiseview/LineArcRotSideWiseView.kt
@@ -25,7 +25,42 @@ val strokeFactor : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
 val rot : Float = 90f
+val rFactor : Float = 16.2f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineArcRotSideWise(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val r : Float = Math.min(w, h) / rFactor
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate((w / 2 + paint.strokeWidth) * sc4, 0f)
+        for (k in 0..1) {
+            save()
+            rotate(90f * k * sc2)
+            drawLine(0f, 0f, 0f, -size * sc1, paint)
+            restore()
+        }
+        drawArc(RectF(-r, -r, r, r), -rot, rot * sc3, true, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLARSWNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcRotSideWise(scale, w, h, paint)
+}
