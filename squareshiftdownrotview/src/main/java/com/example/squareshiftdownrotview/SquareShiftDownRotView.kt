5010453e6c273c590953f63801ea965848682f9a
Anwesh43 <anweshthecool0@gmail.com>
2022-08-19 00:42:06 +0530

    [squarshiftdownrotview]: adding Canvas extension methods

--- squareshiftdownrotview/src/main/java/com/example/squareshiftdownrotview/SquareShiftDownRotView.kt
+++ squareshiftdownrotview/src/main/java/com/example/squareshiftdownrotview/SquareShiftDownRotView.kt
@@ -29,3 +29,36 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSquareShiftDownRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * sc5)
+    save()
+    translate(-w / 2 + w * 0.5f * sc1, 0f)
+    drawRect(RectF(-size, -size, 0f, 0f), paint)
+    restore()
+    save()
+    translate(0f, -size)
+    rotate(rot * sc3)
+    if (sc2 > 0f) {
+        drawLine(0f, 0f, 0f, -size * sc2, paint)
+    }
+    restore()
+    drawRect(RectF(0f, -size, size, -size + size * sc4), paint)
+    restore()
+}
+
+fun Canvas.drawSSDRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSquareShiftDownRot(scale, w, h, paint)
+}
