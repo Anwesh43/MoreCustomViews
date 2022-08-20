a4bba6ac76f7b24955add421b2688bde6209f12a
Anwesh43 <anweshthecool0@gmail.com>
2022-08-20 10:05:05 +0530

    balllineseparateshiftview: adding constants and libraries

---
+++ balllineseparateshiftview/src/main/java/com/example/balllineseparateshiftview/BallLineSeparateShiftView.kt
@@ -0,0 +1,27 @@
+package com.example.balllineseparateshiftview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#AB47BC",
+    "#1E88E5",
+    "#00E678",
+    "#FF3D00",
+    "#3D5AFE"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val deg : Float = 90f
