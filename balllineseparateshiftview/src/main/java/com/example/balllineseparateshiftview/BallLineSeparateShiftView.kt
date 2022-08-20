d0e56f4a56ac9700230df32bd01b0834b1c4a8ca
Anwesh43 <anweshthecool0@gmail.com>
2022-08-20 10:25:21 +0530

    [balllineseparateshiftview]: adding BallLineSeparateShiftView class

--- balllineseparateshiftview/src/main/java/com/example/balllineseparateshiftview/BallLineSeparateShiftView.kt
+++ balllineseparateshiftview/src/main/java/com/example/balllineseparateshiftview/BallLineSeparateShiftView.kt
@@ -71,3 +71,19 @@ fun Canvas.drawBLSSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBallLineSeparateShift(scale, w, h, paint)
 }
+
+class BallLineSeparateShiftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
