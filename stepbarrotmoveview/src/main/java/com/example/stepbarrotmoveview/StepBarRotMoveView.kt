47e88c899153ed94797d029f1087e9361e4fc354
Anwesh43 <anweshthecool0@gmail.com>
2022-08-22 10:52:00 +0530

    stepbarrotmoveview: adding Canvas extension methods

--- stepbarrotmoveview/src/main/java/com/example/stepbarrotmoveview/StepBarRotMoveView.kt
+++ stepbarrotmoveview/src/main/java/com/example/stepbarrotmoveview/StepBarRotMoveView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -29,3 +29,27 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawStepBarRotMove(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc3 : Float = scale.divideScale(2, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sc3)
+    for (j in 0..1) {
+        save()
+        translate((h / 2) * scale.divideScale(j + 3, parts), size * 0.5f * j)
+        drawRect(RectF(-size * scale.divideScale(j, parts), -size / 2, 0f, 0f), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSBRMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawStepBarRotMove(scale, w, h, paint)
+}
