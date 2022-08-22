982f64436880a2b333fda0b7e47c0c972765ad4c
Anwesh43 <anweshthecool0@gmail.com>
2022-08-22 10:59:21 +0530

    stepbarrotmoveview: adding SBRMNode class

--- stepbarrotmoveview/src/main/java/com/example/stepbarrotmoveview/StepBarRotMoveView.kt
+++ stepbarrotmoveview/src/main/java/com/example/stepbarrotmoveview/StepBarRotMoveView.kt
@@ -116,4 +116,45 @@ class StepBarRotMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBRMNode(var i : Int, val state : State = State()) {
+
+        private var next : SBRMNode? = null
+        private var prev : SBRMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SBRMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBRMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBRMNode {
+            var curr : SBRMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
