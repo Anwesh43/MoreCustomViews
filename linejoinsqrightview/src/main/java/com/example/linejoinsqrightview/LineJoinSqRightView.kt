aa1001ed7a1257e75bbd5895b929f2e4b8e3dd83
Anwesh43 <anweshthecool0@gmail.com>
2022-08-23 11:47:31 +0530

    linejoinsqrightview: adding LJSRNode class

--- linejoinsqrightview/src/main/java/com/example/linejoinsqrightview/LineJoinSqRightView.kt
+++ linejoinsqrightview/src/main/java/com/example/linejoinsqrightview/LineJoinSqRightView.kt
@@ -121,4 +121,45 @@ class LineJoinSqRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LJSRNode(var i : Int, val state : State = State()) {
+
+        private var next : LJSRNode? = null
+        private var prev : LJSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LJSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLJSRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LJSRNode {
+            var curr : LJSRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
