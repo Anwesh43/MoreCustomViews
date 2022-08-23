23fbc50cfa2d0aedcc59412bff28b69d0e564069
Anwesh43 <anweshthecool0@gmail.com>
2022-08-23 11:25:47 +0530

    linejoinsqrightview: adding Canvas extension methods

--- linejoinsqrightview/src/main/java/com/example/linejoinsqrightview/LineJoinSqRightView.kt
+++ linejoinsqrightview/src/main/java/com/example/linejoinsqrightview/LineJoinSqRightView.kt
@@ -25,7 +25,36 @@ val sizeFactor : Float = 4.9f
 val sqFactor : Float = 11.2f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+
+fun Canvas.drawLineJoinSqRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    val sq : Float = Math.min(w, h) / sqFactor
+    save()
+    translate(w / 2 + (w / 2 + size) * dsc(4), h / 2)
+    rotate(deg * dsc(3))
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        translate(0f, (size / 2 - sq / 2) * (1 - dsc(2)))
+        drawRect(RectF(0f, -sq / 2, sq * dsc(1), sq / 2), paint)
+        restore()
+    }
+    drawLine(0f, -size * 0.5f * dsc(0), 0f, size * 0.5f * dsc(0), paint)
+    restore()
+}
+
+fun Canvas.drawLJSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineJoinSqRight(scale, w, h, paint)
+}
