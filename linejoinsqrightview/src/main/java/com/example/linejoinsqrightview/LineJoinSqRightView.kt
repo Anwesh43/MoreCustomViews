f10424429051275d2419f2dfacc7f57e2311225d
Anwesh43 <anweshthecool0@gmail.com>
2022-08-23 11:26:47 +0530

    linejoinsqrightview: adding LineJoinSqRightView class

--- linejoinsqrightview/src/main/java/com/example/linejoinsqrightview/LineJoinSqRightView.kt
+++ linejoinsqrightview/src/main/java/com/example/linejoinsqrightview/LineJoinSqRightView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawLJSRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineJoinSqRight(scale, w, h, paint)
 }
+
+class LineJoinSqRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
