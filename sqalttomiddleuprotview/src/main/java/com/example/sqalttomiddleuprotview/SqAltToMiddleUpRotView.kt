8badbdd72eae23729800794e642df05f820d4209
Anwesh43 <anweshthecool0@gmail.com>
2022-08-24 09:10:02 +0530

    [sqalttomiddleuprotview] adding Canvas extension methods

--- sqalttomiddleuprotview/src/main/java/com/example/sqalttomiddleuprotview/SqAltToMiddleUpRotView.kt
+++ sqalttomiddleuprotview/src/main/java/com/example/sqalttomiddleuprotview/SqAltToMiddleUpRotView.kt
@@ -28,3 +28,30 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSATMUR(scale : Float, w: Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    save()
+    translate(w / 2 + (w / 2 + size) * sc4, h / 2)
+    rotate(rot * sc3)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        translate((w / 2) * (1 - sc1), 0f)
+        drawRect(RectF(0f, -size, size, 0f), paint)
+        restore()
+    }
+    drawRect(RectF(-size / 2, 0f, -size / 2, size * sc2), paint)
+    restore()
+}
+
+fun Canvas.drawSAMURNode(i : Int, scale : Float, paint : Paint) {
+    val  w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawSATMUR(scale, w, h, paint)
+}
