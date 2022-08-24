00fbbe2294f125a361cca2b85279d556e46bd625
Anwesh43 <anweshthecool0@gmail.com>
2022-08-24 09:25:37 +0530

    sqalttomiddleuprotview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linejoinsqrightview.LineJoinSqRightView
+import com.example.sqalttomiddleuprotview.SqAltToMiddleUpRotView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
         //BiLineParallelBarView.create(this)
-        LineJoinSqRightView.create(this)
+        SqAltToMiddleUpRotView.create(this)
         fullScreen()
     }
 }
