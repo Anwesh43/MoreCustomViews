e4c5229b28af86c25fec792b911b14789a0a7f84
Anwesh43 <anweshthecool0@gmail.com>
2022-08-24 09:21:46 +0530

    sqalttomiddleuprotview: adding SqAltToMiddleUpRot class

--- sqalttomiddleuprotview/src/main/java/com/example/sqalttomiddleuprotview/SqAltToMiddleUpRotView.kt
+++ sqalttomiddleuprotview/src/main/java/com/example/sqalttomiddleuprotview/SqAltToMiddleUpRotView.kt
@@ -159,4 +159,27 @@ class SqAltToMiddleUpRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SqAltToMiddleUpRot(var i : Int) {
+
+        private var curr : SATMURNode = SATMURNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
