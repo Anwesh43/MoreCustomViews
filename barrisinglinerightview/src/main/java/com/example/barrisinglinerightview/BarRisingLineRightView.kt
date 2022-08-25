4385a9baf330e93d625fcea740911bcf99712e31
Anwesh43 <anweshthecool0@gmail.com>
2022-08-25 14:01:41 +0530

    barrisinglinerightview: adding BRLRNode class

--- barrisinglinerightview/src/main/java/com/example/barrisinglinerightview/BarRisingLineRightView.kt
+++ barrisinglinerightview/src/main/java/com/example/barrisinglinerightview/BarRisingLineRightView.kt
@@ -118,4 +118,45 @@ class BarRisingLineRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BRLRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BRLRNode? = null
+        private var prev : BRLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BRLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBRLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BRLRNode {
+            var curr : BRLRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
