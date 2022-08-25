a111dfbd6b94df5053342f142b1b3f91f9df1308
Anwesh43 <anweshthecool0@gmail.com>
2022-08-25 13:54:48 +0530

    barrisinglinerightview: adding BarRisingLineRightView class

--- barrisinglinerightview/src/main/java/com/example/barrisinglinerightview/BarRisingLineRightView.kt
+++ barrisinglinerightview/src/main/java/com/example/barrisinglinerightview/BarRisingLineRightView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawBRLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBarRisingLineRight(scale, w, h, paint)
 }
+
+class BarRisingLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
