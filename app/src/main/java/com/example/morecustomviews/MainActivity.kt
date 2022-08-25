5d1744cc4a3da361f558cdbaad87b2afe8b81cc8
Anwesh43 <anweshthecool0@gmail.com>
2022-08-25 14:19:10 +0530

    barrisinglinerightview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.sqalttomiddleuprotview.SqAltToMiddleUpRotView
+import com.example.barrisinglinerightview.BarRisingLineRightView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
         //BiLineParallelBarView.create(this)
-        SqAltToMiddleUpRotView.create(this)
+        BarRisingLineRightView.create(this)
         fullScreen()
     }
 }
