aef6a293ac1b66fb0248fc3df924ccfe94978176
Anwesh43 <anweshthecool0@gmail.com>
2022-08-26 02:21:04 +0530

    breakbartoaltsideview: adding BreakBarToAltSideView

--- breakbartoaltsideview/src/main/java/com/example/breakbartoaltsideview/BreakBarToAltSideView.kt
+++ breakbartoaltsideview/src/main/java/com/example/breakbartoaltsideview/BreakBarToAltSideView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawBBTASNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBreakBarToAltSide(scale, w, h, paint)
 }
+
+class BreakBarToAltSideView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
