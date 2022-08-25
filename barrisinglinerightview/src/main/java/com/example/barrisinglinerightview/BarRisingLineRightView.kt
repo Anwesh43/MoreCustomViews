3490218c2a9704831ed31a9f605a15bd0c423683
Anwesh43 <anweshthecool0@gmail.com>
2022-08-25 13:48:37 +0530

    barrisinglinerightview: adding Canvas extension methods

--- barrisinglinerightview/src/main/java/com/example/barrisinglinerightview/BarRisingLineRightView.kt
+++ barrisinglinerightview/src/main/java/com/example/barrisinglinerightview/BarRisingLineRightView.kt
@@ -30,3 +30,28 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBarRisingLineRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val barH : Float = Math.min(w, h) / barHFactor
+    val barW : Float = Math.min(w, h)
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    save()
+    translate(w / 2 + (w / 2 + barW / 2) * dsc(4), h / 2)
+    rotate(-rot * dsc(3))
+    save()
+    translate(0f, (h / 2 - barH) * (1 - dsc(2)))
+    drawRect(RectF(-barW * 0.5f * dsc(0), 0f, barW * 0.5f * dsc(0), barH), paint)
+    drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawBRLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarRisingLineRight(scale, w, h, paint)
+}
