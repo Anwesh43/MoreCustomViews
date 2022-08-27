57cad6e6298caaec2bc9d3731b326a61e01b6d3a
Anwesh43 <anweshthecool0@gmail.com>
2022-08-27 09:33:16 +0530

    barhandlerrotdownview: adding BarHandlerRotDownView class

--- barhandlerrotdownview/src/main/java/com/example/barhandlerrotdownview/BarHandlerRotDownView.kt
+++ barhandlerrotdownview/src/main/java/com/example/barhandlerrotdownview/BarHandlerRotDownView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawBHRDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBarHandlerRotDown(scale, w, h, paint)
 }
+
+class BarHandlerRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
