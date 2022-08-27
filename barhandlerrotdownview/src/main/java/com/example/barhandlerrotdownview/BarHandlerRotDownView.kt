dd2c7b6889c930e41241edf02b9c9dce8b8fa59b
Anwesh43 <anweshthecool0@gmail.com>
2022-08-27 09:21:13 +0530

    barhandlerrotdownview: adding Canvas extension methods

--- barhandlerrotdownview/src/main/java/com/example/barhandlerrotdownview/BarHandlerRotDownView.kt
+++ barhandlerrotdownview/src/main/java/com/example/barhandlerrotdownview/BarHandlerRotDownView.kt
@@ -29,3 +29,30 @@ val sizeFactor : Float = 5.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarHandlerRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2, h / 2 + (h /2 + size) * dsc(4)) {
+        rotate(rot * dsc(3))
+        drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+        drawRect(RectF(0f, -size, size * dsc(2), 0f), paint)
+    }
+}
+
+fun Canvas.drawBHRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarHandlerRotDown(scale, w, h, paint)
+}
