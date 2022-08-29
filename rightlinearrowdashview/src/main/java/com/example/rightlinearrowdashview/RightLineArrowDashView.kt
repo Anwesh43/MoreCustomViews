2ce656fb2e325693e14a709dc316b50d69787060
Anwesh43 <anweshthecool0@gmail.com>
2022-08-29 13:32:27 +0530

    rightlinearrowdashview: adding Animator class

--- rightlinearrowdashview/src/main/java/com/example/rightlinearrowdashview/RightLineArrowDashView.kt
+++ rightlinearrowdashview/src/main/java/com/example/rightlinearrowdashview/RightLineArrowDashView.kt
@@ -105,4 +105,32 @@ class RightLineArrowDashView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (animated) {
+                animated = false
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
