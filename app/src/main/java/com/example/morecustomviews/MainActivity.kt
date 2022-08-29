3f58501be8b5a282a44c92682802ac71d8c90c56
Anwesh43 <anweshthecool0@gmail.com>
2022-08-30 01:20:45 +0530

    circleexpandparallelview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.rightlinearrowdashview.RightLineArrowDashView
+import com.example.circleexpandparalleldropview.CircleExpandParallelDropView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
         //BiLineParallelBarView.create(this)
-        RightLineArrowDashView.create(this)
+        CircleExpandParallelDropView.create(this)
         fullScreen()
     }
 }
