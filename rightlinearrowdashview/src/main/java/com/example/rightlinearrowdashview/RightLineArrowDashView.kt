3b46d3d4500b37406e9068ee5dc603fafcf66d2f
Anwesh43 <anweshthecool0@gmail.com>
2022-08-29 14:55:24 +0530

    rightlinearrowdashview: adding RightLineArrowDash class

--- rightlinearrowdashview/src/main/java/com/example/rightlinearrowdashview/RightLineArrowDashView.kt
+++ rightlinearrowdashview/src/main/java/com/example/rightlinearrowdashview/RightLineArrowDashView.kt
@@ -174,4 +174,27 @@ class RightLineArrowDashView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RightLineArrowDash(var i : Int) {
+
+        private var curr : RLADNode = RLADNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startupdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
