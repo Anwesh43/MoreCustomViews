c5afa260114697401775c18d7a42d8446b43fd7d
Anwesh43 <anweshthecool0@gmail.com>
2022-08-29 13:25:06 +0530

    rgithlinearrowdashview: adding RightLineArrowDashView class

--- rightlinearrowdashview/src/main/java/com/example/rightlinearrowdashview/RightLineArrowDashView.kt
+++ rightlinearrowdashview/src/main/java/com/example/rightlinearrowdashview/RightLineArrowDashView.kt
@@ -70,3 +70,19 @@ fun Canvas.drawRLADNode(i : Int, scale : Float, paint : Paint) {
     val size : Float = Math.min(w, h) / sizeFactor
     drawRightLineArrowDash(scale, w, h, paint)
 }
+
+class RightLineArrowDashView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
