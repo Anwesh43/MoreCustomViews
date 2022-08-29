026a91668a345d8d6681e6271fc4711521592823
Anwesh43 <anweshthecool0@gmail.com>
2022-08-29 14:50:48 +0530

    rightlinearrowdashview: adding RLADNode class

--- rightlinearrowdashview/src/main/java/com/example/rightlinearrowdashview/RightLineArrowDashView.kt
+++ rightlinearrowdashview/src/main/java/com/example/rightlinearrowdashview/RightLineArrowDashView.kt
@@ -133,4 +133,45 @@ class RightLineArrowDashView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RLADNode(var i : Int, val state : State = State()) {
+
+        private var prev : RLADNode? = null
+        private var next : RLADNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RLADNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRLADNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RLADNode {
+            var curr : RLADNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
