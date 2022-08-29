e8677b1fb0020e9b52f828a7a631735dbf49fd10
Anwesh43 <anweshthecool0@gmail.com>
2022-08-29 13:23:52 +0530

    rightlinearrowdashview: adding Canvas extension methods

--- rightlinearrowdashview/src/main/java/com/example/rightlinearrowdashview/RightLineArrowDashView.kt
+++ rightlinearrowdashview/src/main/java/com/example/rightlinearrowdashview/RightLineArrowDashView.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.graphics.Paint
 import android.graphics.Color
 import android.graphics.Canvas
+import android.graphics.Path
 import android.graphics.RectF
 
 val colors : Array<Int> = arrayOf(
@@ -29,3 +30,43 @@ val sizeFactor : Float = 4.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.clipTriPath(scale : Float, size : Float, paint : Paint) {
+    save()
+    val path : Path = Path()
+    path.moveTo(0f, -size)
+    path.lineTo(size, -size / 2)
+    path.lineTo(0f, 0f)
+    clipPath(path)
+    drawRect(RectF(0f, -size, size * scale, 0f), paint)
+    restore()
+}
+
+fun Canvas.drawRightLineArrowDash(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(4)) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(-rot * j * dsc(1))
+                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            }
+        }
+        clipTriPath(scale, size, paint)
+    }
+}
+
+fun Canvas.drawRLADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawRightLineArrowDash(scale, w, h, paint)
+}
