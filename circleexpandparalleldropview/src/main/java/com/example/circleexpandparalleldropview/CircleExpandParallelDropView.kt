1eddb1d6593f8a27de2b292eeb37be71aceddfb7
Anwesh43 <anweshthecool0@gmail.com>
2022-08-30 01:05:18 +0530

    circleexpandparalleldropview: adding CircleExpandParallelDropView class

--- circleexpandparalleldropview/src/main/java/com/example/circleexpandparalleldropview/CircleExpandParallelDropView.kt
+++ circleexpandparalleldropview/src/main/java/com/example/circleexpandparalleldropview/CircleExpandParallelDropView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawCEPDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawCircleExpandParallelDrop(scale, w, h, paint)
 }
+
+class CircleExpandParallelDropView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
