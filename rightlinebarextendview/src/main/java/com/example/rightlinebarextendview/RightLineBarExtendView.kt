742a95ff863b0d15cc41fca160e23a5c8e4279a2
Anwesh43 <anweshthecool0@gmail.com>
2022-08-31 01:17:41 +0530

    rightlinebarextendview: adding RightLineBarExtend class

--- rightlinebarextendview/src/main/java/com/example/rightlinebarextendview/RightLineBarExtendView.kt
+++ rightlinebarextendview/src/main/java/com/example/rightlinebarextendview/RightLineBarExtendView.kt
@@ -157,4 +157,27 @@ class RightLineBarExtendView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RightLineBarExtend(var i : Int) {
+
+        private var curr : RLBENode = RLBENode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
