ec8d199c8307bb8f793ed0642e688d40e43efaec
Anwesh43 <anweshthecool0@gmail.com>
2022-09-06 14:11:33 +0530

    squareuphalfcircleview: adding SquareUpHalfCircleView class

--- sqaureuphalfcircleview/src/main/java/com/example/sqaureuphalfcircleview/SquareUpHalfCircleView.kt
+++ sqaureuphalfcircleview/src/main/java/com/example/sqaureuphalfcircleview/SquareUpHalfCircleView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawSUHCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawSquareUpHalfCircle(scale, w, h, paint)
 }
+
+class SquareUpHalfCircleView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
