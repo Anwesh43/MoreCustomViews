72a307fa00d51d1846d9e79caff627b27a927533
Anwesh43 <anweshthecool0@gmail.com>
2022-09-06 13:59:44 +0530

    squareuphalfcircleview: adding Canvas extension methods

--- sqaureuphalfcircleview/src/main/java/com/example/sqaureuphalfcircleview/SquareUpHalfCircleView.kt
+++ sqaureuphalfcircleview/src/main/java/com/example/sqaureuphalfcircleview/SquareUpHalfCircleView.kt
@@ -25,7 +25,34 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
+val rFactor : Float = 14.2f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawSquareUpHalfCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    save()
+    translate(w / 2, h / 2 + (h / 2 + size) * dsc(3))
+    rotate(rot * dsc(2))
+    drawRect(RectF(-size * 0.5f * dsc(0), -size / 5, size * 0.5f * dsc(0), size / 5), paint)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f - 2 * j)
+        translate(-size / 2, -size / 5)
+        drawArc(RectF(-r, -r, r, r), 180f , 180f * dsc(1), true, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSUHCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    drawSquareUpHalfCircle(scale, w, h, paint)
+}
