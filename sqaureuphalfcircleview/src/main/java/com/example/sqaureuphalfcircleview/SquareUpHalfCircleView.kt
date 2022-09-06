23e724d4f80fac63a28d7c4d49e47691d7a21a11
Anwesh43 <anweshthecool0@gmail.com>
2022-09-06 15:02:52 +0530

    squareuphalfcircleview: adding renderer in view

--- sqaureuphalfcircleview/src/main/java/com/example/sqaureuphalfcircleview/SquareUpHalfCircleView.kt
+++ sqaureuphalfcircleview/src/main/java/com/example/sqaureuphalfcircleview/SquareUpHalfCircleView.kt
@@ -59,14 +59,16 @@ fun Canvas.drawSUHCNode(i : Int, scale : Float, paint : Paint) {
 
 class SquareUpHalfCircleView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
