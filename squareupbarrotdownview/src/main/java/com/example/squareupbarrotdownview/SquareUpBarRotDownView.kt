97208fef8b380efb74611331c8689d4a5364c169
Anwesh43 <anweshthecool0@gmail.com>
2022-09-07 13:56:18 +0530

    squareupbarrotdownview: adding SqaureUpBarRotDownView class

--- squareupbarrotdownview/src/main/java/com/example/squareupbarrotdownview/SquareUpBarRotDownView.kt
+++ squareupbarrotdownview/src/main/java/com/example/squareupbarrotdownview/SquareUpBarRotDownView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawSUBRDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawSquareUpBarRotDown(scale, w, h, paint)
 }
+
+class SquareUpBarRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
