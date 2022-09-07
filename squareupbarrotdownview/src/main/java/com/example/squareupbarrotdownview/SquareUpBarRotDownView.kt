8eb1690b373dc5449806e39e416df860e63a2cac
Anwesh43 <anweshthecool0@gmail.com>
2022-09-07 14:21:47 +0530

    squareupbarrotdownview: adding Renderer class

--- squareupbarrotdownview/src/main/java/com/example/squareupbarrotdownview/SquareUpBarRotDownView.kt
+++ squareupbarrotdownview/src/main/java/com/example/squareupbarrotdownview/SquareUpBarRotDownView.kt
@@ -183,4 +183,27 @@ class SquareUpBarRotDownView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SquareUpBarRotDownView) {
+
+        private val animator : Animator = Animator(view)
+        private val subrd : SquareUpBarRotDown = SquareUpBarRotDown(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            subrd.draw(canvas, paint)
+            animator.animate {
+                subrd.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            subrd.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
