0d829e592c4987b1496d0a921f6cf26c9ec9fb0c
Anwesh43 <anweshthecool0@gmail.com>
2022-09-08 13:44:31 +0530

    squarebicircularwheelsview: adding Canvas extension methods

--- squarebicircularwheelsview/src/main/java/com/example/squarebicircularwheelsview/SquareBiCircularWheelsView.kt
+++ squarebicircularwheelsview/src/main/java/com/example/squarebicircularwheelsview/SquareBiCircularWheelsView.kt
@@ -29,3 +29,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSquareBiCircularWheels(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2 + (w / 2 + size / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawRect(RectF(size / 2 - size * dsc(0), -size / 2, (size / 2), size / 2), paint)
+        for (j in 0..1) {
+            drawXY(w / 2 + r - (w / 2 - size / 2) * dsc(1), 0f) {
+                scale(1f, 1f - 2 * j)
+                drawCircle(0f, 0f, r, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawSBCWNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawSquareBiCircularWheels(scale, w, h, paint)
+}
