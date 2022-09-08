2f8eed42f96500d2f1ecfb7f67d1e84db739886d
Anwesh43 <anweshthecool0@gmail.com>
2022-09-08 13:45:31 +0530

    squarebicircularwheelsview: adding SquareBiCircularWheelsView class

--- squarebicircularwheelsview/src/main/java/com/example/squarebicircularwheelsview/SquareBiCircularWheelsView.kt
+++ squarebicircularwheelsview/src/main/java/com/example/squarebicircularwheelsview/SquareBiCircularWheelsView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawSBCWNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawSquareBiCircularWheels(scale, w, h, paint)
 }
+
+class SquareBiCircularWheelsView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
