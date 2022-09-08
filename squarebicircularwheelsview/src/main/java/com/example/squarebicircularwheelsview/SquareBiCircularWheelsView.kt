260fa48d9cf4a99a45e4d22a47eb072e339da126
Anwesh43 <anweshthecool0@gmail.com>
2022-09-08 13:58:03 +0530

    squarebicircularwheelsview: adding SBCWNode class

--- squarebicircularwheelsview/src/main/java/com/example/squarebicircularwheelsview/SquareBiCircularWheelsView.kt
+++ squarebicircularwheelsview/src/main/java/com/example/squarebicircularwheelsview/SquareBiCircularWheelsView.kt
@@ -122,4 +122,45 @@ class SquareBiCircularWheelsView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBCWNode(var i : Int, val state : State = State()) {
+
+        private var prev : SBCWNode? = null
+        private var next : SBCWNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SBCWNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBCWNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBCWNode {
+            var curr : SBCWNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
