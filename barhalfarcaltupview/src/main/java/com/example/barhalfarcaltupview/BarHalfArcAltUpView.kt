f49925789d913e4243d0fff8ad96d5effb280dad
Anwesh43 <anweshthecool0@gmail.com>
2022-09-09 13:57:20 +0530

    barhalfarcaltupview: adding Canvas extension methods

--- barhalfarcaltupview/src/main/java/com/example/barhalfarcaltupview/BarHalfArcAltUpView.kt
+++ barhalfarcaltupview/src/main/java/com/example/barhalfarcaltupview/BarHalfArcAltUpView.kt
@@ -30,3 +30,46 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawScaleAt(scaleX : Float, scaleY : Float, cb : () -> Unit) {
+    save()
+    scale(scaleX, scaleY)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarHalfArcAltUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc  : (Int) -> Float = { scale.divideScale(it, parts) }
+    val r : Float = Math.min(w, h) / rFactor
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+           drawScaleAt(1f - 2 * j, 1f - 2 * j) {
+               drawXY(size * 0.5f * dsc(2), h * 0.5f * dsc(3)) {
+                   drawRect(RectF(-r / 2, 0f, r / 2, size * 0.5f *  dsc(1)), paint)
+                   drawArc(
+                       RectF(-r / 2, -r / 2, r / 2, r / 2), 0f, rot * dsc(0),
+                       true,
+                       paint
+                   )
+               }
+           }
+        }
+    }
+}
+
+fun Canvas.drawBHAAUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarHalfArcAltUp(scale, w, h, paint)
+}
