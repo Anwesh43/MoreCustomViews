2eedd2ba3c14ae5209d626f6680987b39b6b9e29
Anwesh43 <anweshthecool0@gmail.com>
2022-09-10 04:45:30 +0530

    linesquareuprightview: adding LineSquareUpRight class

--- linesquareuprightview/src/main/java/com/example/linesquareuprightview/LineSquareUpRightView.kt
+++ linesquareuprightview/src/main/java/com/example/linesquareuprightview/LineSquareUpRightView.kt
@@ -172,4 +172,27 @@ class LineSquareUpRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineSquareUpRight(var i : Int) {
+
+        private var curr : LSURNode = LSURNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
