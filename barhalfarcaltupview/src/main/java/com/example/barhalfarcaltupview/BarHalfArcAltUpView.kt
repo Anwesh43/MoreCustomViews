b37e5f7ea86bf2ddf5d65583860b7387d932d79c
Anwesh43 <anweshthecool0@gmail.com>
2022-09-09 14:10:01 +0530

    barhalfarcaltupview: adding BHAAUNode class

--- barhalfarcaltupview/src/main/java/com/example/barhalfarcaltupview/BarHalfArcAltUpView.kt
+++ barhalfarcaltupview/src/main/java/com/example/barhalfarcaltupview/BarHalfArcAltUpView.kt
@@ -136,4 +136,45 @@ class BarHalfArcAltUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BHAAUNode(var i : Int, private val state : State = State()) {
+
+        private var prev : BHAAUNode? = null
+        private var next : BHAAUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BHAAUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBHAAUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BHAAUNode {
+            var curr : BHAAUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
