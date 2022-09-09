cadde515efefb07c5ee48a34e2cadbfef9bc174e
Anwesh43 <anweshthecool0@gmail.com>
2022-09-09 14:01:06 +0530

    barhalfarcaltupview: adding BarHalfArcAltUpView class

--- barhalfarcaltupview/src/main/java/com/example/barhalfarcaltupview/BarHalfArcAltUpView.kt
+++ barhalfarcaltupview/src/main/java/com/example/barhalfarcaltupview/BarHalfArcAltUpView.kt
@@ -73,3 +73,19 @@ fun Canvas.drawBHAAUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBarHalfArcAltUp(scale, w, h, paint)
 }
+
+class BarHalfArcAltUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) :Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
