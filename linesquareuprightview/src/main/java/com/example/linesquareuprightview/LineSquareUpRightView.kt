51aa4b15422fe56a1717b78999f7f3ee46bc58b9
Anwesh43 <anweshthecool0@gmail.com>
2022-09-10 04:37:01 +0530

    linesquareuprightview: adding LineSquareUpRightView class

--- linesquareuprightview/src/main/java/com/example/linesquareuprightview/LineSquareUpRightView.kt
+++ linesquareuprightview/src/main/java/com/example/linesquareuprightview/LineSquareUpRightView.kt
@@ -68,3 +68,19 @@ fun Canvas.drawLSURNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSquareUpRight(scale, w, h, paint)
 }
+
+class LineSquareUpRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
