2f4d5e55a19f7db8f0f0f440238e629f54ba0073
Anwesh43 <anweshthecool0@gmail.com>
2022-09-11 11:36:28 +0530

    blocktravellerview: adding BTNode class

--- blocktravellerview/src/main/java/com/example/blocktravellerview/BlockTravellerView.kt
+++ blocktravellerview/src/main/java/com/example/blocktravellerview/BlockTravellerView.kt
@@ -110,4 +110,45 @@ class BlockTravellerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BTNode(var i : Int, val state : State = State()) {
+
+        private var prev : BTNode? = null
+        private var next : BTNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BTNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBTNode(i, state.scale, paint)
+        }
+
+        fun udpate(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BTNode {
+            var curr : BTNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
