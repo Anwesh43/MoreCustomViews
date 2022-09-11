b11b2e4b225ac716d3688c8ca076237aca7257ef
Anwesh43 <anweshthecool0@gmail.com>
2022-09-12 02:07:53 +0530

    rightlinetrainglerotview: adding constants and libraries

---
+++ rightlinetrianglerotview/src/main/java/com/example/rightlinetrianglerotview/RightLineTriangleRotView.kt
@@ -0,0 +1,26 @@
+package com.example.rightlinetrianglerotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.03f / parts
+val rot : Float = 45f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val sizeFactor : Float = 4.2f
