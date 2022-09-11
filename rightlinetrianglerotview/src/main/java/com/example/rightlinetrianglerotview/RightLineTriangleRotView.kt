92b7fd0ddf766ff004fa7321cf2a6d844173830c
Anwesh43 <anweshthecool0@gmail.com>
2022-09-12 02:17:05 +0530

    rightlinetrainglerotview: adding Canvas extension methods

--- rightlinetrianglerotview/src/main/java/com/example/rightlinetrianglerotview/RightLineTriangleRotView.kt
+++ rightlinetrianglerotview/src/main/java/com/example/rightlinetrianglerotview/RightLineTriangleRotView.kt
@@ -5,9 +5,10 @@ import android.view.MotionEvent
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.RectF
+import android.graphics.Path
 import android.app.Activity
 import android.content.Context
+
 
 val colors : Array<Int> = arrayOf(
     "#1A237E",
@@ -28,3 +29,35 @@ val sizeFactor : Float = 4.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTriangle(size : Float, paint : Paint) {
+    val path : Path = Path()
+    path.moveTo(0f, 0f)
+    path.lineTo(size, 0f)
+    path.lineTo(0f, -size)
+    path.lineTo(0f, 0f)
+    drawPath(path, paint)
+}
+
+fun Canvas.drawRightLineTriangleRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2 - (w / 2 + size) * dsc(2), h / 2) {
+        rotate(rot * dsc(1))
+        drawTriangle(size * dsc(0), paint)
+    }
+}
+
+fun Canvas.drawRLTRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawRightLineTriangleRot(scale, w, h, paint)
+}
