3263937b9a879611bd4930e4486c97bedf7502ea
Anwesh43 <anweshthecool0@gmail.com>
2022-09-12 02:36:27 +0530

    rightlinetrainglerotview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.blocktravellerview.BlockTravellerView
+import com.example.rightlinetrianglerotview.RightLineTriangleRotView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
         //BiLineParallelBarView.create(this)
-        BlockTravellerView.create(this)
+        RightLineTriangleRotView.create(this)
         fullScreen()
     }
 }
