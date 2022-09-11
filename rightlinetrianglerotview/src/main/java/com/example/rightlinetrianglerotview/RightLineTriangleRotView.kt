9d819db7e71332e6593d7e826d8b36b7fbd1a23e
Anwesh43 <anweshthecool0@gmail.com>
2022-09-12 02:18:04 +0530

    rightlinetrainglerotview: adding RightLineTriangleRotView class

--- rightlinetrianglerotview/src/main/java/com/example/rightlinetrianglerotview/RightLineTriangleRotView.kt
+++ rightlinetrianglerotview/src/main/java/com/example/rightlinetrianglerotview/RightLineTriangleRotView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawRLTRNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawRightLineTriangleRot(scale, w, h, paint)
 }
+
+class RightLineTriangleRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
