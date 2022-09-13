4f6b5730e2613f1f9e2e4029bc2a5095ba058568
Anwesh43 <anweshthecool0@gmail.com>
2022-09-13 14:24:47 +0530

    linerotperplineview: adding LRPLNode class

--- linerotperplineview/src/main/java/com/example/linerotperplineview/LineRotPerpLineView.kt
+++ linerotperplineview/src/main/java/com/example/linerotperplineview/LineRotPerpLineView.kt
@@ -121,4 +121,45 @@ class LineRotPerpLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRPLNode(var i : Int, val state : State = State()) {
+
+        private var prev : LRPLNode? = null
+        private var next : LRPLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRPLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRPLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRPLNode {
+            var curr : LRPLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
