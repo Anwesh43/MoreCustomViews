ffa34c799494cceb326ee4225ac8a6309b08cd20
Anwesh43 <anweshthecool0@gmail.com>
2022-09-14 13:16:01 +0530

    squaredroplineshiftview: adding SquareDropLineShiftView class

--- squaredroplineshiftvew/src/main/java/com/example/squaredroplineshiftvew/SquareDropLineShiftView.kt
+++ squaredroplineshiftvew/src/main/java/com/example/squaredroplineshiftvew/SquareDropLineShiftView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawSDLSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSquareDropLineShift(scale, w, h, paint)
 }
+
+class SquareDropLineShiftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
