0658ac86672189a2a7118dc5e31e75434cb60474
Anwesh43 <anweshthecool0@gmail.com>
2022-09-14 12:57:18 +0530

    squaredroplineshiftview: adding Int and Float extension methods

---
+++ squaredroplineshiftvew/src/main/java/com/example/squaredroplineshiftvew/SquareDropLineShiftView.kt
@@ -0,0 +1,31 @@
+package com.example.squaredroplineshiftvew
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 8.9f
+val delay : Long = 20
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
