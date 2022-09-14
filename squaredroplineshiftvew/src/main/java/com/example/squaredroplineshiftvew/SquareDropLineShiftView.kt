803d67f63418d15e84ecd71bbff781451808ba9a
Anwesh43 <anweshthecool0@gmail.com>
2022-09-14 13:15:04 +0530

    squaredroplineshiftview: adding Canvas extension methods

--- squaredroplineshiftvew/src/main/java/com/example/squaredroplineshiftvew/SquareDropLineShiftView.kt
+++ squaredroplineshiftvew/src/main/java/com/example/squaredroplineshiftvew/SquareDropLineShiftView.kt
@@ -18,8 +18,8 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
-val scGap : Float = 0.04f / parts
+val parts : Int = 3
+val scGap : Float = 0.03f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 8.9f
 val delay : Long = 20
@@ -29,3 +29,31 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSquareDropLineShift(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY((-(w / 2 + paint.strokeWidth)) * (1 - dsc(0)) + (w / 2 + size) * dsc(2), h / 2) {
+        drawRect(RectF(-size, -size, 0f, 0f), paint)
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+    }
+}
+
+fun Canvas.drawSDLSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSquareDropLineShift(scale, w, h, paint)
+}
