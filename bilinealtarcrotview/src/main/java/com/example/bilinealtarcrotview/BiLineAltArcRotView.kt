b576d81d0c421175ccd61a6f511b685227f91a73
Anwesh43 <anweshthecool0@gmail.com>
2022-09-16 05:22:54 +0530

    bilinealtarcrotview: adding Canvas extension methods

--- bilinealtarcrotview/src/main/java/com/example/bilinealtarcrotview/BiLineAltArcRotView.kt
+++ bilinealtarcrotview/src/main/java/com/example/bilinealtarcrotview/BiLineAltArcRotView.kt
@@ -30,3 +30,38 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y: Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+fun Canvas.drawBiLineAltRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    val r : Float = Math.min(w, h) / arcFactor
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
+        rotate(deg * dsc(2))
+        drawXY(-(w / 2 + size / 2) * (1 - dsc(1)), 0f) {
+            drawLine(-size / 2, 0f, size / 2, 0f, paint)
+        }
+        for (j in 0..1) {
+            save()
+            scale(1f - 2 * j, 1f - 2 * j)
+            drawXY((size / 2  - r), 0f) {
+                drawArc(RectF(-r, -r, r, r), 0f, 180f * dsc(1), true, paint)
+            }
+            restore()
+        }
+    }
+}
+
+fun Canvas.drawBLARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiLineAltRot(scale, w, h, paint)
+}
