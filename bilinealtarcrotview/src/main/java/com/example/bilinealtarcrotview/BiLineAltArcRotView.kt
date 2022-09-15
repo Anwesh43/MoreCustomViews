486cbf3b9530798165691bae00e882f1f1da62fa
Anwesh43 <anweshthecool0@gmail.com>
2022-09-16 05:24:06 +0530

    bilinealtarcrotview: adding BiLineAltArcRotView class

--- bilinealtarcrotview/src/main/java/com/example/bilinealtarcrotview/BiLineAltArcRotView.kt
+++ bilinealtarcrotview/src/main/java/com/example/bilinealtarcrotview/BiLineAltArcRotView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawBLARNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineAltRot(scale, w, h, paint)
 }
+
+class BiLineAltArcRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
