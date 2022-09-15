c5c8583759f40208a751d9b339a9afad30b26084
Anwesh43 <anweshthecool0@gmail.com>
2022-09-15 12:17:41 +0530

    squarearcrotdownview: adding SquareArcRotDownView class

--- squarearcrotdownview/src/main/java/com/example/squarearcrotdownview/SquareArcRotDownView.kt
+++ squarearcrotdownview/src/main/java/com/example/squarearcrotdownview/SquareArcRotDownView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawSARDNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawSquareArcRotDown(scale, w, h, paint)
 }
+
+class SquareArcRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
