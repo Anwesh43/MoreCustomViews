c07f51780518b48419e45a4732a1d76d2d8cf1fc
Anwesh43 <anweshthecool0@gmail.com>
2022-09-17 12:17:12 +0530

    linebaruprotrightview: adding LBURRNode class

--- linebaruprotrightview/src/main/java/com/example/linebaruprotrightview/LineBarUpRotRightView.kt
+++ linebaruprotrightview/src/main/java/com/example/linebaruprotrightview/LineBarUpRotRightView.kt
@@ -124,4 +124,45 @@ class LineBarUpRotRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBURRNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LBURRNode? = null
+        private var next : LBURRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBURRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBURRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBURRNode {
+            var curr : LBURRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
