87f5c2b3fbb5d17a68d2094f01ae3394d8f91b04
Anwesh43 <anweshthecool0@gmail.com>
2022-09-17 12:07:52 +0530

    linebaruprotrightview: adding Canvas extension methods

--- linebaruprotrightview/src/main/java/com/example/linebaruprotrightview/LineBarUpRotRightView.kt
+++ linebaruprotrightview/src/main/java/com/example/linebaruprotrightview/LineBarUpRotRightView.kt
@@ -29,3 +29,35 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBarUpRotRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2 + (w / 2 + size) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        drawLine(-size * 0.5f * dsc(0), 0f, size * 0.5f * dsc(0), 0f, paint)
+        drawRect(RectF(-size / 4, -size * 0.5f * dsc(1), size / 4, 0f), paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawLine(-size / 2, 0f, -size / 2, size * dsc(2), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBURRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBarUpRotRight(scale, w, h, paint)
+}
