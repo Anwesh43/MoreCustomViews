bdf14c7ddd118e5178828b210f79b7fdba268330
Anwesh43 <anweshthecool0@gmail.com>
2022-09-18 02:11:27 +0530

    springblockballview: adding SpringBlockBallView class

--- springblockballview/src/main/java/com/example/springblockballview/SpringBlockBallView.kt
+++ springblockballview/src/main/java/com/example/springblockballview/SpringBlockBallView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawSBBNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawSpringBlockBall(scale, w, h, paint)
 }
+
+class SpringBlockBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
