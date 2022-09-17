3e90a3fac32754c430d064ba1f7f3c0b6de48bdb
Anwesh43 <anweshthecool0@gmail.com>
2022-09-18 02:20:48 +0530

    springblockballview: adding SBBNode class

--- springblockballview/src/main/java/com/example/springblockballview/SpringBlockBallView.kt
+++ springblockballview/src/main/java/com/example/springblockballview/SpringBlockBallView.kt
@@ -117,4 +117,45 @@ class SpringBlockBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBBNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : SBBNode? = null
+        private var next : SBBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SBBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBBNode {
+            var curr : SBBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
