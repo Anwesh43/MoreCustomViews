e790244e686326cbb5184cdb26a5ce287b96a629
Anwesh43 <anweshthecool0@gmail.com>
2022-09-19 01:45:15 +0530

    altcornerarcjoinview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.springblockballview.SpringBlockBallView
+import com.example.altcornerarcjoinview.AltCornerArcJoinView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
         //BiLineParallelBarView.create(this)
-        SpringBlockBallView.create(this)
+        AltCornerArcJoinView.create(this)
         fullScreen()
     }
 }
