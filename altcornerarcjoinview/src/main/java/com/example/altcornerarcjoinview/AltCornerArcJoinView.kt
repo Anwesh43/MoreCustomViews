a1597c24f2ad3c150f8b1a792996ff9e72de4a4d
Anwesh43 <anweshthecool0@gmail.com>
2022-09-19 01:24:12 +0530

    altcornerarcjoinview: adding constants and libraries

---
+++ altcornerarcjoinview/src/main/java/com/example/altcornerarcjoinview/AltCornerArcJoinView.kt
@@ -0,0 +1,26 @@
+package com.example.altcornerarcjoinview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val rot : Float = 360f
+val backColor : Int = Color.parseColor("#BDBDBD")
