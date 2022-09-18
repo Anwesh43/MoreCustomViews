4ecec48facec261037ce79ecf2910373edf05610
Anwesh43 <anweshthecool0@gmail.com>
2022-09-19 01:38:46 +0530

    altcornerarcjoinview: adding AltCornerArcJoin class

--- altcornerarcjoinview/src/main/java/com/example/altcornerarcjoinview/AltCornerArcJoinView.kt
+++ altcornerarcjoinview/src/main/java/com/example/altcornerarcjoinview/AltCornerArcJoinView.kt
@@ -158,4 +158,27 @@ class AltCornerArcJoinView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class AltCornerArcJoin(var i : Int) {
+
+        private var curr : ACAJNode = ACAJNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit)  {
+            curr.startUpdating(cb)
+        }
+    }
 }
