59add3e110efdff00a45aeadbdb5d964a9d4d6da
Anwesh43 <anweshthecool0@gmail.com>
2022-09-21 02:01:19 +0530

    lineexpandbardownview: adding LEBDNode class

--- lineexpandbardownview/src/main/java/com/example/lineexpandbardownview/LineExpandBarDownView.kt
+++ lineexpandbardownview/src/main/java/com/example/lineexpandbardownview/LineExpandBarDownView.kt
@@ -125,4 +125,45 @@ class LineExpandBarDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LEBDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LEBDNode? = null
+        private var prev : LEBDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LEBDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLEBDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LEBDNode {
+            var curr : LEBDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
