0ab35f2ae2e81d04df06c7a31d022523b4aadc0b
Anwesh43 <anweshthecool0@gmail.com>
2022-09-20 11:22:18 +0530

    linerightojoindownview: adding LRJDNode class

--- linerighttojoindownview/src/main/java/com/example/linerighttojoindownview/LineRightJoinDownView.kt
+++ linerighttojoindownview/src/main/java/com/example/linerighttojoindownview/LineRightJoinDownView.kt
@@ -121,4 +121,45 @@ class LineRightJoinDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRJDNode(var i : Int, val state : State = State()) {
+
+        private var prev : LRJDNode? = null
+        private var next : LRJDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRJDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRJDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRJDNode {
+            var curr : LRJDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
