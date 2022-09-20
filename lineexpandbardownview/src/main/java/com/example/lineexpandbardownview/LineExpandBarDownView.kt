bd0f927bc03f69a87223143b421e00e5b34d57d0
Anwesh43 <anweshthecool0@gmail.com>
2022-09-21 01:54:19 +0530

    lineexpandbardownview: adding Canvas extension methods

--- lineexpandbardownview/src/main/java/com/example/lineexpandbardownview/LineExpandBarDownView.kt
+++ lineexpandbardownview/src/main/java/com/example/lineexpandbardownview/LineExpandBarDownView.kt
@@ -29,3 +29,36 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineExpandBarDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            save()
+            scale(1f - 2 * j, 1f)
+            drawXY((w / 2 + paint.strokeWidth) * (1 - dsc(0)) + size * 0.5f * dsc(1), 0f) {
+                drawLine(0f, 0f, 0f, -size, paint)
+            }
+            restore()
+        }
+        drawRect(RectF(-size * 0.5f * dsc(1), -size / 2, size * 0.5f * dsc(1), size / 2), paint)
+    }
+}
+
+fun Canvas.drawLEBDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineExpandBarDown(scale, w, h, paint)
+}
