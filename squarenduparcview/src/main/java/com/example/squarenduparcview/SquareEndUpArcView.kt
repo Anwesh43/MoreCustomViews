15c295b29d0e39f6d5ef86dafc9070b3b4d2be16
Anwesh43 <anweshthecool0@gmail.com>
2022-09-22 00:58:21 +0530

    squarenduparcview: adding SquareEndUpArc class

--- squarenduparcview/src/main/java/com/example/squarenduparcview/SquareEndUpArcView.kt
+++ squarenduparcview/src/main/java/com/example/squarenduparcview/SquareEndUpArcView.kt
@@ -165,4 +165,27 @@ class SquareEndUpArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SquareEndUpArc(var i : Int) {
+
+        private var curr : SEUANode = SEUANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
