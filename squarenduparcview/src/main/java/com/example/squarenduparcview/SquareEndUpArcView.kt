1a4aaa6420189d78c8ee24ec9e34efa5bd343a7e
Anwesh43 <anweshthecool0@gmail.com>
2022-09-22 00:53:43 +0530

    squarenduparcview: adding SEUANode class

--- squarenduparcview/src/main/java/com/example/squarenduparcview/SquareEndUpArcView.kt
+++ squarenduparcview/src/main/java/com/example/squarenduparcview/SquareEndUpArcView.kt
@@ -124,4 +124,45 @@ class SquareEndUpArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SEUANode(var i : Int, val state : State = State()) {
+
+        private var next : SEUANode? = null
+        private var prev : SEUANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SEUANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSEUANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SEUANode {
+            var curr : SEUANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
