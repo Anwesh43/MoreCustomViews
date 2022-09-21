03cf0c6e42cdbff269db2ee92e2a3129aaa9246a
Anwesh43 <anweshthecool0@gmail.com>
2022-09-22 00:34:09 +0530

    squarenduparcview: adding constants and libraries

---
+++ squarenduparcview/src/main/java/com/example/squarenduparcview/SquareEndUpArcView.kt
@@ -0,0 +1,28 @@
+package com.example.squarenduparcview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#039BE5",
+    "#BF360C",
+    "#006064",
+    "#E91E63",
+    "#0D47A1"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val rFactor : Float = 7.8f
+val delay : Long = 20
+val rot : Float = 360f
+val deg : Float = 45f
+val backColor : Int = Color.parseColor("#BDBDBD")
