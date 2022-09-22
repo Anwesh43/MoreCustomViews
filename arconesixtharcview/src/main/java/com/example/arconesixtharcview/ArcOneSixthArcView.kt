bfaa5be310fab0acd689f84846e6cd83c18ef15b
Anwesh43 <anweshthecool0@gmail.com>
2022-09-23 01:22:53 +0530

    arconesixtharcview: adding AOSANode clas

--- arconesixtharcview/src/main/java/com/example/arconesixtharcview/ArcOneSixthArcView.kt
+++ arconesixtharcview/src/main/java/com/example/arconesixtharcview/ArcOneSixthArcView.kt
@@ -121,4 +121,45 @@ class ArcOneSixthArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class AOSANode(var i : Int, val state : State = State()) {
+
+        private var next : AOSANode? = null
+        private var prev : AOSANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = AOSANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawAOSANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : AOSANode {
+            var curr : AOSANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
