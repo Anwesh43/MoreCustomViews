6ddb039683e0c0077a91fa4f733d7e16403ba652
Anwesh43 <anweshthecool0@gmail.com>
2022-09-23 01:27:03 +0530

    arconesixtharcview: adding ArcOneSixthArc class

--- arconesixtharcview/src/main/java/com/example/arconesixtharcview/ArcOneSixthArcView.kt
+++ arconesixtharcview/src/main/java/com/example/arconesixtharcview/ArcOneSixthArcView.kt
@@ -162,4 +162,27 @@ class ArcOneSixthArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArcOneSixthArc(var i : Int) {
+
+        private var curr : AOSANode = AOSANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
