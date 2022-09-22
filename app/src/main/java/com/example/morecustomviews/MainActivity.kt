e334919264d3958ec062e4d53321fd249b4e13e8
Anwesh43 <anweshthecool0@gmail.com>
2022-09-23 01:32:39 +0530

    arconesixtharcview: adding static create method

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.squarenduparcview.SquareEndUpArcView
+import com.example.arconesixtharcview.ArcOneSixthArcView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
         //BiLineParallelBarView.create(this)
-        SquareEndUpArcView.create(this)
+        ArcOneSixthArcView.create(this)
         fullScreen()
     }
 }
