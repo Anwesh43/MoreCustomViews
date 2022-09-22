7f2a061e628e3f8ba2321f10e24ce7ab1859a71c
Anwesh43 <anweshthecool0@gmail.com>
2022-09-23 01:15:07 +0530

    arconesixtharcview: adding Canvas extension methods

--- arconesixtharcview/src/main/java/com/example/arconesixtharcview/ArcOneSixthArcView.kt
+++ arconesixtharcview/src/main/java/com/example/arconesixtharcview/ArcOneSixthArcView.kt
@@ -29,3 +29,31 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcOneSixthArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2, h / 2 + (h / 2 + size / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        paint.style = Paint.Style.FILL
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, (360f - rot) * dsc(0), true, paint)
+        paint.style = Paint.Style.STROKE
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), (360f - rot), rot * dsc(1), false, paint)
+    }
+}
+
+fun Canvas.drawAOSANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcOneSixthArc(scale, w, h, paint)
+}
