4cbc80f0f96eb28d8cdfb1c7a0317cae82000d73
Anwesh43 <anweshthecool0@gmail.com>
2022-09-24 09:45:50 +0530

    linearcattachdownview: adding Canvas extension methods

--- linearcattachdownview/src/main/java/com/example/linearcattachdownview/LineArcAttachDownView.kt
+++ linearcattachdownview/src/main/java/com/example/linearcattachdownview/LineArcAttachDownView.kt
@@ -25,7 +25,45 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 3.9f
 val arcFactor : Float = 11.2f
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+
+fun Canvas.drawLineArcAttachDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / strokeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    val r : Float = Math.min(w, h) / arcFactor
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f - 2 * j)
+                drawXY(0f, (h / 2) * (dsc(0) - dsc(3))) {
+                    drawLine(0f, 0f, 0f, size, paint)
+                    drawXY(0f, size - r) {
+                        drawArc(RectF(-r, -r, r, r), 90f, 180f * dsc(1), true, paint)
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawLAADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcAttachDown(scale, w, h, paint)
+}
