a6da87a8f0eaafde6d1679aff9642583e03cd544
Anwesh43 <anweshthecool0@gmail.com>
2022-09-25 10:41:13 +0530

    jointbarrotupview: adding module setup

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linearcattachdownview.LineArcAttachDownView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -15,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
         //BiLineParallelBarView.create(this)
-        LineArcAttachDownView.create(this)
         fullScreen()
     }
 }
