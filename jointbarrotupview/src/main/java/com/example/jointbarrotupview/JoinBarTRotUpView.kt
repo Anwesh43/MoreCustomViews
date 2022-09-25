8976eefdef21da0fd01d1f20ca6fc8111c9ff8ba
Anwesh43 <anweshthecool0@gmail.com>
2022-09-25 11:14:08 +0530

    jointbarrotupview: adding JBTRUNode class

--- jointbarrotupview/src/main/java/com/example/jointbarrotupview/JoinBarTRotUpView.kt
+++ jointbarrotupview/src/main/java/com/example/jointbarrotupview/JoinBarTRotUpView.kt
@@ -133,4 +133,45 @@ class JoinBarTRotUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class JBTRUNode(var i : Int, val state : State = State()) {
+
+        private var prev : JBTRUNode? = null
+        private var next : JBTRUNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = JBTRUNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawJBTRUNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : JBTRUNode {
+            var curr : JBTRUNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
