35a7ba4ad69973503ee101a6db8436bc7365653e
Anwesh43 <anweshthecool0@gmail.com>
2022-09-25 11:07:22 +0530

    jointbarrotupview: adding Canvas extension methods

--- jointbarrotupview/src/main/java/com/example/jointbarrotupview/JoinBarTRotUpView.kt
+++ jointbarrotupview/src/main/java/com/example/jointbarrotupview/JoinBarTRotUpView.kt
@@ -29,3 +29,44 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawWithoutDotLine(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    if (Math.abs(x1 - x2) < 0.1f && Math.abs(y1 - y2) < 0.1f) {
+        return
+    }
+    drawLine(x1, y1, x2, y2, paint)
+}
+fun Canvas.drawJoinBarTRotUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2, h / 2 - (h /2 + size) * dsc(4)) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f, 1f - 2 * j)
+                drawXY(0f, (-h / 2) * (1 - dsc(0))) {
+                    drawRect(RectF(-size, -size, 0f, 0f), paint)
+                }
+            }
+            drawXY(0f, size * (1f - 2 * j) * dsc(2)) {
+                drawLine(0f, 0f, size * dsc(1), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawJBTRUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawJoinBarTRotUp(scale, w, h, paint)
+}
