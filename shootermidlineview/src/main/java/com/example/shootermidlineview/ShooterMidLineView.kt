7183a052c9071157961479c23a482d0d4cb0a446
Anwesh43 <anweshthecool0@gmail.com>
2022-09-26 14:21:47 +0530

    shootermidlineview: adding static create method

--- shootermidlineview/src/main/java/com/example/shootermidlineview/ShooterMidLineView.kt
+++ shootermidlineview/src/main/java/com/example/shootermidlineview/ShooterMidLineView.kt
@@ -213,4 +213,13 @@ class ShooterMidLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : ShooterMidLineView {
+            val view : ShooterMidLineView = ShooterMidLineView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
