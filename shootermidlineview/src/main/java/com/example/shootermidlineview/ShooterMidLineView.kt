6431614c6f6097c586d252d13ea3b9c35358373d
Anwesh43 <anweshthecool0@gmail.com>
2022-09-26 14:18:32 +0530

    shootermidlineview: adding ShooterMidLine class

--- shootermidlineview/src/main/java/com/example/shootermidlineview/ShooterMidLineView.kt
+++ shootermidlineview/src/main/java/com/example/shootermidlineview/ShooterMidLineView.kt
@@ -165,4 +165,27 @@ class ShooterMidLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ShooterMidLine(var i : Int) {
+
+        private var curr : SMLNode = SMLNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
