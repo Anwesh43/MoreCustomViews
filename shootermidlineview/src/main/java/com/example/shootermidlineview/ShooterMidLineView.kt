7346fe2273a09063e73621f9d2091b8ab2913bc9
Anwesh43 <anweshthecool0@gmail.com>
2022-09-26 13:27:42 +0530

    shootermidlineview: adding Canvas extension methods

--- shootermidlineview/src/main/java/com/example/shootermidlineview/ShooterMidLineView.kt
+++ shootermidlineview/src/main/java/com/example/shootermidlineview/ShooterMidLineView.kt
@@ -29,3 +29,35 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawShooterMidLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2, h / 2) {
+        drawXY(0f, (h / 2) * dsc(2)) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size * dsc(0) * 0.5f, paint)
+        }
+        for (j in 0..1) {
+            drawXY(0f, (-h / 2) * dsc(3)) {
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawSMLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawShooterMidLine(scale, w, h, paint)
+}
