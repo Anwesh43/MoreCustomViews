080f0d3e89d4b08783e4656a6591bd0521a8b9e8
Anwesh43 <anweshthecool0@gmail.com>
2022-09-27 04:13:50 +0530

    birotuprotrightcircleview: adding BiRotUpToRightCircleView class

--- birotuptorightcircleview/src/main/java/com/example/birotuptorightcircleview/BiRotUpToRightCircleView.kt
+++ birotuptorightcircleview/src/main/java/com/example/birotuptorightcircleview/BiRotUpToRightCircleView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawBRUTRCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiRotUpToRightCircle(scale, w, h, paint)
 }
+
+class BiRotUpToRightCircleView(ctx : Context) : View(ctx) {
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+}
