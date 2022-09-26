982d86d81e90eac45ef4dbf1e425f928a3167af0
Anwesh43 <anweshthecool0@gmail.com>
2022-09-27 04:28:18 +0530

    birotuprotrightcircleview: adding BiRotUpToRight class

--- birotuptorightcircleview/src/main/java/com/example/birotuptorightcircleview/BiRotUpToRightCircleView.kt
+++ birotuptorightcircleview/src/main/java/com/example/birotuptorightcircleview/BiRotUpToRightCircleView.kt
@@ -152,7 +152,7 @@ class BiRotUpToRightCircleView(ctx : Context) : View(ctx) {
             state.startUpdating(cb)
         }
 
-        fun getNext(dir : Int, cb : () -> Unit) : BUTRCNode? {
+        fun getNext(dir : Int, cb : () -> Unit) : BUTRCNode {
             var curr : BUTRCNode? = prev
             if (dir == 1) {
                 curr = next
@@ -162,6 +162,29 @@ class BiRotUpToRightCircleView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class BiRotUpToRight(var i : Int) {
+
+        private var curr : BUTRCNode = BUTRCNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
