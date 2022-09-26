0b618dd263ebdbba8618f7c778a27729613054f4
Anwesh43 <anweshthecool0@gmail.com>
2022-09-27 04:02:27 +0530

    birotuprotrightcircleview: adding constants and libraries

---
+++ birotuptorightcircleview/src/main/java/com/example/birotuptorightcircleview/BiRotUpToRightCircleView.kt
@@ -0,0 +1,27 @@
+package com.example.birotuptorightcircleview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#AB47BC",
+    "#1E88E5",
+    "#00E678",
+    "#FF3D00",
+    "#3D5AFE"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
