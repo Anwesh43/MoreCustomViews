c47e9e64860a6aa5fbd3a5216d47178d6335ce73
Anwesh43 <anweshthecool0@gmail.com>
2022-09-27 04:11:58 +0530

    birotuprotrightcircleview: adding Canvas extension methods

--- birotuptorightcircleview/src/main/java/com/example/birotuptorightcircleview/BiRotUpToRightCircleView.kt
+++ birotuptorightcircleview/src/main/java/com/example/birotuptorightcircleview/BiRotUpToRightCircleView.kt
@@ -25,7 +25,38 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val rot : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 21.2f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiRotUpToRightCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    val r : Float = Math.min(w, h) / rFactor
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(-size * (1f - 2 * j), -h / 2 - r + (h / 2 + r) * dsc(j) + (w / 2 + r) * dsc(3 + j)) {
+                drawCircle(0f, 0f, r, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBRUTRCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiRotUpToRightCircle(scale, w, h, paint)
+}
