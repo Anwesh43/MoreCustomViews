14e6849b9974798bd53d1e57a52fadf9ec8ae73d
Anwesh43 <anweshthecool0@gmail.com>
2022-09-26 13:53:59 +0530

    shootermidlineview: adding ShooterMidLineView class

--- shootermidlineview/src/main/java/com/example/shootermidlineview/ShooterMidLineView.kt
+++ shootermidlineview/src/main/java/com/example/shootermidlineview/ShooterMidLineView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawSMLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawShooterMidLine(scale, w, h, paint)
 }
+
+class ShooterMidLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
