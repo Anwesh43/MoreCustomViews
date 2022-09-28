824cb25f62c9241e335de29df0581b5055014e57
Anwesh43 <anweshthecool0@gmail.com>
2022-09-28 10:35:44 +0530

    bilineshooterarcview: adding BLSANode class

--- bilineshooterarcview/src/main/java/com/example/bilineshooterarcview/BiLineShooterArcView.kt
+++ bilineshooterarcview/src/main/java/com/example/bilineshooterarcview/BiLineShooterArcView.kt
@@ -127,4 +127,45 @@ class BiLineShooterArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLSANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLSANode? = null
+        private var prev : BLSANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLSANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLSANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLSANode {
+            var curr : BLSANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
