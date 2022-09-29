5cf78ce4b643587700139e50b006102555cf7241
Anwesh43 <anweshthecool0@gmail.com>
2022-09-30 04:26:17 +0530

    bicircleoversqview: adding BCOSNode class

--- bicircleoversqview/src/main/java/com/example/bicircleoversqview/BiCircleOverSqView.kt
+++ bicircleoversqview/src/main/java/com/example/bicircleoversqview/BiCircleOverSqView.kt
@@ -122,4 +122,45 @@ class BiCircleOverSqView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BCOSNode(var i : Int, val state : State = State()) {
+
+        private var next : BCOSNode? = null
+        private var prev : BCOSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BCOSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBCOSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BCOSNode {
+            var curr : BCOSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
