247bf02f68906befdc7760547b0f0f47d5bfff7f
Anwesh43 <anweshthecool0@gmail.com>
2022-09-29 13:03:52 +0530

    barexpandwithcirleview: adding BEWCNode class

--- barexpandwithcircleview/src/main/java/com/example/barexpandwithcircleview/BarExpandWithCircleView.kt
+++ barexpandwithcircleview/src/main/java/com/example/barexpandwithcircleview/BarExpandWithCircleView.kt
@@ -127,4 +127,45 @@ class BarExpandWithCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BEWCNode(var i : Int, val state : State = State()) {
+
+        private var next : BEWCNode? = null
+        private var prev : BEWCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BEWCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBEWCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BEWCNode {
+            var curr : BEWCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
