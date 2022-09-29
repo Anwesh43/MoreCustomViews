af100de8aae8aef0510f900ae77629566102a536
Anwesh43 <anweshthecool0@gmail.com>
2022-09-29 12:46:48 +0530

    barexpandwithcircleview: adding Canvas extension methods

--- barexpandwithcircleview/src/main/java/com/example/barexpandwithcircleview/BarExpandWithCircleView.kt
+++ barexpandwithcircleview/src/main/java/com/example/barexpandwithcircleview/BarExpandWithCircleView.kt
@@ -25,7 +25,41 @@ val sizeFactor : Float = 4.9f
 val rFactor : Float = 23.1f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarExpandWithCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    val r : Float = Math.min(w, h) / rFactor
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f - 2 * j)
+                drawXY((h / 2 + size) * dsc(3), 0f) {
+                    drawRect(RectF(-size / 2, 0f, -size / 2 + size * dsc(0), size / 4), paint)
+                    drawCircle(size / 2 + 2 * r, (h / 2 + r) * (1 - dsc(1)), r, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBEWCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val size : Float = Math.min(w, h) / sizeFactor
+    paint.color = colors[i]
+    drawBarExpandWithCircle(scale, w, h, paint)
+}
