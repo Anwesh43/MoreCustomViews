e681d18d804b67fdf594e00ef6c0e5d6c053d793
Anwesh43 <anweshthecool0@gmail.com>
2022-09-30 04:32:17 +0530

    bicircleoversqview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.barexpandwithcircleview.BarExpandWithCircleView
+import com.example.bicircleoversqview.BiCircleOverSqView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
         //BiLineParallelBarView.create(this)
-        BarExpandWithCircleView.create(this)
+        BiCircleOverSqView.create(this)
         fullScreen()
     }
 }
