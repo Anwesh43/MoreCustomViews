37e0ba44e77cc0335070d1cbf2e05326226ea13e
Anwesh43 <anweshthecool0@gmail.com>
2022-09-30 04:16:23 +0530

    bicircleoversqview: adding BiCircleOverSqView class

--- bicircleoversqview/src/main/java/com/example/bicircleoversqview/BiCircleOverSqView.kt
+++ bicircleoversqview/src/main/java/com/example/bicircleoversqview/BiCircleOverSqView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawBCOSNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBiCircleOverSq(scale, w, h, paint)
 }
+
+class BiCircleOverSqView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
