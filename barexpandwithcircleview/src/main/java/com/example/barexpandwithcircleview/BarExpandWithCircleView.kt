bd2350a5fb18f4e837664dcbbf4735c107e38833
Anwesh43 <anweshthecool0@gmail.com>
2022-09-29 12:22:49 +0530

    barexpandwithcircleview: adding constants and libraries

---
+++ barexpandwithcircleview/src/main/java/com/example/barexpandwithcircleview/BarExpandWithCircleView.kt
@@ -0,0 +1,28 @@
+package com.example.barexpandwithcircleview
+
+import android.view.MotionEvent
+import android.view.View
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#AB47BC",
+    "#1E88E5",
+    "#00E678",
+    "#FF3D00",
+    "#3D5AFE"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val rFactor : Float = 23.1f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+
