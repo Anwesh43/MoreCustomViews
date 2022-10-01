2ef412d357b92404e4e4bd0f81b9ab296e4a1b43
Anwesh43 <anweshthecool0@gmail.com>
2022-10-02 01:41:17 +0530

    smallbaroversqview: adding Canvas extension methods

--- smallbaroversqview/src/main/java/com/example/smallbaroversqview/SmallBarOverSqView.kt
+++ smallbaroversqview/src/main/java/com/example/smallbaroversqview/SmallBarOverSqView.kt
@@ -30,3 +30,30 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSmallBarOverSq(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    val sqSize : Float = Math.min(w, h) / sqSizeFactor
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawRect(RectF(-size / 2, -size * dsc(0), size / 2, 0f), paint)
+        drawXY(0f, -size) {
+            drawRect(RectF(-sqSize * 0.5f * dsc(1), -sqSize, sqSize * 0.5f * dsc(1), 0f), paint)
+        }
+    }
+}
+
+fun Canvas.drawSBOSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawSmallBarOverSq(scale, w, h, paint)
+}
