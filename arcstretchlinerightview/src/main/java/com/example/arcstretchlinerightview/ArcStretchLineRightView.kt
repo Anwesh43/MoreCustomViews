ae421b2bafb39ce92aaa4728a8f87f2076eff8f2
Anwesh43 <anweshthecool0@gmail.com>
2022-10-01 05:43:54 +0530

    arcstretchlinerightview: adding constants and libraries

---
+++ arcstretchlinerightview/src/main/java/com/example/arcstretchlinerightview/ArcStretchLineRightView.kt
@@ -0,0 +1,28 @@
+package com.example.arcstretchlinerightview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#AB47BC",
+    "#1E88E5",
+    "#00E678",
+    "#FF3D00",
+    "#3D5AFE"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val rFactor : Float = 18.4f
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val strokeFactor : Float = 90f
+val rot : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val parts : Int = 4
+val scGap : Float = 0.02f
