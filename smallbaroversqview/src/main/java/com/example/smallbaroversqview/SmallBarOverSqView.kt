116aef4563ea625409f1a02f1492e3fce76a487f
Anwesh43 <anweshthecool0@gmail.com>
2022-10-02 01:52:37 +0530

    smallbaroversqview: adding Renderer class

--- smallbaroversqview/src/main/java/com/example/smallbaroversqview/SmallBarOverSqView.kt
+++ smallbaroversqview/src/main/java/com/example/smallbaroversqview/SmallBarOverSqView.kt
@@ -184,4 +184,27 @@ class SmallBarOverSqView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SmallBarOverSqView) {
+
+        private var sbos : SmallBarOverSq = SmallBarOverSq(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            sbos.draw(canvas, paint)
+            animator.animate {
+                sbos.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            sbos.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
