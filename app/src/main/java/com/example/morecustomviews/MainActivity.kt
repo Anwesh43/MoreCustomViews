c648ba3b8295fb7e5fe0e9bc6c3bb9440b58abe6
Anwesh43 <anweshthecool0@gmail.com>
2022-10-03 12:07:58 +0530

    rightlinefullcircleview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.smallbaroversqview.SmallBarOverSqView
+import com.example.rightlinefullcircleview.RightLineFullCircleView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
         //BiLineParallelBarView.create(this)
-        SmallBarOverSqView.create(this)
+        RightLineFullCircleView.create(this)
         fullScreen()
     }
 }
