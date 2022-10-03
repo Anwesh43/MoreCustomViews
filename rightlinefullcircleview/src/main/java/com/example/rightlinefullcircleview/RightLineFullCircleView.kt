f498f0a7fdd684d7d4584be90739e90df8b88316
Anwesh43 <anweshthecool0@gmail.com>
2022-10-03 11:06:20 +0530

    rightlinefullcircleview: adding Canvas extension methods

--- rightlinefullcircleview/src/main/java/com/example/rightlinefullcircleview/RightLineFullCircleView.kt
+++ rightlinefullcircleview/src/main/java/com/example/rightlinefullcircleview/RightLineFullCircleView.kt
@@ -26,7 +26,48 @@ val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 360f
 val rFactor : Float = 21.2f
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineWithoutDot(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    if (Math.abs(x1 - x2) < 0.1f && Math.abs(y1 - y2) < 0.1f) {
+        return
+    }
+    drawLine(x1, y1, x2, y2, paint)
+}
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightLineFullCircle(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * j * dsc(1))
+                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            }
+        }
+        drawArc(RectF(-r, -r, r, r), 0f, deg * dsc(1), true, paint)
+    }
+}
+
+fun Canvas.drawRLFCNode(i : Int, scale : Float, paint : Paint) {
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightLineFullCircle(scale, w, h, paint)
+}
