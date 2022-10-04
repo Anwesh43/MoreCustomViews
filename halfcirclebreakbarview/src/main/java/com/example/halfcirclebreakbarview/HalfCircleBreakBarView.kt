383a682373f3deba47ede01dfe446e2d2efbd048
Anwesh43 <anweshthecool0@gmail.com>
2022-10-04 12:27:12 +0530

    halfcirclebreakbarview: adding Renderer class

--- halfcirclebreakbarview/src/main/java/com/example/halfcirclebreakbarview/HalfCircleBreakBarView.kt
+++ halfcirclebreakbarview/src/main/java/com/example/halfcirclebreakbarview/HalfCircleBreakBarView.kt
@@ -193,4 +193,27 @@ class HalfCircleBreakBarView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : HalfCircleBreakBarView) {
+
+        private val animator : Animator = Animator(view)
+        private val hcbb : HalfCircleBreakBar = HalfCircleBreakBar(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            hcbb.draw(canvas, paint)
+            animator.animate {
+                hcbb.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            hcbb.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
