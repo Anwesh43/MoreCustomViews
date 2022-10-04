976d19dbd71f849262cde054aa4566c461393f5f
Anwesh43 <anweshthecool0@gmail.com>
2022-10-04 11:12:53 +0530

    halfcirclebreakbarview: adding Canvas extension methods

--- halfcirclebreakbarview/src/main/java/com/example/halfcirclebreakbarview/HalfCircleBreakBarView.kt
+++ halfcirclebreakbarview/src/main/java/com/example/halfcirclebreakbarview/HalfCircleBreakBarView.kt
@@ -29,3 +29,40 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawHalfCircleBreakBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY((w /2) * (1 - 2 * j) * dsc(3), 0f) {
+                rotate(rot * (1f - 2 * j) * dsc(0))
+                drawArc(
+                    RectF(-size / 2, -size / 2, size / 2, size / 2),
+                    0f,
+                    deg * dsc(0),
+                    true,
+                    paint
+                )
+                drawRect(
+                    RectF(-size / 2, -size * 0.5f * dsc(2), size / 2, 0f),
+                    paint
+                )
+            }
+        }
+    }
+}
+
+fun Canvas.drawHCBBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawHalfCircleBreakBar(scale, w, h, paint)
+}
