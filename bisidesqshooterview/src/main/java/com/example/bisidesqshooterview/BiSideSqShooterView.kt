4fe255d19f2059627ce294445749458fde31ff67
Anwesh43 <anweshthecool0@gmail.com>
2022-10-05 11:23:19 +0530

    bisdiesqshooterview: adding BiSideSqShooter class

--- bisidesqshooterview/src/main/java/com/example/bisidesqshooterview/BiSideSqShooterView.kt
+++ bisidesqshooterview/src/main/java/com/example/bisidesqshooterview/BiSideSqShooterView.kt
@@ -159,4 +159,27 @@ class BiSideSqShooterView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiSideSqShooter(var i : Int) {
+
+        private var curr : BSSSNode = BSSSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
