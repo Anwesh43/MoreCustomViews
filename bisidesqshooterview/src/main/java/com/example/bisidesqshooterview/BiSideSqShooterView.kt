830f091df3df3acdeefecd0d9780a00c39c4eb13
Anwesh43 <anweshthecool0@gmail.com>
2022-10-05 11:21:01 +0530

    bisdiesqshooterview: adding BSSSNode class

--- bisidesqshooterview/src/main/java/com/example/bisidesqshooterview/BiSideSqShooterView.kt
+++ bisidesqshooterview/src/main/java/com/example/bisidesqshooterview/BiSideSqShooterView.kt
@@ -118,4 +118,45 @@ class BiSideSqShooterView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BSSSNode(var i : Int, val state : State = State()) {
+
+        private var next : BSSSNode? = null
+        private var prev : BSSSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BSSSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBSSSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BSSSNode {
+            var curr : BSSSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
