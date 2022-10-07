db5e9aa361fad587a59b1f892fa5a51664997136
Anwesh43 <anweshthecool0@gmail.com>
2022-10-07 11:53:36 +0530

    semifoldedlineview: adding SFLNode class

--- semifoldedlineview/src/main/java/com/example/semifoldedlineview/SemiFoldedLineView.kt
+++ semifoldedlineview/src/main/java/com/example/semifoldedlineview/SemiFoldedLineView.kt
@@ -119,4 +119,45 @@ class SemiFoldedLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SFLNode(var i : Int, val state : State = State()) {
+
+        private var next : SFLNode? = null
+        private var prev : SFLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SFLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSFLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SFLNode {
+            var curr : SFLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
