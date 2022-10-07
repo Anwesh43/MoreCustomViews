fa60b94ed47069fbcab97be885b481770569bdf1
Anwesh43 <anweshthecool0@gmail.com>
2022-10-07 11:04:18 +0530

    semifoldedlineview: adding Canvas extension methods

--- semifoldedlineview/src/main/java/com/example/semifoldedlineview/SemiFoldedLineView.kt
+++ semifoldedlineview/src/main/java/com/example/semifoldedlineview/SemiFoldedLineView.kt
@@ -29,3 +29,30 @@ val rot : Float = -90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSemiFoldedLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawLine(-size * 0.5f * dsc(0), 0f, size * 0.5f * dsc(0), 0f, paint)
+        drawLine(-size / 2, 0f, -size / 2 + (size / 2) * dsc(1), -size * 0.5f * dsc(1), paint)
+        drawLine(size / 2, 0f, size / 2, -size * 0.5f * dsc(1), paint)
+    }
+}
+
+fun Canvas.drawSFLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSemiFoldedLine(scale, w, h, paint)
+}
