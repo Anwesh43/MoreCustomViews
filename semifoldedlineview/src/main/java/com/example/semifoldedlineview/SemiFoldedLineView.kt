038c555dd4d9ad83542a52e7e1524f74581d3eb0
Anwesh43 <anweshthecool0@gmail.com>
2022-10-07 12:21:25 +0530

    semifoldedlineview: adding Renderer class

--- semifoldedlineview/src/main/java/com/example/semifoldedlineview/SemiFoldedLineView.kt
+++ semifoldedlineview/src/main/java/com/example/semifoldedlineview/SemiFoldedLineView.kt
@@ -183,4 +183,27 @@ class SemiFoldedLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SemiFoldedLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val sfl : SemiFoldedLine = SemiFoldedLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            sfl.draw(canvas, paint)
+            animator.animate {
+                sfl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            sfl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
