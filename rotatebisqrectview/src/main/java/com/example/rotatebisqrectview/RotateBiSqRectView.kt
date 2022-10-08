9ce0fd94ece975d6f537039eccc6d501989ba9ba
Anwesh43 <anweshthecool0@gmail.com>
2022-10-08 10:00:04 +0530

    rotatebisqrectview: adding Canvas extension methods

--- rotatebisqrectview/src/main/java/com/example/rotatebisqrectview/RotateBiSqRectView.kt
+++ rotatebisqrectview/src/main/java/com/example/rotatebisqrectview/RotateBiSqRectView.kt
@@ -28,3 +28,31 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRotateBiSqRect(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2, h / 2 + (h / 2 + size)) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 *j, 1f)
+                rotate(rot * dsc(2))
+                drawRect(RectF(0f, -size * dsc(j), size / 2, 0f), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawRBSRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawRotateBiSqRect(scale, w, h, paint)
+}
