99da5170a802f22a54be2b87134c0409e9774b16
Anwesh43 <anweshthecool0@gmail.com>
2022-10-09 09:07:36 +0530

    rightlinecirclepusherview: adding Canvas extension methods

--- rightlinecirclepusherview/src/main/java/com/example/rightlinecirclepusherview/RightLineCirclePusherView.kt
+++ rightlinecirclepusherview/src/main/java/com/example/rightlinecirclepusherview/RightLineCirclePusherView.kt
@@ -25,7 +25,39 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
+val rFactor : Float = 23.2f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightLineCirclePusher(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+       for (j in 0..1) {
+           drawXY(-size, 0f) {
+               rotate(rot * (1 - dsc(2)))
+               drawLine(0f, 0f, (size - r) * dsc(1), 0f, paint)
+           }
+       }
+       drawArc(RectF(-r, -r, r, r), 0f, 360f * dsc(0), true, paint)
+    }
+}
+
+fun Canvas.drawRLCPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightLineCirclePusher(scale, w, h, paint)
+}
