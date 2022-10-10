a19f61e29514ab8db4c71da17ba422af63d6c5da
Anwesh43 <anweshthecool0@gmail.com>
2022-10-10 11:43:43 +0530

    linerotthreelinerightview: adding Canvas extension methods

--- linerotthreelinerightview/src/main/java/com/example/linerotthreelinerightview/LineRotThreeLineRightView.kt
+++ linerotthreelinerightview/src/main/java/com/example/linerotthreelinerightview/LineRotThreeLineRightView.kt
@@ -30,3 +30,35 @@ val lines : Int = 3
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotThreeRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        rotate(-rot * dsc(1))
+        drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        for (j in 0..(lines - 1)) {
+            drawXY(-size * 0.5f * j, 0f) {
+                drawLine(0f, 0f, 0f, size * 0.5f * dsc(2), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLRTRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawLineRotThreeRight(scale, w, h, paint)
+}
