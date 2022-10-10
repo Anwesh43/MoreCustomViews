3f0f2be3e2578e880c0c1d6ffa3962170f4078a9
Anwesh43 <anweshthecool0@gmail.com>
2022-10-10 13:23:44 +0530

    linerotthreelinerightview: adding LineRotThreeLineRightView class

--- linerotthreelinerightview/src/main/java/com/example/linerotthreelinerightview/LineRotThreeLineRightView.kt
+++ linerotthreelinerightview/src/main/java/com/example/linerotthreelinerightview/LineRotThreeLineRightView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLRTRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawLineRotThreeRight(scale, w, h, paint)
 }
+
+class LineRotThreeLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
