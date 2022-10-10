4d229bcd58e03adbe72f905b5c7969ec52e30508
Anwesh43 <anweshthecool0@gmail.com>
2022-10-10 14:01:24 +0530

    linerotthreelinerightview: adding Renderer clas

--- linerotthreelinerightview/src/main/java/com/example/linerotthreelinerightview/LineRotThreeLineRightView.kt
+++ linerotthreelinerightview/src/main/java/com/example/linerotthreelinerightview/LineRotThreeLineRightView.kt
@@ -190,4 +190,27 @@ class LineRotThreeLineRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineRotThreeLineRightView) {
+
+        private val lrtrl : LineRotThreeLineRight = LineRotThreeLineRight(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lrtrl.draw(canvas, paint)
+            animator.animate {
+                lrtrl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lrtrl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
