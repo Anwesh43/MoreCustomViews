dbea7b53cfc3e2689cc22661568887720ff60a64
Anwesh43 <anweshthecool0@gmail.com>
2022-10-11 14:05:28 +0530

    linefoldmoreview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotthreelinerightview.LineRotThreeLineRightView
+import com.example.linefoldmoreview.LineFoldMoreView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
         //BiLineParallelBarView.create(this)
-        LineRotThreeLineRightView.create(this)
+        LineFoldMoreView.create(this)
         fullScreen()
     }
 }
