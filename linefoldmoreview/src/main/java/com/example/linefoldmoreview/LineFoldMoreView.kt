04f39d7dadc7d5078ef09b934f0fa8eb830e09e0
Anwesh43 <anweshthecool0@gmail.com>
2022-10-11 12:45:16 +0530

    linefoldmoreview: adding constants and libraries

---
+++ linefoldmoreview/src/main/java/com/example/linefoldmoreview/LineFoldMoreView.kt
@@ -0,0 +1,26 @@
+package com.example.linefoldmoreview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#AB47BC",
+    "#1E88E5",
+    "#00E678",
+    "#FF3D00",
+    "#3D5AFE"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val rot : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
