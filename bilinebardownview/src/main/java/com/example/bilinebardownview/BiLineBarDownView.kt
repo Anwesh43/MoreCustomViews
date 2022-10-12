9b56ce68daddadccbf28731cedd3c5ff94301daa
Anwesh43 <anweshthecool0@gmail.com>
2022-10-12 11:22:42 +0530

    bilinebardownview: adding BiLineBarDownView class

--- bilinebardownview/src/main/java/com/example/bilinebardownview/BiLineBarDownView.kt
+++ bilinebardownview/src/main/java/com/example/bilinebardownview/BiLineBarDownView.kt
@@ -74,3 +74,19 @@ fun Canvas.drawBLBDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineBarDown(scale, w, h, paint)
 }
+
+class BiLineBarDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
