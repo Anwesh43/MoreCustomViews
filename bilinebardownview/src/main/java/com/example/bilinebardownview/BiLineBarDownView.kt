e5ac245afb1e4fc9822b152fe469fb9a8e4372db
Anwesh43 <anweshthecool0@gmail.com>
2022-10-12 11:18:30 +0530

    bilinebardownview: adding Canvas extension methods

--- bilinebardownview/src/main/java/com/example/bilinebardownview/BiLineBarDownView.kt
+++ bilinebardownview/src/main/java/com/example/bilinebardownview/BiLineBarDownView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val barWFactor : Float = 11.2f
@@ -31,3 +31,46 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLine(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    if (Math.abs(x1 - x2) < 0.1f && Math.abs(y1 - y2) < 0.1f) {
+        return
+    }
+    drawLine(x1, y1, x2, y2, paint)
+}
+
+fun Canvas.drawBiLineBarDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val barSize : Float = Math.min(w, h) / barWFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(4)) {
+        rotate(deg * dsc(2))
+        drawLine(-size * 0.5f * dsc(0), 0f, size * 0.5f * dsc(0), 0f, paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f - 2 * j)
+                drawXY(size / 2 - barSize, 0f ) {
+                    drawRect(RectF(0f, 0f, barSize, size * dsc(1)), paint)
+                }
+            }
+        }
+        drawLine(0f, 0f, 0f, -size * dsc(3), paint)
+    }
+}
+
+fun Canvas.drawBLBDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiLineBarDown(scale, w, h, paint)
+}
