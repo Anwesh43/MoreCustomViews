16e86b9a5f7d92e4b82f008b18d82a3279cf0bc4
Anwesh43 <anweshthecool0@gmail.com>
2022-10-12 10:45:28 +0530

    bilinebardownview: adding constants and libraries

---
+++ bilinebardownview/src/main/java/com/example/bilinebardownview/BiLineBarDownView.kt
@@ -0,0 +1,28 @@
+package com.example.bilinebardownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#AB47BC",
+    "#1E88E5",
+    "#00E678",
+    "#FF3D00",
+    "#3D5AFE"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val barWFactor : Float = 11.2f
+val sizeFactor : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
