5151a3dfea410b57760715d344679224bca91ae6
Anwesh43 <anweshthecool0@gmail.com>
2022-10-12 11:38:22 +0530

    bilinebardownview: adding BiLineBarDown class

--- bilinebardownview/src/main/java/com/example/bilinebardownview/BiLineBarDownView.kt
+++ bilinebardownview/src/main/java/com/example/bilinebardownview/BiLineBarDownView.kt
@@ -178,4 +178,27 @@ class BiLineBarDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiLineBarDown(var i : Int) {
+
+        private var curr : BLBDNode = BLBDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
