7f457a5d4b06ae01a26b085659ca32a4c52f7984
Anwesh43 <anweshthecool0@gmail.com>
2022-10-13 13:35:00 +0530

    linedivideafterrotview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.bilinebardownview.BiLineBarDownView
+import com.example.linedivideafterotview.LineDivideAfterRotView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
         //BiLineParallelBarView.create(this)
-        BiLineBarDownView.create(this)
+        LineDivideAfterRotView.create(this)
         fullScreen()
     }
 }
