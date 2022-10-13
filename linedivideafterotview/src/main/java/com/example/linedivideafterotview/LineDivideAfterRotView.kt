80d895f01d726e8810bb26354ac7bd029aa8e750
Anwesh43 <anweshthecool0@gmail.com>
2022-10-13 13:31:14 +0530

    linedivideafterrotview: adding drawing fixes

--- linedivideafterotview/src/main/java/com/example/linedivideafterotview/LineDivideAfterRotView.kt
+++ linedivideafterotview/src/main/java/com/example/linedivideafterotview/LineDivideAfterRotView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 11.2f
@@ -40,13 +40,14 @@ fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
 fun Canvas.drawLineDivideAfterRot(scale : Float, w : Float, h : Float, paint : Paint) {
     val size : Float = Math.min(w, h) / sizeFactor
     val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
-    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(4)) {
         rotate(rot * dsc(1))
         drawLine(0f, -size * 0.5f * dsc(2), 0f, size * 0.5f * dsc(2), paint)
         for (j in 0..1) {
             translate(0f, -size * 0.5f * dsc(2) * (1f - 2 * j))
             drawLine(0f, 0f, -size * dsc(0), 0f, paint)
         }
+        drawRect(RectF(-size * dsc(3),  -size / 2, 0f, size / 2), paint)
     }
 }
 
