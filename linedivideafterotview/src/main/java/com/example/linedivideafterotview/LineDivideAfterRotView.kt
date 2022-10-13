5b013cf26b0d80ddadde7c06c73c75acda41c8db
Anwesh43 <anweshthecool0@gmail.com>
2022-10-13 13:20:55 +0530

    linedivideaferrotview: adding LDARNode class

--- linedivideafterotview/src/main/java/com/example/linedivideafterotview/LineDivideAfterRotView.kt
+++ linedivideafterotview/src/main/java/com/example/linedivideafterotview/LineDivideAfterRotView.kt
@@ -120,4 +120,45 @@ class LineDivideAfterRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDARNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LDARNode? = null
+        private var next : LDARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDARNode {
+            var curr : LDARNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
