51942ecd275b28e2b53e92e612f488ad19f782ab
Anwesh43 <anweshthecool0@gmail.com>
2022-10-14 11:09:27 +0530

    lineeitherslantview: adding LineEitherSlantView class

--- lineeitherslantview/src/main/java/com/example/lineeitherslantview/LineEitherSlantView.kt
+++ lineeitherslantview/src/main/java/com/example/lineeitherslantview/LineEitherSlantView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLESNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineEitherSlant(scale, w, h, paint)
 }
+
+class LineEitherSlantView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
