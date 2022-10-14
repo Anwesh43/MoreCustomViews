502de80af524d1d4a54d08bdd0e4cbc3897a5a30
Anwesh43 <anweshthecool0@gmail.com>
2022-10-14 11:24:58 +0530

    lineeitherslantview: adding LineEitehrSlant class

--- lineeitherslantview/src/main/java/com/example/lineeitherslantview/LineEitherSlantView.kt
+++ lineeitherslantview/src/main/java/com/example/lineeitherslantview/LineEitherSlantView.kt
@@ -165,4 +165,27 @@ class LineEitherSlantView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineEitherSlant(var i : Int) {
+
+        private var curr : LESNode = LESNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
