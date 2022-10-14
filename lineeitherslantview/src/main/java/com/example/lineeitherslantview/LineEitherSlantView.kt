8d4f2258642b8f81a8d479a2fe0b604c6943c391
Anwesh43 <anweshthecool0@gmail.com>
2022-10-14 11:05:38 +0530

    lineeitherslantview: adding Canvas extension methods

--- lineeitherslantview/src/main/java/com/example/lineeitherslantview/LineEitherSlantView.kt
+++ lineeitherslantview/src/main/java/com/example/lineeitherslantview/LineEitherSlantView.kt
@@ -29,3 +29,35 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineEitherSlant(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2, h / 2 + (h / 2 + 2 * size) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawLine(-size * 0.5f * dsc(0), 0f, size * 0.5f * dsc(0), 0f, paint)
+        drawLine(size / 2, 0f, size / 2, size * 0.5f * dsc(1), paint)
+        drawXY(size / 2, 0f) {
+            drawLine(0f, 0f, 0f, size * 0.5f * dsc(1), paint)
+        }
+        drawXY(-size / 2, 0f) {
+            drawLine(0f, 0f, -size * 0.5f * dsc(1), -size * 0.5f * dsc(1), paint)
+        }
+    }
+}
+
+fun Canvas.drawLESNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineEitherSlant(scale, w, h, paint)
+}
