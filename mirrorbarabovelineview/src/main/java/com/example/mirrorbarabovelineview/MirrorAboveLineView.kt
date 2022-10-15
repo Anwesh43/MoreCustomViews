81abd80585282693bb9b091e84d1e970ff35c83c
Anwesh43 <anweshthecool0@gmail.com>
2022-10-15 14:13:07 +0530

    mirrorbarabovelineview: adding MirrorAboveLine class

--- mirrorbarabovelineview/src/main/java/com/example/mirrorbarabovelineview/MirrorAboveLineView.kt
+++ mirrorbarabovelineview/src/main/java/com/example/mirrorbarabovelineview/MirrorAboveLineView.kt
@@ -167,4 +167,27 @@ class MirrorBarAboveLineView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class MirrorAboveLine(var i : Int) {
+
+        private var curr : MALNode = MALNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
