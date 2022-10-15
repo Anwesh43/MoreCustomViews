3e86072ef8ab21392fb5adfdc7302297930a8fa7
Anwesh43 <anweshthecool0@gmail.com>
2022-10-15 14:36:51 +0530

    mirrorbaraboveline: adding MirrorBarAboveLine class

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.lineeitherslantview.LineEitherSlantView
+import com.example.mirrorbarabovelineview.MirrorBarAboveLineView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         //LineDownExtenderRotView.create(this)
         //BiLineParallelBarView.create(this)
-        LineEitherSlantView.create(this)
+        MirrorBarAboveLineView.create(this)
         fullScreen()
     }
 }
