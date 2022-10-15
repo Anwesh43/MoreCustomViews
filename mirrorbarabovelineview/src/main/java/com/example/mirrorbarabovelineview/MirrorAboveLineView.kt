992f6fbc0caa32261f04d623a7a223c7570aee86
Anwesh43 <anweshthecool0@gmail.com>
2022-10-15 14:01:04 +0530

    mirrorbarabovelineview: adding MirrorBarAboveLineView class

--- mirrorbarabovelineview/src/main/java/com/example/mirrorbarabovelineview/MirrorAboveLineView.kt
+++ mirrorbarabovelineview/src/main/java/com/example/mirrorbarabovelineview/MirrorAboveLineView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawMBALNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawMirrorBarAboveLine(scale, w, h, paint)
 }
+
+class MirrorBarAboveLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
