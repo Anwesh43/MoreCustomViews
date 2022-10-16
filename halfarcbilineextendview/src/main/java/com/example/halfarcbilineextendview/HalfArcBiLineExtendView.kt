aaa7b3d6de1de402c34877801fdd80764f8276de
Anwesh43 <anweshthecool0@gmail.com>
2022-10-16 11:07:35 +0530

    halfarcbilineextendview: adding HalfArcBiLineExtendView class

--- halfarcbilineextendview/src/main/java/com/example/halfarcbilineextendview/HalfArcBiLineExtendView.kt
+++ halfarcbilineextendview/src/main/java/com/example/halfarcbilineextendview/HalfArcBiLineExtendView.kt
@@ -70,3 +70,19 @@ fun Canvas.drawHABLENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawHalfArcBiLineExtend(scale, w, h, paint)
 }
+
+class HalfArcBiLineExtendView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
