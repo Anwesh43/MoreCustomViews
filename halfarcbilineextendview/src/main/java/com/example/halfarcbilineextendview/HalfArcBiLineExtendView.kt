6846f94983074fca05b6345434eee5e43258219e
Anwesh43 <anweshthecool0@gmail.com>
2022-10-16 11:06:05 +0530

    halfarcbilineextendview: adding Canvas extension methods

--- halfarcbilineextendview/src/main/java/com/example/halfarcbilineextendview/HalfArcBiLineExtendView.kt
+++ halfarcbilineextendview/src/main/java/com/example/halfarcbilineextendview/HalfArcBiLineExtendView.kt
@@ -29,3 +29,44 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineWithoutDot(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    if (Math.abs(x1 - x2) < 0.1f && Math.abs(y1 - y2) < 0.1f) {
+        return
+    }
+    drawLine(x1, y1, x2, y2, paint)
+}
+
+fun Canvas.drawHalfArcBiLineExtend(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, 180f * dsc(0), true, paint)
+
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f - 2 * j)
+                drawXY(size / 2, 0f) {
+                    drawLine(0f, 0f, 0f, size * 0.5f * dsc(1), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawHABLENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawHalfArcBiLineExtend(scale, w, h, paint)
+}
