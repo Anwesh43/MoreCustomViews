dc34ea701b0a248b5baf95547041211c6996864f
Anwesh43 <anweshthecool0@gmail.com>
2022-10-17 17:04:48 +0530

    tabletoplinedownview: adding TableTopLineDownView class

--- tabletoplinedownview/src/main/java/com/example/tabletoplinedownview/TableTopLineDownView.kt
+++ tabletoplinedownview/src/main/java/com/example/tabletoplinedownview/TableTopLineDownView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawTTLDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTableTopLineDown(scale, w, h, paint)
 }
+
+class TableTopLineDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
