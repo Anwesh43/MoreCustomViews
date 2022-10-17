1d5b421298bcf16840e6cff1b40cba3a76cd4135
Anwesh43 <anweshthecool0@gmail.com>
2022-10-17 17:20:46 +0530

    tabletoplinedownview: adding TTLDNode class

--- tabletoplinedownview/src/main/java/com/example/tabletoplinedownview/TableTopLineDownView.kt
+++ tabletoplinedownview/src/main/java/com/example/tabletoplinedownview/TableTopLineDownView.kt
@@ -127,4 +127,44 @@ class TableTopLineDownView(ctx : Context) : View(ctx) {
         }
     }
 
+    data class TTLDNode(var i : Int, val state : State = State()) {
+
+        private var next : TTLDNode? = null
+        private var prev : TTLDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TTLDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTTLDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TTLDNode {
+            var curr : TTLDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
