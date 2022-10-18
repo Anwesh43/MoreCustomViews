c8193ac8091f5899225035c80600efe00aa0a0e4
Anwesh43 <anweshthecool0@gmail.com>
2022-10-18 12:52:57 +0530

    biarcbarholderview: adding Canvas extension methods

--- biarcbarholderview/src/main/java/com/example/biarcbarholderview/BiArcBarHolderView.kt
+++ biarcbarholderview/src/main/java/com/example/biarcbarholderview/BiArcBarHolderView.kt
@@ -29,3 +29,34 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiArcBarHolder(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, deg * dsc(0), true, paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(size / 2, 0f) {
+                    drawRect(RectF(0f, 0f, size * 0.5f * dsc(1), size * 0.5f), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawBABHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBiArcBarHolder(scale, w, h, paint)
+}
