83235112360276fd20f6d5d277b6379fc4a64909
Anwesh43 <anweshthecool0@gmail.com>
2022-10-18 13:36:04 +0530

    biarcbarholderview: adding Renderer class

--- biarcbarholderview/src/main/java/com/example/biarcbarholderview/BiArcBarHolderView.kt
+++ biarcbarholderview/src/main/java/com/example/biarcbarholderview/BiArcBarHolderView.kt
@@ -187,4 +187,27 @@ class BiArcBarHolderView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiArcBarHolderView) {
+
+        private val animator : Animator = Animator(view)
+        private val bbah : BiArcBarHolder = BiArcBarHolder(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bbah.draw(canvas, paint)
+            animator.animate {
+                bbah.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bbah.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
