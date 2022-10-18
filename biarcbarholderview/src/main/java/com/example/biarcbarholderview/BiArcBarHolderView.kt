98522290579f61a019f0ab8e42ee44536ddb98a0
Anwesh43 <anweshthecool0@gmail.com>
2022-10-18 12:59:20 +0530

    biarcbarholderview: adding BiArcBarHolderView class

--- biarcbarholderview/src/main/java/com/example/biarcbarholderview/BiArcBarHolderView.kt
+++ biarcbarholderview/src/main/java/com/example/biarcbarholderview/BiArcBarHolderView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawBABHNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBiArcBarHolder(scale, w, h, paint)
 }
+
+class BiArcBarHolderView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
