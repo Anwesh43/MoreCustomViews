ee0292925a99ba421a54984ef0d5010781fc536f
Anwesh43 <anweshthecool0@gmail.com>
2022-10-18 13:34:31 +0530

    biarcbarholderview: adding BiArcBarHolder class

--- biarcbarholderview/src/main/java/com/example/biarcbarholderview/BiArcBarHolderView.kt
+++ biarcbarholderview/src/main/java/com/example/biarcbarholderview/BiArcBarHolderView.kt
@@ -164,4 +164,27 @@ class BiArcBarHolderView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiArcBarHolder(var i : Int) {
+
+        private var curr : BABHNode = BABHNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
