f022a32064f389678dd4c0caf24503afaaea57ae
Anwesh43 <anweshthecool0@gmail.com>
2022-10-19 12:00:07 +0530

    linearcanglerotview: adding constants and libraries

---
+++ linearcanglerotview/src/main/java/com/example/linearcanglerotview/LineArcAngleRotView.kt
@@ -0,0 +1,27 @@
+package com.example.linearcanglerotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val deg : Float = 90f
+val rot : Float = 60f
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
