d86a9c698b25e99e87e0d4582696ac888db93b82
Anwesh43 <anweshthecool0@gmail.com>
2022-10-19 12:12:42 +0530

    linearcanglerotview: Adding LineArcAngleRotView class

--- linearcanglerotview/src/main/java/com/example/linearcanglerotview/LineArcAngleRotView.kt
+++ linearcanglerotview/src/main/java/com/example/linearcanglerotview/LineArcAngleRotView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLAARNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineArcAngleRot(scale, w, h, paint)
 }
+
+class LineArcAngleRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
