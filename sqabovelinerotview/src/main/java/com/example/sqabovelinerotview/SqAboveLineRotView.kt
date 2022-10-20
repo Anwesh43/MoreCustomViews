ba78f0490ee7d943b32a0e3ebff5813259912015
Anwesh43 <anweshthecool0@gmail.com>
2022-10-20 13:43:10 +0530

    sqabovelinerotview: adding Canvas extension methods

--- sqabovelinerotview/src/main/java/com/example/sqabovelinerotview/SqAboveLineRotView.kt
+++ sqabovelinerotview/src/main/java/com/example/sqabovelinerotview/SqAboveLineRotView.kt
@@ -30,3 +30,35 @@ val sqSizeFactor : Float = 11.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqAboveLineRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val barSize : Float = Math.min(w, h) / sqSizeFactor
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        }
+        drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        drawRect(RectF(-barSize, -barSize * dsc(1), 0f, 0f), paint)
+    }
+}
+
+fun Canvas.drawSALRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSqAboveLineRot(scale, w, h, paint)
+}
