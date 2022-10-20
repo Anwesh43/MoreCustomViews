2ba99c09472402c48bcf2e6dcafe20e3bbf2866c
Anwesh43 <anweshthecool0@gmail.com>
2022-10-20 15:12:52 +0530

    sqabovelinerotview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linearcanglerotview.LineArcAngleRotView
+import com.example.sqabovelinerotview.SqAboveLineRotView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-        LineArcAngleRotView.create(this)
+        SqAboveLineRotView.create(this)
         fullScreen()
     }
 }
