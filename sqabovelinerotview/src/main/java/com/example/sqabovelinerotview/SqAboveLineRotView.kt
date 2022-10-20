8f27cbc4921d9b658bd83f04ffa169b3111a3b16
Anwesh43 <anweshthecool0@gmail.com>
2022-10-20 13:55:42 +0530

    sqabovelinerotview: adding SALRNode class

--- sqabovelinerotview/src/main/java/com/example/sqabovelinerotview/SqAboveLineRotView.kt
+++ sqabovelinerotview/src/main/java/com/example/sqabovelinerotview/SqAboveLineRotView.kt
@@ -125,4 +125,45 @@ class SqAboveLineRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SALRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SALRNode? = null
+        private var prev : SALRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SALRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSALRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SALRNode {
+            var curr : SALRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
