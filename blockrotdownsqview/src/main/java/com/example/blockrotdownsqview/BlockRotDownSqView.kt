3cc95934c93f7031bc7048b4caeddc65c69e0b2c
Anwesh43 <anweshthecool0@gmail.com>
2022-10-22 03:45:49 +0530

    blockrotdownsqview: adding BRDSNode class

--- blockrotdownsqview/src/main/java/com/example/blockrotdownsqview/BlockRotDownSqView.kt
+++ blockrotdownsqview/src/main/java/com/example/blockrotdownsqview/BlockRotDownSqView.kt
@@ -122,4 +122,45 @@ class BlockRotDownSqView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BRDSNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : BRDSNode? = null
+        private var next : BRDSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BRDSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBRDSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BRDSNode {
+            var curr : BRDSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
