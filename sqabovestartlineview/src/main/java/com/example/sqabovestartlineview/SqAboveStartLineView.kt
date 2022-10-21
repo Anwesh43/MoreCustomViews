a7cf2e1c9b0ee058c958c5a23657cdbeb1986e6e
Anwesh43 <anweshthecool0@gmail.com>
2022-10-21 08:52:39 +0530

    sqaboestartlineview: adding SASLNode class

--- sqabovestartlineview/src/main/java/com/example/sqabovestartlineview/SqAboveStartLineView.kt
+++ sqabovestartlineview/src/main/java/com/example/sqabovestartlineview/SqAboveStartLineView.kt
@@ -122,4 +122,45 @@ class SqAboveStartLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SASLNode(var i : Int, val state : State = State()) {
+
+        private var next : SASLNode? = null
+        private var prev : SASLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SASLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSASLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SASLNode {
+            var curr : SASLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
