3e15de2c7cb139c1cf496bd6374c6dd859f304cc
Anwesh43 <anweshthecool0@gmail.com>
2022-10-21 08:58:26 +0530

    sqaboestartlineview: adding Renderer class

--- sqabovestartlineview/src/main/java/com/example/sqabovestartlineview/SqAboveStartLineView.kt
+++ sqabovestartlineview/src/main/java/com/example/sqabovestartlineview/SqAboveStartLineView.kt
@@ -186,4 +186,27 @@ class SqAboveStartLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SqAboveStartLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val sasl : SqAboveStartLine = SqAboveStartLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            sasl.draw(canvas, paint)
+            animator.animate {
+                sasl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            sasl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
