efcd50ec647df3fbc46dcbfe42464e5ec0f29025
Anwesh43 <anweshthecool0@gmail.com>
2022-10-21 08:42:07 +0530

    sqaboestartlineview: adding Canvas extension methods

--- sqabovestartlineview/src/main/java/com/example/sqabovestartlineview/SqAboveStartLineView.kt
+++ sqabovestartlineview/src/main/java/com/example/sqabovestartlineview/SqAboveStartLineView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -29,3 +29,33 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqAboveStartLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawXY(0f, -h / 2 + (h / 2) * dsc(1) + (h / 2) * dsc(3)) {
+            rotate(rot * dsc(2))
+            drawRect(RectF(-size / 2, -size, size / 2, 0f), paint)
+        }
+        drawLine(-w / 2 + w * dsc(4), 0f, -w / 2 + w * dsc(0), 0f, paint)
+    }
+}
+
+fun Canvas.drawSASLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSqAboveStartLine(scale, w, h, paint)
+}
