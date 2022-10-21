2c3e1b8de3b51a3dd0b75963f41176de7537a574
Anwesh43 <anweshthecool0@gmail.com>
2022-10-21 08:43:23 +0530

    sqaboestartlineview: adding SqAboveStartLineView class

--- sqabovestartlineview/src/main/java/com/example/sqabovestartlineview/SqAboveStartLineView.kt
+++ sqabovestartlineview/src/main/java/com/example/sqabovestartlineview/SqAboveStartLineView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawSASLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSqAboveStartLine(scale, w, h, paint)
 }
+
+class SqAboveStartLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
