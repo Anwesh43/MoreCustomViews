340d6e22c8c169641855a06ab082ee1f02e0cd65
Anwesh43 <anweshthecool0@gmail.com>
2022-10-22 03:39:52 +0530

    blockrotdownsqview: adding BlockRotDownSqView class

--- blockrotdownsqview/src/main/java/com/example/blockrotdownsqview/BlockRotDownSqView.kt
+++ blockrotdownsqview/src/main/java/com/example/blockrotdownsqview/BlockRotDownSqView.kt
@@ -60,3 +60,18 @@ fun Canvas.drawBRDSNode(i : Int, scale : Float, paint : Paint) {
     drawBlockRotDownSq(scale, w, h, paint)
 }
 
+class BlockRotDownSqView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
