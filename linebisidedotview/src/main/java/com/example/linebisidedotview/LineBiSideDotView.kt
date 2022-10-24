35380aa8ffebc7a5b1d9ea1a1669f0b9595a01d3
Anwesh43 <anweshthecool0@gmail.com>
2022-10-24 15:43:28 +0530

    linebisidedotview: adding constants and libraries

---
+++ linebisidedotview/src/main/java/com/example/linebisidedotview/LineBiSideDotView.kt
@@ -0,0 +1,28 @@
+package com.example.linebisidedotview
+
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.9f
+val rFactor : Float = 27.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
