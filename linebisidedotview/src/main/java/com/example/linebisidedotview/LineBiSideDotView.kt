734e06a0b7126f7fd2295d04588d1eb016f89b7f
Anwesh43 <anweshthecool0@gmail.com>
2022-10-24 15:57:47 +0530

    linebisidedotview: adding LineBiSideDotView class

--- linebisidedotview/src/main/java/com/example/linebisidedotview/LineBiSideDotView.kt
+++ linebisidedotview/src/main/java/com/example/linebisidedotview/LineBiSideDotView.kt
@@ -69,3 +69,18 @@ fun Canvas.drawLBSDNode(i : Int, scale : Float, paint : Paint) {
     drawLineBiSideDot(scale, w, h, paint)
 }
 
+class LineBiSideDotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
