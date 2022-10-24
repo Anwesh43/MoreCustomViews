a9486f981b1b554f7ec7db55fa8fee657c8cd99c
Anwesh43 <anweshthecool0@gmail.com>
2022-10-24 15:56:26 +0530

    linebisidedotview: adding Canvas extension methods

--- linebisidedotview/src/main/java/com/example/linebisidedotview/LineBiSideDotView.kt
+++ linebisidedotview/src/main/java/com/example/linebisidedotview/LineBiSideDotView.kt
@@ -30,3 +30,42 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBiSideDot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        drawXY(0f, 0f) {
+            for (j in 0..1) {
+                scale(1f - 2 * j, 1f)
+                drawXY(-size / 2, -size / 2 - (h / 2 + size) * dsc(3)) {
+                    drawLine(0f, 0f, 0f, size * dsc(0), paint)
+                    drawXY(size / 4, size / 4) {
+                        drawArc(RectF(-r, -r, r, r), 0f, 360f * dsc(1), true, paint)
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBSDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBiSideDot(scale, w, h, paint)
+}
+
