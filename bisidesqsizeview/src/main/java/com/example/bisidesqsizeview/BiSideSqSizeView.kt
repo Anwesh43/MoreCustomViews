8c5e2a4d56f0ee918d9545b740cd63fe2ddc5367
Anwesh43 <anweshthecool0@gmail.com>
2022-10-25 03:25:45 +0530

    bisidesqsizeview: adding Canvas extension methods

--- bisidesqsizeview/src/main/java/com/example/bisidesqsizeview/BiSideSqSizeView.kt
+++ bisidesqsizeview/src/main/java/com/example/bisidesqsizeview/BiSideSqSizeView.kt
@@ -29,3 +29,35 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBiSideSqSize(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(size / 2, 0f)
+                drawLine(0f, 0f, 0f, -size / 4 - size * 0.25f * dsc(0) * j, paint)
+                drawRect(RectF(0f, -size * 0.25f * (1 + j), size * 0.5f * dsc(1), 0f), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBSSSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiSideSqSize(scale, w, h, paint)
+}
