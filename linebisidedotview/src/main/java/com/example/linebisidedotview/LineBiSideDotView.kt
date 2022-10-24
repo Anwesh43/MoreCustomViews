c1e034d428953a7da295f03dda965631f27e6d57
Anwesh43 <anweshthecool0@gmail.com>
2022-10-24 16:03:38 +0530

    linebisidedotview: adding LBSDNode class

--- linebisidedotview/src/main/java/com/example/linebisidedotview/LineBiSideDotView.kt
+++ linebisidedotview/src/main/java/com/example/linebisidedotview/LineBiSideDotView.kt
@@ -131,4 +131,45 @@ class LineBiSideDotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBSDNode(var i : Int, val state : State = State()) {
+
+        private var next : LBSDNode? = null
+        private var prev : LBSDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBSDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBSDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBSDNode {
+            var curr : LBSDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
