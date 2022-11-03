e18e483f9095475fad1028956cd749e7fa44976f
Anwesh43 <anweshthecool0@gmail.com>
2022-11-03 15:30:31 +0530

    rotlineabovebarview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.bisidesqsizeview.BiSideSqSizeView
+import com.example.rotlineabovebarview.RotLineAboveBarView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-        BiSideSqSizeView.create(this)
+        RotLineAboveBarView.create(this)
         fullScreen()
     }
 }
