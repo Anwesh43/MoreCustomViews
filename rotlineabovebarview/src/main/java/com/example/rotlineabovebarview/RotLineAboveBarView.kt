2cc15c2a49ca5b4cfd42f473267373b78de1359a
Anwesh43 <anweshthecool0@gmail.com>
2022-11-03 14:33:15 +0530

    rotlineabovebarview: adding Canvas extension methods

--- rotlineabovebarview/src/main/java/com/example/rotlineabovebarview/RotLineAboveBarView.kt
+++ rotlineabovebarview/src/main/java/com/example/rotlineabovebarview/RotLineAboveBarView.kt
@@ -31,3 +31,42 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRotLineAboveBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val barH : Float = Math.min(w, h) / barHFactor
+    drawXY(w / 2 + (w / 2 + barH) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(size / 2, 0f) {
+                    rotate(deg * dsc(2))
+                    drawLine(0f, 0f, 0f, -barH * dsc(0), paint)
+                }
+            }
+        }
+        drawXY(0f, h * 0.5f * (1 - dsc(1))) {
+            drawRect(RectF(-size / 2, 0f, size / 2, barH), paint)
+        }
+    }
+}
+
+fun Canvas.drawRLABNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRotLineAboveBar(scale, w, h, paint)
+}
