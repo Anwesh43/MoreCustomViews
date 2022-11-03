dc401a68070c56cc24d9470a5ce2a2888a14c515
Anwesh43 <anweshthecool0@gmail.com>
2022-11-03 14:20:15 +0530

    rotlineabovebarview: adding Int and Float extension methods

--- rotlineabovebarview/src/main/java/com/example/rotlineabovebarview/RotLineAboveBarView.kt
+++ rotlineabovebarview/src/main/java/com/example/rotlineabovebarview/RotLineAboveBarView.kt
@@ -27,3 +27,7 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = -90f
 val barHFactor : Float = 11.2f
 val deg : Float = 45f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
