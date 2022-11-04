bf8178cc88cb5d606488b265f7801f5f4c0a9fa9
Anwesh43 <anweshthecool0@gmail.com>
2022-11-04 10:35:08 +0530

    arcsweeplinemoveview: adding Canvas extension methods

--- arcsweeplinemoveview/src/main/java/com/example/arcsweeplinemoveview/ArcSweepLineMoveView.kt
+++ arcsweeplinemoveview/src/main/java/com/example/arcsweeplinemoveview/ArcSweepLineMoveView.kt
@@ -31,3 +31,38 @@ val lSizeFactor : Float = 11.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcSweepLineMove(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val lSize : Float = Math.min(w, h) / lSizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2 + (w  / 2) * dsc(3), h / 2) {
+        drawArc(
+            RectF(-size / 2, -size / 2, size / 2, size / 2),
+            -rot,
+            sweep * dsc(0),
+            true,
+            paint
+        )
+        drawXY(0f, -size / 2) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, 0f, -lSize * dsc(1), paint)
+        }
+    }
+}
+
+fun Canvas.drawASLMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcSweepLineMove(scale, w, h, paint)
+}
