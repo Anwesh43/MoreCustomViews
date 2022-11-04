0d696e0147d964161c4fe7e58e35cb8c53e454e6
Anwesh43 <anweshthecool0@gmail.com>
2022-11-04 10:36:07 +0530

    arcsweeplinemoveview: adding ArcSweepLineMoveView class

--- arcsweeplinemoveview/src/main/java/com/example/arcsweeplinemoveview/ArcSweepLineMoveView.kt
+++ arcsweeplinemoveview/src/main/java/com/example/arcsweeplinemoveview/ArcSweepLineMoveView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawASLMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcSweepLineMove(scale, w, h, paint)
 }
+
+class ArcSweepLineMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
