e2df390c6a0618807e113def7ff8afb516a4d421
Anwesh43 <anweshthecool0@gmail.com>
2022-11-04 10:44:15 +0530

    arcsweeplinemoveview: adding ALSMNode class

--- arcsweeplinemoveview/src/main/java/com/example/arcsweeplinemoveview/ArcSweepLineMoveView.kt
+++ arcsweeplinemoveview/src/main/java/com/example/arcsweeplinemoveview/ArcSweepLineMoveView.kt
@@ -129,4 +129,45 @@ class ArcSweepLineMoveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALSMNode(var i : Int, val state : State = State()) {
+
+        private var next : ALSMNode? = null
+        private var prev: ALSMNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALSMNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawASLMNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALSMNode {
+            var curr : ALSMNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
