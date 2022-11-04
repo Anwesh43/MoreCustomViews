f4099c98be9652fb7fcd7bcfd89dd5c73f800f8c
Anwesh43 <anweshthecool0@gmail.com>
2022-11-04 10:29:45 +0530

    arcsweeplinemoveview: adding Int and Float extension methods

--- arcsweeplinemoveview/src/main/java/com/example/arcsweeplinemoveview/ArcSweepLineMoveView.kt
+++ arcsweeplinemoveview/src/main/java/com/example/arcsweeplinemoveview/ArcSweepLineMoveView.kt
@@ -27,3 +27,7 @@ val sizeFactor : Float = 4.9f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val sweep : Float = 180f
 val lSizeFactor : Float = 11.2f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
