d273b897354efcd3c2fdc319bf59994e4d2d2db8
Anwesh43 <anweshthecool0@gmail.com>
2022-11-05 12:26:55 +0530

    doublesquarealtrotview: adding constants and libraries

---
+++ doublesquarealtrotview/src/main/java/com/example/doublesquarealtrotview/DoubleSquareAltRotView.kt
@@ -0,0 +1,27 @@
+package com.example.doublesquarealtrotview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#AB47BC",
+    "#1E88E5",
+    "#00E678",
+    "#FF3D00",
+    "#3D5AFE"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 3.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBBD")
+val rot : Float = 180f
+val barSizeFactor : Float = 6.2f
