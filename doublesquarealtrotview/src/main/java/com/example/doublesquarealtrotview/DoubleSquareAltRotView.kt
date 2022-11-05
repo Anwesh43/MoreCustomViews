8eb745499b33af292c95c3d12e4522903f6ced8d
Anwesh43 <anweshthecool0@gmail.com>
2022-11-05 13:23:30 +0530

    doublesqurealtrotview: adding fixes

--- doublesquarealtrotview/src/main/java/com/example/doublesquarealtrotview/DoubleSquareAltRotView.kt
+++ doublesquarealtrotview/src/main/java/com/example/doublesquarealtrotview/DoubleSquareAltRotView.kt
@@ -22,9 +22,9 @@ val parts : Int = 4
 val scGap : Float = 0.04f / parts
 val sizeFactor : Float = 3.9f
 val delay : Long = 20
-val backColor : Int = Color.parseColor("#BDBDBBD")
+val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 180f
-val barSizeFactor : Float = 6.2f
+val barSizeFactor : Float = 4.2f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
