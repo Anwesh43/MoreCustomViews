5d99b66ea108783f25e4e9bb639e18af96458a71
Anwesh43 <anweshthecool0@gmail.com>
2022-11-05 12:39:40 +0530

    doublesquarealtrotview: adding Canvas extension methods

--- doublesquarealtrotview/src/main/java/com/example/doublesquarealtrotview/DoubleSquareAltRotView.kt
+++ doublesquarealtrotview/src/main/java/com/example/doublesquarealtrotview/DoubleSquareAltRotView.kt
@@ -29,3 +29,37 @@ val barSizeFactor : Float = 6.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawDoubleSquareAltRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val barSize : Float = size / barSizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    val upBarSize : Float = barSize * dsc(0)
+    val a : Float = (size / 2 - barSize / 2) * (1 - dsc(2))
+    drawXY(w / 2, h / 2 + (h / 2 + barSize) * dsc(3)) {
+        rotate(rot * dsc(1))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f - 2 * j)
+                drawXY(a, a) {
+                    drawRect(RectF(-upBarSize / 2, -upBarSize / 2, upBarSize / 2, upBarSize / 2), paint)
+                }
+            }
+        }
+
+    }
+}
+
+fun Canvas.drawDSARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawDoubleSquareAltRot(scale, w, h, paint)
+}
