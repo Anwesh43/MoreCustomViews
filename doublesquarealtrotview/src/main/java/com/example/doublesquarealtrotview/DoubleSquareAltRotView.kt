499cd41396613749373791738263105eb33c7964
Anwesh43 <anweshthecool0@gmail.com>
2022-11-05 12:56:30 +0530

    doublesquarealtrotview: adding DSARNode class

--- doublesquarealtrotview/src/main/java/com/example/doublesquarealtrotview/DoubleSquareAltRotView.kt
+++ doublesquarealtrotview/src/main/java/com/example/doublesquarealtrotview/DoubleSquareAltRotView.kt
@@ -126,4 +126,45 @@ class DoubleSquareAltRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DSARNode(var i : Int, val state : State = State()) {
+
+        private var next : DSARNode? = null
+        private var prev : DSARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DSARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDSARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DSARNode {
+            var curr : DSARNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
