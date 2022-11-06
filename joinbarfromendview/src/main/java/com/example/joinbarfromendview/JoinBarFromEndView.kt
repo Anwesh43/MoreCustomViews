30cc99dd178f95852e1fda553966296c21c3197f
Anwesh43 <anweshthecool0@gmail.com>
2022-11-06 09:48:19 +0530

    joinbarfromendview: adding constants and libraries

---
+++ joinbarfromendview/src/main/java/com/example/joinbarfromendview/JoinBarFromEndView.kt
@@ -0,0 +1,26 @@
+package com.example.joinbarfromendview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+
+val colors : Array<Int> = arrayOf(
+    "#AB47BC",
+    "#1E88E5",
+    "#00E678",
+    "#FF3D00",
+    "#3D5AFE"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 5.2f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val rot : Float = 90f
