c666820f03a573df77b12128b096b35a0ca3df40
Anwesh43 <anweshthecool0@gmail.com>
2022-11-06 10:13:35 +0530

    joinbarfromendview: adding Canvas extension methods

--- joinbarfromendview/src/main/java/com/example/joinbarfromendview/JoinBarFromEndView.kt
+++ joinbarfromendview/src/main/java/com/example/joinbarfromendview/JoinBarFromEndView.kt
@@ -29,3 +29,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawJoinBarFromEnd(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2, h / 2 + (h / 2 + w / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            scale(1f - 2 * j, 1f - 2 * j)
+            drawXY(0f, (h / 2 - size / 2) * (1 - dsc(1))) {
+                drawRect(RectF(w / 2 - (w / 2) * dsc(0), -size / 2, w / 2, size / 2), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawJBFENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawJoinBarFromEnd(scale, w, h, paint)
+}
