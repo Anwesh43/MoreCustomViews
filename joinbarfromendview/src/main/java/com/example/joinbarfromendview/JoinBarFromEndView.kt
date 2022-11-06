d96d426affb651c1eb55465255938cc0ffa3e7c5
Anwesh43 <anweshthecool0@gmail.com>
2022-11-06 10:59:50 +0530

    joinbarfromendview: adding JBFENode class

--- joinbarfromendview/src/main/java/com/example/joinbarfromendview/JoinBarFromEndView.kt
+++ joinbarfromendview/src/main/java/com/example/joinbarfromendview/JoinBarFromEndView.kt
@@ -119,4 +119,45 @@ class JoinBarFromEndView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class JBFENode(var i : Int, val state : State = State()) {
+
+        private var next : JBFENode? = null
+        private var prev : JBFENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = JBFENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawJBFENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : JBFENode {
+            var curr : JBFENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
