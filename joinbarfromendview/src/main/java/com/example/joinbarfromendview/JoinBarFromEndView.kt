c29093a6ac9262a82148fdeda4a340b9ec197730
Anwesh43 <anweshthecool0@gmail.com>
2022-11-06 10:14:56 +0530

    joinbarfromendview: adding JoinBarFromEndView class

--- joinbarfromendview/src/main/java/com/example/joinbarfromendview/JoinBarFromEndView.kt
+++ joinbarfromendview/src/main/java/com/example/joinbarfromendview/JoinBarFromEndView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawJBFENode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawJoinBarFromEnd(scale, w, h, paint)
 }
+
+class JoinBarFromEndView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
