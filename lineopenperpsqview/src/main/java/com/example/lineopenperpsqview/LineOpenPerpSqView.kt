1ed81c403bc5b2ba8c7da11b6fd1c32e12108aa4
Anwesh43 <anweshthecool0@gmail.com>
2022-11-07 09:15:10 +0530

    lineopenperpsqview: adding LineOpenPerpSqView class

--- lineopenperpsqview/src/main/java/com/example/lineopenperpsqview/LineOpenPerpSqView.kt
+++ lineopenperpsqview/src/main/java/com/example/lineopenperpsqview/LineOpenPerpSqView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawLOPSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineOpenPerpSq(scale, w, h, paint)
 }
+
+class LineOpenPerpSqView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
