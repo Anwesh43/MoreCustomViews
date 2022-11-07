acfc08d95cd3a096b8d5957b5d695ec6d4ffbbcd
Anwesh43 <anweshthecool0@gmail.com>
2022-11-07 09:28:43 +0530

    lineopenperpsqview: adding Renderer class

--- lineopenperpsqview/src/main/java/com/example/lineopenperpsqview/LineOpenPerpSqView.kt
+++ lineopenperpsqview/src/main/java/com/example/lineopenperpsqview/LineOpenPerpSqView.kt
@@ -192,4 +192,27 @@ class LineOpenPerpSqView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineOpenPerpSqView) {
+
+        private val animator : Animator = Animator(view)
+        private val lops : LineOpenPerpSquare = LineOpenPerpSquare(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lops.draw(canvas, paint)
+            animator.animate {
+                lops.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lops.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
