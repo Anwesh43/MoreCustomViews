0a78853baf6ed487cc647614a6bacf0ef680f701
Anwesh43 <anweshthecool0@gmail.com>
2022-11-08 10:31:23 +0530

    twointoonedroplineview: adding fixes

--- twointoonedroplineview/src/main/java/com/example/twointoonedroplineview/TwoIntoOneDropLineView.kt
+++ twointoonedroplineview/src/main/java/com/example/twointoonedroplineview/TwoIntoOneDropLineView.kt
@@ -29,6 +29,13 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawLineWithoutDot(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+        return
+    }
+    drawLine(x1, y1, x2, y2, paint)
+}
+
 fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
     save()
     translate(x, y)
@@ -201,7 +208,7 @@ class TwoIntoOneDropLineView(ctx : Context) : View(ctx) {
             tiodl.draw(canvas, paint)
             animator.animate {
                 tiodl.update {
-
+                    animator.stop()
                 }
             }
         }
