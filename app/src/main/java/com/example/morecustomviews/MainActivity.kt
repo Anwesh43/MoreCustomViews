83fa0dcafe6195e12de854c5a7b4195b77d0a82d
Anwesh43 <anweshthecool0@gmail.com>
2022-11-08 07:42:43 +0530

    lineopenperpsqview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.joinbarfromendview.JoinBarFromEndView
+import com.example.lineopenperpsqview.LineOpenPerpSqView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
-        JoinBarFromEndView.create(this)
+        LineOpenPerpSqView.create(this)
         fullScreen()
     }
 }
