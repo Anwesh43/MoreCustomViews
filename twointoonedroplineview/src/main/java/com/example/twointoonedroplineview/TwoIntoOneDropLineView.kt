8a8aea6bb19a7bb4278326965c58d87700135d18
Anwesh43 <anweshthecool0@gmail.com>
2022-11-08 10:08:46 +0530

    twointoonedroplineview: adding TIODLNode class

--- twointoonedroplineview/src/main/java/com/example/twointoonedroplineview/TwoIntoOneDropLineView.kt
+++ twointoonedroplineview/src/main/java/com/example/twointoonedroplineview/TwoIntoOneDropLineView.kt
@@ -123,4 +123,45 @@ class TwoIntoOneDropLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TIODLNode(var i : Int, val state : State = State()) {
+
+        private var prev : TIODLNode? = null
+        private var next : TIODLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TIODLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTIODLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TIODLNode {
+            var curr : TIODLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
