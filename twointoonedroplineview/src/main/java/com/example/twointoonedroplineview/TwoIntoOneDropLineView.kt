fdf833e605d98883640c596ff87409e8c1d745e5
Anwesh43 <anweshthecool0@gmail.com>
2022-11-08 09:48:16 +0530

    twointoonedroplineview: adding Canvas extension methods

--- twointoonedroplineview/src/main/java/com/example/twointoonedroplineview/TwoIntoOneDropLineView.kt
+++ twointoonedroplineview/src/main/java/com/example/twointoonedroplineview/TwoIntoOneDropLineView.kt
@@ -28,3 +28,35 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTwoIntoOneDropLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * (1f - 2 * j) * dsc(1))
+                drawLine(0f, 0f, 0f, -size * (dsc(0) - dsc(2)), paint)
+            }
+            drawXY(0f, (h / 2) * dsc(3)) {
+                drawLine(0f, 0f, 0f, size * dsc(2), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawTIODLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTwoIntoOneDropLine(scale, w, h, paint)
+}
