4b0cfa21cb0a4d8b7cb16014cc035f0e4be7d366
Anwesh43 <anweshthecool0@gmail.com>
2022-11-09 12:45:06 +0530

    barcreatroelinerotview: adding BCLRNode class

--- barcreatorlinerotview/src/main/java/com/example/barcreatorlinerotview/BarCreatorLineRotView.kt
+++ barcreatorlinerotview/src/main/java/com/example/barcreatorlinerotview/BarCreatorLineRotView.kt
@@ -123,4 +123,45 @@ class BarCreatorLineRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BCLRNode(var i : Int, val state : State = State()) {
+
+        private var next : BCLRNode? = null
+        private var prev : BCLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BCLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBCLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BCLRNode {
+            var curr : BCLRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
