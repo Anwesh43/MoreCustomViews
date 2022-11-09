0a49842dc8d6a465a27b12573305fb64a87592d7
Anwesh43 <anweshthecool0@gmail.com>
2022-11-09 12:29:22 +0530

    barcreatroelinerotview: adding BarCreatorRotLineView class

--- barcreatorlinerotview/src/main/java/com/example/barcreatorlinerotview/BarCreatorLineRotView.kt
+++ barcreatorlinerotview/src/main/java/com/example/barcreatorlinerotview/BarCreatorLineRotView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawBCLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBarCreatorLineRot(scale, w, h, paint)
 }
+
+class BarCreatorLineRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
