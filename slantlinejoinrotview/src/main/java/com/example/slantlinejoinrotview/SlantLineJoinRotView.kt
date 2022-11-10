4803511f39e6c7d7576a9c22db83a8faf69f0d72
Anwesh43 <anweshthecool0@gmail.com>
2022-11-10 13:19:01 +0530

    slantlinejoinrotview: adding SlantLineJoinRot class

--- slantlinejoinrotview/src/main/java/com/example/slantlinejoinrotview/SlantLineJoinRotView.kt
+++ slantlinejoinrotview/src/main/java/com/example/slantlinejoinrotview/SlantLineJoinRotView.kt
@@ -169,4 +169,27 @@ class SlantLineJoinRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SlantLineJoinRot(var i : Int, val state : State = State()) {
+
+        private var curr : SLJRNode = SLJRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
