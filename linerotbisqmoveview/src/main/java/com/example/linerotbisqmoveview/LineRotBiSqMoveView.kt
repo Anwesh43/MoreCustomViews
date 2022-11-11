47a53bb64682cc8df43a14b459ae50921ff3e6ae
Anwesh43 <anweshthecool0@gmail.com>
2022-11-11 08:02:33 +0530

    linerotbisqmoveview: adding LineRotBiSqMoveView class

--- linerotbisqmoveview/src/main/java/com/example/linerotbisqmoveview/LineRotBiSqMoveView.kt
+++ linerotbisqmoveview/src/main/java/com/example/linerotbisqmoveview/LineRotBiSqMoveView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLRBSMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotBiSqMove(scale, w, h, paint)
 }
+
+class LineRotBiSqMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
