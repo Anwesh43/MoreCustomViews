187864ddc53e1ca53e25e925573d9d68028fcea7
Anwesh43 <anweshthecool0@gmail.com>
2022-11-11 08:50:46 +0530

    linerotbisqmoveview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.slantlinejoinrotview.SlantLineJoinRotView
+import com.example.linerotbisqmoveview.LineRotBiSqMoveView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        SlantLineJoinRotView.create(this)
+        LineRotBiSqMoveView.create(this)
         fullScreen()
     }
 }
