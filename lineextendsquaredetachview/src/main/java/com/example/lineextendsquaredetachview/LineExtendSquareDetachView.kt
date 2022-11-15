c05633e4ce43a45c45968f1c9f80681b166cd397
Anwesh43 <anweshthecool0@gmail.com>
2022-11-15 09:37:11 +0530

    lineextendsquaredetachview: adding LESDNode class

--- lineextendsquaredetachview/src/main/java/com/example/lineextendsquaredetachview/LineExtendSquareDetachView.kt
+++ lineextendsquaredetachview/src/main/java/com/example/lineextendsquaredetachview/LineExtendSquareDetachView.kt
@@ -130,4 +130,45 @@ class LineExtendSquareDetachView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LESDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LESDNode? = null
+        private var prev : LESDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LESDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLESDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LESDNode {
+            var curr : LESDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
