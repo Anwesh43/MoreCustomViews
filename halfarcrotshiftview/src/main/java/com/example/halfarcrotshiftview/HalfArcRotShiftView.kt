87ad9e299789d53fde6f657cb6585f65bc1653cf
Anwesh43 <anweshthecool0@gmail.com>
2022-11-18 08:37:05 +0530

    halfarcrotshiftview: adding HalfArcRotShiftView class

--- halfarcrotshiftview/src/main/java/com/example/halfarcrotshiftview/HalfArcRotShiftView.kt
+++ halfarcrotshiftview/src/main/java/com/example/halfarcrotshiftview/HalfArcRotShiftView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawHARSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawHalfArcRotShift(scale, w, h, paint)
 }
+
+class HalfArcRotShiftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
