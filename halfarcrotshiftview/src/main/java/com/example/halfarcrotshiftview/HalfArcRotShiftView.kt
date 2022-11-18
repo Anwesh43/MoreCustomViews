5aff1ac5a7f62189699dfd0d2ff6937d6aded086
Anwesh43 <anweshthecool0@gmail.com>
2022-11-18 08:34:29 +0530

    halfarcrotshiftview: adding Canvas extension methods

--- halfarcrotshiftview/src/main/java/com/example/halfarcrotshiftview/HalfArcRotShiftView.kt
+++ halfarcrotshiftview/src/main/java/com/example/halfarcrotshiftview/HalfArcRotShiftView.kt
@@ -25,7 +25,38 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val rot : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawHalfArcRotShift(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(-rot * dsc(1))
+            drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 0f, deg * dsc(0), true, paint)
+        }
+        drawXY(-w / 2 + (w / 2) * dsc(2), 0f) {
+            drawLine(0f, 0f, -size, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawHARSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawHalfArcRotShift(scale, w, h, paint)
+}
