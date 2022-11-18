9931d496111c19efae2ac46e75da1b8ba063caa9
Anwesh43 <anweshthecool0@gmail.com>
2022-11-18 08:51:58 +0530

    halfarcrotshiftview: adding Renderer class

--- halfarcrotshiftview/src/main/java/com/example/halfarcrotshiftview/HalfArcRotShiftView.kt
+++ halfarcrotshiftview/src/main/java/com/example/halfarcrotshiftview/HalfArcRotShiftView.kt
@@ -187,4 +187,27 @@ class HalfArcRotShiftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : HalfArcRotShiftView) {
+
+        private val animator : Animator = Animator(view)
+        private val hars : HalfArcRotShift = HalfArcRotShift(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            hars.draw(canvas, paint)
+            animator.animate {
+                hars.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            hars.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
