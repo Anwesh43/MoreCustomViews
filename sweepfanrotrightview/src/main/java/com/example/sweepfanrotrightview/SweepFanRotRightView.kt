d54cf6204014bd7a0cb4beaf3fb0f8a05c6bf070
Anwesh43 <anweshthecool0@gmail.com>
2022-11-19 11:05:58 +0530

    sweepfanrotrightview: adding SFRRNode class

--- sweepfanrotrightview/src/main/java/com/example/sweepfanrotrightview/SweepFanRotRightView.kt
+++ sweepfanrotrightview/src/main/java/com/example/sweepfanrotrightview/SweepFanRotRightView.kt
@@ -128,4 +128,45 @@ class SweepFanRotRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SFRRNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : SFRRNode? = null
+        private var next : SFRRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SFRRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSFRRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SFRRNode {
+            var curr : SFRRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
