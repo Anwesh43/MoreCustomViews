94bfc0be07140718a93579ace725d49098327c0f
Anwesh43 <anweshthecool0@gmail.com>
2022-11-21 08:54:04 +0530

    expandsemicriclesqview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linesteplinesrotview.LineStepLinesRotView
+import com.example.expandsemicirclesqview.ExpandSemiCircleSqView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineStepLinesRotView.create(this)
+        ExpandSemiCircleSqView.create(this)
         fullScreen()
     }
 }
