e7ef7373aa8350e682b9a5e0ed008ae25a2f8b79
Anwesh43 <anweshthecool0@gmail.com>
2022-11-21 08:29:55 +0530

    expandsemicriclesqview: adding Canvas extension methods

--- expandsemicirclesqview/src/main/java/com/example/expandsemicirclesqview/ExpandSemiCircleSqView.kt
+++ expandsemicirclesqview/src/main/java/com/example/expandsemicirclesqview/ExpandSemiCircleSqView.kt
@@ -18,8 +18,9 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
@@ -28,3 +29,37 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawExpandSemiCircleSq(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2, h / 2 + (h / 2 + 2 * size) * dsc(4)) {
+        rotate(rot * dsc(3))
+        drawArc(RectF(0f, -size, 2 * size, size), -90f, 180f * dsc(0), true, paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f, 1f - 2 * j)
+                drawXY(0f, size * dsc(2)) {
+                    drawLine(0f, 0f, -2 * size * dsc(1), 0f, paint)
+                }
+            }
+        }
+        drawRect(RectF(-2 * size, -size * dsc(2), 0f, size * dsc(2)), paint)
+    }
+}
+
+fun Canvas.drawESCSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawExpandSemiCircleSq(scale, w, h, paint)
+}
