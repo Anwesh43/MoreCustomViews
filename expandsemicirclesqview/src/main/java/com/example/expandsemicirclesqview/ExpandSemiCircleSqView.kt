ee32b36dcb9001f2aadf4800aaaabe67b5cb557f
Anwesh43 <anweshthecool0@gmail.com>
2022-11-21 08:31:27 +0530

    expandsemicriclesqview: adding ExpandSemiCircleSqView class

--- expandsemicirclesqview/src/main/java/com/example/expandsemicirclesqview/ExpandSemiCircleSqView.kt
+++ expandsemicirclesqview/src/main/java/com/example/expandsemicirclesqview/ExpandSemiCircleSqView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawESCSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawExpandSemiCircleSq(scale, w, h, paint)
 }
+
+class ExpandSemiCircleSqView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
