923c319ac3c2e0a98ef55ed1648a86dd68ea5d13
Anwesh43 <anweshthecool0@gmail.com>
2022-11-22 09:19:34 +0530

    sqabovearcrotview: adding SqAboveArcRotView class

--- sqabovearcrotview/src/main/java/com/example/sqabovearcrotview/SqAboveArcRotView.kt
+++ sqabovearcrotview/src/main/java/com/example/sqabovearcrotview/SqAboveArcRotView.kt
@@ -56,3 +56,19 @@ fun Canvas.drawSARNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawSqAboveRot(scale,w , h, paint)
 }
+
+class SqAboveArcRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
