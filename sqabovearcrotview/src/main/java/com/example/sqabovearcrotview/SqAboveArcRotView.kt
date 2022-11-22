d00688da8801355c68a993f0753c66185ceefa23
Anwesh43 <anweshthecool0@gmail.com>
2022-11-22 11:03:20 +0530

    sqabovearcrotview: adding Renderer class

--- sqabovearcrotview/src/main/java/com/example/sqabovearcrotview/SqAboveArcRotView.kt
+++ sqabovearcrotview/src/main/java/com/example/sqabovearcrotview/SqAboveArcRotView.kt
@@ -183,4 +183,27 @@ class SqAboveArcRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SqAboveArcRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val saar : SqAboveArcRot = SqAboveArcRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            saar.draw(canvas, paint)
+            animator.animate {
+                saar.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            saar.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
