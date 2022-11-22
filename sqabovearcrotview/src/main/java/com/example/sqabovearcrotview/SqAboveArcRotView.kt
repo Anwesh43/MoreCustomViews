0d7c8ac5fec69e4326b6ce0b31f8d8022ea142a3
Anwesh43 <anweshthecool0@gmail.com>
2022-11-22 09:18:35 +0530

    sqabovearcrotview: adding Canvas extension methods

--- sqabovearcrotview/src/main/java/com/example/sqabovearcrotview/SqAboveArcRotView.kt
+++ sqabovearcrotview/src/main/java/com/example/sqabovearcrotview/SqAboveArcRotView.kt
@@ -24,7 +24,35 @@ val rot : Float = 90f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val sizeFactor : Float = 4.9f
+val sqFactor : Float = 5.2f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqAboveRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    val sqSize : Float = size / sqFactor
+    drawXY(w / 2 + (w / 2 + size) * dsc(4), h / 2) {
+        rotate(-rot * dsc(3))
+        drawArc(RectF(-size, -size, size, size), 0f, 180f * dsc(0), true, paint)
+        drawXY(-size + (size - sqSize / 2) * dsc(2), 0f) {
+            drawRect(RectF(0f, -sqSize * dsc(1), sqSize, 0f), paint)
+        }
+    }
+}
+
+fun Canvas.drawSARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawSqAboveRot(scale,w , h, paint)
+}
