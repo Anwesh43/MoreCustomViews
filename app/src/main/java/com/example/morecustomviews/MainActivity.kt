b59b980d0d8cff9602d79a988624d012316b8a9f
Anwesh43 <anweshthecool0@gmail.com>
2022-11-23 09:01:45 +0530

    trilinesqrotview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.sqabovearcrotview.SqAboveArcRotView
+import com.example.trilinesqrotview.TriLineSqRotView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        SqAboveArcRotView.create(this)
+        TriLineSqRotView.create(this)
         fullScreen()
     }
 }
