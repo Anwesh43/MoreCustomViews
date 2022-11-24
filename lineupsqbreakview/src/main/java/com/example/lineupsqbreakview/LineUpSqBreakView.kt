8c7f4b9e74b2836706041a959d17e9db2a892410
Anwesh43 <anweshthecool0@gmail.com>
2022-11-24 08:21:51 +0530

    lineupsqbreakview: adding Canvas extension methods

--- lineupsqbreakview/src/main/java/com/example/lineupsqbreakview/LineUpSqBreakView.kt
+++ lineupsqbreakview/src/main/java/com/example/lineupsqbreakview/LineUpSqBreakView.kt
@@ -30,3 +30,36 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineUpSqBreak(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    val barW : Float = Math.min(w, h) / barWFactor
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(2) * j)
+                drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+                drawXY(-size, 0f) {
+                    drawRect(RectF(0f, -size * dsc(1), barW, 0f), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawLUSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineUpSqBreak(scale, w, h, paint)
+}
