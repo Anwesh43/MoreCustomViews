32f5f6e98cc7c320984160e5f8f0e8bb0a91b4af
Anwesh43 <anweshthecool0@gmail.com>
2022-11-25 15:55:56 +0530

    tbarbreakdownview: adding TBBDNode class

--- tbarbreakdownview/src/main/java/com/example/tbarbreakdownview/TBarBreakDownView.kt
+++ tbarbreakdownview/src/main/java/com/example/tbarbreakdownview/TBarBreakDownView.kt
@@ -127,4 +127,45 @@ class TBarBreakDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TBBDNode(var i : Int, val state : State = State()) {
+
+        private var next : TBBDNode? = null
+        private var prev : TBBDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TBBDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTBBDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TBBDNode {
+            var curr : TBBDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
