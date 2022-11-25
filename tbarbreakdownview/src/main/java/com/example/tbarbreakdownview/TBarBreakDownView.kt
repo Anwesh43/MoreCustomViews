882dd5fa5ffa05f96e32d4af931cc8b6140f767d
Anwesh43 <anweshthecool0@gmail.com>
2022-11-25 15:34:48 +0530

    tbarbreakdownview: adding Canvas extension methods

--- tbarbreakdownview/src/main/java/com/example/tbarbreakdownview/TBarBreakDownView.kt
+++ tbarbreakdownview/src/main/java/com/example/tbarbreakdownview/TBarBreakDownView.kt
@@ -29,3 +29,38 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTBarBreakDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY((w / 2) * dsc(3) * (1f - 2 * j), 0f) {
+                scale(1f, 1f - 2 * j)
+                rotate(rot * j * dsc(2))
+                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+                drawXY(size, 0f) {
+                    drawRect(RectF(0f, -size * dsc(1), size, 0f), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawTBBDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTBarBreakDown(scale, w, h, paint)
+}
