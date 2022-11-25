91f0c3dd3eb5c2100be3b28c865a3f173ede3b3e
Anwesh43 <anweshthecool0@gmail.com>
2022-11-25 17:44:39 +0530

    tbarbreakdownview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.lineupsqbreakview.LineSqUpBreakView
-
+import com.example.tbarbreakdownview.TBarBreakDownView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -16,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineSqUpBreakView.create(this)
+        TBarBreakDownView.create(this)
         fullScreen()
     }
 }
