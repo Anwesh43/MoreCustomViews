f046293a659103745a84a6f4041fa6311f722a12
Anwesh43 <anweshthecool0@gmail.com>
2022-11-25 17:38:57 +0530

    tbarbreakdownview: adding TBarBreakDown class

--- tbarbreakdownview/src/main/java/com/example/tbarbreakdownview/TBarBreakDownView.kt
+++ tbarbreakdownview/src/main/java/com/example/tbarbreakdownview/TBarBreakDownView.kt
@@ -168,4 +168,27 @@ class TBarBreakDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class TBarBreakDown(var i : Int) {
+
+        private var curr : TBBDNode = TBBDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
