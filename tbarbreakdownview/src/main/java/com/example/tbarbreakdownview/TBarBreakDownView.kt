54066c022edde03d5690619232d46ea524aaa407
Anwesh43 <anweshthecool0@gmail.com>
2022-11-25 17:42:28 +0530

    tbarbreakdownview: adding static create method

--- tbarbreakdownview/src/main/java/com/example/tbarbreakdownview/TBarBreakDownView.kt
+++ tbarbreakdownview/src/main/java/com/example/tbarbreakdownview/TBarBreakDownView.kt
@@ -215,4 +215,13 @@ class TBarBreakDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : TBarBreakDownView {
+            val view : TBarBreakDownView = TBarBreakDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
