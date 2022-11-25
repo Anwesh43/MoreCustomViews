c550e6d8579d4536b466401f269b43c2d1704d47
Anwesh43 <anweshthecool0@gmail.com>
2022-11-25 17:47:54 +0530

    tbarbreakdownview: adding line fix

--- tbarbreakdownview/src/main/java/com/example/tbarbreakdownview/TBarBreakDownView.kt
+++ tbarbreakdownview/src/main/java/com/example/tbarbreakdownview/TBarBreakDownView.kt
@@ -37,6 +37,13 @@ fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
     restore()
 }
 
+fun Canvas.drawLineWithoutDot(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    if (Math.abs(x1 - x2) < 0.1f && Math.abs(y1 - y2) < 0.1f) {
+        return
+    }
+    drawLine(x1, y1, x2, y2, paint)
+}
+
 fun Canvas.drawTBarBreakDown(scale : Float, w : Float, h : Float, paint : Paint) {
     val size : Float = Math.min(w, h) / sizeFactor
     val dsc : (Int) -> Float = {
@@ -47,7 +54,7 @@ fun Canvas.drawTBarBreakDown(scale : Float, w : Float, h : Float, paint : Paint)
             drawXY((w / 2) * dsc(3) * (1f - 2 * j), 0f) {
                 scale(1f, 1f - 2 * j)
                 rotate(rot * j * dsc(2))
-                drawLine(0f, 0f, size * dsc(0), 0f, paint)
+                drawLineWithoutDot(0f, 0f, size * dsc(0), 0f, paint)
                 drawXY(size, 0f) {
                     drawRect(RectF(0f, -size * dsc(1), size, 0f), paint)
                 }
