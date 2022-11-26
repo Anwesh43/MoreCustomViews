eaf13d5e98290c97706807b2a1d405a1821c59e7
Anwesh43 <anweshthecool0@gmail.com>
2022-11-26 11:21:07 +0530

    squarebiarcsweepview: adding SBASNode class

--- squarebiarcsweepview/src/main/java/com/example/squarebiarcsweepview/SquareBiArcSweepView.kt
+++ squarebiarcsweepview/src/main/java/com/example/squarebiarcsweepview/SquareBiArcSweepView.kt
@@ -131,4 +131,45 @@ class SquareBiArcSweepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBASNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : SBASNode? = null
+        private var next : SBASNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SBASNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBASNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBASNode {
+            var curr : SBASNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
