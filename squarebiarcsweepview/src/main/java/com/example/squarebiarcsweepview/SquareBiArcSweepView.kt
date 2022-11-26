710d495156622179f29e825493830c79dd322b08
Anwesh43 <anweshthecool0@gmail.com>
2022-11-26 11:08:51 +0530

    squarebiarcsweepview: adding SquareBiArcSweepView class

--- squarebiarcsweepview/src/main/java/com/example/squarebiarcsweepview/SquareBiArcSweepView.kt
+++ squarebiarcsweepview/src/main/java/com/example/squarebiarcsweepview/SquareBiArcSweepView.kt
@@ -68,3 +68,19 @@ fun Canvas.drawSBASNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSquareBiArcSweep(scale, w, h, paint)
 }
+
+class SquareBiArcSweepView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
