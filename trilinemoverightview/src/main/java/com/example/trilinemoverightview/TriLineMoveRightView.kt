5ab6571390cf9fb1d09f320bb85f50c5c6861cae
Anwesh43 <anweshthecool0@gmail.com>
2022-11-27 12:18:32 +0530

    trilinemoverightview: adding constants and libraries

---
+++ trilinemoverightview/src/main/java/com/example/trilinemoverightview/TriLineMoveRightView.kt
@@ -0,0 +1,28 @@
+package com.example.trilinemoverightview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val colors : Array<Int> = arrayOf(
+    "#AB47BC",
+    "#1E88E5",
+    "#00E678",
+    "#FF3D00",
+    "#3D5AFE"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
+val rot : Float = 90f
+val scGap : Float = 0.04f / parts
+val deg : Float = 45f
+
