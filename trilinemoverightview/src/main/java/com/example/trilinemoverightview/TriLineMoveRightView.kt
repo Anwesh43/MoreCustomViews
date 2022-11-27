e3e23492ec31084033a27d23b9455f395b4c28a3
Anwesh43 <anweshthecool0@gmail.com>
2022-11-27 12:59:49 +0530

    trilinemoverightview: adding TLMRNode class

--- trilinemoverightview/src/main/java/com/example/trilinemoverightview/TriLineMoveRightView.kt
+++ trilinemoverightview/src/main/java/com/example/trilinemoverightview/TriLineMoveRightView.kt
@@ -127,4 +127,46 @@ class TriLineMoveRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TLMRNode(var i : Int) {
+
+        private var prev : TLMRNode? = null
+        private var next : TLMRNode? = null
+        private val state : State = State()
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = TLMRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTLMRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TLMRNode {
+            var curr : TLMRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
