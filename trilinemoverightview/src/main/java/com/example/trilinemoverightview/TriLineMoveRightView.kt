65f6e80d27756ee4438691532a0abbdef7d9500d
Anwesh43 <anweshthecool0@gmail.com>
2022-11-27 12:51:42 +0530

    trilinemoverightview: adding Canvas extension methods

--- trilinemoverightview/src/main/java/com/example/trilinemoverightview/TriLineMoveRightView.kt
+++ trilinemoverightview/src/main/java/com/example/trilinemoverightview/TriLineMoveRightView.kt
@@ -29,3 +29,38 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTriLineMoveRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = { scale.divideScale(it, parts) }
+    drawXY(w / 2 + (w / 2) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        drawXY(-(w / 2 + size / 2) * (1 - dsc(0)), 0f) {
+            drawLine(-size / 2, 0f, size / 2, 0f, paint)
+        }
+        drawXY(0f, h * 0.5f * (1 - dsc(1))) {
+            for (j in 0..1) {
+                drawXY(0f, 0f) {
+                    rotate(deg * (1f - 2 * j) * dsc(2))
+                    drawLine(0f, 0f, 0f, size, paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawTLMRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTriLineMoveRight(scale, w, h, paint)
+}
