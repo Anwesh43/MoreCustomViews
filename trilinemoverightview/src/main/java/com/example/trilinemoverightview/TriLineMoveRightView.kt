a736c08ad5b48850cc12a09af0e9c512ecf2a28f
Anwesh43 <anweshthecool0@gmail.com>
2022-11-27 13:10:30 +0530

    trilinemoverightview: adding static create method

--- trilinemoverightview/src/main/java/com/example/trilinemoverightview/TriLineMoveRightView.kt
+++ trilinemoverightview/src/main/java/com/example/trilinemoverightview/TriLineMoveRightView.kt
@@ -217,4 +217,13 @@ class TriLineMoveRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : TriLineMoveRightView {
+            val view : TriLineMoveRightView = TriLineMoveRightView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
