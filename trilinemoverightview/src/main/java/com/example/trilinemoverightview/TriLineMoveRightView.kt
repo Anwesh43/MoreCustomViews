aa65302f966761926d4ed878d7a3f2a98054b573
Anwesh43 <anweshthecool0@gmail.com>
2022-11-27 12:53:06 +0530

    trilinemoverightview: adding TriLineMoveRightView class

--- trilinemoverightview/src/main/java/com/example/trilinemoverightview/TriLineMoveRightView.kt
+++ trilinemoverightview/src/main/java/com/example/trilinemoverightview/TriLineMoveRightView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawTLMRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTriLineMoveRight(scale, w, h, paint)
 }
+
+class TriLineMoveRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
