e3048abd5eb9695cf39d88823185f8bfe4cd3f1f
Anwesh43 <anweshthecool0@gmail.com>
2022-11-29 12:23:01 +0530

    linesquarerightdownview: adding LineSquareRightDownView class

--- linesquarerightdownview/src/main/java/com/example/linesquarerightdownview/LineSuqareRightDownView.kt
+++ linesquarerightdownview/src/main/java/com/example/linesquarerightdownview/LineSuqareRightDownView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLSRDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSquareRightDown(scale, w, h, paint)
 }
+
+class LineSquareRightDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
