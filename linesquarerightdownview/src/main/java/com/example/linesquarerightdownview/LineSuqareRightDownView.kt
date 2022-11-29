698698eb0bb0f9f028754faa26ef5fe469ed818c
Anwesh43 <anweshthecool0@gmail.com>
2022-11-29 13:25:38 +0530

    linesquarerightdownview: adding LineSquareRightDown class

--- linesquarerightdownview/src/main/java/com/example/linesquarerightdownview/LineSuqareRightDownView.kt
+++ linesquarerightdownview/src/main/java/com/example/linesquarerightdownview/LineSuqareRightDownView.kt
@@ -165,4 +165,27 @@ class LineSquareRightDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineSquareRightDown(var i : Int) {
+
+        private var curr : LSRDNode = LSRDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
