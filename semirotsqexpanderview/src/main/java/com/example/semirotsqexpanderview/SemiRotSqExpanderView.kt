44263a51b4d637c630fe79e5bb289ea89069e072
Anwesh43 <anweshthecool0@gmail.com>
2022-11-30 14:53:27 +0530

    semirotsqexpanderview: adding SRSENode class

--- semirotsqexpanderview/src/main/java/com/example/semirotsqexpanderview/SemiRotSqExpanderView.kt
+++ semirotsqexpanderview/src/main/java/com/example/semirotsqexpanderview/SemiRotSqExpanderView.kt
@@ -118,4 +118,45 @@ class SemiRotSqExpanderView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SRSENode(var i : Int, val state : State = State()) {
+
+        private var next : SRSENode? = null
+        private var prev : SRSENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SRSENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSRSENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SRSENode {
+            var curr : SRSENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
