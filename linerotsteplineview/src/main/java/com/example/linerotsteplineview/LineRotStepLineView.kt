83656d44e628da969ca2fc57bcb1a7cccec036e6
Anwesh43 <anweshthecool0@gmail.com>
2022-12-01 10:26:00 +0530

    linerotsteplineview: adding Renderer class

--- linerotsteplineview/src/main/java/com/example/linerotsteplineview/LineRotStepLineView.kt
+++ linerotsteplineview/src/main/java/com/example/linerotsteplineview/LineRotStepLineView.kt
@@ -193,4 +193,27 @@ class LineRotStepLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineRotStepLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val lrsl : LineRotStepLine = LineRotStepLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lrsl.draw(canvas, paint)
+            animator.animate {
+                lrsl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lrsl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
