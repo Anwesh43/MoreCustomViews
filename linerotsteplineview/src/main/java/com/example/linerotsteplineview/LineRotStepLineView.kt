7a4dc7dfb6a0925c4d9d7d6d434e0f6732dc99d9
Anwesh43 <anweshthecool0@gmail.com>
2022-12-01 10:03:23 +0530

    linerotsteplineview: adding LineRotStepLineView class

--- linerotsteplineview/src/main/java/com/example/linerotsteplineview/LineRotStepLineView.kt
+++ linerotsteplineview/src/main/java/com/example/linerotsteplineview/LineRotStepLineView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawLRSLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotStepLine(scale, w, h, paint)
 }
+
+class LineRotStepLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
