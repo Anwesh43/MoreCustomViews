11e4ea6afdbec847fe29e24258ce49183600fbc2
Anwesh43 <anweshthecool0@gmail.com>
2022-12-01 10:01:21 +0530

    linerotsteplineview: adding Canvas extension methods

--- linerotsteplineview/src/main/java/com/example/linerotsteplineview/LineRotStepLineView.kt
+++ linerotsteplineview/src/main/java/com/example/linerotsteplineview/LineRotStepLineView.kt
@@ -25,7 +25,44 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
+val lSizeFactor : Float = 6.9f
+val lines : Int = 5
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotStepLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val lGap : Float = size / (lines - 1)
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        }
+        for (j in 0..(lines - 1)) {
+            drawXY(j * lGap, 0f) {
+                drawLine(0f, 0f, 0f, -lGap * scale.divideScale(j, parts), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLRSLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotStepLine(scale, w, h, paint)
+}
