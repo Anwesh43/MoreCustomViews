b07244331d1d808cf10a9b2150f7b35bf353a888
Anwesh43 <anweshthecool0@gmail.com>
2022-12-03 09:23:32 +0530

    linerotbaraboveview: adding constants and libraries

---
+++ linerotbaraboveview/src/main/java/com/example/linerotbaraboveview/LineRotBrAboveView.kt
@@ -0,0 +1,28 @@
+package com.example.linerotbaraboveview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#AB47BC",
+    "#1E88E5",
+    "#00E678",
+    "#FF3D00",
+    "#3D5AFE"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val rot : Float = -90f
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val barHFactor : Float = 7.2f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
