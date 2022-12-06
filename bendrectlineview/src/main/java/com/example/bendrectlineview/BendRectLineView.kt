62e53a876f19a09a56affc6de4670d1182bd7fc4
Anwesh43 <anweshthecool0@gmail.com>
2022-12-06 15:23:04 +0530

    bendrectlineview: adding BRLNode class

--- bendrectlineview/src/main/java/com/example/bendrectlineview/BendRectLineView.kt
+++ bendrectlineview/src/main/java/com/example/bendrectlineview/BendRectLineView.kt
@@ -128,4 +128,45 @@ class BendRectLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BRLNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : BRLNode? = null
+        private var next : BRLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BRLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBRLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BRLNode {
+            var curr : BRLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
