628c00c7f21c8deee12961b82d0ba58a3fba0a58
Anwesh43 <anweshthecool0@gmail.com>
2022-12-06 15:00:05 +0530

    bendrectlineview: adding BendRectLineView class

--- bendrectlineview/src/main/java/com/example/bendrectlineview/BendRectLineView.kt
+++ bendrectlineview/src/main/java/com/example/bendrectlineview/BendRectLineView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawBRLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBendRectLine(scale, w, h, paint)
 }
+
+class BendRectLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
