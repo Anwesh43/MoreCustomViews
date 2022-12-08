549fb8cc9535fef9777c0a16dc6e3092cb323034
Anwesh43 <anweshthecool0@gmail.com>
2022-12-08 16:11:03 +0530

    linerotarcrightview: adding LineRotArcRight class

--- linerotarcrightview/src/main/java/com/example/linerotarcrightview/LineRotArcRightView.kt
+++ linerotarcrightview/src/main/java/com/example/linerotarcrightview/LineRotArcRightView.kt
@@ -154,7 +154,7 @@ class LineRotArcRightView(ctx : Context) : View(ctx) {
             state.startUpdating(cb)
         }
 
-        fun getNex(dir : Int, cb : () -> Unit) : LRARNode {
+        fun getNext(dir : Int, cb : () -> Unit) : LRARNode {
             var curr : LRARNode? = prev
             if (dir == 1) {
                 curr = next
@@ -164,6 +164,29 @@ class LineRotArcRightView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class LineRotArcRight(var i : Int, val state : State = State()) {
+
+        private var curr : LRARNode = LRARNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
