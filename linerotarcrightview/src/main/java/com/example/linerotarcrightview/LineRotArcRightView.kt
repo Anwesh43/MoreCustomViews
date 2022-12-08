f6438cd4fc7c52e3564440d5ff1fda20771e4cec
Anwesh43 <anweshthecool0@gmail.com>
2022-12-08 17:26:07 +0530

    linerotarcrightview: adding static create method

--- linerotarcrightview/src/main/java/com/example/linerotarcrightview/LineRotArcRightView.kt
+++ linerotarcrightview/src/main/java/com/example/linerotarcrightview/LineRotArcRightView.kt
@@ -213,4 +213,12 @@ class LineRotArcRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+   companion object {
+       fun create(activity : Activity) : LineRotArcRightView {
+           val view : LineRotArcRightView = LineRotArcRightView(activity)
+           activity.setContentView(view)
+           return view
+       }
+   }
 }
