bb7fc72b682eda5e07714fb598ebe51e36bc8ab6
Anwesh43 <anweshthecool0@gmail.com>
2022-12-08 11:40:08 +0530

    linerotarcrightview: adding constants and libraries

---
+++ linerotarcrightview/src/main/java/com/example/linerotarcrightview/LineRotArcRightView.kt
@@ -0,0 +1,28 @@
+package com.example.linerotarcrightview
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 5
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val rot : Float = 90f
+val delay : Long = 20
+val deg : Float = 180f
+val backColor : Int = Color.parseColor("#BDBDBD")
