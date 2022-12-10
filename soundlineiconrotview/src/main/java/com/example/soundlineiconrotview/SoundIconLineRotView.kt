26edcf18ede6d4c7c2c6f323b9d52e79595065a1
Anwesh43 <anweshthecool0@gmail.com>
2022-12-10 11:54:38 +0530

    soundlineiconrotview: adding SoundIconLineRot class

--- soundlineiconrotview/src/main/java/com/example/soundlineiconrotview/SoundIconLineRotView.kt
+++ soundlineiconrotview/src/main/java/com/example/soundlineiconrotview/SoundIconLineRotView.kt
@@ -178,4 +178,27 @@ class SoundIconLineRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SoundIconLineRot(var i : Int) {
+
+        private var curr : SILRNode = SILRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
