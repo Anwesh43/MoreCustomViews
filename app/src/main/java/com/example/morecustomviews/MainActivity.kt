16fd9d9576fdbbbb10eab6bf4dafafdcb3adb71c
Anwesh43 <anweshthecool0@gmail.com>
2022-12-10 12:13:13 +0530

    soundlineiconrotview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotarcrightview.LineRotArcRightView
+import com.example.soundlineiconrotview.SoundIconLineRotView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineRotArcRightView.create(this)
+        SoundIconLineRotView.create(this)
         fullScreen()
     }
 }
