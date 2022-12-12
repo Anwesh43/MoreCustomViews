34ff5bb188575664827603b0bf9b4f7c938fc8cd
Anwesh43 <anweshthecool0@gmail.com>
2022-12-12 10:36:25 +0530

    lineextendbarrotview: adding Animator clas

--- lineextendbarrotview/src/main/java/com/example/lineextendbarrotview/LineExtendBarRotView.kt
+++ lineextendbarrotview/src/main/java/com/example/lineextendbarrotview/LineExtendBarRotView.kt
@@ -97,4 +97,33 @@ class LineExtendBarRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun stop() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+    }
 }
