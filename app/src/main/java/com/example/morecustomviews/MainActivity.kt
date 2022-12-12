af74cf795d777e747d4cc2499f7ab0253852af68
Anwesh43 <anweshthecool0@gmail.com>
2022-12-12 11:41:50 +0530

    lineextendbarrotview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.soundlineiconrotview.SoundIconLineRotView
+import com.example.lineextendbarrotview.LineExtendBarRotView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        SoundIconLineRotView.create(this)
+        LineExtendBarRotView.create(this)
         fullScreen()
     }
 }
