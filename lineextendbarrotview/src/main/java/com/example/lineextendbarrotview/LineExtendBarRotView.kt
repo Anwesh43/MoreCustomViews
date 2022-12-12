fc6bc44c71514a97178f726b02c7053b34b5ba85
Anwesh43 <anweshthecool0@gmail.com>
2022-12-12 10:27:42 +0530

    linextendbarrotview: adding LineExtendBarRotView class

--- lineextendbarrotview/src/main/java/com/example/lineextendbarrotview/LineExtendBarRotView.kt
+++ lineextendbarrotview/src/main/java/com/example/lineextendbarrotview/LineExtendBarRotView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawLEBRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawLineExtendBarRot(scale, w, h, paint)
 }
+
+class LineExtendBarRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
