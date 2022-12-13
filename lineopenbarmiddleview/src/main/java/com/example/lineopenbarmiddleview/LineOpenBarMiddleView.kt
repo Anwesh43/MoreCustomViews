8ac4ac948391d97ebb711109c0d05febd4116488
Anwesh43 <anweshthecool0@gmail.com>
2022-12-13 11:32:52 +0530

    lineopenbarmiddleview: adding static create method

--- lineopenbarmiddleview/src/main/java/com/example/lineopenbarmiddleview/LineOpenBarMiddleView.kt
+++ lineopenbarmiddleview/src/main/java/com/example/lineopenbarmiddleview/LineOpenBarMiddleView.kt
@@ -215,4 +215,13 @@ class LineOpenBarMiddleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineOpenBarMiddleView {
+            val view : LineOpenBarMiddleView = LineOpenBarMiddleView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
