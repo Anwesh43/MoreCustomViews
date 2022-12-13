171cb8354f5329b3febcd1885cc8cec7308a978a
Anwesh43 <anweshthecool0@gmail.com>
2022-12-13 11:34:44 +0530

    lineopenbarmiddleview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.lineextendbarrotview.LineExtendBarRotView
+import com.example.lineopenbarmiddleview.LineOpenBarMiddleView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineExtendBarRotView.create(this)
+        LineOpenBarMiddleView.create(this)
         fullScreen()
     }
 }
