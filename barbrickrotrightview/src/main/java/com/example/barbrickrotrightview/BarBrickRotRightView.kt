ea894380e328807046f8e613ed84d8bd5c39f36a
Anwesh43 <anweshthecool0@gmail.com>
2022-12-15 08:48:15 +0530

    barbrickrotrightview: adding Canvas extension methods

--- barbrickrotrightview/src/main/java/com/example/barbrickrotrightview/BarBrickRotRightView.kt
+++ barbrickrotrightview/src/main/java/com/example/barbrickrotrightview/BarBrickRotRightView.kt
@@ -29,3 +29,34 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarBrickRotRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(1))
+            drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        }
+        drawRect(RectF(-size, -size * dsc(0), 0f, 0f), paint)
+        drawRect(RectF(0f, -size * 0.5f * dsc(2), 0f, size), paint)
+    }
+}
+
+fun Canvas.drawBBRRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarBrickRotRight(scale, w, h, paint)
+}
