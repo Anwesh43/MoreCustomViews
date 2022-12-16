97f9eb56de182d7e32ff8d13bacd00c363446f17
Anwesh43 <anweshthecool0@gmail.com>
2022-12-16 07:15:19 +0530

    rightlinesqrightview: adding RightLineSqRight class

--- rightlinesqrightview/src/main/java/com/example/rightlinesqrightview/RightLineSqRightView.kt
+++ rightlinesqrightview/src/main/java/com/example/rightlinesqrightview/RightLineSqRightView.kt
@@ -164,4 +164,27 @@ class RightLineSqRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RightLineSqRight(var i : Int) {
+
+        private var curr : RLSRNode = RLSRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
