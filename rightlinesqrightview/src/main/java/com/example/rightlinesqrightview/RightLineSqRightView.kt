ddaa4321b3518d5ed33f593e2be432667f7db90b
Anwesh43 <anweshthecool0@gmail.com>
2022-12-16 07:12:21 +0530

    rightlinesqrightview: adding RLSRNode class

--- rightlinesqrightview/src/main/java/com/example/rightlinesqrightview/RightLineSqRightView.kt
+++ rightlinesqrightview/src/main/java/com/example/rightlinesqrightview/RightLineSqRightView.kt
@@ -123,4 +123,45 @@ class RightLineSqRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RLSRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RLSRNode? = null
+        private var prev : RLSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RLSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRLSRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RLSRNode {
+            var curr : RLSRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
