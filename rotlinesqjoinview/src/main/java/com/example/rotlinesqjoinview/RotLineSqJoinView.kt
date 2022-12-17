9ce31f2f1235e5664aa636b37b866908c6ec70a8
Anwesh43 <anweshthecool0@gmail.com>
2022-12-17 10:16:59 +0530

    rotlinesqjoinview: adding Renderer class

--- rotlinesqjoinview/src/main/java/com/example/rotlinesqjoinview/RotLineSqJoinView.kt
+++ rotlinesqjoinview/src/main/java/com/example/rotlinesqjoinview/RotLineSqJoinView.kt
@@ -188,4 +188,27 @@ class RotLineSqJoinView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : RotLineSqJoinView) {
+
+        private val animator : Animator = Animator(view)
+        private val rlsj : RotLineSqJoin = RotLineSqJoin(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            rlsj.draw(canvas, paint)
+            animator.animate {
+                rlsj.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            rlsj.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
