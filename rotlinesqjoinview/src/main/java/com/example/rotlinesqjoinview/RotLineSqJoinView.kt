ed84e263b059bafb6c7b956b1e361ea5e65809da
Anwesh43 <anweshthecool0@gmail.com>
2022-12-17 09:34:57 +0530

    rotlinesqjoinview: adding RotLineSqJoinView class

--- rotlinesqjoinview/src/main/java/com/example/rotlinesqjoinview/RotLineSqJoinView.kt
+++ rotlinesqjoinview/src/main/java/com/example/rotlinesqjoinview/RotLineSqJoinView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawRLSJNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRotLineSqJoin(scale, w, h, paint)
 }
+
+class RotLineSqJoinView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
