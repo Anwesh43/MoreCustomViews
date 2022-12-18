b1fd308dba91578152abd965e7c319eed1026136
Anwesh43 <anweshthecool0@gmail.com>
2022-12-18 11:12:50 +0530

    multirotbarextenderview: adding fixes

--- multirotbarextenderview/src/main/java/com/example/multirotbarextenderview/MultiRotBarExenderView.kt
+++ multirotbarextenderview/src/main/java/com/example/multirotbarextenderview/MultiRotBarExenderView.kt
@@ -30,6 +30,13 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawLineWithoutDot(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    if (Math.abs(x1 - x2) < 0.1f && Math.abs(y1 - y2) < 0.1f) {
+        return
+    }
+    drawLine(x1, y1, x2, y2, paint)
+}
+
 fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
     save()
     translate(x, y)
@@ -42,11 +49,13 @@ fun Canvas.drawMultiRotBarExtender(scale : Float, w : Float, h : Float, paint : 
     val dsc : (Int) -> Float = {
         scale.divideScale(it, parts)
     }
-    drawXY(w / 2, h / 2) {
+    drawXY(w / 2 + (w / 2 + size) * dsc(4), h / 2) {
         for (j in 0..1) {
-            scale(1f - 2 * j, 1f)
-            rotate(rot * (1 - j) * (dsc(1) + dsc(3)))
-            drawLine(0f, 0f, size * dsc(0), 0f, paint)
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(rot * (1 - j) * (dsc(3) - dsc(1)))
+                drawLineWithoutDot(0f, 0f, size * dsc(0), 0f, paint)
+            }
         }
         drawXY(0f, 0f) {
             rotate(90f * dsc(3))
