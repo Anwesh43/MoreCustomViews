77501da282ed51c7d3672ac1a4a75565f519a94f
Anwesh43 <anweshthecool0@gmail.com>
2022-12-18 11:00:25 +0530

    multirotbarextenderview: adding static create method

--- multirotbarextenderview/src/main/java/com/example/multirotbarextenderview/MultiRotBarExenderView.kt
+++ multirotbarextenderview/src/main/java/com/example/multirotbarextenderview/MultiRotBarExenderView.kt
@@ -216,4 +216,12 @@ class MultiRotBarExtenderView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : MultiRotBarExtenderView {
+            val view : MultiRotBarExtenderView = MultiRotBarExtenderView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
