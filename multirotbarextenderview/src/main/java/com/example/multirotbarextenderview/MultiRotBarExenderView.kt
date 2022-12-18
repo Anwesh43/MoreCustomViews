6535202c5af13f206299d7e5001bd505dfcfccf0
Anwesh43 <anweshthecool0@gmail.com>
2022-12-18 10:50:50 +0530

    multirotbarextenderview: adding MRBENode class

--- multirotbarextenderview/src/main/java/com/example/multirotbarextenderview/MultiRotBarExenderView.kt
+++ multirotbarextenderview/src/main/java/com/example/multirotbarextenderview/MultiRotBarExenderView.kt
@@ -127,4 +127,45 @@ class MultiRotBarExtenderView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class MRBENode(var i : Int = 0, val state : State = State()) {
+
+        private var next : MRBENode? = null
+        private var prev : MRBENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = MRBENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawMRBENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : MRBENode {
+            var curr : MRBENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
