78e3983004bee6efef0feb50127e6d0c008242e6
Anwesh43 <anweshthecool0@gmail.com>
2022-12-18 10:30:46 +0530

    multirotbarextenderview: adding Canvas extension methods

--- multirotbarextenderview/src/main/java/com/example/multirotbarextenderview/MultiRotBarExenderView.kt
+++ multirotbarextenderview/src/main/java/com/example/multirotbarextenderview/MultiRotBarExenderView.kt
@@ -29,3 +29,38 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawMultiRotBarExtender(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            scale(1f - 2 * j, 1f)
+            rotate(rot * (1 - j) * (dsc(1) + dsc(3)))
+            drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        }
+        drawXY(0f, 0f) {
+            rotate(90f * dsc(3))
+            drawRect(RectF(0f, -size, size * dsc(2), 0f), paint)
+        }
+
+    }
+}
+
+fun Canvas.drawMRBENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawMultiRotBarExtender(scale, w, h, paint)
+}
