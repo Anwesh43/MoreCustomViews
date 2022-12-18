41f467ba5e6db74c5815dd40739d4ac594c853d1
Anwesh43 <anweshthecool0@gmail.com>
2022-12-18 10:35:09 +0530

    multirotbarextenderview: adding MultiRotBarExtenderView class

--- multirotbarextenderview/src/main/java/com/example/multirotbarextenderview/MultiRotBarExenderView.kt
+++ multirotbarextenderview/src/main/java/com/example/multirotbarextenderview/MultiRotBarExenderView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawMRBENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawMultiRotBarExtender(scale, w, h, paint)
 }
+
+class MultiRotBarExtenderView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
