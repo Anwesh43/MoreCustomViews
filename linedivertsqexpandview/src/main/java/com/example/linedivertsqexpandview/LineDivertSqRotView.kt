3f7dd0aaf9445b40e686bb49ca145db854c14de8
Anwesh43 <anweshthecool0@gmail.com>
2022-12-20 09:45:07 +0530

    linedivertsqexpandview: adding LDSENode class

--- linedivertsqexpandview/src/main/java/com/example/linedivertsqexpandview/LineDivertSqRotView.kt
+++ linedivertsqexpandview/src/main/java/com/example/linedivertsqexpandview/LineDivertSqRotView.kt
@@ -125,4 +125,45 @@ class LineDivertSqExpandView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LDSENode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LDSENode? = null
+        private var prev : LDSENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LDSENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLDSENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LDSENode {
+            var curr : LDSENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
