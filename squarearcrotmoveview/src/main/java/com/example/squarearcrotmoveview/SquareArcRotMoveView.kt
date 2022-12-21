33f0dd41e7b07f6f924495f6bfd47df1ef55d845
Anwesh43 <anweshthecool0@gmail.com>
2022-12-21 08:52:23 +0530

    squarearcrotmoveview: adding SquareArcRotMove class

--- squarearcrotmoveview/src/main/java/com/example/squarearcrotmoveview/SquareArcRotMoveView.kt
+++ squarearcrotmoveview/src/main/java/com/example/squarearcrotmoveview/SquareArcRotMoveView.kt
@@ -146,7 +146,7 @@ class SquareArcRotMoveView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
-        fun startUpddating(cb : () -> Unit) {
+        fun startUpdating(cb : () -> Unit) {
             state.startUpdating(cb)
         }
 
@@ -160,6 +160,29 @@ class SquareArcRotMoveView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class SquareArcRotMove(var i : Int = 0) {
+
+        private var curr : SARMNode = SARMNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
