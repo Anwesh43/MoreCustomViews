2d21c77b1dd9073b503638dd5622f6f658dc43ef
Anwesh43 <anweshthecool0@gmail.com>
2022-12-21 08:35:46 +0530

    squarearcrotmoveview: adding SquareArcRotMoveView class

--- squarearcrotmoveview/src/main/java/com/example/squarearcrotmoveview/SquareArcRotMoveView.kt
+++ squarearcrotmoveview/src/main/java/com/example/squarearcrotmoveview/SquareArcRotMoveView.kt
@@ -58,3 +58,19 @@ fun Canvas.drawSARNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawSquareArcRotMove(scale, w, h, paint)
 }
+
+class SquareArcRotMoveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
