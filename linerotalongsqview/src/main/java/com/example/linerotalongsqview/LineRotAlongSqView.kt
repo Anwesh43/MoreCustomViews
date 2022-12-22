2423d7ab7c880e77f8db9aefbb117149436b3063
Anwesh43 <anweshthecool0@gmail.com>
2022-12-22 07:47:56 +0530

    linerotalongsqview: adding Canvas extension methods

--- linerotalongsqview/src/main/java/com/example/linerotalongsqview/LineRotAlongSqView.kt
+++ linerotalongsqview/src/main/java/com/example/linerotalongsqview/LineRotAlongSqView.kt
@@ -29,3 +29,35 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotAlongSq(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(4)) {
+        drawXY(-size * 0.5f * dsc(3), 0f) {
+            drawRect(RectF(0f, 0f, size * dsc(0), size), paint)
+        }
+        drawXY(0f, 0f) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, -size * dsc(1), 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLRASNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotAlongSq(scale, w, h, paint)
+}
