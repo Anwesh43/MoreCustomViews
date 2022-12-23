52466e7e7b814adddcc4d076300e3f2750717342
Anwesh43 <anweshthecool0@gmail.com>
2022-12-23 08:16:55 +0530

    lineshiftrotarcview: adding view class

--- lineshiftrotarcview/src/main/java/com/example/lineshiftrotarcview/LineShiftArcRotView.kt
+++ lineshiftrotarcview/src/main/java/com/example/lineshiftrotarcview/LineShiftArcRotView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawLSARNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawLineShiftArcRot(scale, w, h, paint)
 }
+
+class LineShiftArcRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
