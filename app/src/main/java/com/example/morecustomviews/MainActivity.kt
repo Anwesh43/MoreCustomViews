7708c6bd8707d81226399c2e89f40447b7d6a077
Anwesh43 <anweshthecool0@gmail.com>
2022-12-23 08:49:06 +0530

    lineshiftrotarcview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotalongsqview.LineRotAlongSqView
+import com.example.lineshiftrotarcview.LineShiftArcRotView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineRotAlongSqView.create(this)
+        LineShiftArcRotView.create(this)
         fullScreen()
     }
 }
