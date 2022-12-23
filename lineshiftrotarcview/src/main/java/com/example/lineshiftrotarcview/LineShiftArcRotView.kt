177f4aee8776fcb2c9411f1d57b8485920d40d94
Anwesh43 <anweshthecool0@gmail.com>
2022-12-23 08:28:46 +0530

    lineshiftrotarcview: adding LSARNode class

--- lineshiftrotarcview/src/main/java/com/example/lineshiftrotarcview/LineShiftArcRotView.kt
+++ lineshiftrotarcview/src/main/java/com/example/lineshiftrotarcview/LineShiftArcRotView.kt
@@ -124,4 +124,45 @@ class LineShiftArcRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSARNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LSARNode? = null
+        private var next : LSARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSARNode {
+            var curr : LSARNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
