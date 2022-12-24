6f021bc80128d0f7b73c59074f319f23d9884d59
Anwesh43 <anweshthecool0@gmail.com>
2022-12-24 10:33:39 +0530

    barmiddlelinerotview: adding BMRLNode class

--- barmiddlelinerotview/src/main/java/com/example/barmiddlelinerotview/BarMiddleRotLineView.kt
+++ barmiddlelinerotview/src/main/java/com/example/barmiddlelinerotview/BarMiddleRotLineView.kt
@@ -127,4 +127,45 @@ class BarMiddleRotLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BMRLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BMRLNode? = null
+        private var prev : BMRLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BMRLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBMRLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BMRLNode {
+            var curr : BMRLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
