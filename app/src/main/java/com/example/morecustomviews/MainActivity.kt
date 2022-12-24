d2c03bb74f2a7062e0943efc2a45bb1da35dba43
Anwesh43 <anweshthecool0@gmail.com>
2022-12-24 10:59:31 +0530

    barmiddlelinerotview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.lineshiftrotarcview.LineShiftArcRotView
+import com.example.barmiddlelinerotview.BarMiddleRotLineView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineShiftArcRotView.create(this)
+        BarMiddleRotLineView.create(this)
         fullScreen()
     }
 }
