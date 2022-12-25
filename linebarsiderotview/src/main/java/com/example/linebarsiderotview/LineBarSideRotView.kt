30322f91b681e2f668a09cca6e691f15b1b39b84
Anwesh43 <anweshthecool0@gmail.com>
2022-12-25 09:33:08 +0530

    linebarsiderotview: adding LineBarSideRotView class

--- linebarsiderotview/src/main/java/com/example/linebarsiderotview/LineBarSideRotView.kt
+++ linebarsiderotview/src/main/java/com/example/linebarsiderotview/LineBarSideRotView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawLBSRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBarSideRot(scale, w, h, paint)
 }
+
+class LineBarSideRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
