8c522bd386b6002a91a23b44183c5e52ec785bf4
Anwesh43 <anweshthecool0@gmail.com>
2022-12-27 02:32:00 +0530

    rothorizbarvertview: adding RHBVNode class

--- rothorizbarvertview/src/main/java/com/example/rothorizbarvertview/RotHorizBarVertView.kt
+++ rothorizbarvertview/src/main/java/com/example/rothorizbarvertview/RotHorizBarVertView.kt
@@ -131,4 +131,45 @@ class RotHorizBarVertView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RHBVNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : RHBVNode? = null
+        private var next : RHBVNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RHBVNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRHBVNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RHBVNode {
+            var curr : RHBVNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
