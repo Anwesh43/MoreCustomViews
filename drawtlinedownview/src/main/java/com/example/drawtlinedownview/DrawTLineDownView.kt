fade8a13a4ed62e0305122788719c0eee65adda4
Anwesh43 <anweshthecool0@gmail.com>
2022-12-26 09:36:35 +0530

    drawtlinedownview: adding constants and libraries

---
+++ drawtlinedownview/src/main/java/com/example/drawtlinedownview/DrawTLineDownView.kt
@@ -0,0 +1,27 @@
+package com.example.drawtlinedownview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#3f51b5",
+    "#f44336",
+    "#7c4dff",
+    "#4bcbcc",
+    "#b4004e"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val rot : Float = 90f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
