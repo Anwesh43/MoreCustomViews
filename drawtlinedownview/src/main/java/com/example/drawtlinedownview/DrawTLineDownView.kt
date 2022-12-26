1d426e4f741233a7444ad20079e3c0668b364741
Anwesh43 <anweshthecool0@gmail.com>
2022-12-26 09:52:54 +0530

    drawtlinedownview: adding DrawTLineView class

--- drawtlinedownview/src/main/java/com/example/drawtlinedownview/DrawTLineDownView.kt
+++ drawtlinedownview/src/main/java/com/example/drawtlinedownview/DrawTLineDownView.kt
@@ -70,3 +70,19 @@ fun Canvas.drawTLDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTLDNode(i, scale, paint)
 }
+
+class DrawTLineDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
