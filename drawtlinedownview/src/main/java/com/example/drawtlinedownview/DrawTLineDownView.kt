7a5c4463d3b2b25b7f1b44637f599b006e186553
Anwesh43 <anweshthecool0@gmail.com>
2022-12-26 09:46:04 +0530

    drawtlinedownview: adding Canvas extension methods

--- drawtlinedownview/src/main/java/com/example/drawtlinedownview/DrawTLineDownView.kt
+++ drawtlinedownview/src/main/java/com/example/drawtlinedownview/DrawTLineDownView.kt
@@ -18,14 +18,55 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val rot : Float = 90f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 180f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTLineDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(4)) {
+        rotate(deg * dsc(3))
+        drawLine(0f, 0f, 0f, size * dsc(0), paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(size * 0.5f * dsc(1), 0f) {
+                    for(j in 0..1) {
+                        drawXY(0f, 0f) {
+                            rotate(rot * dsc(2))
+                            drawLine(-size * 0.5f * dsc(1), 0f, 0f, 0f, paint)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawTLDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTLDNode(i, scale, paint)
+}
