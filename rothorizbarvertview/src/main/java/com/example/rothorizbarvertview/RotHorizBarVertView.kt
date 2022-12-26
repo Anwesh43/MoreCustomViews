96566f410d5c47fef9e7eaf88ab6949c0cfaab83
Anwesh43 <anweshthecool0@gmail.com>
2022-12-27 01:48:42 +0530

    rothorizbarvertview: adding RotHorizBarVertView class

--- rothorizbarvertview/src/main/java/com/example/rothorizbarvertview/RotHorizBarVertView.kt
+++ rothorizbarvertview/src/main/java/com/example/rothorizbarvertview/RotHorizBarVertView.kt
@@ -69,3 +69,18 @@ fun Canvas.drawRHBVNode(i : Int, scale : Float, paint : Paint) {
     drawRotHorizBarVert(scale, w, h, paint)
 }
 
+class RotHorizBarVertView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
