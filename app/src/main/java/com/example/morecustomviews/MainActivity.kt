7a7959a08244d8b373f5c87447f2cc75b15404bf
Anwesh43 <anweshthecool0@gmail.com>
2022-12-26 10:16:35 +0530

    drawtlinedownview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linebarsiderotview.LineBarSideRotView
+import com.example.drawtlinedownview.DrawTLineDownView
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
 
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineBarSideRotView.create(this)
+        DrawTLineDownView.create(this)
         fullScreen()
     }
 }
