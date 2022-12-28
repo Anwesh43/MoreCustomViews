29254606794a5082abfe8e7243278851ef4c6417
Anwesh43 <anweshthecool0@gmail.com>
2022-12-28 09:46:13 +0530

    arcrotabovesqview: adding constants and libraries

---
+++ arcrotabovesqview/src/main/java/com/example/arcrotabovesqview/ArcRotAboveSqView.kt
@@ -0,0 +1,27 @@
+package com.example.arcrotabovesqview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+    "#1abc9c",
+    "#e74c3c",
+    "#8e44ad",
+    "#f1c40f",
+    "#3498db"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val sizeFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 180f
+val deg : Float = 180f
