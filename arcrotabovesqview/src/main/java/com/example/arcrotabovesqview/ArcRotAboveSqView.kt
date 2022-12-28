e5f93843d9b95f816bd0845186ebcb9f15a28359
Anwesh43 <anweshthecool0@gmail.com>
2022-12-28 10:06:53 +0530

    arcrotabovesqview: adding ArcRotAboveSqView class

--- arcrotabovesqview/src/main/java/com/example/arcrotabovesqview/ArcRotAboveSqView.kt
+++ arcrotabovesqview/src/main/java/com/example/arcrotabovesqview/ArcRotAboveSqView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawARASNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawArcRotAboveSq(scale, w, h, paint)
 }
+
+class ArcRotAboveSqView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
