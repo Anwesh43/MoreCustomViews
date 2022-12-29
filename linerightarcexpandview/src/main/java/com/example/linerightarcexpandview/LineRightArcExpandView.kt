9fc7d86f91982c6bef9065405347f1eb1e30c084
Anwesh43 <anweshthecool0@gmail.com>
2022-12-30 01:04:20 +0530

    linerightarcexpandview: adding LineRightArcExpandView class

--- linerightarcexpandview/src/main/java/com/example/linerightarcexpandview/LineRightArcExpandView.kt
+++ linerightarcexpandview/src/main/java/com/example/linerightarcexpandview/LineRightArcExpandView.kt
@@ -71,3 +71,19 @@ fun Canvas.drawLRAENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRightArcExpand(scale, w, h, paint)
 }
+
+class LineRightArcExpandView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
