7af18f0f2a71376fbdcee20fd1c202bbb2e10fd0
Anwesh43 <anweshthecool0@gmail.com>
2022-12-30 01:03:12 +0530

    linerightarcexpandview: adding Canvas extension methods

--- linerightarcexpandview/src/main/java/com/example/linerightarcexpandview/LineRightArcExpandView.kt
+++ linerightarcexpandview/src/main/java/com/example/linerightarcexpandview/LineRightArcExpandView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -29,3 +29,45 @@ val deg : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineWithoutDot(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    if (Math.abs(x1 - x2) < 0.1f && Math.abs(y1 - y2) < 0.1f) {
+        return
+    }
+    drawLine(x1, y1, x2, y2, paint)
+}
+
+fun Canvas.drawLineRightArcExpand(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(4)) {
+        rotate(deg * dsc(3))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(deg * dsc(1) * (1 - 2 * j))
+                drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+            }
+            drawXY(0f, -size + size * j) {
+                drawArc(RectF(0f, -size / 2, size, size / 2), rot * j, rot * dsc(2), true, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLRAENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRightArcExpand(scale, w, h, paint)
+}
