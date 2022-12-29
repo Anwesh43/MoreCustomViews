0327c303cf86dc9533f155b43d30e8ed428682f6
Anwesh43 <anweshthecool0@gmail.com>
2022-12-30 01:19:48 +0530

    linerightarcexpandview: adding LRAENode class

--- linerightarcexpandview/src/main/java/com/example/linerightarcexpandview/LineRightArcExpandView.kt
+++ linerightarcexpandview/src/main/java/com/example/linerightarcexpandview/LineRightArcExpandView.kt
@@ -134,4 +134,44 @@ class LineRightArcExpandView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRAENode(var i : Int = 0, val state : State = State()) {
+        private var prev : LRAENode? = null
+        private var next : LRAENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRAENode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRAENode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRAENode {
+            var curr : LRAENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
