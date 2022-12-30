82a86d838b1d9a41947b3b9559bd1b08f8498ae5
Anwesh43 <anweshthecool0@gmail.com>
2022-12-31 01:19:13 +0530

    linetoxdownview: adding LTXDNode class

--- linetoxdownview/src/main/java/com/example/linetoxdownview/LineToXDownView.kt
+++ linetoxdownview/src/main/java/com/example/linetoxdownview/LineToXDownView.kt
@@ -125,4 +125,45 @@ class LineToXDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTXDNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LTXDNode? = null
+        private var next : LTXDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LTXDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTXDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTXDNode {
+            var curr : LTXDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
