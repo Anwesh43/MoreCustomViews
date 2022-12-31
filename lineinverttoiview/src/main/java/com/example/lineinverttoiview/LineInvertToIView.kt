c6c427ab7fcfaccc2f78cd5cf8bcd55de491bffc
Anwesh43 <anweshthecool0@gmail.com>
2023-01-01 03:36:29 +0530

    lineinverttoiview: adding Canvas extension methods

--- lineinverttoiview/src/main/java/com/example/lineinverttoiview/LineInvertToIView.kt
+++ lineinverttoiview/src/main/java/com/example/lineinverttoiview/LineInvertToIView.kt
@@ -28,3 +28,35 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineInvertToI(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(size * (1f - 2 * j) * dsc(1), 0f) {
+                drawLine(0f, -size * 0.5f * dsc(0), 0f, size * 0.5f * dsc(0), paint)
+            }
+        }
+        drawLine(-size * dsc(1), 0f, size * dsc(1), 0f, paint)
+    }
+}
+
+fun Canvas.drawLITINode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineInvertToI(scale, w, h, paint)
+}
