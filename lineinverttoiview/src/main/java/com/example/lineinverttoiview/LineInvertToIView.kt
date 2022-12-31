c3c2c298534a8922bccf81f2337b1e8c63c98358
Anwesh43 <anweshthecool0@gmail.com>
2023-01-01 03:38:46 +0530

    lineinverttoiview: adding LineInvertToIView class

--- lineinverttoiview/src/main/java/com/example/lineinverttoiview/LineInvertToIView.kt
+++ lineinverttoiview/src/main/java/com/example/lineinverttoiview/LineInvertToIView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLITINode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineInvertToI(scale, w, h, paint)
 }
+
+class LineInvertToIView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
