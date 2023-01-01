df34dc21c220b724272b856b71d524cf1400fbc8
Anwesh43 <anweshthecool0@gmail.com>
2023-01-02 01:00:47 +0530

    lineinverttoiview: adding BHANode class

--- bulletheadarcview/src/main/java/com/example/bulletheadarcview/BulletHeadArcView.kt
+++ bulletheadarcview/src/main/java/com/example/bulletheadarcview/BulletHeadArcView.kt
@@ -129,4 +129,45 @@ class BulletHeadArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BHANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BHANode? = null
+        private var prev : BHANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BHANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBHANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BHANode {
+            var curr : BHANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
