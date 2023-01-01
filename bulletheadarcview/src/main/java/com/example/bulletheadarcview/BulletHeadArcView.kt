b54ab420cf275b7652e9b59142d38b70c0a838de
Anwesh43 <anweshthecool0@gmail.com>
2023-01-02 01:03:02 +0530

    lineinverttoiview: adding BulletHeadArc class

--- bulletheadarcview/src/main/java/com/example/bulletheadarcview/BulletHeadArcView.kt
+++ bulletheadarcview/src/main/java/com/example/bulletheadarcview/BulletHeadArcView.kt
@@ -170,4 +170,27 @@ class BulletHeadArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BulletHeadArc(var i : Int) {
+
+        private var curr : BHANode = BHANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
