ced98bf90741f3345fb6e3b247dfef37d22327e5
Anwesh43 <anweshthecool0@gmail.com>
2023-01-02 00:53:26 +0530

    lineinverttoiview: adding BulletHeadArcView class

--- bulletheadarcview/src/main/java/com/example/bulletheadarcview/BulletHeadArcView.kt
+++ bulletheadarcview/src/main/java/com/example/bulletheadarcview/BulletHeadArcView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawBHANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawBulletHeadArc(scale, w, h, paint)
 }
+
+class BulletHeadArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
