be82a68e4137e53e6b5eb7263cb6da352166c41c
Anwesh43 <anweshthecool0@gmail.com>
2023-01-03 00:41:59 +0530

    squaretotlineview: adding Renderer class

--- squaretotlineview/src/main/java/com/example/squaretotlineview/SquareToTLineView.kt
+++ squaretotlineview/src/main/java/com/example/squaretotlineview/SquareToTLineView.kt
@@ -193,4 +193,27 @@ class SquareToTLineView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SquareToTLineView) {
+
+        private val animator : Animator = Animator(view)
+        private val sttl : SquareToTLine = SquareToTLine(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            sttl.draw(canvas, paint)
+            animator.animate {
+                sttl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            sttl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
