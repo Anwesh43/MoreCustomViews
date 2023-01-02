ea68640accad2781d926df7e4aadb1cbcc2f84fb
Anwesh43 <anweshthecool0@gmail.com>
2023-01-03 00:38:17 +0530

    squaretotlineview: adding STTLNode class

--- squaretotlineview/src/main/java/com/example/squaretotlineview/SquareToTLineView.kt
+++ squaretotlineview/src/main/java/com/example/squaretotlineview/SquareToTLineView.kt
@@ -129,4 +129,45 @@ class SquareToTLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class STTLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : STTLNode? = null
+        private var prev : STTLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = STTLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSTTLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : STTLNode {
+            var curr : STTLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
