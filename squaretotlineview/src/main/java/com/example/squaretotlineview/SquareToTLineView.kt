6231d9c2791bb0c3fe78ba920ae7a3c93c1b5f1e
Anwesh43 <anweshthecool0@gmail.com>
2023-01-03 00:32:43 +0530

    squaretotlineview: adding SquareToTLineView class

--- squaretotlineview/src/main/java/com/example/squaretotlineview/SquareToTLineView.kt
+++ squaretotlineview/src/main/java/com/example/squaretotlineview/SquareToTLineView.kt
@@ -66,3 +66,19 @@ fun Canvas.drawSTTLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSquareToTLine(scale, w, h, paint)
 }
+
+class SquareToTLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
