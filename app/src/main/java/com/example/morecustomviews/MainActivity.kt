2e9d2e7db202a686ee07c68b940520d6e2b8a43e
Anwesh43 <anweshthecool0@gmail.com>
2023-01-08 11:51:35 +0530

    linerotarcfillview: adding static create method

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.biendarcdownview.BiEndArcDownView
+import com.example.linerotarcfillview.LineRotFillArcView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        BiEndArcDownView.create(this)
+        LineRotFillArcView.create(this)
         fullScreen()
     }
 }
