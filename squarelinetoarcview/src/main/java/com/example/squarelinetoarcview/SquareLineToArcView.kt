afa373051db1ddfb27056f5705df35a3b8171199
Anwesh43 <anweshthecool0@gmail.com>
2023-01-09 09:42:22 +0530

    squarelinetoarcview: adding SquareLineToArc class

--- squarelinetoarcview/src/main/java/com/example/squarelinetoarcview/SquareLineToArcView.kt
+++ squarelinetoarcview/src/main/java/com/example/squarelinetoarcview/SquareLineToArcView.kt
@@ -177,4 +177,27 @@ class SquareLineToArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SquareLineToArc(var i : Int = 0) {
+
+        private var curr : SLTANode = SLTANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
