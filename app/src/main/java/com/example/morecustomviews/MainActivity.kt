fa64bce3e6a1aecf4fcace5efc6127bc172a70ce
Anwesh43 <anweshthecool0@gmail.com>
2023-01-09 09:48:46 +0530

    squarelinetoarcview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotarcfillview.LineRotFillArcView
+import com.example.squarelinetoarcview.SquareLineToArcView
 
 
 //import com.example.bilineparallelbarview.BiLineParallelBarView
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineRotFillArcView.create(this)
+        SquareLineToArcView.create(this)
         fullScreen()
     }
 }
