d424a68bff6a51f2d502e02c368111341896b0d2
Anwesh43 <anweshthecool0@gmail.com>
2023-01-09 09:40:03 +0530

    squarelinetoarcview: adding SLTANode class

--- squarelinetoarcview/src/main/java/com/example/squarelinetoarcview/SquareLineToArcView.kt
+++ squarelinetoarcview/src/main/java/com/example/squarelinetoarcview/SquareLineToArcView.kt
@@ -136,4 +136,45 @@ class SquareLineToArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SLTANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SLTANode? = null
+        private var prev : SLTANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SLTANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSLTANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SLTANode {
+            var curr : SLTANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
