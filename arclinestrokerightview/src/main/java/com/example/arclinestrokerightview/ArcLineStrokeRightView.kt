43616840468d36e2275506ae948a584d9edbbfbd
Anwesh43 <anweshthecool0@gmail.com>
2023-01-11 08:15:51 +0530

    arclinestrokerightview: adding ALSRNode class

--- arclinestrokerightview/src/main/java/com/example/arclinestrokerightview/ArcLineStrokeRightView.kt
+++ arclinestrokerightview/src/main/java/com/example/arclinestrokerightview/ArcLineStrokeRightView.kt
@@ -124,4 +124,45 @@ class ArcLineStrokeRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALSRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ALSRNode? = null
+        private var prev : ALSRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALSRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALSRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALSRNode {
+            var curr : ALSRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
