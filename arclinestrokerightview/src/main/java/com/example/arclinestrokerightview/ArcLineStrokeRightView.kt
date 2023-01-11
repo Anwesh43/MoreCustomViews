f3c8ee70d6d257613d73d717e45be769c2725f08
Anwesh43 <anweshthecool0@gmail.com>
2023-01-11 07:53:24 +0530

    arclinestrokerightview: adding ArcLineStrokeRightView class

--- arclinestrokerightview/src/main/java/com/example/arclinestrokerightview/ArcLineStrokeRightView.kt
+++ arclinestrokerightview/src/main/java/com/example/arclinestrokerightview/ArcLineStrokeRightView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawALSRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcLineStrokeRight(scale, w, h, paint)
 }
+
+class ArcLineStrokeRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
