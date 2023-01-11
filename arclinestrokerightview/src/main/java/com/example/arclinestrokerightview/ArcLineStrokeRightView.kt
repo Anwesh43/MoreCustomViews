299f0c44bab1e255790219a2cb6df3d23de7da44
Anwesh43 <anweshthecool0@gmail.com>
2023-01-11 08:17:21 +0530

    arclinestrokerightview: adding ArcLineStrokeRight class

--- arclinestrokerightview/src/main/java/com/example/arclinestrokerightview/ArcLineStrokeRightView.kt
+++ arclinestrokerightview/src/main/java/com/example/arclinestrokerightview/ArcLineStrokeRightView.kt
@@ -165,4 +165,27 @@ class ArcLineStrokeRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class ArcLineStrokeRight(var i : Int) {
+
+        private var curr : ALSRNode = ALSRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
