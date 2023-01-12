4ed6fce0596b0d8ddfbf5da7238e30affe2429d0
Anwesh43 <anweshthecool0@gmail.com>
2023-01-12 10:09:35 +0530

    linebarjoinrotview: adding LBJRNode class

--- linebarjoinrotview/src/main/java/com/example/linebarjoinrotview/LineBarJoinRotView.kt
+++ linebarjoinrotview/src/main/java/com/example/linebarjoinrotview/LineBarJoinRotView.kt
@@ -124,4 +124,45 @@ class LineBarJoinRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBJRNode(var i : Int, val state : State = State()) {
+
+        private var next : LBJRNode? = null
+        private var prev : LBJRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBJRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBJRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBJRNode {
+            var curr : LBJRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
