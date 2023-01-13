b04244ec6db68a5a9e5adb11f3138a512b10fe1b
Anwesh43 <anweshthecool0@gmail.com>
2023-01-13 08:24:41 +0530

    dotlinesquareview: adding Canvas extension methods

--- dotlinesquareview/src/main/java/com/example/dotlinesquareview/DotLineSquareView.kt
+++ dotlinesquareview/src/main/java/com/example/dotlinesquareview/DotLineSquareView.kt
@@ -29,3 +29,40 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawDotLineSquare(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size / 2) * dsc(4), h / 2) {
+        rotate(360f * dsc(4))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(2) * j)
+                for (k in 0..1) {
+                    drawXY(0f, -size * 0.5f * (1f - 2 * k) * dsc(3)) {
+                        val sx : Float = size * 0.5f * dsc(1)
+                        drawLine(-sx, 0f, sx, 0f, paint)
+                        drawCircle(sx * (1f - 2 * k), 0f, size * 0.2f * dsc(0), paint)
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawDLSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawDotLineSquare(scale, w, h, paint)
+}
