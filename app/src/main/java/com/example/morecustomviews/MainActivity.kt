c5810e5f6890aa829018e1c2a3c4c77109adf118
Anwesh43 <anweshthecool0@gmail.com>
2023-01-23 14:28:07 +0530

    linerotsqexpandview: adding view in activiy

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linesweepcircleupview.LineSweepCircleUpView
+import com.example.linerotsqexpandview.LineRotSqExpandView
 
 //import com.example.dotlinesquareview.DotLineSquareView
 
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineSweepCircleUpView.create(this)
+        LineRotSqExpandView.create(this)
         fullScreen()
     }
 }
