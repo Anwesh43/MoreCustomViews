1df99bb7056496d93692111906dcd56315d2eb15
Anwesh43 <anweshthecool0@gmail.com>
2023-01-23 13:34:44 +0530

    linerotsqexpandview: adding Canvas extension methods

--- linerotsqexpandview/src/main/java/com/example/linerotsqexpandview/LineRotSqExpandView.kt
+++ linerotsqexpandview/src/main/java/com/example/linerotsqexpandview/LineRotSqExpandView.kt
@@ -5,6 +5,7 @@ import android.view.MotionEvent
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.RectF
 import android.app.Activity
 import android.content.Context
 
@@ -29,3 +30,38 @@ val sweep : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRotSqExpand(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(5), h / 2) {
+        rotate(rot * dsc(4))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * j * dsc(1))
+                drawXY(-(size / 2) * dsc(3), 0f) {
+                    drawLine(0f, 0f, size * dsc(0), 0f, paint)
+                    drawRect(RectF(0f, 0f, size * dsc(2), size), paint)
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawLRSENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotSqExpand(scale, w, h, paint)
+}
