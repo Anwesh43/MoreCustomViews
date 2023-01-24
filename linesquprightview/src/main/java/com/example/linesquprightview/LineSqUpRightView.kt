8573a53727a52f80389f4b68f441dd288a4dfd56
Anwesh43 <anweshthecool0@gmail.com>
2023-01-24 08:21:34 +0530

    linesquprightview: adding Renderer class

--- linesquprightview/src/main/java/com/example/linesquprightview/LineSqUpRightView.kt
+++ linesquprightview/src/main/java/com/example/linesquprightview/LineSqUpRightView.kt
@@ -189,4 +189,27 @@ class LineSqUpRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineSqUpRightView) {
+
+        private val animator : Animator = Animator(view)
+        private val lsur : LineSqUpRight = LineSqUpRight(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lsur.draw(canvas, paint)
+            animator.animate {
+                lsur.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lsur.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
