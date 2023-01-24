9014c0c61f0ba2c72265aef315bae6d06251e815
Anwesh43 <anweshthecool0@gmail.com>
2023-01-24 08:23:37 +0530

    linesquprightview: adding fixes

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotsqexpandview.LineRotSqExpandView
+import com.example.linesquprightview.LineSqUpRightView
+
+//import com.example.linerotsqexpandview.LineRotSqExpandView
 
 //import com.example.dotlinesquareview.DotLineSquareView
 
@@ -17,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineRotSqExpandView.create(this)
+        LineSqUpRightView.create(this)
         fullScreen()
     }
 }
