13648be4046bca366e6b79ac4da6c86b26e13e4f
Anwesh43 <anweshthecool0@gmail.com>
2023-01-25 16:48:01 +0530

    boxtomidlineview: adding Canvas extension methods

--- boxtomidlineview/src/main/java/com/example/boxtomidlineview/BoxToMidLineView.kt
+++ boxtomidlineview/src/main/java/com/example/boxtomidlineview/BoxToMidLineView.kt
@@ -25,7 +25,39 @@ val sizeFactor : Float = 4.9f
 val boxSizeFactor : Float = 11.2f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBoxToMidLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    val blockSize : Float = Math.min(w, h) / boxSizeFactor
+    drawXY(w / 2 - (w / 2) * dsc(3), h / 2) {
+        rotate(-rot * dsc(2))
+        drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        drawXY((w / 2) * (1 - dsc(1)), -size / 2) {
+            drawRect(RectF(0f, -blockSize / 2, blockSize, blockSize / 2), paint)
+        }
+    }
+}
+
+fun Canvas.drawBTMLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBoxToMidLine(scale, w, h, paint)
+}
