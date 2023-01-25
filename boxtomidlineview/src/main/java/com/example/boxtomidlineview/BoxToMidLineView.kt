7b50f9bdb6888c22223a302b5612e9d73f29c778
Anwesh43 <anweshthecool0@gmail.com>
2023-01-25 17:32:52 +0530

    boxtomidlineview: adding BTMLNode class

--- boxtomidlineview/src/main/java/com/example/boxtomidlineview/BoxToMidLineView.kt
+++ boxtomidlineview/src/main/java/com/example/boxtomidlineview/BoxToMidLineView.kt
@@ -124,4 +124,45 @@ class BoxToMidLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BTMLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BTMLNode? = null
+        private var prev : BTMLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BTMLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBTMLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BTMLNode {
+            var curr : BTMLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
