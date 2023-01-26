11e402f891bd6fc8d7e8d851c05b616d2cf0253a
Anwesh43 <anweshthecool0@gmail.com>
2023-01-26 11:24:38 +0530

    circlebilinejoindownview: adding renderer in view

--- circlebilinejoindownview/src/main/java/com/example/circlebilinejoindownview/CircleBiLineJoinDownView.kt
+++ circlebilinejoindownview/src/main/java/com/example/circlebilinejoindownview/CircleBiLineJoinDownView.kt
@@ -69,14 +69,16 @@ fun Canvas.drawCBLJDNode(i : Int, scale : Float, paint : Paint) {
 
 class CircleBiLineJoinDownView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
