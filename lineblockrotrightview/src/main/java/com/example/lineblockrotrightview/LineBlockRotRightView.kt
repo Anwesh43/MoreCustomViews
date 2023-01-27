0d76aaa36a2175b0371b072ef455f37b4146ac8b
Anwesh43 <anweshthecool0@gmail.com>
2023-01-28 01:28:11 +0530

    lineblockrotrightview: adding LineBlockRotRight class

--- lineblockrotrightview/src/main/java/com/example/lineblockrotrightview/LineBlockRotRightView.kt
+++ lineblockrotrightview/src/main/java/com/example/lineblockrotrightview/LineBlockRotRightView.kt
@@ -166,4 +166,27 @@ class LineBlockRotRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineBlockBoxRotRight(var i : Int) {
+
+        private var curr : LBRRNode = LBRRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
