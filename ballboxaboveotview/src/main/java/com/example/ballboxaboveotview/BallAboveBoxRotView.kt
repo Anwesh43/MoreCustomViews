e6fe79df0defee27d9a2bc11a5737a83b00a6063
Anwesh43 <anweshthecool0@gmail.com>
2023-01-27 12:21:29 +0530

    ballboxaboverotview: adding BallAboveBarRot class

--- ballboxaboveotview/src/main/java/com/example/ballboxaboveotview/BallAboveBoxRotView.kt
+++ ballboxaboveotview/src/main/java/com/example/ballboxaboveotview/BallAboveBoxRotView.kt
@@ -163,4 +163,27 @@ class BallAboveBoxRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BarAboveBoxRot(var i : Int = 0) {
+
+        private var curr : BABRNode = BABRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
