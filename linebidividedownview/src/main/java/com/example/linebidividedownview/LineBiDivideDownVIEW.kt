13577db524b749637d7f90c0debc8ec8e3d6f68d
Anwesh43 <anweshthecool0@gmail.com>
2023-01-29 01:08:54 +0530

    linebidividedownview: adding Canvas extension methods

--- linebidividedownview/src/main/java/com/example/linebidividedownview/LineBiDivideDownVIEW.kt
+++ linebidividedownview/src/main/java/com/example/linebidividedownview/LineBiDivideDownVIEW.kt
@@ -30,3 +30,37 @@ val smallLineFactor : Float = 0.25f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBiDivideDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val lSize : Float = size * smallLineFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + lSize) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(deg * dsc(1) * (1 - 2 * j))
+                drawLine(0f, 0f, -lSize * dsc(0), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBDDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBiDivideDown(scale, w, h, paint)
+}
