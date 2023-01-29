0448f76cc705911d6c2da3b1d39233f98a12880b
Anwesh43 <anweshthecool0@gmail.com>
2023-01-29 10:19:29 +0530

    linebidividedownview: adding LBDDNode class

--- linebidividedownview/src/main/java/com/example/linebidividedownview/LineBiDivideDownVIEW.kt
+++ linebidividedownview/src/main/java/com/example/linebidividedownview/LineBiDivideDownVIEW.kt
@@ -126,4 +126,45 @@ class LineBiDivideDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBDDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBDDNode? = null
+        private var prev : LBDDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBDDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBDDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBDDNode {
+            var curr : LBDDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
