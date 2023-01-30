941a84a849527843b5fd8f43be2f3079627bea62
Anwesh43 <anweshthecool0@gmail.com>
2023-01-30 08:51:42 +0530

    squarebihalfcircleview: adding SBHCNode class

--- squarebihalfcircleview/src/main/java/com/example/squarebihalfcircleview/SquareBiHalfCircleView.kt
+++ squarebihalfcircleview/src/main/java/com/example/squarebihalfcircleview/SquareBiHalfCircleView.kt
@@ -128,4 +128,45 @@ class SquareBiHalfCircleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBHCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SBHCNode? = null
+        private var prev : SBHCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SBHCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBHCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBHCNode {
+            var curr : SBHCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
