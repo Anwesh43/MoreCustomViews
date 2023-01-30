77a7279f412f840e8c2be8190bce450d8303a788
Anwesh43 <anweshthecool0@gmail.com>
2023-01-30 09:02:54 +0530

    squarebihalfcircleview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linebidividedownview.LineBiDivideDownView
+import com.example.squarebihalfcircleview.SquareBiHalfCircleView
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
 
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineBiDivideDownView.create(this)
+        SquareBiHalfCircleView.create(this)
         fullScreen()
     }
 }
