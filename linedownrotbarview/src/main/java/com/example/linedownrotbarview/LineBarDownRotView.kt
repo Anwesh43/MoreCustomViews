c93a2d45a060c192854022a0c5ff5e187dd64ed3
Anwesh43 <anweshthecool0@gmail.com>
2023-02-02 13:54:11 +0530

    linedownrotbarview: adding Renderer class

--- linedownrotbarview/src/main/java/com/example/linedownrotbarview/LineBarDownRotView.kt
+++ linedownrotbarview/src/main/java/com/example/linedownrotbarview/LineBarDownRotView.kt
@@ -191,4 +191,27 @@ class LineBarDownRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineBarDownRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val lbdr : LineBarDownRot = LineBarDownRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lbdr.draw(canvas, paint)
+            animator.animate {
+                lbdr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lbdr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
