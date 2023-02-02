9d43e7bee45035f083e2462adf1ebc2141afec8c
Anwesh43 <anweshthecool0@gmail.com>
2023-02-02 13:55:27 +0530

    linedownrotbarview: adding static create method

--- linedownrotbarview/src/main/java/com/example/linedownrotbarview/LineBarDownRotView.kt
+++ linedownrotbarview/src/main/java/com/example/linedownrotbarview/LineBarDownRotView.kt
@@ -216,4 +216,12 @@ class LineBarDownRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : LineBarDownRotView {
+            val view : LineBarDownRotView = LineBarDownRotView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
