bb5136fe747f0bcd6c34ea08296fb9878f0bab0b
Anwesh43 <anweshthecool0@gmail.com>
2023-02-02 07:22:56 +0530

    linedownrotbarview: adding LBDRNode class

--- linedownrotbarview/src/main/java/com/example/linedownrotbarview/LineBarDownRotView.kt
+++ linedownrotbarview/src/main/java/com/example/linedownrotbarview/LineBarDownRotView.kt
@@ -127,4 +127,45 @@ class LineBarDownRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBDRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBDRNode? = null
+        private var prev : LBDRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBDRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBDRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBDRNode {
+            var curr : LBDRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
