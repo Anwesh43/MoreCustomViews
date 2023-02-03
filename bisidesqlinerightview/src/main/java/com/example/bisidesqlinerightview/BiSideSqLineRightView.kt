60139c3a689b51d908cb79e0f36c9b03da88f93c
Anwesh43 <anweshthecool0@gmail.com>
2023-02-03 14:53:55 +0530

    bisidesqlinerightview: adding BSSLRNode class

--- bisidesqlinerightview/src/main/java/com/example/bisidesqlinerightview/BiSideSqLineRightView.kt
+++ bisidesqlinerightview/src/main/java/com/example/bisidesqlinerightview/BiSideSqLineRightView.kt
@@ -126,4 +126,45 @@ class BiSideSqLineRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BSSLRNode(var i : Int, val state : State = State()) {
+
+        private var next : BSSLRNode? = null
+        private var prev : BSSLRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BSSLRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBSSLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BSSLRNode {
+            var curr : BSSLRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
