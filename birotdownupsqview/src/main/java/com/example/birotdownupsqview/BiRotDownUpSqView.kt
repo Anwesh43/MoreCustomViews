1d8fcfadd4c4d12c4c208fac3b864ff7a5f6861f
Anwesh43 <anweshthecool0@gmail.com>
2023-02-04 11:09:57 +0530

    birotdownupsqview: adding BiRotDownUpSq class

--- birotdownupsqview/src/main/java/com/example/birotdownupsqview/BiRotDownUpSqView.kt
+++ birotdownupsqview/src/main/java/com/example/birotdownupsqview/BiRotDownUpSqView.kt
@@ -171,4 +171,27 @@ class BiRotDownUpSqView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiRotDownUp(var i : Int) {
+
+        private var curr : BRDUSNode = BRDUSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
