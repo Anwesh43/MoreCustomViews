afc78a7951db0017400ab6f2c9adef6bd3af7a8a
Anwesh43 <anweshthecool0@gmail.com>
2023-02-04 10:44:59 +0530

    birotdownupsqview: adding BiRotDownUpSqView class

--- birotdownupsqview/src/main/java/com/example/birotdownupsqview/BiRotDownUpSqView.kt
+++ birotdownupsqview/src/main/java/com/example/birotdownupsqview/BiRotDownUpSqView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawBRDUSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiRotDownUpSq(scale, w, h, paint)
 }
+
+class BiRotDownUpSqView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
