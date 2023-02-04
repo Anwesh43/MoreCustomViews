e2aa61822233d65473858ed53bfdefbf4a26bd53
Anwesh43 <anweshthecool0@gmail.com>
2023-02-04 11:13:15 +0530

    birotdownupsqview: adding Renderer class

--- birotdownupsqview/src/main/java/com/example/birotdownupsqview/BiRotDownUpSqView.kt
+++ birotdownupsqview/src/main/java/com/example/birotdownupsqview/BiRotDownUpSqView.kt
@@ -194,4 +194,27 @@ class BiRotDownUpSqView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiRotDownUpSqView) {
+
+        private val animator : Animator = Animator(view)
+        private val brdus : BiRotDownUp = BiRotDownUp(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            brdus.draw(canvas, paint)
+            animator.animate {
+                brdus.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            brdus.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
