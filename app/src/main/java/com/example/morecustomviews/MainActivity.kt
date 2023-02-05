5c118804aef2dbe18f7de0d7a4e8b90605ef4672
Anwesh43 <anweshthecool0@gmail.com>
2023-02-05 07:46:34 +0530

    sqblockfourarcview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.birotdownupsqview.BiRotDownUpSqView
+import com.example.sqblockfourarcview.SqBlockFourArcView
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
 
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        BiRotDownUpSqView.create(this)
+        SqBlockFourArcView.create(this)
         fullScreen()
     }
 }
