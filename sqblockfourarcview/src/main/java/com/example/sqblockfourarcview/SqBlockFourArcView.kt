8ce99bbfe7288c9dc18d3c04f6a49dcc31a7dbd8
Anwesh43 <anweshthecool0@gmail.com>
2023-02-05 07:27:14 +0530

    sqblockfourarcview: adding SqBlockFourArcView class

--- sqblockfourarcview/src/main/java/com/example/sqblockfourarcview/SqBlockFourArcView.kt
+++ sqblockfourarcview/src/main/java/com/example/sqblockfourarcview/SqBlockFourArcView.kt
@@ -74,3 +74,19 @@ fun Canvas.drawSBFANode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawSqBlockFourArc(scale, w, h, paint)
 }
+
+class SqBlockFourArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
