27f8354f8f9f8346ed4fa7e69c3189283d5532c4
Anwesh43 <anweshthecool0@gmail.com>
2023-02-05 07:43:55 +0530

    sqblockfourarcview: adding renderer in view

--- sqblockfourarcview/src/main/java/com/example/sqblockfourarcview/SqBlockFourArcView.kt
+++ sqblockfourarcview/src/main/java/com/example/sqblockfourarcview/SqBlockFourArcView.kt
@@ -76,15 +76,16 @@ fun Canvas.drawSBFANode(i : Int, scale : Float, paint : Paint) {
 }
 
 class SqBlockFourArcView(ctx : Context) : View(ctx) {
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
