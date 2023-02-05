eb2447f2d700981cb5218f770bf6db658c3687ba
Anwesh43 <anweshthecool0@gmail.com>
2023-02-05 07:38:30 +0530

    sqblockfourarcview: adding SBFANode class

--- sqblockfourarcview/src/main/java/com/example/sqblockfourarcview/SqBlockFourArcView.kt
+++ sqblockfourarcview/src/main/java/com/example/sqblockfourarcview/SqBlockFourArcView.kt
@@ -137,4 +137,45 @@ class SqBlockFourArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBFANode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SBFANode? = null
+        private var prev : SBFANode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SBFANode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBFANode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBFANode {
+            var curr : SBFANode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
