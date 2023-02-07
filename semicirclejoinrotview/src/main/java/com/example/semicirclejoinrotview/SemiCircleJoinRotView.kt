5e34f08cce521a41f5abc2f0636f6ad03fe30951
Anwesh43 <anweshthecool0@gmail.com>
2023-02-07 07:04:18 +0530

    semicirclejoinrotview: adding Canvas extension methods

--- semicirclejoinrotview/src/main/java/com/example/semicirclejoinrotview/SemiCircleJoinRotView.kt
+++ semicirclejoinrotview/src/main/java/com/example/semicirclejoinrotview/SemiCircleJoinRotView.kt
@@ -29,3 +29,33 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSemiCircleJoinRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2) * dsc(3)) {
+        rotate(deg * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(rot * dsc(1))
+                drawArc(RectF(0f, -size / 2, size, size / 2), deg, deg * dsc(0), true, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawSCJRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawSemiCircleJoinRot(scale, w, h, paint)
+}
