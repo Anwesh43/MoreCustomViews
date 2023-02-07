0135a6015412d4e5f72c90c683d64b609be3f07f
Anwesh43 <anweshthecool0@gmail.com>
2023-02-07 07:18:04 +0530

    semicirclejoinrotview: adding Renderer class

--- semicirclejoinrotview/src/main/java/com/example/semicirclejoinrotview/SemiCircleJoinRotView.kt
+++ semicirclejoinrotview/src/main/java/com/example/semicirclejoinrotview/SemiCircleJoinRotView.kt
@@ -186,4 +186,27 @@ class SemiCircleJoinRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SemiCircleJoinRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val scjr : SemiCircleJoinRot = SemiCircleJoinRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            scjr.draw(canvas, paint)
+            animator.animate {
+                scjr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            scjr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
