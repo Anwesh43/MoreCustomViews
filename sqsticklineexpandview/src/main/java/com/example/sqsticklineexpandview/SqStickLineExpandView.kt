cf9c8a787d8c8c55455d862dded32c32254ee67f
Anwesh43 <anweshthecool0@gmail.com>
2023-02-09 01:36:50 +0530

    sqsticklineexpandview: adding Renderer class

--- sqsticklineexpandview/src/main/java/com/example/sqsticklineexpandview/SqStickLineExpandView.kt
+++ sqsticklineexpandview/src/main/java/com/example/sqsticklineexpandview/SqStickLineExpandView.kt
@@ -188,4 +188,27 @@ class SqStickLineExpandView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : SqStickLineExpandView) {
+
+        private val animator : Animator = Animator(view)
+        private val ssle : SqStickLineExpand = SqStickLineExpand(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            ssle.draw(canvas, paint)
+            animator.animate {
+                ssle.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            ssle.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
