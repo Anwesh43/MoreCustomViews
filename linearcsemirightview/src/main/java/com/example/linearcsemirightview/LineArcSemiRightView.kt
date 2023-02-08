25dcf69bfa371b1c8098d7a5bbba103f49abb7fc
Anwesh43 <anweshthecool0@gmail.com>
2023-02-08 08:23:35 +0530

    linearcsemirightview: adding LASRNode class

--- linearcsemirightview/src/main/java/com/example/linearcsemirightview/LineArcSemiRightView.kt
+++ linearcsemirightview/src/main/java/com/example/linearcsemirightview/LineArcSemiRightView.kt
@@ -123,4 +123,45 @@ class LineArcSemiRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LASRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LASRNode? = null
+        private var prev : LASRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LASRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLASRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LASRNode {
+            var curr : LASRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
