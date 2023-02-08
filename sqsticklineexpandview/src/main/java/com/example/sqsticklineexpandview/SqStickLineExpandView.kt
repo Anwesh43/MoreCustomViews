84a73eaf900b343c0561c29e0d316fb064e66089
Anwesh43 <anweshthecool0@gmail.com>
2023-02-09 01:26:28 +0530

    sqsticklineexpandview: adding Canvas extension methods

--- sqsticklineexpandview/src/main/java/com/example/sqsticklineexpandview/SqStickLineExpandView.kt
+++ sqsticklineexpandview/src/main/java/com/example/sqsticklineexpandview/SqStickLineExpandView.kt
@@ -30,3 +30,34 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqStickLineExpand(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sqSize : Float = Math.min(w, h) / sqFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2 ) * dsc(4), h / 2) {
+        rotate(-rot * dsc(3))
+        drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        drawXY(w / 2 * (1 - dsc(1)), 0f) {
+            drawRect(RectF(0f, -sqSize - (size - sqSize) * dsc(2), size, 0f), paint)
+        }
+    }
+}
+
+fun Canvas.drawSSLENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSqStickLineExpand(scale, w, h, paint)
+}
