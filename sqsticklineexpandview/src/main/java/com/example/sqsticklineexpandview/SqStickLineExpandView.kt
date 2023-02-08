27e8a1fbee8abdecf36be4c9d1fef67134b9e090
Anwesh43 <anweshthecool0@gmail.com>
2023-02-09 01:27:56 +0530

    sqsticklineexpandview: adding SqStickLineExpandView class

--- sqsticklineexpandview/src/main/java/com/example/sqsticklineexpandview/SqStickLineExpandView.kt
+++ sqsticklineexpandview/src/main/java/com/example/sqsticklineexpandview/SqStickLineExpandView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawSSLENode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSqStickLineExpand(scale, w, h, paint)
 }
+
+class SqStickLineExpandView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
