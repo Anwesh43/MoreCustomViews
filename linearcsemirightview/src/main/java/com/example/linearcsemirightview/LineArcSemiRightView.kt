2a06f68be89cc0a0caa0c83b8092eb9c09be87a2
Anwesh43 <anweshthecool0@gmail.com>
2023-02-08 08:16:56 +0530

    linearcsemirightview: adding Canvas extension methods

--- linearcsemirightview/src/main/java/com/example/linearcsemirightview/LineArcSemiRightView.kt
+++ linearcsemirightview/src/main/java/com/example/linearcsemirightview/LineArcSemiRightView.kt
@@ -30,3 +30,33 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcSemiRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w * 0.5f * (1 + dsc(4)), h / 2) {
+        drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        drawXY((size - r) * dsc(2), 0f) {
+            drawArc(RectF(0f, -r / 2, r, r / 2), rot, rot * (dsc(1) + dsc(3)), true, paint)
+        }
+    }
+}
+
+fun Canvas.drawLASRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcSemiRight(scale, w, h, paint)
+}
