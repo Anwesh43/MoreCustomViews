2b2a904a14550200f0749a3a1a256bad328d8fc2
Anwesh43 <anweshthecool0@gmail.com>
2023-02-09 01:38:42 +0530

    sqsticklineexpandview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linearcsemirightview.LineArcSemiRightView
+import com.example.sqsticklineexpandview.SqStickLineExpandView
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
 
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineArcSemiRightView.create(this)
+        SqStickLineExpandView.create(this)
         fullScreen()
     }
 }
