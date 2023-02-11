013dcee805d95a72ba2f737b962ea8a13b417913
Anwesh43 <anweshthecool0@gmail.com>
2023-02-11 10:15:39 +0530

    barballjoinrightview : adding BarBallJoinRight class

--- barballjoinrightview/src/main/java/com/example/barballjoinrightview/BarBallJoinRightView.kt
+++ barballjoinrightview/src/main/java/com/example/barballjoinrightview/BarBallJoinRightView.kt
@@ -147,6 +147,10 @@ class BarBallJoinRightView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
         fun getNext(dir : Int, cb : () -> Unit) : BBJRNode {
             var curr : BBJRNode? = prev
             if (dir == 1) {
@@ -157,6 +161,29 @@ class BarBallJoinRightView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class BarBallJoinRight(var i : Int) {
+
+        private var curr : BBJRNode = BBJRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
