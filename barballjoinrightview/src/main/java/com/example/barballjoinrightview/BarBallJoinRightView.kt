793a30f7c31449c41e605746fe74f6b53b6f6b42
Anwesh43 <anweshthecool0@gmail.com>
2023-02-11 09:43:05 +0530

    barballjoinrightview : adding BarBallJoinRightView class

--- barballjoinrightview/src/main/java/com/example/barballjoinrightview/BarBallJoinRightView.kt
+++ barballjoinrightview/src/main/java/com/example/barballjoinrightview/BarBallJoinRightView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawBBJRNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawBarBallJoinRight(scale, w, h, paint)
 }
+
+class BarBallJoinRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
