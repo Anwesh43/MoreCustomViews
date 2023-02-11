67afb50d23cc5b44ad5d9d19c3ee6047742213bd
Anwesh43 <anweshthecool0@gmail.com>
2023-02-11 10:26:42 +0530

    barballjoinrightview : adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotsqdownview.LineRotSqDownView
+import com.example.barballjoinrightview.BarBallJoinRightView
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
 
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineRotSqDownView.create(this)
+        BarBallJoinRightView.create(this)
         fullScreen()
     }
 }
