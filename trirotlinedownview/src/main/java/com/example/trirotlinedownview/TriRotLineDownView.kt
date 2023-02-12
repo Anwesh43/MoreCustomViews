dd23eef2fdd3bd3e61682b1189c1489aafb898e0
Anwesh43 <anweshthecool0@gmail.com>
2023-02-12 11:11:58 +0530

    trirotlinedownview: adding Canvas extension methods

--- trirotlinedownview/src/main/java/com/example/trirotlinedownview/TriRotLineDownView.kt
+++ trirotlinedownview/src/main/java/com/example/trirotlinedownview/TriRotLineDownView.kt
@@ -8,6 +8,7 @@ import android.graphics.Paint
 import android.graphics.Color
 import android.graphics.Canvas
 import android.graphics.RectF
+import android.graphics.Path
 
 val colors : Array<Int> = arrayOf(
     "#1A237E",
@@ -30,3 +31,48 @@ val sizeFactor : Float = 4.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTriangle(x : Float, y : Float, size : Float, paint : Paint) {
+    drawXY(x, y) {
+        val path : Path = Path()
+        path.moveTo(0f, -size / 2)
+        path.lineTo(size, 0f)
+        path.lineTo(0f, size / 2)
+        path.lineTo(0f, -size / 2)
+        drawPath(path, paint)
+    }
+}
+
+fun Canvas.drawTriRotLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val triSize : Float = Math.min(w, h) / triSizeFactor
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(4)) {
+        drawXY(-w / 2 + (w / 2) * dsc(0), 0f) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, -size, 0f, paint)
+        }
+        drawXY(w / 2 - (w / 2) * dsc(1), 0f) {
+            rotate(rot * dsc(3))
+            drawTriangle(0f, 0f, triSize, paint)
+        }
+    }
+}
+
+fun Canvas.drawTRLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTriRotLine(scale, w, h, paint)
+}
