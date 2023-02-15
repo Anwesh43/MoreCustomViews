bc126eeee4e5ad384059a6f96397ec24f4f8f315
Anwesh43 <anweshthecool0@gmail.com>
2023-02-15 08:02:22 +0530

    balllinealternaterotview: adding Canvas extension methods

--- balllinealternaterotview/src/main/java/com/example/balllinealternaterotview/BallLineAlternateRotView.kt
+++ balllinealternaterotview/src/main/java/com/example/balllinealternaterotview/BallLineAlternateRotView.kt
@@ -25,7 +25,40 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
+val rFactor : Float = 16.2f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBallLineAlternateRot(scale : Float, w : Float, h : Float, sf : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = Math.min(w, h) / rFactor
+    drawXY(w / 2 + (w / 2 + size) * sf, h / 2) {
+        rotate(rot * dsc(2) * sf)
+        drawXY(0f, h / 2 * (1 - dsc(0))) {
+            drawLine(0f, 0f, 0f, size, paint)
+        }
+        drawCircle(-(w / 2 + r) * (1 - dsc(1)), -r, r, paint)
+    }
+}
+
+fun Canvas.drawBLARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBallLineAlternateRot(scale, w, h, 1f - 2 * (i % 2), paint)
+}
+
