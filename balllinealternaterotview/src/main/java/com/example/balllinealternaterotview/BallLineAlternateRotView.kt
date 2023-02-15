38a84b529d7786ec1bbeb4d243e24eebc6ef9ca6
Anwesh43 <anweshthecool0@gmail.com>
2023-02-15 08:13:00 +0530

    balllinealternaterotview: adding BLARNode class

--- balllinealternaterotview/src/main/java/com/example/balllinealternaterotview/BallLineAlternateRotView.kt
+++ balllinealternaterotview/src/main/java/com/example/balllinealternaterotview/BallLineAlternateRotView.kt
@@ -124,4 +124,45 @@ class BallLineAlternateRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLARNode? = null
+        private var prev : BLARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLARNode {
+            var curr : BLARNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
