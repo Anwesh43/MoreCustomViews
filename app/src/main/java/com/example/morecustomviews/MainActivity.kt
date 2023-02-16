6484dc212338bee75de812d26c21052741515324
Anwesh43 <anweshthecool0@gmail.com>
2023-02-16 15:57:25 +0530

    sqarcrotrightview: removing library

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.balllinealternaterotview.BallLineAlternateRotView
 import com.example.sqarcrotrightview.SqArcRotRightView
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
