d49e2f30ae0064647c40d3f2d865b762d44b71bf
Anwesh43 <anweshthecool0@gmail.com>
2023-02-17 10:54:13 +0530

    linerotbiardcdownview: adding LineRotBiArcDownView class

--- linerotbiarcdownview/src/main/java/com/example/linerotbiarcdownview/LineRotBiArcDownView.kt
+++ linerotbiarcdownview/src/main/java/com/example/linerotbiarcdownview/LineRotBiArcDownView.kt
@@ -177,4 +177,27 @@ class LineRotBiArcDownView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRotBiArcDown(var i : Int) {
+
+        private var curr : LRADNode = LRADNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
