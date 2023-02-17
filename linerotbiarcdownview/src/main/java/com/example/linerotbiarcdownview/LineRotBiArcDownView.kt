4ce4c2b30e38179d7e0111c0812fe2a9a6ae282e
Anwesh43 <anweshthecool0@gmail.com>
2023-02-17 10:00:17 +0530

    linerotbiarcdownview: adding Canvas extension methods

--- linerotbiarcdownview/src/main/java/com/example/linerotbiarcdownview/LineRotBiArcDownView.kt
+++ linerotbiarcdownview/src/main/java/com/example/linerotbiarcdownview/LineRotBiArcDownView.kt
@@ -25,7 +25,51 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
+val arcSizeFactor : Float = 2f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int,  n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineWithoutDot(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    if (Math.abs(x1 - x2) < 0.1 && Math.abs(y1 - y2) < 0.1) {
+        return
+    }
+    drawLine(x1, y1, x2, y2, paint)
+}
+
+fun Canvas.drawLineRotArcDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / strokeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val arcSize : Float = size / arcSizeFactor
+    val r : Float = arcSize / 2
+    drawXY(w / 2, h / 2) {
+        for(j in 0..1) {
+            drawXY(0f, 0f) {
+                rotate(-rot * dsc(1) * j)
+                drawLineWithoutDot(0f, 0f, size * dsc(0), 0f, paint)
+            }
+            drawXY(arcSize * j + r, 0f) {
+                drawArc(RectF(-r, -r, r, r), 0f, 180f * dsc(2), true, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLRADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotArcDown(scale, w, h, paint)
+}
