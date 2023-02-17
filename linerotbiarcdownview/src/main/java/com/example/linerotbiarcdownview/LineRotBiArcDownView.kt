9698cfb9e46daf4f7cdb6e780de482315168af24
Anwesh43 <anweshthecool0@gmail.com>
2023-02-17 10:05:24 +0530

    linerotbiarcdownview: adding LineRotBiArcDownView class

--- linerotbiarcdownview/src/main/java/com/example/linerotbiarcdownview/LineRotBiArcDownView.kt
+++ linerotbiarcdownview/src/main/java/com/example/linerotbiarcdownview/LineRotBiArcDownView.kt
@@ -73,3 +73,19 @@ fun Canvas.drawLRADNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotArcDown(scale, w, h, paint)
 }
+
+class LineRotBiArcDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
