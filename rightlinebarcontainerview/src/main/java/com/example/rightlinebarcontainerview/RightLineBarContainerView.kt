123a68f6738e3d54c2a2cbdbc89df506252bde36
Anwesh43 <anweshthecool0@gmail.com>
2023-02-18 09:09:12 +0530

    rightlinebarcontainerview: adding Canvas extension methods

--- rightlinebarcontainerview/src/main/java/com/example/rightlinebarcontainerview/RightLineBarContainerView.kt
+++ rightlinebarcontainerview/src/main/java/com/example/rightlinebarcontainerview/RightLineBarContainerView.kt
@@ -20,13 +20,47 @@ val colors : Array<Int> = arrayOf(
 }.toTypedArray()
 val parts : Int = 5
 val scGap : Float = 0.04f / parts
-val strokeFactor : Float = 4.9f
+val sizeFactor : Float = 4.9f
 val barHFactor : Float = 11.2f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 90f
 val deg : Float = 270f
+val strokeFactor : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRightLineBarContainer(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val barH : Float = Math.min(w, h) / barHFactor
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            rotate((-rot * dsc(1) - deg * dsc(3)) * j)
+            drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        }
+        drawXY((w / 2) * (1 - dsc(2)), 0f) {
+            drawRect(RectF(0f, -barH, size,0f), paint)
+        }
+    }
+}
+
+fun Canvas.drawRLBCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRightLineBarContainer(scale, w, h, paint)
+}
