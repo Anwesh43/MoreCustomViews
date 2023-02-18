dc75de884357b49f0b0c0c980534ac23dd5b6d11
Anwesh43 <anweshthecool0@gmail.com>
2023-02-18 09:13:21 +0530

    rightlinebarcontainerview: adding RightLineBarContainerView class

--- rightlinebarcontainerview/src/main/java/com/example/rightlinebarcontainerview/RightLineBarContainerView.kt
+++ rightlinebarcontainerview/src/main/java/com/example/rightlinebarcontainerview/RightLineBarContainerView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawRLBCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawRightLineBarContainer(scale, w, h, paint)
 }
+
+class RightLineBarContainerView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
