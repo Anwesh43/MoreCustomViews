8a537cd80b62bc483a4c2bbbce9b0a451e5abd1d
Anwesh43 <anweshthecool0@gmail.com>
2023-02-19 10:05:59 +0530

    linerightmoverotview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.rightlinebarcontainerview.RightLineBarContainerView
+import com.example.linerightmoverotview.LineRightMoveRotView
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
 
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        RightLineBarContainerView.create(this)
+        LineRightMoveRotView.create(this)
         fullScreen()
     }
 }
