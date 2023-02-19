c1c55908e61a9bad28676491ccc7f5b391f78624
Anwesh43 <anweshthecool0@gmail.com>
2023-02-19 09:05:19 +0530

    linerightmoverotview: adding Canvas extension methods

--- linerightmoverotview/src/main/java/com/example/linerightmoverotview/LineRightMoveRotView.kt
+++ linerightmoverotview/src/main/java/com/example/linerightmoverotview/LineRightMoveRotView.kt
@@ -29,3 +29,43 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineWithoutDot(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    if (Math.abs(x1 - x2) < 0.1f && Math.abs(y1 - y2) < 0.1f) {
+        return
+    }
+    drawLine(x1, y1, x2, y2, paint)
+}
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRightMoveRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        drawXY(0f, -h * 0.5f * (1 - dsc(0))) {
+            rotate(rot * dsc(2))
+            drawLineWithoutDot(0f, 0f, 0f, -size, paint)
+        }
+
+        drawXY(0f, h * 0.5f * (1 - dsc(1))) {
+            drawLineWithoutDot(0f, 0f, size, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawLRMRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRightMoveRot(scale, w, h, paint)
+}
