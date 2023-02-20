c39df21a6d557243779ee178e3d2463558d8eb89
Anwesh43 <anweshthecool0@gmail.com>
2023-02-20 09:54:47 +0530

    linerotintosingleview: adding LRISNode class

--- linerotintosingleview/src/main/java/com/example/linerotintosingleview/LineRotIntoSingleView.kt
+++ linerotintosingleview/src/main/java/com/example/linerotintosingleview/LineRotIntoSingleView.kt
@@ -125,4 +125,45 @@ class LineRotIntoSingleView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRISNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRISNode? = null
+        private var prev : LRISNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRISNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRISNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRISNode {
+            var curr : LRISNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
