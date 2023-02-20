d78df72f71a5f3548034263253dcbc3db40ee272
Anwesh43 <anweshthecool0@gmail.com>
2023-02-20 09:56:28 +0530

    linerotintosingleview: adding LineRotIntoSingle class

--- linerotintosingleview/src/main/java/com/example/linerotintosingleview/LineRotIntoSingleView.kt
+++ linerotintosingleview/src/main/java/com/example/linerotintosingleview/LineRotIntoSingleView.kt
@@ -166,4 +166,27 @@ class LineRotIntoSingleView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRotIntoSingle(var i : Int) {
+
+        private var curr : LRISNode = LRISNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
