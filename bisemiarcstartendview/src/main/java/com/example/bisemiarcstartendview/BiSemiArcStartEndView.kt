b1eff5395b335a97a32618495ddaf4a2978fa3ff
Anwesh43 <anweshthecool0@gmail.com>
2023-02-24 09:09:01 +0530

    bisemiarcstartendview: adding Renderer class

--- bisemiarcstartendview/src/main/java/com/example/bisemiarcstartendview/BiSemiArcStartEndView.kt
+++ bisemiarcstartendview/src/main/java/com/example/bisemiarcstartendview/BiSemiArcStartEndView.kt
@@ -187,4 +187,27 @@ class BiSemiArcStartEndView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BiSemiArcStartEndView) {
+
+        private val animator : Animator = Animator(view)
+        private val bsase : BiSemiArcStartEnd = BiSemiArcStartEnd(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bsase.draw(canvas, paint)
+            animator.animate {
+                bsase.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bsase.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
