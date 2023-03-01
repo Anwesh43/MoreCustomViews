569a23e3205adca2ab0acec7a8862577fc63d6f6
Anwesh43 <anweshthecool0@gmail.com>
2023-03-01 07:05:11 +0530

    linebendrotview: adding LineBendRotRightView class

--- linebendrotrightview/src/main/java/com/example/linebendrotrightview/LineBendRotRightView.kt
+++ linebendrotrightview/src/main/java/com/example/linebendrotrightview/LineBendRotRightView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLBRRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBendRotRight(scale, w, h, paint)
 }
+
+class LineBendRotRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
