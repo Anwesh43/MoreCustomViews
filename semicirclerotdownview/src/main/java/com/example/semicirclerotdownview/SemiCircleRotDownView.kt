fde84622687141ca86e8ca12b3e831b938ca3b01
Anwesh43 <anweshthecool0@gmail.com>
2023-03-03 23:30:02 +0530

    semicirclerotdownview: adding fixes

--- semicirclerotdownview/src/main/java/com/example/semicirclerotdownview/SemiCircleRotDownView.kt
+++ semicirclerotdownview/src/main/java/com/example/semicirclerotdownview/SemiCircleRotDownView.kt
@@ -54,6 +54,7 @@ fun Canvas.drawSCRNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     paint.strokeCap = Paint.Cap.ROUND
     paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.style = Paint.Style.STROKE
     drawSemiCircleRot(scale, w, h, paint)
 }
 
