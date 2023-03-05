eb11ae293fb4f0d85a1ed5f69e772efea7e049a0
Anwesh43 <anweshthecool0@gmail.com>
2023-03-05 16:24:37 +0530

    stepballldroplineview: adding SBDLNode class

--- stepballldroplineview/src/main/java/com/example/stepballldroplineview/StepBallDropLineView.kt
+++ stepballldroplineview/src/main/java/com/example/stepballldroplineview/StepBallDropLineView.kt
@@ -127,4 +127,45 @@ class StepBallDropLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SBDLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SBDLNode? = null
+        private var prev : SBDLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SBDLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSBDLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SBDLNode {
+            var curr : SBDLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
