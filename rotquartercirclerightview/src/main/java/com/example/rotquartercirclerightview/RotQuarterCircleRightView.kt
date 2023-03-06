ebac8f15cf463361a01bf60b2796544659484e46
Anwesh43 <anweshthecool0@gmail.com>
2023-03-06 17:30:31 +0530

    rotquartercirclerightview: adding RotQuarterCircleRight class

--- rotquartercirclerightview/src/main/java/com/example/rotquartercirclerightview/RotQuarterCircleRightView.kt
+++ rotquartercirclerightview/src/main/java/com/example/rotquartercirclerightview/RotQuarterCircleRightView.kt
@@ -163,4 +163,27 @@ class RotQuarterCircleRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class RotQuarterCircleRight(var i : Int) {
+
+        private var curr : RQCRNode = RQCRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
