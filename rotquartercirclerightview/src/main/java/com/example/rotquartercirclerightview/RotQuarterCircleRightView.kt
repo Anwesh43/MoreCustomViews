bda88fcf4e1aa212c7a1a3ec1b9dd0c8d90b73ad
Anwesh43 <anweshthecool0@gmail.com>
2023-03-06 14:38:36 +0530

    rotquartercirclerightview: adding Canvas exension methods

--- rotquartercirclerightview/src/main/java/com/example/rotquartercirclerightview/RotQuarterCircleRightView.kt
+++ rotquartercirclerightview/src/main/java/com/example/rotquartercirclerightview/RotQuarterCircleRightView.kt
@@ -29,3 +29,33 @@ val strokeFactor : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRotQuarterCircleRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawXY(0f, h * 0.5f * (1 - dsc(1))) {
+            drawLine(0f, 0f, 0f, size, paint)
+        }
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -90f, rot * dsc(0), true, paint)
+    }
+}
+
+fun Canvas.drawRQCRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRotQuarterCircleRight(scale, w, h, paint)
+}
