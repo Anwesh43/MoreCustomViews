149f9753a75acdd9d2299d8fffb84366b38b85f2
Anwesh43 <anweshthecool0@gmail.com>
2023-03-06 17:28:22 +0530

    rotquartercirclerightview: adding RQCRNode class

--- rotquartercirclerightview/src/main/java/com/example/rotquartercirclerightview/RotQuarterCircleRightView.kt
+++ rotquartercirclerightview/src/main/java/com/example/rotquartercirclerightview/RotQuarterCircleRightView.kt
@@ -122,4 +122,45 @@ class RotQuarterCircleRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RQCRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RQCRNode? = null
+        private var prev : RQCRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RQCRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRQCRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RQCRNode {
+            var curr : RQCRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
