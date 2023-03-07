04dfc8ff3c869bade2842edfa1cd30d5da89481f
Anwesh43 <anweshthecool0@gmail.com>
2023-03-08 02:18:37 +0530

    linesqexprotview: adding LSERNode class

--- linesqexprotview/src/main/java/com/example/linesqexprotview/LineSqExpRotView.kt
+++ linesqexprotview/src/main/java/com/example/linesqexprotview/LineSqExpRotView.kt
@@ -120,4 +120,45 @@ class LineSqExpRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSERNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LSERNode? = null
+        private var next : LSERNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LSERNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSERNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSERNode {
+            var curr : LSERNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
