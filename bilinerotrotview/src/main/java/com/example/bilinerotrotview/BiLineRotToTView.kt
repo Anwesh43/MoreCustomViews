538face07e39827b5e9b2a556fd33e8ac5136158
Anwesh43 <anweshthecool0@gmail.com>
2023-03-07 08:16:50 +0530

    bilinerottotview: adding BiLineRotToTView class

--- bilinerotrotview/src/main/java/com/example/bilinerotrotview/BiLineRotToTView.kt
+++ bilinerotrotview/src/main/java/com/example/bilinerotrotview/BiLineRotToTView.kt
@@ -164,4 +164,27 @@ class BiLineRotToTView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BiLineRotToTView(var i : Int = 0) {
+
+        private var curr : BLRTTNode = BLRTTNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdaitng(cb)
+        }
+    }
 }
