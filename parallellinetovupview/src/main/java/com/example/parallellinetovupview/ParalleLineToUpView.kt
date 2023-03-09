d8f87283d921232f338f5f61a8009fb21d1e8c1e
Anwesh43 <anweshthecool0@gmail.com>
2023-03-09 06:34:57 +0530

    parallellinetovupview: adding Renderer class

--- parallellinetovupview/src/main/java/com/example/parallellinetovupview/ParalleLineToUpView.kt
+++ parallellinetovupview/src/main/java/com/example/parallellinetovupview/ParalleLineToUpView.kt
@@ -190,4 +190,27 @@ class ParallelLineToUpView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : ParallelLineToUpView) {
+
+        private val animator : Animator = Animator(view)
+        private val pltu : ParallelLineToUp = ParallelLineToUp(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            pltu.draw(canvas, paint)
+            animator.animate {
+                pltu.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            pltu.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
