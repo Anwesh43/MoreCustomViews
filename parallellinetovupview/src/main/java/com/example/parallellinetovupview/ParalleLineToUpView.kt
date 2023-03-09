0f027b6c5f307439c79ba6fd4855cb7dd720c411
Anwesh43 <anweshthecool0@gmail.com>
2023-03-09 06:19:15 +0530

    parallellinetovupview: adding ParallelLineToUpView class

--- parallellinetovupview/src/main/java/com/example/parallellinetovupview/ParalleLineToUpView.kt
+++ parallellinetovupview/src/main/java/com/example/parallellinetovupview/ParalleLineToUpView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawPLTUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawParallelLineToUp(scale, w, h, paint)
 }
+
+class ParallelLineToUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
