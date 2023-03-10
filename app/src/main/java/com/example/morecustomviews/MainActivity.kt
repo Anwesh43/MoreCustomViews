6d640067e1bbbaa8b229542933496a77ea0c3b15
Anwesh43 <anweshthecool0@gmail.com>
2023-03-10 17:32:11 +0530

    jumpshrinkbarview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.parallellinetovupview.ParallelLineToUpView
+import com.example.jumpshrinkbarview.JumpShrinkBarView
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
 
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        ParallelLineToUpView.create(this)
+        JumpShrinkBarView.create(this)
         fullScreen()
     }
 }
