4352bf93cd4da4d74bfd83770a6a6edad6c1a022
Anwesh43 <anweshthecool0@gmail.com>
2023-03-10 16:53:35 +0530

    jumpshrinkbarview: adding JSBNode class

--- jumpshrinkbarview/src/main/java/com/example/jumpshrinkbarview/JumpShrinkBarView.kt
+++ jumpshrinkbarview/src/main/java/com/example/jumpshrinkbarview/JumpShrinkBarView.kt
@@ -121,4 +121,45 @@ class JumpShrinkBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class JSBNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : JSBNode? = null
+        private var prev : JSBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = JSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawJSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNet(dir : Int, cb : () -> Unit) : JSBNode {
+            var curr : JSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
