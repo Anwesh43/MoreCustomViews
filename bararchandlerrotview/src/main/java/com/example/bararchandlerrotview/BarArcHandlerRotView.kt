2fa2eec64a1fd017e37eabd7252f57cac2625392
Anwesh43 <anweshthecool0@gmail.com>
2023-03-11 07:45:51 +0530

    bararchandlerotview: adding Canvas extension methods

--- bararchandlerrotview/src/main/java/com/example/bararchandlerrotview/BarArcHandlerRotView.kt
+++ bararchandlerrotview/src/main/java/com/example/bararchandlerrotview/BarArcHandlerRotView.kt
@@ -29,3 +29,33 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int,  n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarArcHandler(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        paint.style = Paint.Style.FILL
+        drawRect(RectF(-size / 2, -size * 0.25f * dsc(0), size / 2, 0f), paint)
+        paint.style = Paint.Style.STROKE
+        drawArc(RectF(-size / 2, -size / 2, -size / 4, size / 4), 0f, rot * dsc(1), false, paint)
+    }
+}
+
+fun Canvas.drawBAHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarArcHandler(scale, w, h, paint)
+}
