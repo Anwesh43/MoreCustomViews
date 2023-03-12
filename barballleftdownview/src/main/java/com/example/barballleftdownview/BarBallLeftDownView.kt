b95e6161ee7cfcc7b0ac5fcc5219ce01bfe9a195
Anwesh43 <anweshthecool0@gmail.com>
2023-03-12 12:07:01 +0530

    barballjoinrightiview: adding Canvas extension methods

--- barballleftdownview/src/main/java/com/example/barballleftdownview/BarBallLeftDownView.kt
+++ barballleftdownview/src/main/java/com/example/barballleftdownview/BarBallLeftDownView.kt
@@ -29,3 +29,31 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarBallLeftDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawRect(RectF(-size, -size * dsc(0), 0f, 0f), paint)
+        drawXY((w / 2) * (1 - dsc(1)), 0f) {
+            drawArc(RectF(0f, -size, size, 0f), 0f, 360f, true, paint)
+        }
+    }
+}
+
+fun Canvas.drawBBLDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBarBallLeftDown(scale, w, h, paint)
+}
