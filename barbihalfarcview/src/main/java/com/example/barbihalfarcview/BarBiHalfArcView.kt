cebe4556e336e61b5d1dca6a5483a19225bface4
Anwesh43 <anweshthecool0@gmail.com>
2023-03-14 07:56:48 +0530

    barbihalfarcview: adding Canvas extension methods

--- barbihalfarcview/src/main/java/com/example/barbihalfarcview/BarBiHalfArcView.kt
+++ barbihalfarcview/src/main/java/com/example/barbihalfarcview/BarBiHalfArcView.kt
@@ -29,3 +29,36 @@ val delay : Long = 20
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int,  n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarBiHalfArc(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawRect(RectF(-size / 2, -size * 0.25f * dsc(0), size / 2, 0f), paint)
+        for (j in 0..1) {
+            drawXY(0f, -size / 4) {
+                scale(1f - 2 * j, 1f)
+                drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(1), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBBHANode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    drawBarBiHalfArc(scale, w, h, paint)
+}
