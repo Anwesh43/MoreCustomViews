cbf7f2e9bbd79e3c9996436080fc2a170468ba8b
Anwesh43 <anweshthecool0@gmail.com>
2023-03-14 07:58:51 +0530

    barbihalfarcview: adding BarBiHalfArcView class

--- barbihalfarcview/src/main/java/com/example/barbihalfarcview/BarBiHalfArcView.kt
+++ barbihalfarcview/src/main/java/com/example/barbihalfarcview/BarBiHalfArcView.kt
@@ -62,3 +62,19 @@ fun Canvas.drawBBHANode(i : Int, scale : Float, paint : Paint) {
     paint.strokeCap = Paint.Cap.ROUND
     drawBarBiHalfArc(scale, w, h, paint)
 }
+
+class BarBiHalfArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
