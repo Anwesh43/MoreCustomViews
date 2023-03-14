b079894a3e70f4c7249119e648d4fa0da9c11ba3
Anwesh43 <anweshthecool0@gmail.com>
2023-03-14 08:18:04 +0530

    barbihalfarcview: adding static create method

--- barbihalfarcview/src/main/java/com/example/barbihalfarcview/BarBiHalfArcView.kt
+++ barbihalfarcview/src/main/java/com/example/barbihalfarcview/BarBiHalfArcView.kt
@@ -214,4 +214,12 @@ class BarBiHalfArcView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+        fun create(activity : Activity) : BarBiHalfArcView {
+            val view : BarBiHalfArcView = BarBiHalfArcView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
