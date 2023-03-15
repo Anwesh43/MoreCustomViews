b36c68cac75566619cec59a9877b1355bd8956ae
Anwesh43 <anweshthecool0@gmail.com>
2023-03-16 00:58:48 +0530

    arcrotdividelineview: adding ARDLNode class

--- arcrotdividelineview/src/main/java/com/example/arcrotdividelineview/ArcRotDivideLineView.kt
+++ arcrotdividelineview/src/main/java/com/example/arcrotdividelineview/ArcRotDivideLineView.kt
@@ -128,4 +128,45 @@ class ArcRotDivideLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ARDLNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : ARDLNode? = null
+        private var next : ARDLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ARDLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawARDLNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ARDLNode {
+            var curr : ARDLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
