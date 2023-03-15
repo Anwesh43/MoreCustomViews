5c3073aa3a64b41829bd623e70a060e31224e526
Anwesh43 <anweshthecool0@gmail.com>
2023-03-15 08:31:43 +0530

    bowcreatorrotview: adding BowCreatorRot class

--- bowcreatorrotview/src/main/java/com/example/bowcreatorrotview/BowCreatorRotView.kt
+++ bowcreatorrotview/src/main/java/com/example/bowcreatorrotview/BowCreatorRotView.kt
@@ -172,4 +172,28 @@ class BowCreatorRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BowCreatorRot(var i : Int) {
+
+        private var curr: BCRNode = BCRNode(0)
+        private var dir: Int = 1
+
+        fun draw(canvas: Canvas, paint: Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb: (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb: () -> Unit) {
+            curr.startUpdating(cb)
+
+        }
+    }
 }
