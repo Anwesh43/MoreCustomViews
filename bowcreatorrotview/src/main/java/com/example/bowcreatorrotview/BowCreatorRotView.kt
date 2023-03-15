76cdb89c03f89d58c1fd9270b3e8359953be9fb5
Anwesh43 <anweshthecool0@gmail.com>
2023-03-15 08:24:27 +0530

    bowcreatorrotview: adding BowCreatorRotView class

--- bowcreatorrotview/src/main/java/com/example/bowcreatorrotview/BowCreatorRotView.kt
+++ bowcreatorrotview/src/main/java/com/example/bowcreatorrotview/BowCreatorRotView.kt
@@ -68,3 +68,19 @@ fun Canvas.drawBCRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBowCreatorRot(scale, w, h, paint)
 }
+
+class BowCreatorRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
