7fc7ad8582807b703b90921fa971c96bce9c7616
Anwesh43 <anweshthecool0@gmail.com>
2023-03-16 00:52:22 +0530

    arcrotdividelineview: adding Canvas extension methods

--- arcrotdividelineview/src/main/java/com/example/arcrotdividelineview/ArcRotDivideLineView.kt
+++ arcrotdividelineview/src/main/java/com/example/arcrotdividelineview/ArcRotDivideLineView.kt
@@ -29,3 +29,39 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcRotDivideLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2 - (w / 2 + size) * dsc(3) , h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                rotate(-rot * dsc(1))
+                drawArc(RectF(0f, 0f, size, -size / 2), 180f, 180f * dsc(0), false, paint)
+            }
+        }
+        drawXY(w * 0.5f * (1f - dsc(2)), 0f) {
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawARDLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.style = Paint.Style.STROKE
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcRotDivideLine(scale, w, h, paint)
+}
