c83d342f74e55f02e86f98797e090334bece729e
Anwesh43 <anweshthecool0@gmail.com>
2023-03-16 00:53:23 +0530

    arcrotdividelineview: adding ArcRotDivideLineView class

--- arcrotdividelineview/src/main/java/com/example/arcrotdividelineview/ArcRotDivideLineView.kt
+++ arcrotdividelineview/src/main/java/com/example/arcrotdividelineview/ArcRotDivideLineView.kt
@@ -65,3 +65,19 @@ fun Canvas.drawARDLNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcRotDivideLine(scale, w, h, paint)
 }
+
+class ArcRotDivideLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
