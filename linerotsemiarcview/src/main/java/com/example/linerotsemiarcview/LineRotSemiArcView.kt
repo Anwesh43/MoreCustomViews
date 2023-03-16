7ff718059787e62b08f4837bf0896fc7626fca57
Anwesh43 <anweshthecool0@gmail.com>
2023-03-17 03:49:21 +0530

    linerotsemiarcview: adding LineRotSemiArc class

--- linerotsemiarcview/src/main/java/com/example/linerotsemiarcview/LineRotSemiArcView.kt
+++ linerotsemiarcview/src/main/java/com/example/linerotsemiarcview/LineRotSemiArcView.kt
@@ -167,4 +167,27 @@ class LineRotSemiArcView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRotSemiArc(var i : Int = 0, val state : State = State()) {
+
+        private var curr : LRSANode? = null
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
