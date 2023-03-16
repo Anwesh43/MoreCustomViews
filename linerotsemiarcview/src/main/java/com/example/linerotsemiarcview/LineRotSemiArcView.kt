e234beca4997782b2ac3f91aea39aab750320c69
Anwesh43 <anweshthecool0@gmail.com>
2023-03-17 02:10:12 +0530

    linerotsemiarcview: adding LineRotSemiArcView class

--- linerotsemiarcview/src/main/java/com/example/linerotsemiarcview/LineRotSemiArcView.kt
+++ linerotsemiarcview/src/main/java/com/example/linerotsemiarcview/LineRotSemiArcView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLRSANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineRotSemiArc(scale, w, h, paint)
 }
+
+class LineRotSemiArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
