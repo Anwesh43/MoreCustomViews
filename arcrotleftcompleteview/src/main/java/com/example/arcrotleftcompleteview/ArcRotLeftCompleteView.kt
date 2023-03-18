465308f853367c8037dac200bbe2007132d87700
Anwesh43 <anweshthecool0@gmail.com>
2023-03-18 12:09:30 +0530

    arcrotleftcompleteview: adding ARLCNode class

--- arcrotleftcompleteview/src/main/java/com/example/arcrotleftcompleteview/ArcRotLeftCompleteView.kt
+++ arcrotleftcompleteview/src/main/java/com/example/arcrotleftcompleteview/ArcRotLeftCompleteView.kt
@@ -126,4 +126,45 @@ class ArcRotLeftCompleteView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ARLCNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ARLCNode? = null
+        private var prev : ARLCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ARLCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawARLCNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ARLCNode {
+            var curr : ARLCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
