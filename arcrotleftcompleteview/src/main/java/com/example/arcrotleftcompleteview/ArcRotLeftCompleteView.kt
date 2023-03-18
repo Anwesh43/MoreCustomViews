b032fca7ea51b8afe4a0b981b989a265c767d9c0
Anwesh43 <anweshthecool0@gmail.com>
2023-03-18 11:59:36 +0530

    arcrotleftcompleteview: adding ArcRotLeftCompleteView class

--- arcrotleftcompleteview/src/main/java/com/example/arcrotleftcompleteview/ArcRotLeftCompleteView.kt
+++ arcrotleftcompleteview/src/main/java/com/example/arcrotleftcompleteview/ArcRotLeftCompleteView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawARLCNode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawArcRotLeftComplete(scale, w, h, paint)
 }
+
+class ArcRotLeftCompleteView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
