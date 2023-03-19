00d85f5e29222c69a73cd576fe8c24239c33c970
Anwesh43 <anweshthecool0@gmail.com>
2023-03-19 14:03:14 +0530

    arcsweepbarleftview: adding Canvas extension methods

--- arcsweepbarleftview/src/main/java/com/example/arcsweepbarleftview/ArcSweepBarLeftView.kt
+++ arcsweepbarleftview/src/main/java/com/example/arcsweepbarleftview/ArcSweepBarLeftView.kt
@@ -29,3 +29,31 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcSweepBarLeft(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(-size, -size, size, size), 180f, 90f * dsc(0), true, paint)
+        drawXY(w * 0.5f * (1 - dsc(1)), 0f) {
+            drawRect(0f, -size, size, 0f, paint)
+        }
+    }
+}
+
+fun Canvas.drawASBLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawArcSweepBarLeft(scale, w, h, paint)
+}
