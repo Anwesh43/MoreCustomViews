d9234e55a38abd50cf014d8a9fddcc0371379750
Anwesh43 <anweshthecool0@gmail.com>
2023-03-19 14:07:38 +0530

    arcsweepbarleftview: adding ArcSweepBarLeftView class

--- arcsweepbarleftview/src/main/java/com/example/arcsweepbarleftview/ArcSweepBarLeftView.kt
+++ arcsweepbarleftview/src/main/java/com/example/arcsweepbarleftview/ArcSweepBarLeftView.kt
@@ -57,3 +57,19 @@ fun Canvas.drawASBLNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawArcSweepBarLeft(scale, w, h, paint)
 }
+
+class ArcSweepBarLeftView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
