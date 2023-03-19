ddb2cf3e9559c0d25e9075dec80340e7a428ebd6
Anwesh43 <anweshthecool0@gmail.com>
2023-03-19 14:53:46 +0530

    arcsweepbarleftview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.arcrotleftcompleteview.ArcRotLeftCompleteView
+import com.example.arcsweepbarleftview.ArcSweepBarLeftView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        ArcRotLeftCompleteView.create(this)
+        ArcSweepBarLeftView.create(this)
         fullScreen()
     }
 }
