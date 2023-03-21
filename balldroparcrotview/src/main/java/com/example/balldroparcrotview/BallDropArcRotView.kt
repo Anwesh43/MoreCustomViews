56f7604d9db7b2505bb4ee5980e9abf2818e4399
Anwesh43 <anweshthecool0@gmail.com>
2023-03-22 00:56:04 +0530

    balldroparcrotview : adding Int and Float extension methods

--- balldroparcrotview/src/main/java/com/example/balldroparcrotview/BallDropArcRotView.kt
+++ balldroparcrotview/src/main/java/com/example/balldroparcrotview/BallDropArcRotView.kt
@@ -25,3 +25,7 @@ val rot : Float = 180f
 val deg : Float = 90f
 val bakcColor : Int = Color.parseColor("#BDBDBD")
 val scGap : Float = 0.04f / parts
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int,  n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n: Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
