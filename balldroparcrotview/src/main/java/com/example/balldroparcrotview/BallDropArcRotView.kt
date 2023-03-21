ec9903af8e359015a51368cfc98336899d35dfa2
Anwesh43 <anweshthecool0@gmail.com>
2023-03-22 01:06:23 +0530

    balldroparcrotview : adding BDARNode class

--- balldroparcrotview/src/main/java/com/example/balldroparcrotview/BallDropArcRotView.kt
+++ balldroparcrotview/src/main/java/com/example/balldroparcrotview/BallDropArcRotView.kt
@@ -119,4 +119,45 @@ class BallDropArcRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BDARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BDARNode? = null
+        private var prev : BDARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BDARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBDARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BDARNode {
+            var curr : BDARNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
