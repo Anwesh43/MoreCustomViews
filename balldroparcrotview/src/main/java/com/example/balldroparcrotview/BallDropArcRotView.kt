5c76ae91e245b2194e5a2232c9a6e98629539b3c
Anwesh43 <anweshthecool0@gmail.com>
2023-03-22 01:00:13 +0530

    balldroparcrotview : adding Canvas extension methods

--- balldroparcrotview/src/main/java/com/example/balldroparcrotview/BallDropArcRotView.kt
+++ balldroparcrotview/src/main/java/com/example/balldroparcrotview/BallDropArcRotView.kt
@@ -29,3 +29,30 @@ val scGap : Float = 0.04f / parts
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int,  n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n: Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBallDropArcRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        rotate(deg * dsc(2))
+        drawXY(0f, -(h / 2 + size / 2) * (1 - dsc(0))) {
+            drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), rot * dsc(1), 360 - rot * dsc(1), true, paint)
+        }
+    }
+}
+
+fun Canvas.drawBDARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBallDropArcRot(scale, w, h, paint)
+}
