a6e3382e0b79f535ffb219a5de051250c3302d68
Anwesh43 <anweshthecool0@gmail.com>
2023-03-22 01:14:32 +0530

    balldroparcrotview : adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.arcsweepbarleftview.ArcSweepBarLeftView
+import com.example.balldroparcrotview.BallDropArcRotView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        ArcSweepBarLeftView.create(this)
+        BallDropArcRotView.create(this)
         fullScreen()
     }
 }
