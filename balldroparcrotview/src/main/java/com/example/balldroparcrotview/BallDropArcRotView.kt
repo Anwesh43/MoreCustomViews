63b69a49c5e615866c8a782ee8bfee9e762be974
Anwesh43 <anweshthecool0@gmail.com>
2023-03-22 01:08:06 +0530

    balldroparcrotview : adding BallDropArcRot class

--- balldroparcrotview/src/main/java/com/example/balldroparcrotview/BallDropArcRotView.kt
+++ balldroparcrotview/src/main/java/com/example/balldroparcrotview/BallDropArcRotView.kt
@@ -160,4 +160,27 @@ class BallDropArcRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class BallDropArcRot(var i : Int = 0) {
+
+        private var curr : BDARNode = BDARNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
