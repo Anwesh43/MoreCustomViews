e2ccf0340bf2322920f3ebdfa594ff22deee4ac0
Anwesh43 <anweshthecool0@gmail.com>
2023-03-24 11:15:07 +0530

    squareexpandrotdownview: adding SERDNode class

--- squareexpandrotdownview/src/main/java/com/example/squareexpandrotdownview/SquareExpandRotDownView.kt
+++ squareexpandrotdownview/src/main/java/com/example/squareexpandrotdownview/SquareExpandRotDownView.kt
@@ -122,4 +122,45 @@ class SquareExpandRotDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SERDNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SERDNode? = null
+        private var prev : SERDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SERDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSERDNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SERDNode {
+            var curr : SERDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
