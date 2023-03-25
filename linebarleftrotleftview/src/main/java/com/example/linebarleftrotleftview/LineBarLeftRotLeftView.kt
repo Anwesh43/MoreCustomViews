25541c6f4f1d68e2df6c128224365870c02b66a1
Anwesh43 <anweshthecool0@gmail.com>
2023-03-25 14:07:02 +0530

    linebarleftrotleftview: adding Canvas extension methods

--- linebarleftrotleftview/src/main/java/com/example/linebarleftrotleftview/LineBarLeftRotLeftView.kt
+++ linebarleftrotleftview/src/main/java/com/example/linebarleftrotleftview/LineBarLeftRotLeftView.kt
@@ -30,3 +30,34 @@ val barHFactor : Float = 11.2f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBarLeftRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val barH : Float = Math.min(w, h) / barHFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2 + size) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawXY(-w / 2 + (w / 2) * dsc(0), 0f) {
+            drawRect(RectF(-size, -barH, 0f, 0f), paint)
+        }
+        drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+    }
+}
+
+fun Canvas.drawLBLRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBarLeftRot(scale, w, h, paint)
+}
