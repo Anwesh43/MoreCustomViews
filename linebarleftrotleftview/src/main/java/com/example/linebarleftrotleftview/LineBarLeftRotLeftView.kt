bd2c48fcc45c5f4b8a9753ed2ed326e311bbca2c
Anwesh43 <anweshthecool0@gmail.com>
2023-03-25 14:23:41 +0530

    linebarleftrotleftview: adding LBLRLNode class

--- linebarleftrotleftview/src/main/java/com/example/linebarleftrotleftview/LineBarLeftRotLeftView.kt
+++ linebarleftrotleftview/src/main/java/com/example/linebarleftrotleftview/LineBarLeftRotLeftView.kt
@@ -124,4 +124,45 @@ class LineBarLeftRotLeftView(ctx: Context) : View(ctx) {
             }
         }
     }
+
+    data class LBLRLNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBLRLNode? = null
+        private var prev : LBLRLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBLRLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBLRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LBLRLNode {
+            var curr : LBLRLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
