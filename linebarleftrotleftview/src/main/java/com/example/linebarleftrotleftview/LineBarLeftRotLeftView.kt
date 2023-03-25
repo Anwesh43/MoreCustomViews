b11b9833f9f45a19a7ee5e9aa3cdcdcade9136fa
Anwesh43 <anweshthecool0@gmail.com>
2023-03-25 13:58:23 +0530

    linebarleftrotleftview: adding constants and libraries

---
+++ linebarleftrotleftview/src/main/java/com/example/linebarleftrotleftview/LineBarLeftRotLeftView.kt
@@ -0,0 +1,28 @@
+package com.example.linebarleftrotleftview
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#1A237E",
+    "#EF5350",
+    "#AA00FF",
+    "#C51162",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.04f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.9f
+val rot : Float = 180f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val barHFactor : Float = 11.2f
