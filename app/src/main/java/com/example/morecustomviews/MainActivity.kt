0ea473dd1e5febc4dbbd0cb4b881aa0e330984f8
Anwesh43 <anweshthecool0@gmail.com>
2023-03-25 14:34:56 +0530

    linebarleftrotleftview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.squareexpandrotdownview.SquareExpandRotDownView
+import com.example.linebarleftrotleftview.LineBarLeftRotLeftView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        SquareExpandRotDownView.create(this)
+        LineBarLeftRotLeftView.create(this)
         fullScreen()
     }
 }
