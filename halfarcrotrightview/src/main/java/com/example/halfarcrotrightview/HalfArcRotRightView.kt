ca7e75ac0c502376f73c64072d30ca05e69c997b
Anwesh43 <anweshthecool0@gmail.com>
2023-03-26 17:13:10 +0530

    halfarcrotrightview: adding HARRNode class

--- halfarcrotrightview/src/main/java/com/example/halfarcrotrightview/HalfArcRotRightView.kt
+++ halfarcrotrightview/src/main/java/com/example/halfarcrotrightview/HalfArcRotRightView.kt
@@ -126,4 +126,45 @@ class HalfArcRotRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HARRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HARRNode? = null
+        private var prev : HARRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HARRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHARRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HARRNode {
+            var curr : HARRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
