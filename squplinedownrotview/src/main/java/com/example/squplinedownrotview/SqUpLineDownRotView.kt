58e94af9a5bc55ec969f26cd55105b3f571d109c
Anwesh43 <anweshthecool0@gmail.com>
2023-03-27 13:59:40 +0530

    squplinedownrotview: adding SqUpLineDownRotView class

--- squplinedownrotview/src/main/java/com/example/squplinedownrotview/SqUpLineDownRotView.kt
+++ squplinedownrotview/src/main/java/com/example/squplinedownrotview/SqUpLineDownRotView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawSULDRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSqUpLineDownRot(scale, w, h, paint)
 }
+
+class SqUpLineDownRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
