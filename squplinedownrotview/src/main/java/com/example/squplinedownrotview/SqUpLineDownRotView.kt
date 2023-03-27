b3073a159514a852cf01d661e81e108c7de1453b
Anwesh43 <anweshthecool0@gmail.com>
2023-03-27 18:14:40 +0530

    squplinedownrotview: adding SULDRNode class

--- squplinedownrotview/src/main/java/com/example/squplinedownrotview/SqUpLineDownRotView.kt
+++ squplinedownrotview/src/main/java/com/example/squplinedownrotview/SqUpLineDownRotView.kt
@@ -122,4 +122,45 @@ class SqUpLineDownRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SULDRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : SULDRNode? = null
+        private var prev : SULDRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SULDRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSULDRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SULDRNode {
+            var curr : SULDRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
