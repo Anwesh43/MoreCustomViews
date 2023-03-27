c91946aef12a166944a07a94ed52b5ee3e8eb3a3
Anwesh43 <anweshthecool0@gmail.com>
2023-03-27 13:57:02 +0530

    squplinedownrotview: adding Canvas extension methods

--- squplinedownrotview/src/main/java/com/example/squplinedownrotview/SqUpLineDownRotView.kt
+++ squplinedownrotview/src/main/java/com/example/squplinedownrotview/SqUpLineDownRotView.kt
@@ -30,3 +30,32 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(),  maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSqUpLineDownRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(4), h / 2) {
+        drawRect(RectF(0f, -size * (dsc(0) - dsc(3)), size, 0f), paint)
+        drawXY(0f, -h / 2 + (h / 2 - size) * dsc(1) + size * dsc(3)) {
+            rotate(rot * dsc(2))
+            drawLine(0f, 0f, 0f, -size, paint)
+        }
+    }
+}
+
+fun Canvas.drawSULDRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSqUpLineDownRot(scale, w, h, paint)
+}
