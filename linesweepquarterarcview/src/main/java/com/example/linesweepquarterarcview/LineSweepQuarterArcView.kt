39972ff1d022554b4c42ce185d3290f5b2771489
Anwesh43 <anweshthecool0@gmail.com>
2023-03-28 08:48:27 +0530

    linesweepquarterarcview: adding LineSweepQuarterArc class

--- linesweepquarterarcview/src/main/java/com/example/linesweepquarterarcview/LineSweepQuarterArcView.kt
+++ linesweepquarterarcview/src/main/java/com/example/linesweepquarterarcview/LineSweepQuarterArcView.kt
@@ -155,7 +155,7 @@ class LineSweepQuarterArcView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
-        fun startUpadting(cb : () -> Unit) {
+        fun startUpdating(cb : () -> Unit) {
             state.startUpdating(cb)
         }
 
@@ -169,6 +169,29 @@ class LineSweepQuarterArcView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class LineSweepQuarterArc(var i : Int) {
+
+        private var curr : LSQANode = LSQANode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
