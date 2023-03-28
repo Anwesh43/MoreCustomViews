8fdf63b8a11f286fa4106d26a9c104eba945a006
Anwesh43 <anweshthecool0@gmail.com>
2023-03-28 08:40:16 +0530

    linesweepquarterarcview: adding LineSweepQuarterArcView class

--- linesweepquarterarcview/src/main/java/com/example/linesweepquarterarcview/LineSweepQuarterArcView.kt
+++ linesweepquarterarcview/src/main/java/com/example/linesweepquarterarcview/LineSweepQuarterArcView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawLSQANode(i : Int, scale : Float, paint : Paint) {
     paint.style = Paint.Style.STROKE
     drawLineSweepQuarterArc(scale, w, h, paint)
 }
+
+class LineSweepQuarterArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
