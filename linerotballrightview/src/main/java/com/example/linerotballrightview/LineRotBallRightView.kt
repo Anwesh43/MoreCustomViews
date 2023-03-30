27b177fddfe0007fbccfe3f90874ae18691bb8f1
Anwesh43 <anweshthecool0@gmail.com>
2023-03-30 18:26:33 +0530

    linerotballrightview: adding LineRotBallRightView class

--- linerotballrightview/src/main/java/com/example/linerotballrightview/LineRotBallRightView.kt
+++ linerotballrightview/src/main/java/com/example/linerotballrightview/LineRotBallRightView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawLRBRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotBallRight(scale, w, h, paint)
 }
+
+class LineRotBallRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
