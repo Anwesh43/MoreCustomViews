a5f9f10d0ac8a32ca9a6c88bd01bf92bbcb92c5c
Anwesh43 <anweshthecool0@gmail.com>
2023-03-30 18:46:23 +0530

    linerotballrightview: adding Renderer class

--- linerotballrightview/src/main/java/com/example/linerotballrightview/LineRotBallRightView.kt
+++ linerotballrightview/src/main/java/com/example/linerotballrightview/LineRotBallRightView.kt
@@ -184,4 +184,27 @@ class LineRotBallRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineRotBallRightView) {
+
+        private val lrbr : LineRotBallRight = LineRotBallRight(0)
+        private val animator : Animator = Animator(view)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lrbr.draw(canvas, paint)
+            animator.animate {
+                lrbr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lrbr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
