77b3c61408206004c3652fb5319ce3a42fc5f47d
Anwesh43 <anweshthecool0@gmail.com>
2023-03-30 18:40:34 +0530

    linerotballrightview: adding LineRotBallRight class

--- linerotballrightview/src/main/java/com/example/linerotballrightview/LineRotBallRightView.kt
+++ linerotballrightview/src/main/java/com/example/linerotballrightview/LineRotBallRightView.kt
@@ -161,4 +161,27 @@ class LineRotBallRightView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRotBallRight(var i : Int = 0) {
+
+        private var curr : LRBRNode = LRBRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
