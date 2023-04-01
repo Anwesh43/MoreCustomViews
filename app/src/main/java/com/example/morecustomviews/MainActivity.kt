c5ace56f1ae0ce993552c1baa03a9dc6ef6c64d0
Anwesh43 <anweshthecool0@gmail.com>
2023-04-01 13:31:01 +0530

    bisqlinerotrightview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerotballrightview.LineRotBallRightView
+import com.example.bisqlinerotrightview.BiSqLineRotRightView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineRotBallRightView.create(this)
+        BiSqLineRotRightView.create(this)
         fullScreen()
     }
 }
