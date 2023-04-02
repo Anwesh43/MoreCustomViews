40d9b7a3b1bb5f012f66996ebc677c2f23011940
Anwesh43 <anweshthecool0@gmail.com>
2023-04-02 08:16:36 +0530

    mirrorarclinemoverview: adding Int and Flaot extension methods

--- mirrorarclinemoverview/src/main/java/com/example/mirrorarclinemoverview/MirrorArcLineMoverView.kt
+++ mirrorarclinemoverview/src/main/java/com/example/mirrorarclinemoverview/MirrorArcLineMoverView.kt
@@ -27,3 +27,6 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
 val deg : Float = 180f
 
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
