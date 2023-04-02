9cd62935ac76cd4c0369e5612c64f2010ff3473a
Anwesh43 <anweshthecool0@gmail.com>
2023-04-02 08:27:08 +0530

    mirrorarclinemoverview: adding Canvas extension methods

--- mirrorarclinemoverview/src/main/java/com/example/mirrorarclinemoverview/MirrorArcLineMoverView.kt
+++ mirrorarclinemoverview/src/main/java/com/example/mirrorarclinemoverview/MirrorArcLineMoverView.kt
@@ -21,7 +21,7 @@ val colors : Array<Int> = arrayOf(
 val parts : Int  = 4
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
-val sizeFactor : Float = 4.9f
+val sizeFactor : Float = 7.9f
 val rot : Float = 90f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
@@ -30,3 +30,40 @@ val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawMirrorArcLineMover(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(h * 0.5f * dsc(3), 0f) {
+                    drawArc(RectF(-size, -size, size, size), -rot, deg * dsc(0), true, paint)
+                    drawXY(w * 0.5f * (1 - dsc(1)), 0f) {
+                        drawLine(0f, 0f, size, 0f, paint)
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawMALMNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawMirrorArcLineMover(scale, w, h, paint)
+}
