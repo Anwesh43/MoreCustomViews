d9443deec77569807f2b17700736a4138219b88d
Anwesh43 <anweshthecool0@gmail.com>
2023-04-02 08:30:47 +0530

    mirrorarclinemoverview: adding MirrorArcLineMoverView class

--- mirrorarclinemoverview/src/main/java/com/example/mirrorarclinemoverview/MirrorArcLineMoverView.kt
+++ mirrorarclinemoverview/src/main/java/com/example/mirrorarclinemoverview/MirrorArcLineMoverView.kt
@@ -67,3 +67,19 @@ fun Canvas.drawMALMNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawMirrorArcLineMover(scale, w, h, paint)
 }
+
+class MirrorArcLineMoverView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
