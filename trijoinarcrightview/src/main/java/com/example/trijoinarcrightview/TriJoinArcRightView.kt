87425b997394b921155040bddd55549f979634ef
Anwesh43 <anweshthecool0@gmail.com>
2023-04-03 08:32:09 +0530

    trijoinarcrightview: adding Canvas extension methods

--- trijoinarcrightview/src/main/java/com/example/trijoinarcrightview/TriJoinArcRightView.kt
+++ trijoinarcrightview/src/main/java/com/example/trijoinarcrightview/TriJoinArcRightView.kt
@@ -29,3 +29,37 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawTriJoinRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 180f, 180f * dsc(0), true, paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawLine(size * dsc(2), 0f, size * dsc(1), 0f, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawTJRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawTriJoinRight(scale, w , h, paint)
+}
+
