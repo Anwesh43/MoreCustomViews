006841b8a5bd3c6c39d4decbbd36d9ef04d87db5
Anwesh43 <anweshthecool0@gmail.com>
2023-04-03 08:51:46 +0530

    trijoinarcrightview: adding static create method

--- trijoinarcrightview/src/main/java/com/example/trijoinarcrightview/TriJoinArcRightView.kt
+++ trijoinarcrightview/src/main/java/com/example/trijoinarcrightview/TriJoinArcRightView.kt
@@ -214,4 +214,13 @@ class TriJoinArcRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : TriJoinArcRightView {
+            val view : TriJoinArcRightView = TriJoinArcRightView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
