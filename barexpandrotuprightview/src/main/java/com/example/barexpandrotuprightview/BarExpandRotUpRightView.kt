303ca32d0ee92a6a76d204c1bb38046782eb0294
Anwesh43 <anweshthecool0@gmail.com>
2023-04-05 09:00:02 +0530

    barexpandrotuprightview: adding BERURNode class

--- barexpandrotuprightview/src/main/java/com/example/barexpandrotuprightview/BarExpandRotUpRightView.kt
+++ barexpandrotuprightview/src/main/java/com/example/barexpandrotuprightview/BarExpandRotUpRightView.kt
@@ -123,4 +123,45 @@ class BarExpandRotUpRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BERURNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BERURNode? = null
+        private var prev : BERURNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BERURNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBERURNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BERURNode {
+            var curr : BERURNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
