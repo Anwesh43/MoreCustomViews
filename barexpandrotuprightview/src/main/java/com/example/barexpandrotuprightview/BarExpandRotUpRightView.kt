69da2f80ebe44d3b44603997deb15d7294df6441
Anwesh43 <anweshthecool0@gmail.com>
2023-04-05 09:08:41 +0530

    barexpandrotuprightview: adding renderer in view

--- barexpandrotuprightview/src/main/java/com/example/barexpandrotuprightview/BarExpandRotUpRightView.kt
+++ barexpandrotuprightview/src/main/java/com/example/barexpandrotuprightview/BarExpandRotUpRightView.kt
@@ -63,14 +63,16 @@ fun Canvas.drawBERURNode(i : Int, scale : Float, paint : Paint) {
 
 class BarExpandRotUpRightView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
