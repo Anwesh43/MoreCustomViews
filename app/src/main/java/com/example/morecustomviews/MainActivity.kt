2e3b171d7d6c8b1771f6e270bb61e37e1001207c
Anwesh43 <anweshthecool0@gmail.com>
2023-04-05 09:12:47 +0530

    barexpandrotuprightview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linehalfarcrightview.LineHalfArcRotView
+import com.example.barexpandrotuprightview.BarExpandRotUpRightView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineHalfArcRotView.create(this)
+        BarExpandRotUpRightView.create(this)
         fullScreen()
     }
 }
