e872ffbebb7fdf61a4bc5ea06ce67a93d658e8cb
Anwesh43 <anweshthecool0@gmail.com>
2023-04-07 08:31:45 +0530

    fillarclinerightview: adding Canvas extension methods

--- fillarclinerightview/src/main/java/com/example/fillarclinerightview/FillArcLineRIghtView.kt
+++ fillarclinerightview/src/main/java/com/example/fillarclinerightview/FillArcLineRIghtView.kt
@@ -30,3 +30,36 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawFillArcLineRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val r : Float = Math.min(w, h) / rFactor
+    drawXY(w / 2 + (w / 2 + size) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        drawXY(0f, r) {
+            for (j in 0..1) {
+                drawArc(RectF(-r, -r, r, r), 90f + 180f * j, 180f * dsc(j * 2), true, paint)
+            }
+        }
+        drawLine(0f, 0f, 0f, -size * dsc(1), paint)
+    }
+}
+
+fun Canvas.drawFALRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawFillArcLineRight(scale, w, h, paint)
+}
