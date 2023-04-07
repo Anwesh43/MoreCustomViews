593148316af9a10ac3172f31e7dbafb2bf13ab39
Anwesh43 <anweshthecool0@gmail.com>
2023-04-07 08:42:56 +0530

    fillarclinerightview: adding Renderer class

--- fillarclinerightview/src/main/java/com/example/fillarclinerightview/FillArcLineRIghtView.kt
+++ fillarclinerightview/src/main/java/com/example/fillarclinerightview/FillArcLineRIghtView.kt
@@ -190,4 +190,27 @@ class FillArcLineRightView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : FillArcLineRightView) {
+
+        private val animator : Animator = Animator(view)
+        private val falr : FillArcLineRotRight = FillArcLineRotRight(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            falr.draw(canvas, paint)
+            animator.animate {
+                falr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            falr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
