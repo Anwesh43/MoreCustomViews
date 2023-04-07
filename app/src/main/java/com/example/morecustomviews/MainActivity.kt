7432a45284f4b3ee928cbace5d653c497ff8d4f8
Anwesh43 <anweshthecool0@gmail.com>
2023-04-07 08:45:13 +0530

    fillarclinerightview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.barexpandrotuprightview.BarExpandRotUpRightView
+import com.example.fillarclinerightview.FillArcLineRightView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        BarExpandRotUpRightView.create(this)
+        FillArcLineRightView.create(this)
         fullScreen()
     }
 }
