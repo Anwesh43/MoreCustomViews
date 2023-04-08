58802b394d18074a006f3acd01d6b02e2c00cd4f
Anwesh43 <anweshthecool0@gmail.com>
2023-04-08 13:29:25 +0530

    linerotarcshiftview: adding LineRotArcShift class

--- linerotarcshiftview/src/main/java/com/example/linerotarcshiftview/LineRotArcShiftView.kt
+++ linerotarcshiftview/src/main/java/com/example/linerotarcshiftview/LineRotArcShiftView.kt
@@ -165,4 +165,27 @@ class LineRotArcShiftView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRotArcShift(var i : Int = 0) {
+
+        private var curr : LRASNode = LRASNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
