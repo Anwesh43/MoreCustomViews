9b4a6d26234d5e0cf3297c1d14ffaea7fc5f86da
Anwesh43 <anweshthecool0@gmail.com>
2023-04-09 02:12:42 +0530

    mirrorrotlineview: adding MirrorRotLineView class

--- mirrorrotlineview/src/main/java/com/example/mirrorrotlineview/MirrorRotLineView.kt
+++ mirrorrotlineview/src/main/java/com/example/mirrorrotlineview/MirrorRotLineView.kt
@@ -64,3 +64,19 @@ fun Canvas.drawMRLNode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawMirrorRotLine(scale, w, h, paint)
 }
+
+class MirrorRotLineView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
