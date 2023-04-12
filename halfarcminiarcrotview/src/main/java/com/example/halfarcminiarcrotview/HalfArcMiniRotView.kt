a89423f9881c40f1f4c1126df5b7175b06a3e931
Anwesh43 <anweshthecool0@gmail.com>
2023-04-13 01:04:36 +0530

    halfarcminiarcrotview: adding HAMRNode class

--- halfarcminiarcrotview/src/main/java/com/example/halfarcminiarcrotview/HalfArcMiniRotView.kt
+++ halfarcminiarcrotview/src/main/java/com/example/halfarcminiarcrotview/HalfArcMiniRotView.kt
@@ -123,4 +123,45 @@ class HalfArcMiniRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class HAMRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : HAMRNode? = null
+        private var prev : HAMRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = HAMRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawHAMRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : HAMRNode {
+            var curr : HAMRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
