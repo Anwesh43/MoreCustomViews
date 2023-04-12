9f4b2f15fd79b245e2781d90871687e8cf61213a
Anwesh43 <anweshthecool0@gmail.com>
2023-04-12 13:38:50 +0530

    linebendrotupview: adding Canvas extension methods

--- linebendrotupview/src/main/java/com/example/linebendrotupview/LineBendRotUpView.kt
+++ linebendrotupview/src/main/java/com/example/linebendrotupview/LineBendRotUpView.kt
@@ -29,3 +29,46 @@ val sizeFactor : Float = 4.9f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBendRotUp(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 - (h / 2 + size / 2) * dsc(3)) {
+        rotate(rot * dsc(2))
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                val sc1 : Float = dsc(0)
+                for (k in 0..1) {
+                    drawXY(size * 0.5f * k, 0f) {
+                        drawLine(
+                            0f,
+                            -size * 0.5f * dsc(1) * k,
+                            size * 0.5f * sc1.divideScale(k, parts),
+                            -size * 0.5f * dsc(1) * (1 - k),
+                            paint
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun Canvas.drawLBRUNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBendRotUp(scale, w, h, paint)
+}
