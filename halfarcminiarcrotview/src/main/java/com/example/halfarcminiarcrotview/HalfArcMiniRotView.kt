34ef67f33b820f05c5511b49da212bccc94f1d44
Anwesh43 <anweshthecool0@gmail.com>
2023-04-13 00:55:58 +0530

    halfarcminiarcrotview: adding Canvas extension methods

--- halfarcminiarcrotview/src/main/java/com/example/halfarcminiarcrotview/HalfArcMiniRotView.kt
+++ halfarcminiarcrotview/src/main/java/com/example/halfarcminiarcrotview/HalfArcMiniRotView.kt
@@ -20,12 +20,43 @@ val colors : Array<Int> = arrayOf(
 }.toTypedArray()
 val parts : Int = 4
 val scGap : Float = 0.04f / parts
-val rot : Float = 180f
+val rot : Float = 90f
 val delay : Long = 20
 val strokeFactor : Float= 90f
-val sizeFactory : Float = 4.9f
+val sizeFactor : Float = 4.9f
 val backColor : Int = Color.parseColor("#BDBDBD")
+
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawHalfArcMiniRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 - (w / 2 + size) * dsc(0), h / 2) {
+        rotate(rot * dsc(2))
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -90f, 180f * dsc(0), true, paint)
+        drawXY(0f, size / 4) {
+            drawArc(RectF(-size / 4, -size / 4, size / 4, size / 4), 90f, 180f * dsc(1), true, paint)
+        }
+    }
+}
+
+fun Canvas.drawHAMRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawHalfArcMiniRot(scale, w, h, paint)
+}
