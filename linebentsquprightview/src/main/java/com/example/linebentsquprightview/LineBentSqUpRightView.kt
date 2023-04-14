3df76cb76065e73fcd187fe9568a1b1192b18ed8
Anwesh43 <anweshthecool0@gmail.com>
2023-04-14 14:04:50 +0530

    linebentsquprightview: adding Canvas extension methods

--- linebentsquprightview/src/main/java/com/example/linebentsquprightview/LineBentSqUpRightView.kt
+++ linebentsquprightview/src/main/java/com/example/linebentsquprightview/LineBentSqUpRightView.kt
@@ -30,3 +30,33 @@ val deg : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBentSqUpRight(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawXY(0f, 0f) {
+            rotate(-deg * dsc(1))
+            drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        }
+        drawRect(RectF(0f, -size * dsc(0), size, 0f), paint)
+    }
+}
+
+fun Canvas.drawLBSURNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+}
