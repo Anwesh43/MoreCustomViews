930340cdb7bdf8315c711c8cb8add6fea56b9e1e
Anwesh43 <anweshthecool0@gmail.com>
2023-04-15 13:50:27 +0530

    altlinebarshootview: adding ALBSNode class

--- altlinebarshootview/src/main/java/com/example/altlinebarshootview/AltLineBarShootView.kt
+++ altlinebarshootview/src/main/java/com/example/altlinebarshootview/AltLineBarShootView.kt
@@ -124,4 +124,45 @@ class AltLineBarShootView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALBSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : ALBSNode? = null
+        private var prev : ALBSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALBSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALBSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALBSNode {
+            var curr : ALBSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
