bf0a6db0d1d3834f98e23726e1147e8e4d3402b7
Anwesh43 <anweshthecool0@gmail.com>
2023-04-15 13:40:54 +0530

    altlinebarshootview: adding AltLineBarShootView class

--- altlinebarshootview/src/main/java/com/example/altlinebarshootview/AltLineBarShootView.kt
+++ altlinebarshootview/src/main/java/com/example/altlinebarshootview/AltLineBarShootView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawALBSNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawAltLineBarShoot(scale, w, h, paint)
 }
+
+class AltLineBarShootView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
