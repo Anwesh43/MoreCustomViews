68f6761cdb90929098e036fa5fdf403cb0bd7c6c
Anwesh43 <anweshthecool0@gmail.com>
2023-04-17 12:36:19 +0530

    linerightcarrybarview: adding LineRightCarryBarView class

--- linerightcarrybarview/src/main/java/com/example/linerightcarrybarview/LineRightCarryBarView.kt
+++ linerightcarrybarview/src/main/java/com/example/linerightcarrybarview/LineRightCarryBarView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLRCBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRightCarryBar(scale,w , h, paint)
 }
+
+class LineRightCarryBarView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
