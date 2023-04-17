696a7a0082adf597b3ba400d02382211ee579621
Anwesh43 <anweshthecool0@gmail.com>
2023-04-17 12:27:02 +0530

    linerightcarrybarview: adding Canvas extension methods

--- linerightcarrybarview/src/main/java/com/example/linerightcarrybarview/LineRightCarryBarView.kt
+++ linerightcarrybarview/src/main/java/com/example/linerightcarrybarview/LineRightCarryBarView.kt
@@ -29,3 +29,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i: Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRightCarryBar(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        drawXY(0f, 0f) {
+            rotate(-rot * dsc(1))
+            drawLine(0f, 0f, size, 0f, paint)
+        }
+        drawRect(RectF(0f, -size * 0.5f * dsc(2), size, 0f), paint)
+    }
+}
+
+fun Canvas.drawLRCBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRightCarryBar(scale,w , h, paint)
+}
