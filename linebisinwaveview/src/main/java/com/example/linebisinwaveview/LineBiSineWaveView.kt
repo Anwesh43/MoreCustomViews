59fa14e7fd2ed7f2806ddc4f77305258e4e753c6
Anwesh43 <anweshthecool0@gmail.com>
2023-04-18 20:31:03 +0530

    linebisinewaveview: adding Renderer clas

--- linebisinwaveview/src/main/java/com/example/linebisinwaveview/LineBiSineWaveView.kt
+++ linebisinwaveview/src/main/java/com/example/linebisinwaveview/LineBiSineWaveView.kt
@@ -200,4 +200,27 @@ class LineBiSineWaveView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineBiSineWaveView) {
+
+        private val lbsw : LineBiSinWave = LineBiSinWave(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+        private val animator : Animator = Animator(view)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lbsw.draw(canvas, paint)
+            animator.animate {
+                lbsw.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lbsw.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
