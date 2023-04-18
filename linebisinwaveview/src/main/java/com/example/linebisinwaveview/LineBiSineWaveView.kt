091d926791dc6b15c00fa0bca035b3f706184b5c
Anwesh43 <anweshthecool0@gmail.com>
2023-04-18 10:53:34 +0530

    linebisinewaveview: adding Canvas extension methods

--- linebisinwaveview/src/main/java/com/example/linebisinwaveview/LineBiSineWaveView.kt
+++ linebisinwaveview/src/main/java/com/example/linebisinwaveview/LineBiSineWaveView.kt
@@ -8,6 +8,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.RectF
 import android.graphics.Paint
+import android.graphics.Path
 
 val colors : Array<Int> = arrayOf(
     "#1A237E",
@@ -29,3 +30,46 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawSineWave(a : Float, r : Float, sc : Float, paint : Paint) {
+
+    val path : Path = Path()
+    for (j in 0..360) {
+        val x : Float = ((j * 1f) / 360f) * r
+        val y : Float = -a * Math.sin(j * Math.PI / 180 ).toFloat()
+        if (j == 0) {
+            path.moveTo(x, y)
+        } else {
+            path.lineTo(x, y)
+        }
+    }
+    drawPath(path, paint)
+}
+
+fun Canvas.drawLineBiSineWave(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2, h / 2 + (h / 2 + size) * dsc(3)) {
+        rotate(rot * dsc(2))
+        drawLine(-size * dsc(0), 0f, size * dsc(0), 0f, paint)
+        drawSineWave(size / 2, size, dsc(1), paint)
+    }
+}
+
+fun Canvas.drawLBSWNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBiSineWave(scale, w, h, paint)
+}
