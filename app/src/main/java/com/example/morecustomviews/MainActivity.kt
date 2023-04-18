13debd2599e521c7f766f826f9d9d1dcd7786363
Anwesh43 <anweshthecool0@gmail.com>
2023-04-18 20:33:21 +0530

    linebisinewaveview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerightcarrybarview.LineRightCarryBarView
+import com.example.linebisinwaveview.LineBiSineWaveView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineRightCarryBarView.create(this)
+        LineBiSineWaveView.create(this)
         fullScreen()
     }
 }
