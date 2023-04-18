1573ee4f928d0132b8ae481598bb40290c9739d3
Anwesh43 <anweshthecool0@gmail.com>
2023-04-18 20:21:52 +0530

    linebisinewaveview: adding LineBiSinWave class

--- linebisinwaveview/src/main/java/com/example/linebisinwaveview/LineBiSineWaveView.kt
+++ linebisinwaveview/src/main/java/com/example/linebisinwaveview/LineBiSineWaveView.kt
@@ -177,4 +177,27 @@ class LineBiSineWaveView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineBiSinWave(var i : Int) {
+
+        private var curr : LBSWNode = LBSWNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
