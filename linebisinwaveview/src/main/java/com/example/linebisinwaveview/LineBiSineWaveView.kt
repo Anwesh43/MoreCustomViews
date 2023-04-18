d1f73c02989e2b165c30e252a9d6db050674df04
Anwesh43 <anweshthecool0@gmail.com>
2023-04-18 20:17:28 +0530

    linebisinewaveview: adding LBSWNode class

--- linebisinwaveview/src/main/java/com/example/linebisinwaveview/LineBiSineWaveView.kt
+++ linebisinwaveview/src/main/java/com/example/linebisinwaveview/LineBiSineWaveView.kt
@@ -136,4 +136,45 @@ class LineBiSineWaveView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LBSWNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LBSWNode? = null
+        private var prev : LBSWNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LBSWNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLBSWNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit)  : LBSWNode {
+            var curr : LBSWNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
