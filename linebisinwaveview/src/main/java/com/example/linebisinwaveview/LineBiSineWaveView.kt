e072f1917ff42da065be12e1f607539dc4f45289
Anwesh43 <anweshthecool0@gmail.com>
2023-04-18 10:55:03 +0530

    linebisinewaveview: adding LineBiSineWaveView class

--- linebisinwaveview/src/main/java/com/example/linebisinwaveview/LineBiSineWaveView.kt
+++ linebisinwaveview/src/main/java/com/example/linebisinwaveview/LineBiSineWaveView.kt
@@ -73,3 +73,19 @@ fun Canvas.drawLBSWNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBiSineWave(scale, w, h, paint)
 }
+
+class LineBiSineWaveView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
