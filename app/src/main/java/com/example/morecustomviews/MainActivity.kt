538f6eb5519582334d20a68a7c98dce9011b334a
Anwesh43 <anweshthecool0@gmail.com>
2023-04-19 08:40:27 +0530

    linerectrotdownview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linebisinwaveview.LineBiSineWaveView
+import com.example.linerectrotdownview.LineRectRotDownView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineBiSineWaveView.create(this)
+        LineRectRotDownView.create(this)
         fullScreen()
     }
 }
