d9f1783bbc89f96028ac2651318225660d361d3f
Anwesh43 <anweshthecool0@gmail.com>
2023-04-19 08:14:45 +0530

    linerectrotdownview: adding LineRectRotDownView class

--- linerectrotdownview/src/main/java/com/example/linerectrotdownview/LineRectRotDownView.kt
+++ linerectrotdownview/src/main/java/com/example/linerectrotdownview/LineRectRotDownView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLRRDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRectRotDown(scale, w, h, paint)
 }
+
+class LineRectRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
