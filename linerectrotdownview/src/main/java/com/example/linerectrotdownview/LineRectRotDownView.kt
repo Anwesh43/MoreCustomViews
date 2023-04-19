1f1966b9325bb38cfb32936c49513e1a0626d282
Anwesh43 <anweshthecool0@gmail.com>
2023-04-19 08:13:15 +0530

    linerectrotdownview: adding Canvas extension methods

--- linerectrotdownview/src/main/java/com/example/linerectrotdownview/LineRectRotDownView.kt
+++ linerectrotdownview/src/main/java/com/example/linerectrotdownview/LineRectRotDownView.kt
@@ -18,8 +18,8 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
-val scGap : Float = 0.04f / parts
+val parts : Int = 6
+val scGap : Float = 0.05f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
 val rot : Float = 90f
@@ -29,3 +29,33 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineRectRotDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(5)) {
+        rotate(rot * dsc(2))
+        drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        drawXY(size / 2 + (size / 2) * dsc(3), -size * 0.5f * dsc(4)) {
+            drawRect(RectF(0f, 0f, size, size * dsc(1)), paint)
+        }
+    }
+}
+
+fun Canvas.drawLRRDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRectRotDown(scale, w, h, paint)
+}
