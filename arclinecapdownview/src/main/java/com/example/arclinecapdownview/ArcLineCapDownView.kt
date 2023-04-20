80956a7c01c8499aaf0f96cfc06bf144a278480e
Anwesh43 <anweshthecool0@gmail.com>
2023-04-20 17:07:13 +0530

    arclinecapdownview: adding Canvas extension methods

--- arclinecapdownview/src/main/java/com/example/arclinecapdownview/ArcLineCapDownView.kt
+++ arclinecapdownview/src/main/java/com/example/arclinecapdownview/ArcLineCapDownView.kt
@@ -30,3 +30,33 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawArcLineCapDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2, h / 2) {
+        rotate(rot * dsc(2))
+        drawXY(0f, 0f) {
+            drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 90f, 180f * dsc(0), true, paint)
+        }
+        drawLine(0f, 0f, size * dsc(1), 0f, paint)
+        drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), -45f * dsc(2), 90f * dsc(2), false, paint)
+    }
+}
+
+fun Canvas.drawALCDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawArcLineCapDown(scale, w, h, paint)
+}
