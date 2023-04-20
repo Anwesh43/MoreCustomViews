53e16873a3ec6f94573d338e17b416f3069db648
Anwesh43 <anweshthecool0@gmail.com>
2023-04-20 17:08:23 +0530

    arclinecapdownview: adding ArcLineCapDownView class

--- arclinecapdownview/src/main/java/com/example/arclinecapdownview/ArcLineCapDownView.kt
+++ arclinecapdownview/src/main/java/com/example/arclinecapdownview/ArcLineCapDownView.kt
@@ -60,3 +60,19 @@ fun Canvas.drawALCDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawArcLineCapDown(scale, w, h, paint)
 }
+
+class ArcLineCapDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
