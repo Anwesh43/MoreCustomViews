44c798cb5e7808805a409021690e64b76b31e003
Anwesh43 <anweshthecool0@gmail.com>
2023-04-20 17:41:03 +0530

    arclinecapdownview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linerectrotdownview.LineRectRotDownView
+import com.example.arclinecapdownview.ArcLineCapDownView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineRectRotDownView.create(this)
+        ArcLineCapDownView.create(this)
         fullScreen()
     }
 }
