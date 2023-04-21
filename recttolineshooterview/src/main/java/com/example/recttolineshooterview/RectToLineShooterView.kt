a31f24eb1dbe24c6eb57d5cc08f5fe4bcf98fa6c
Anwesh43 <anweshthecool0@gmail.com>
2023-04-21 16:01:51 +0530

    recttolineshooterview: adding RTLSNode class

--- recttolineshooterview/src/main/java/com/example/recttolineshooterview/RectToLineShooterView.kt
+++ recttolineshooterview/src/main/java/com/example/recttolineshooterview/RectToLineShooterView.kt
@@ -122,4 +122,45 @@ class RectToLineShooterView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class RTLSNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : RTLSNode? = null
+        private var prev : RTLSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = RTLSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawRTLSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : RTLSNode {
+            var curr : RTLSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
