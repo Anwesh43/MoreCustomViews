03ed7795a8687e2e1e9ebee884037d4bf0822ffe
Anwesh43 <anweshthecool0@gmail.com>
2023-04-21 15:39:52 +0530

    recttolineshooterview: adding Canvas extension methods

--- recttolineshooterview/src/main/java/com/example/recttolineshooterview/RectToLineShooterView.kt
+++ recttolineshooterview/src/main/java/com/example/recttolineshooterview/RectToLineShooterView.kt
@@ -29,3 +29,33 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawRectToLineShooter(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        rotate(rot * dsc(2))
+        drawXY(-w / 2 + (w / 2) * dsc(0), 0f) {
+            drawRect(RectF(-size + size * dsc(1), -size, 0f, 0f), paint)
+        }
+        drawLine(0f, 0f, 0f, -size * Math.floor(scale.divideScale(1, parts).toDouble()).toFloat(), paint)
+    }
+}
+
+fun Canvas.drawRTLSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawRectToLineShooter(scale, w, h, paint)
+}
