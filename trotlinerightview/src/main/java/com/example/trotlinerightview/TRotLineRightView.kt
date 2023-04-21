50294ad78c89edeeb55c7315e2162a495c35ed7c
Anwesh43 <anweshthecool0@gmail.com>
2023-04-22 03:59:38 +0530

    trotlinerightview: adding TRotLineRightView class

--- trotlinerightview/src/main/java/com/example/trotlinerightview/TRotLineRightView.kt
+++ trotlinerightview/src/main/java/com/example/trotlinerightview/TRotLineRightView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawTRLRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawTRotLineRight(scale, w, h, paint)
 }
+
+class TRotLineRightView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
