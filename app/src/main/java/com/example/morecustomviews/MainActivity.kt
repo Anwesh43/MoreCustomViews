90e9f3248790bf8c87cb0b027cac5e1bc8f0e379
Anwesh43 <anweshthecool0@gmail.com>
2023-04-23 08:46:45 +0530

    barlinebiarcrotview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.trotlinerightview.TRotLineRightView
+import com.example.barlinebiarcrotview.BarLineBiArcRotView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        TRotLineRightView.create(this)
+        BarLineBiArcRotView.create(this)
         fullScreen()
     }
 }
