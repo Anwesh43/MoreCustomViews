dfdf7a5caebba616a69f1a183886249201b03472
Anwesh43 <anweshthecool0@gmail.com>
2023-04-23 08:32:59 +0530

    barlinebiarcrotview: adding BLBARNode class

--- barlinebiarcrotview/src/main/java/com/example/barlinebiarcrotview/BarLineBiArcRotView.kt
+++ barlinebiarcrotview/src/main/java/com/example/barlinebiarcrotview/BarLineBiArcRotView.kt
@@ -135,4 +135,45 @@ class BarLineBiArcRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLBARNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : BLBARNode? = null
+        private var prev : BLBARNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BLBARNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLBARNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLBARNode {
+            var curr : BLBARNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
