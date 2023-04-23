4ea72145ef6ea5bc0bc59b9a0f0f8ee273b8b1f9
Anwesh43 <anweshthecool0@gmail.com>
2023-04-23 08:21:11 +0530

    barlinebiarcrotview: adding Canvas extension methods

--- barlinebiarcrotview/src/main/java/com/example/barlinebiarcrotview/BarLineBiArcRotView.kt
+++ barlinebiarcrotview/src/main/java/com/example/barlinebiarcrotview/BarLineBiArcRotView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -30,3 +30,46 @@ val start : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarLineBiArcRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size / 2) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        drawXY((w / 2 + size / 2) * (1 - dsc(0)), 0f) {
+            drawRect(RectF(-size / 2, 0f, size / 2, size / 2), paint)
+        }
+        drawLine(0f, 0f, 0f, -size * 0.5f * dsc(1), paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawArc(
+                    RectF(
+                        -size / 2,
+                        -size / 2,
+                        size / 2,
+                        size / 2
+                    ), start, rot * dsc(2), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawBLBARNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBarLineBiArcRot(scale, w, h, paint)
+}
+
