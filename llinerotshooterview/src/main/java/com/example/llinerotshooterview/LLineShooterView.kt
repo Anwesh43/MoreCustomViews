dee4bae8fefc477976f6ec62bfcaafd8ca35a270
Anwesh43 <anweshthecool0@gmail.com>
2023-04-24 10:39:08 +0530

    llinerotshooterview: adding LLSNode class

--- llinerotshooterview/src/main/java/com/example/llinerotshooterview/LLineShooterView.kt
+++ llinerotshooterview/src/main/java/com/example/llinerotshooterview/LLineShooterView.kt
@@ -123,4 +123,45 @@ class LLineShooterView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LLSNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : LLSNode? = null
+        private var next : LLSNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LLSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLLSNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir: Int, cb : () -> Unit) : LLSNode {
+            var curr : LLSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
