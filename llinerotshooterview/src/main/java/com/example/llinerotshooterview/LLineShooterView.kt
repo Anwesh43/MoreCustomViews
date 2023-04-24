78c07351810e6d4d421d8cc8cc34bb682bf25275
Anwesh43 <anweshthecool0@gmail.com>
2023-04-24 10:30:54 +0530

    llinerotshooterview: adding LLineShooterView class

--- llinerotshooterview/src/main/java/com/example/llinerotshooterview/LLineShooterView.kt
+++ llinerotshooterview/src/main/java/com/example/llinerotshooterview/LLineShooterView.kt
@@ -61,3 +61,18 @@ fun Canvas.drawLLSNode(i : Int, scale : Float, paint : Paint) {
     drawLLineShooter(scale, w, h, paint)
 }
 
+class LLineShooterView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
