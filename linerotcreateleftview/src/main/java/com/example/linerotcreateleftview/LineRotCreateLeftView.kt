f847df5e82616584ee123a4c458fc23f22ec940b
Anwesh43 <anweshthecool0@gmail.com>
2023-04-25 09:09:40 +0530

    linerotcreateleftview: adding Renderer class

--- linerotcreateleftview/src/main/java/com/example/linerotcreateleftview/LineRotCreateLeftView.kt
+++ linerotcreateleftview/src/main/java/com/example/linerotcreateleftview/LineRotCreateLeftView.kt
@@ -186,4 +186,27 @@ class LineRotCreateLeftView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : LineRotCreateLeftView) {
+
+        private val animator : Animator = Animator(view)
+        private val lrcl : LineRotCreateLeft = LineRotCreateLeft(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            lrcl.draw(canvas, paint)
+            animator.animate {
+                lrcl.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            lrcl.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
