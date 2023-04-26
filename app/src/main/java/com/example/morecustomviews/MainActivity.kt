dccc7db460230a453b17771927b32ba42556a915
Anwesh43 <anweshthecool0@gmail.com>
2023-04-27 04:15:18 +0530

    linebentrightarcview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linearcbendrightview.LineArcBendRightView
+import com.example.linebentrightarcview.LineBentRightArcView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineArcBendRightView.create(this)
+        LineBentRightArcView.create(this)
         fullScreen()
     }
 }
