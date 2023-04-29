edc7bc212b40e21c5e2ac153f25c0c6f4a963b97
Anwesh43 <anweshthecool0@gmail.com>
2023-04-29 13:44:44 +0530

    lineslantcreaterotview: adding LineSlantCreateRot class

--- lineslantcreaterotview/src/main/java/com/example/lineslantcreaterotview/LineSlantCreateRotView.kt
+++ lineslantcreaterotview/src/main/java/com/example/lineslantcreaterotview/LineSlantCreateRotView.kt
@@ -173,4 +173,27 @@ class LineSlantCreateRotView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineSlantCreateRot(var i : Int) {
+
+        private var curr : LSCRNode = LSCRNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
