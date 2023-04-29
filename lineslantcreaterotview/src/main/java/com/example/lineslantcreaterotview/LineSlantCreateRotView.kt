fd5e1eca3bde6b8fd8694d910e0600ab3c088b30
Anwesh43 <anweshthecool0@gmail.com>
2023-04-29 12:54:12 +0530

    lineslantcreaterotview: adding Canvas extension methods

--- lineslantcreaterotview/src/main/java/com/example/lineslantcreaterotview/LineSlantCreateRotView.kt
+++ lineslantcreaterotview/src/main/java/com/example/lineslantcreaterotview/LineSlantCreateRotView.kt
@@ -28,3 +28,44 @@ val rot : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float  = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineWithoutDot(x1 : Float, y1 : Float, x2 : Float, y2 : Float, paint : Paint) {
+    if (Math.abs(x1 - x2) < 0.1f && Math.abs(y1 - y2) < 0.1f) {
+        return
+    }
+    drawLine(x1, y1, x2, y2, paint)
+}
+
+fun Canvas.drawLineSlantCreateRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2, h / 2) {
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(-w / 2 + (w / 2 - size) * dsc(0), 0f) {
+                    drawLine(0f, 0f, -size + size * dsc(1), 0f, paint)
+                }
+                drawLine(-size, 0f, -size + size * dsc(1), -size * dsc(1), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLSCRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineSlantCreateRot(scale, w, h, paint)
+}
