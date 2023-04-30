eb15262ea69338362057482baffa1b0aec7421a7
Anwesh43 <anweshthecool0@gmail.com>
2023-04-30 07:26:49 +0530

    seplinerotdownview: adding SepLineRotDownView class

--- seplinerotdownview/src/main/java/com/example/seplinerotdownview/SepLineRotDownView.kt
+++ seplinerotdownview/src/main/java/com/example/seplinerotdownview/SepLineRotDownView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawSRLDNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawSepRotLineDown(scale, w, h, paint)
 }
+
+class SepLineRotDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
