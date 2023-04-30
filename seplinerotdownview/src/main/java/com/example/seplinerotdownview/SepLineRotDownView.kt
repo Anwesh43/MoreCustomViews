4edcc7b7c044b8564523fc20c1fd203163efbfb7
Anwesh43 <anweshthecool0@gmail.com>
2023-04-30 07:35:40 +0530

    seplinerotdownview: adding SepLineRotDown class

--- seplinerotdownview/src/main/java/com/example/seplinerotdownview/SepLineRotDownView.kt
+++ seplinerotdownview/src/main/java/com/example/seplinerotdownview/SepLineRotDownView.kt
@@ -147,7 +147,7 @@ class SepLineRotDownView(ctx : Context) : View(ctx) {
             state.update(cb)
         }
 
-        fun startUpdaitng(cb : () -> Unit) {
+        fun startUpdating(cb : () -> Unit) {
             state.startUpdating(cb)
         }
 
@@ -161,6 +161,29 @@ class SepLineRotDownView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class SepLineRotDown(var i : Int) {
+
+        private var curr : SRLDNode = SRLDNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
