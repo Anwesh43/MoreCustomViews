c891766a39947d2ff0cc5438b9999c4fb01dbe28
Anwesh43 <anweshthecool0@gmail.com>
2023-05-01 19:04:26 +0530

    barsemicricledownview: adding Canvas extension methods

--- barsemicircledownview/src/main/java/com/example/barsemicircledownview/BarSemiCircleDownView.kt
+++ barsemicircledownview/src/main/java/com/example/barsemicircledownview/BarSemiCircleDownView.kt
@@ -18,7 +18,7 @@ val colors : Array<Int> = arrayOf(
 ).map {
     Color.parseColor(it)
 }.toTypedArray()
-val parts : Int = 4
+val parts : Int = 5
 val scGap : Float = 0.04f / parts
 val strokeFactor : Float = 90f
 val sizeFactor : Float = 4.9f
@@ -29,3 +29,41 @@ val backColor : Int = Color.parseColor("#BDBDBD")
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this -  i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawBarSemiCircleDown(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2 + size) * dsc(4), h / 2) {
+        rotate(rot * dsc(3))
+        drawRect(RectF(-size / 2, -size / 2, size / 2, -size / 2 + size * dsc(0)), paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(size / 2, size * 0.5f * dsc(2))
+                drawArc(
+                    RectF(-size / 2, -size / 2, size / 2, size / 2),
+                    90f,
+                    180f * dsc(1),
+                    true,
+                    paint
+                )
+            }
+        }
+    }
+}
+
+fun Canvas.drawBSCDNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawBarSemiCircleDown(scale, w, h, paint)
+}
