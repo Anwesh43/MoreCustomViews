0241d721cb9f6f068fb87e97921e51c525ec95e5
Anwesh43 <anweshthecool0@gmail.com>
2023-05-02 09:44:50 +0530

    linebarbentview: adding LineBarBentView class

--- linebarbentview/src/main/java/com/example/linebarbentview/LineBarBentView.kt
+++ linebarbentview/src/main/java/com/example/linebarbentview/LineBarBentView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLBBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineBarBent(scale, w, h, paint)
 }
+
+class LineBarBentView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
