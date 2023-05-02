fc37a9054912ef01517dc01e37dbd4c74557b9b1
Anwesh43 <anweshthecool0@gmail.com>
2023-05-02 09:36:07 +0530

    linebarbentview: adding Canvas extension methods

--- linebarbentview/src/main/java/com/example/linebarbentview/LineBarBentView.kt
+++ linebarbentview/src/main/java/com/example/linebarbentview/LineBarBentView.kt
@@ -29,3 +29,33 @@ val rot : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineBarBent(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        drawLine(0f, 0f, size * dsc(0), 0f, paint)
+        drawXY(size, 0f) {
+            rotate(rot * dsc(2))
+            drawRect(RectF(0f, 0f, size / 2, size * dsc(1)), paint)
+        }
+    }
+}
+
+fun Canvas.drawLBBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineBarBent(scale, w, h, paint)
+}
