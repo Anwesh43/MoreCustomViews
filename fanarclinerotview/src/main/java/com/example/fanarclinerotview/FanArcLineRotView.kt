94b0fb65857a4885a18b1a9eac3ec571f33ef3ef
Anwesh43 <anweshthecool0@gmail.com>
2023-05-03 08:33:53 +0530

    fanarclinerotview: adding Renderer class

--- fanarclinerotview/src/main/java/com/example/fanarclinerotview/FanArcLineRotView.kt
+++ fanarclinerotview/src/main/java/com/example/fanarclinerotview/FanArcLineRotView.kt
@@ -193,4 +193,27 @@ class FanArcLineRotView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : FanArcLineRotView) {
+
+        private val animator : Animator = Animator(view)
+        private val falr : FanArcLineRot = FanArcLineRot(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            falr.draw(canvas, paint)
+            animator.animate {
+                falr.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            falr.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
