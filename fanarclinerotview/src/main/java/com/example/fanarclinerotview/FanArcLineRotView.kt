b19b3fb5333b98f70a305af904c3bc958d82cf74
Anwesh43 <anweshthecool0@gmail.com>
2023-05-03 08:20:40 +0530

    fanarclinerotview: adding Canvas extension methods

--- fanarclinerotview/src/main/java/com/example/fanarclinerotview/FanArcLineRotView.kt
+++ fanarclinerotview/src/main/java/com/example/fanarclinerotview/FanArcLineRotView.kt
@@ -25,7 +25,44 @@ val sizeFactor : Float = 4.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rot : Float = 30f
-
+val deg : Float = 180f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawFanArcLineRot(scale : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2, h / 2 + (h / 2) * dsc(3)) {
+        rotate(deg * dsc(2))
+        drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f - 2 * j, 1f)
+                drawXY(0f, 0f) {
+                    rotate(rot * dsc(1))
+                    drawLine(0f, 0f, 0f, -size * dsc(0), paint)
+                }
+                drawArc(RectF(-size / 4, -size / 4, size / 4, size / 4), -90f, rot * dsc(1), true, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawFALRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawFanArcLineRot(scale, w, h, paint)
+}
