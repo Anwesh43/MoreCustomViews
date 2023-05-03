02949b9f20ad1e0eea1c88376b199932e6184e70
Anwesh43 <anweshthecool0@gmail.com>
2023-05-03 08:44:43 +0530

    fanarclinerotview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.linebarbentview.LineBarBentView
+import com.example.fanarclinerotview.FanArcLineRotView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        LineBarBentView.create(this)
+        FanArcLineRotView.create(this)
         fullScreen()
     }
 }
