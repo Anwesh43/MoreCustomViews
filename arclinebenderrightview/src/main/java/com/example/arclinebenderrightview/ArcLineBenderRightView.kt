8a7a70cac2bdcb10689c6763c712e5fc1a7b5167
Anwesh43 <anweshthecool0@gmail.com>
2023-05-05 07:35:22 +0530

    arclinebenderrightview: adding ALBRNode class

--- arclinebenderrightview/src/main/java/com/example/arclinebenderrightview/ArcLineBenderRightView.kt
+++ arclinebenderrightview/src/main/java/com/example/arclinebenderrightview/ArcLineBenderRightView.kt
@@ -124,4 +124,45 @@ class ArcLineBenderRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ALBRNode(var i : Int = 0, val state : State = State()) {
+
+        private var prev : ALBRNode? = null
+        private var next : ALBRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = ALBRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawALBRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ALBRNode {
+            var curr : ALBRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
