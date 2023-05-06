dbca04f73b3d2c01ca807655679e75afd3ce3a56
Anwesh43 <anweshthecool0@gmail.com>
2023-05-06 08:30:48 +0530

    linerottoarrowdownview: adding LineRotToArrowDownView class

--- linerottoarrowdownview/src/main/java/com/example/linerottoarrowdownview/LineRotToArrowDownView.kt
+++ linerottoarrowdownview/src/main/java/com/example/linerottoarrowdownview/LineRotToArrowDownView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLRTADNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotToArrow(scale, w, h, paint)
 }
+
+class LineRotToArrowDownView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
