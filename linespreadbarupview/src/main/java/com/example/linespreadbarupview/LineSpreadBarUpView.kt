40ee39eb338d1b04c64e3521ae03f2039b68e273
Anwesh43 <anweshthecool0@gmail.com>
2023-05-08 09:51:47 +0530

    linespreadbarupview: adding LineSpreadBarUpView class

--- linespreadbarupview/src/main/java/com/example/linespreadbarupview/LineSpreadBarUpView.kt
+++ linespreadbarupview/src/main/java/com/example/linespreadbarupview/LineSpreadBarUpView.kt
@@ -63,3 +63,19 @@ fun Canvas.drawLSBUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineSpreadBarUp(scale, w , h, paint)
 }
+
+class LineSpreadBarUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
