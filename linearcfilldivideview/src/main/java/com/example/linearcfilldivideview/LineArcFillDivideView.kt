e318ddfea0c68de0d13c867939ad3358075946e3
Anwesh43 <anweshthecool0@gmail.com>
2023-05-10 10:19:07 +0530

    linearcfilldivideview: adding Canvas extension methods

--- linearcfilldivideview/src/main/java/com/example/linearcfilldivideview/LineArcFillDivideView.kt
+++ linearcfilldivideview/src/main/java/com/example/linearcfilldivideview/LineArcFillDivideView.kt
@@ -29,3 +29,36 @@ val rot : Float = 45f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawLineArcDivide(scale  : Float, w : Float, h : Float, paint : Paint) {
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    val size : Float = Math.min(w, h) / sizeFactor
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        drawLine(0f, 0f, -size * dsc(0), 0f, paint)
+        for (j in 0..1) {
+            drawXY(0f, 0f) {
+                scale(1f, 1f - 2 * j)
+                rotate(-rot * dsc(2))
+                drawArc(RectF(0f, -size / 2, size, size / 2), 180f, 180f * dsc(1), false, paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawLADNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineArcDivide(scale, w, h, paint)
+}
