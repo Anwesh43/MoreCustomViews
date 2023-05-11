f43d7e5089ac53720bbaed8a46234b87abff3125
Anwesh43 <anweshthecool0@gmail.com>
2023-05-11 14:38:16 +0530

    linebatballview: adding LineBatBallView setup

--- linebatballview/src/main/java/com/example/linebatballview/LineBatBallView.kt
+++ linebatballview/src/main/java/com/example/linebatballview/LineBatBallView.kt
@@ -70,3 +70,18 @@ fun Canvas.drawLBBNode(i : Int, scale : Float, paint : Paint) {
     drawLineBatBall(scale, w, h, paint)
 }
 
+class LineBatBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
