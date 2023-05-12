a0a4e88cf306921fee77799cfcd95055eef8f762
Anwesh43 <anweshthecool0@gmail.com>
2023-05-12 09:03:54 +0530

    lineflapsqdownview:: adding static create method

--- lineflapsqdownview/src/main/java/com/example/lineflapsqdownview/LineFlapSqDownView.kt
+++ lineflapsqdownview/src/main/java/com/example/lineflapsqdownview/LineFlapSqDownView.kt
@@ -216,4 +216,13 @@ class LineFlapSqDownView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineFlapSqDownView {
+            val view : LineFlapSqDownView = LineFlapSqDownView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
