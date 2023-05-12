a7f796cfab2be3918d0a2bee0d76ccd7d86a7689
Anwesh43 <anweshthecool0@gmail.com>
2023-05-13 02:07:37 +0530

    paralleltorightlineview: adding Canvas extension methods

--- paralleltorightlineview/src/main/java/com/example/paralleltorightlineview/ParallelToRightLineView.kt
+++ paralleltorightlineview/src/main/java/com/example/paralleltorightlineview/ParallelToRightLineView.kt
@@ -28,3 +28,34 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int)  : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawXY(x : Float, y : Float, cb : () -> Unit) {
+    save()
+    translate(x, y)
+    cb()
+    restore()
+}
+
+fun Canvas.drawParallelToRightLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val dsc : (Int) -> Float = {
+        scale.divideScale(it, parts)
+    }
+    drawXY(w / 2 + (w / 2) * dsc(3), h / 2) {
+        for (j in 0..1) {
+            drawXY(size * j, 0f) {
+                rotate(-rot * dsc(2) * j)
+                drawLine(0f, 0f, 0f, -size * dsc(j), paint)
+            }
+        }
+    }
+}
+
+fun Canvas.drawPTRLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawParallelToRightLine(scale, w, h, paint)
+}
