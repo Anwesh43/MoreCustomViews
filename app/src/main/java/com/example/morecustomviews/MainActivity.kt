2cbe25a999b3495b57745d4c956f152f81b98bdf
Anwesh43 <anweshthecool0@gmail.com>
2023-07-09 05:26:00 +0530

    multilinerotgapview: adding view in activity

--- app/src/main/java/com/example/morecustomviews/MainActivity.kt
+++ app/src/main/java/com/example/morecustomviews/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.morecustomviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.paralleltorightlineview.ParallelToRightLineView
+import com.example.multilinerotgapview.MultiLineRotGapView
 
 
 //import com.example.linerotsqexpandview.LineRotSqExpandView
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         //.create(this)
-        ParallelToRightLineView.create(this)
+        MultiLineRotGapView.create(this)
         fullScreen()
     }
 }
