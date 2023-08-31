bd554c4cf9f5cf674ef3d7da3e5da16d8505c109
Anwesh43 <anweshthecool0@gmail.com>
2023-08-31 11:15:31 +0530

    linerotrightview: adding LRRNode class

--- linerotrightview/src/main/java/com/example/linerotrightview/LineRotRightView.kt
+++ linerotrightview/src/main/java/com/example/linerotrightview/LineRotRightView.kt
@@ -122,4 +122,45 @@ class LineRotRightView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LRRNode(var i : Int = 0, val state : State = State()) {
+
+        private var next : LRRNode? = null
+        private var prev : LRRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = LRRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLRRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LRRNode {
+            var curr : LRRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
